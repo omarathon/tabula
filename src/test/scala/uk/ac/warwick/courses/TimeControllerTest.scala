faac26024e2e5515c1b39685c8e27bbd283fcf69
Nick Howes <n.howes@warwick.ac.uk>
2011-10-05 16:16:12 +0100

    Random extra test to test testing

--- src/test/scala/uk/ac/warwick/courses/TimeControllerTest.scala
+++ src/test/scala/uk/ac/warwick/courses/TimeControllerTest.scala
@@ -1,16 +1,18 @@
-package uk.ac.warwick.scala
+package uk.ac.warwick.courses
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.scalatest.junit.JUnitSuite
 import org.scalatest.junit.ShouldMatchersForJUnit
 
-import uk.ac.warwick.courses.TimeController
-
 class TimeControllerTest extends JUnitSuite with ShouldMatchersForJUnit {
 	@Test def works {
 		controller.showTime.getModel.get("timeWelcome") should be ("Yo")
 	} 
+
+	@Test def something {
+		controller should not be(null)
+	}
 	
 	def controller = new TimeController {
 		timeWelcome = "Yo"
