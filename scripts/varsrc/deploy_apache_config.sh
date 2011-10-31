b447b15f9f9b042144916474a99e36fcdf0d3cce
Nick Howes <n.howes@warwick.ac.uk>
2011-10-31 15:36:25 +0000

    Setting up coursework-test deploy settings

--- scripts/varsrc/deploy_apache_config.sh
+++ scripts/varsrc/deploy_apache_config.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
-cd /var/src/web/sitebuilder2/docs/apache/conf
-cvs update -d
-cd /var/src
+cd /var/src/checkout/
+git pull
 
 /opt/csw/bin/ruby /var/src/web/sitebuilder2/docs/apache/conf/deploy_apache_config.rb /var/src/web/sitebuilder2/docs/apache/conf/config.yaml.www2
 
