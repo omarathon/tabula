c6777e70ecb8c9a8016dda0d93f02f7d7526e95a
Nick Howes <n.howes@warwick.ac.uk>
2011-11-04 09:53:28 +0000

    Bash script to monitor source directory and run tests when anything changes

---
+++ scripts/dev/run-tests-on-save.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# When started, watches the whole source directory for changes. When a change happens it
+# starts the unit tests, and then goes back to waiting.
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=`dirname "$SCRIPT"`
+
+WATCHFILE=/tmp/src-test-watch-output.txt
+ANT="ant -logger org.apache.tools.ant.listener.AnsiColorLogger"
+
+which inotifywait > /dev/null
+if [[ $? == 0 ]]; then
+  echo "Monitoring src folder for changes"
+  pushd ${SCRIPTPATH}/../..
+  # Rather than recursively set up watches for the whole source dir every time,
+  # we instead set them up once to report to a file in the background, then we
+  # can just watch for changes to that log file.
+  inotifywait -r -m src > $WATCHFILE &
+  while inotifywait $WATCHFILE; do
+    $ANT clean test
+  done
+else
+  echo "The inotifywait command is not available. It is available on Ubuntu from the inotify-tools package."
+fi
