dc4511d0c1fe895c9614dd89617170f2ce57fc26
Nick Howes <nick@nickhowes.co.uk>
2012-05-17 13:45:10 +0100

    HFC-212 WIP

--- src/main/scala/uk/ac/warwick/courses/web/controllers/sysadmin/SysadminREPL.scala
+++ src/main/scala/uk/ac/warwick/courses/web/controllers/sysadmin/SysadminREPL.scala
@@ -3,9 +3,6 @@ package uk.ac.warwick.courses.web.controllers.sysadmin
 import uk.ac.warwick.courses.web.controllers.BaseController
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.stereotype.Controller
-import tools.nsc.interpreter.IMain
-import tools.nsc.Settings
-import tools.nsc.interpreter.IR.Result
 import org.springframework.web.bind.annotation.RequestParam
 import java.io.PrintWriter
 import java.io.StringWriter
@@ -18,6 +15,8 @@ import org.springframework.expression.Expression
 import org.springframework.expression.ParserContext
 import org.springframework.expression.spel.support.StandardEvaluationContext
 import uk.ac.warwick.courses.JavaImports._
+import uk.ac.warwick.courses.data.model._
+import uk.ac.warwick.courses.services._
 import collection.JavaConversions._
 import java.util.HashMap
 import org.springframework.expression.spel.SpelEvaluationException
@@ -27,17 +26,16 @@ import org.hibernate.Session
 @RequestMapping(value=Array("/sysadmin/repl"))
 class SysadminREPL extends BaseController with BeanFactoryAware {
 	
-	@BeanProperty var beanFactory:BeanFactory = _
+	@BeanProperty var beanFactory:BeanFactory =_
+	@BeanProperty var assignmentService:AssignmentService =_
+	@BeanProperty var moduleAndDepartmentService:ModuleAndDepartmentService =_
 	
 	val spel:SpelExpressionParser = new SpelExpressionParser
 	
 	@RequestMapping
 	def evaluate(@RequestParam(value="query", defaultValue="") query:String) = {
 		val response = if (query.hasText) {
-			val context = new StandardEvaluationContext(ContextVariables(
-					session,
-					beanFactory		
-			))
+			val context = new StandardEvaluationContext(RootObject(session))
 			val expression = spel.parseExpression(query)
 			try Return(expression.getValue(context))
 			catch { case e:Exception => Return(null, e) }
@@ -46,13 +44,56 @@ class SysadminREPL extends BaseController with BeanFactoryAware {
 		}
 		Mav("sysadmin/repl", "query" -> query, "response" -> response)
 	}
-
+	
+	/**
+	 * Root object for evaluator. Its properties and methods will be available
+	 * as top-level items in your SpEL query.
+	 */
+	case class RootObject(
+		@BeanProperty val session:Session,
+		@BeanProperty val beanFactory:BeanFactory = beanFactory,
+		// expose Assignments as a map of ids
+		@BeanProperty val assignments:MapAccessor[Assignment] = MapAccessor { id =>
+			assignmentService.getAssignmentById(id).orNull
+		},
+		@BeanProperty val departmentCodes:MapAccessor[Department] = MapAccessor { code =>
+			moduleAndDepartmentService.getDepartmentByCode(code).orNull
+		}
+	)
 }
 
-case class ContextVariables(
-		@BeanProperty val session:Session,
-		@BeanProperty val beanFactory:BeanFactory
-)
+
+/**
+ * Implements a Java Map that only has a working get() method, to allow
+ * you to expose some collection of objects as a String-keyed map.
+ */
+abstract class MapAccessor[T] extends JMap[String,T] {
+	def fetch(key:String):T 
+	override def get(key:Any) = fetch(key.asInstanceOf[String]) 
+	override def put(key:String, value:T) = throw strop
+	override def keySet = throw strop
+	override def entrySet = throw strop
+	override def values = throw strop
+	override def putAll(map:JMap[_ <: String, _ <: T]) = throw strop
+	override def size = 1
+	override def isEmpty = false
+	override def clear = throw strop
+	override def containsKey(s:Any) = throw strop
+	override def containsValue(v:Any) = throw strop
+	override def remove(v:Any) = throw strop
+		
+	def strop = new UnsupportedOperationException
+}
+
+object MapAccessor {
+	/**
+	 * Creates a MapAccessor that uses the given function to resolve
+	 * the string to an object.
+	 */
+	def apply[T](fn: String=>T) = new MapAccessor[T] {
+		override def fetch(id:String) = fn(id)
+	}
+}
 
 case class Return(val value:Any, val exception:Exception = null) {
 	lazy val stringValue = value.toString
