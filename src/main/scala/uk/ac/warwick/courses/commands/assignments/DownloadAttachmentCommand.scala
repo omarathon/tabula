bf0eef9c9a4233422f334549e2ec76e2ea2a23f4
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2012-05-21 15:45:06 +0100

    HFC-210 - tidied up some inefficient code.

--- src/main/scala/uk/ac/warwick/courses/commands/assignments/DownloadAttachmentCommand.scala
+++ src/main/scala/uk/ac/warwick/courses/commands/assignments/DownloadAttachmentCommand.scala
@@ -2,7 +2,7 @@ package uk.ac.warwick.courses.commands.assignments
 
 import scala.reflect.BeanProperty
 import scala.collection.JavaConversions.asScalaBuffer
-
+import org.springframework.beans.factory.annotation.Autowired
 import uk.ac.warwick.courses.CurrentUser
 import uk.ac.warwick.courses.commands.Command
 import uk.ac.warwick.courses.commands.Description
@@ -13,35 +13,31 @@ import uk.ac.warwick.courses.data.model.Submission
 import uk.ac.warwick.courses.helpers.StringUtils.StringToSuperString
 import uk.ac.warwick.courses.services.fileserver.RenderableAttachment
 import uk.ac.warwick.courses.services.fileserver.RenderableFile
+import uk.ac.warwick.courses.services.AssignmentService
+import scala.None
+import org.springframework.beans.factory.annotation.Configurable
 
+@Configurable
 class DownloadAttachmentCommand(user: CurrentUser) extends Command[Option[RenderableFile]] with ReadOnly {
-
+  
   @BeanProperty var module: Module = _
   @BeanProperty var assignment: Assignment = _
   @BeanProperty var filename: String = _
-
+  @Autowired var assignmentService:AssignmentService = _
+  
   private var fileFound: Boolean = _
   var callback: (RenderableFile) => Unit = _
 
   def apply() = {
+    val submission = assignmentService.getSubmission(assignment, user.universityId);
+    val allAttachments = submission map {_.allAttachments} getOrElse Nil
+    val attachment = allAttachments find (_.name == filename) map (a => new RenderableAttachment(a))
 
-    // get the currentUsers submissions
-    var submissions:Seq[Submission] = assignment.getSubmissions
-    
-    
-    submissions.filter(name => name == user.universityId)
-    
-    // TODO - maybe multiple submission leads to more than one submission - if so get the latest
-    // should only have one submission per assignment so take the first 
-    var submission = submissions.head      //may explode if empty
-    var attatchment = submission.allAttachments.filter(attatchment => attatchment.name == filename).head; // find instead
-    val result = Option(new RenderableAttachment(attatchment))
-     
-    fileFound = result.isDefined
+    fileFound = attachment.isDefined
     if (callback != null) {
-      result.map { callback(_) }
+      attachment.map { callback(_) }
     }
-    result
+    attachment
   }
 
   override def describe(d: Description) = {
