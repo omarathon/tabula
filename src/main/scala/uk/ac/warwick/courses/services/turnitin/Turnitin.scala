b78dfa6e7f7d6ce0a75a30679b4d03fb724b4cd9
Nick Howes <nick@nickhowes.co.uk>
2012-06-07 16:13:40 +0100

    HFC-222

--- src/main/scala/uk/ac/warwick/courses/services/turnitin/Turnitin.scala
+++ src/main/scala/uk/ac/warwick/courses/services/turnitin/Turnitin.scala
@@ -1,19 +1,67 @@
 package uk.ac.warwick.courses.services.turnitin
 
-sealed abstract class FunctionId(val id:Int, val description:String)
+import uk.ac.warwick.courses.helpers.Logging
+import dispatch._
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.format.DateTimeFormatterBuilder
+import org.joda.time.format.DateTimeFormatter
+import org.joda.time.DateTime
+import scala.collection.immutable.SortedMap
+import org.apache.commons.codec.digest.DigestUtils
+
+sealed abstract class FunctionId(val id: Int, val description: String)
 case class CreateAssignmentFunction() extends FunctionId(4, "Create assignment")
 case class SubmitPaperFunction() extends FunctionId(5, "Submit paper")
 case class GenerateReportFunction() extends FunctionId(6, "Generate report")
 
-class Turnitin {
-	val url = "https://submit.ac.uk/api.asp"
-	def aid = 299
-	def said = 8432
-	var diagnostic = true
-	
-	
-	
-	def doRequest(f:FunctionId) {
-		
+class Turnitin extends Logging {
+	val endpoint = :/ ("submit.ac.uk") / "api.asp" secure
+	def aid = "299"
+	def said = "8432"
+	var sharedSecretKey = "this is not the shared key"
+	var diagnostic = false
+
+	def submitPaper = {
+		doRequest(SubmitPaperFunction())
 	}
+
+	private def doRequest(f: FunctionId) {
+		val parameters = Map("fid" -> f.id.toString) ++ commonParameters
+		val req = endpoint.POST << parameters << Map("md5" -> md5(parameters))
+		Http(req >>> Console.err)
+	}
+	
+	/**
+	 * Parameters that we need in every request.
+	 */
+	def commonParameters = Map(
+		"diagnostic" -> (if (diagnostic) "1" else "0"),
+		"gmtime" -> gmtTimestamp,
+		"encrypt" -> "0",
+		"aid" -> aid,
+		"said" -> said,
+		"fcmd" -> "2",
+		"uem" -> "n.howes@warwick.ac.uk",
+		"ufn" -> "Nick",
+		"uln" -> "Howes",
+		"utp" -> "2"
+	)
+	
+	/**
+	 * Sort parameters by key, concatenate all the values with
+	 * the shared key and MD5hex that.
+	 */
+	def md5(map:Map[String,String]) = {
+		println(map.toSeq sortBy mapKey map mapKey mkString(" + "))
+		println((map.toSeq sortBy mapKey map mapValue mkString("")) + sharedSecretKey)
+		DigestUtils.md5Hex(
+			(map.toSeq sortBy mapKey map mapValue mkString("")) + sharedSecretKey
+		)
+	}
+	
+	val timestampFormat = DateTimeFormat.forPattern("YYYYMMddHHm").withZoneUTC
+	def gmtTimestamp = timestampFormat print DateTime.now
+	
+	private def mapKey[K](pair:Pair[K,_]) = pair._1
+	private def mapValue[V](pair:Pair[_,V]) = pair._2 
 }
--- src/test/scala/uk/ac/warwick/courses/services/turnitin/TryTurnitin.scala
+++ src/test/scala/uk/ac/warwick/courses/services/turnitin/TryTurnitin.scala
@@ -1,5 +1,29 @@
 package uk.ac.warwick.courses.services.turnitin
 
-object TryTurnitin extends App {
+import org.apache.commons.codec.digest.DigestUtils
 
+/**
+ * There should be a functional test for the Turnitin
+ * support but in the meantime I'm just running this
+ * little app manually to find out how it works. 
+ */
+object TryTurnitin extends App {
+	
+	//12a4e7b0bfc5f55b4b1ef252b1b05919
+	println(DigestUtils.md5Hex("100" +
+			"0" +
+			"0" +
+			"1" +
+			"1" +
+			"200310311" +
+			"john.doe@myschool.edu" +
+			"JohnDoe" +
+			"john123" +
+			"2" +
+			"hothouse123"))
+	
+	val api = new Turnitin
+	api.diagnostic = true
+	api.submitPaper
+	
 }
