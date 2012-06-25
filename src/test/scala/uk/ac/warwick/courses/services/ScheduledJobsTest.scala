a84d9910f0300325b2fbc9a852e9d4913fe1b8fe
Nick Howes <nick@nickhowes.co.uk>
2012-06-25 11:39:25 +0100

    Fixed unreliable concurrency test.
    It was using an unreliable way of waiting for a thread to start. It now
    waits on a lock, and explictly releases that lock when it starts.

--- src/test/scala/uk/ac/warwick/courses/services/ScheduledJobsTest.scala
+++ src/test/scala/uk/ac/warwick/courses/services/ScheduledJobsTest.scala
@@ -12,17 +12,25 @@ class ScheduledJobsTest extends TestBase {
 	 * task isn't already running. */
 	case class ControllableTask(sync:SchedulingConcurrency) extends Thread {
 		private val lock = new Object
+		private val startLock = new Object
 		private var paused = true
 		var running = false
 		override def run { 
 			sync.nonconcurrent("a") {
 				running = true
-				while (paused) lock.synchronized { lock.wait }
+				startLock.synchronized { startLock.notifyAll }
+				try while (paused) lock.synchronized { lock.wait }
+				finally running = false
 			}
 		}
 		def go {
 			paused = false
-			lock.synchronized { lock.notify	}
+			lock.synchronized { lock.notifyAll }
+		}
+		def waitToStart {
+			startLock.synchronized {
+				while (!running) startLock.wait
+			}
 		}
 	}
 
@@ -30,14 +38,14 @@ class ScheduledJobsTest extends TestBase {
 	 * Test that when one thread is running in a nonconcurrent block,
 	 * a second thread will skip that block.
 	 */
-	@Test(timeout=5000) def schedulingConcurrency {
+	@Test(timeout=5000) def schedulingConcurrency = for (i <- 1 to 50) {
 		val s = new Object with SchedulingConcurrency
 		val thread = new ControllableTask(s)
 		val thread2 = new ControllableTask(s)
 		
 		// start a paused thread and wait til it's properly running
 		thread.start
-		while (!thread.running) {}
+		thread.waitToStart
 		
 		// run thread2 synchronously - it should return straight away
 		// because thread 1 is holding the metaphorical flag.
@@ -46,6 +54,7 @@ class ScheduledJobsTest extends TestBase {
 		// let thread 1 finish. 
 		thread.go
 		thread.join
+		
 	}
 	
 }
