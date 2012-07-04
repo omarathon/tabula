336dab44c4fe42451ca1b1470433417a9c6e5ea2
Nick Howes <nick@nickhowes.co.uk>
2012-07-04 15:08:59 +0100

    Improve test auto-run script.
    Now ignores open, close and read events, and deletes the watchfile first.

--- scripts/dev/run-tests-on-save.sh
+++ scripts/dev/run-tests-on-save.sh
@@ -9,6 +9,8 @@ SCRIPTPATH=`dirname "$SCRIPT"`
 WATCHFILE=/tmp/src-test-watch-output.txt
 ANT="ant -logger org.apache.tools.ant.listener.AnsiColorLogger"
 
+rm $WATCHFILE > /dev/null
+
 which inotifywait > /dev/null
 if [[ $? == 0 ]]; then
   echo "Monitoring src folder for changes"
@@ -16,9 +18,9 @@ if [[ $? == 0 ]]; then
   # Rather than recursively set up watches for the whole source dir every time,
   # we instead set them up once to report to a file in the background, then we
   # can just watch for changes to that log file.
-  inotifywait -r -m src > $WATCHFILE &
+  inotifywait --excludei "(open|access|close)" -r -m src > $WATCHFILE &
   while inotifywait $WATCHFILE; do
-    $ANT clean test
+    $ANT test
   done
 else
   echo "The inotifywait command is not available. It is available on Ubuntu from the inotify-tools package."
