ea975e29d3d06878d159aafd69a60f6992df95b6
Nick Howes <nick@nickhowes.co.uk>
2012-07-09 13:09:02 +0100

    Add function wrapper for HibernateCallback

--- src/main/scala/uk/ac/warwick/courses/helpers/FunctionConversions.scala
+++ src/main/scala/uk/ac/warwick/courses/helpers/FunctionConversions.scala
@@ -2,6 +2,8 @@ package uk.ac.warwick.courses.helpers
 
 import org.springframework.jdbc.core.ConnectionCallback
 import java.sql.Connection
+import org.springframework.orm.hibernate3.HibernateCallback
+import org.hibernate.Session
 
 /**
  * A collection of implicit conversions from Scala functions to Java callback interfaces.
@@ -11,7 +13,13 @@ import java.sql.Connection
  * not doing much harm.
  */
 object FunctionConversions {
+	
 	implicit def ToConnectionCallback[T](fn: Connection=>T) = new ConnectionCallback[T] {
 		override def doInConnection(connection:Connection) = fn(connection)
 	}
+	
+	implicit def ToHibernateCallback[T](fn: Session=>T) = new HibernateCallback[T] {
+		override def doInHibernate(session:Session) = fn(session)
+	}
+	
 }
--- src/main/scala/uk/ac/warwick/courses/system/DatabaseEncodingChecker.scala
+++ src/main/scala/uk/ac/warwick/courses/system/DatabaseEncodingChecker.scala
@@ -5,10 +5,10 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.StatementCallback
 import javax.sql.DataSource
 import uk.ac.warwick.courses.helpers.Logging
+import uk.ac.warwick.courses.helpers.FunctionConversions._
 import java.sql.Statement
 import org.springframework.beans.factory.BeanInitializationException
 import org.springframework.orm.hibernate3.HibernateTemplate
-import org.springframework.orm.hibernate3.HibernateCallback
 import org.hibernate.Session
 import org.hibernate.SessionFactory
 
@@ -24,13 +24,11 @@ class DatabaseEncodingChecker @Autowired()(val sessionFactory:SessionFactory) ex
 	val hibernate:HibernateTemplate = new HibernateTemplate(sessionFactory)
 	
 	override def afterPropertiesSet {
-	  val fetchedString = hibernate.execute(new HibernateCallback[String]{
-	    override def doInHibernate(session:Session) = {
+	  val fetchedString = hibernate.execute { session:Session =>
 	      val query = session.createSQLQuery("select :string from dual")
 	      query.setString("string", testString)
 	      query.uniqueResult().toString()
-	    }
-	  })
+	  }
 	  if (!(testString equals fetchedString)) {
 	    throw new BeanInitializationException("Database is ruining strings - expected " + testString + ", got "+fetchedString)
 	  } else {
