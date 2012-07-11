cebd6de576e5ccc80f5da9b462d1f968ee2f2bc7
Nick Howes <nick@nickhowes.co.uk>
2012-07-11 15:12:46 +0100

    HFC-33 Add ASSIGNMENT.OCCURRENCE column to schema

--- scripts/schema/migrations/2012-07-10 - assignment members.sql
+++ scripts/schema/migrations/2012-07-10 - assignment members.sql
@@ -7,3 +7,8 @@ ADD MEMBERSGROUP_ID NVARCHAR2(255);
 -- reference to upstream assignment
 ALTER TABLE assignment
 ADD UPSTREAM_ID NVARCHAR2(255);
+
+-- reference to upstream assignment occurrence
+ALTER TABLE assignment
+ADD OCCURRENCE NVARCHAR2(255);
+
