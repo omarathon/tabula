3077c6bc9eb3ce4c5eb1b80257472e591af5fc38
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2012-09-24 11:00:35 +0100

    HFC-234 - fixed tests for extensions

--- src/test/scala/uk/ac/warwick/courses/web/controllers/ExtensionControllerTest.scala
+++ src/test/scala/uk/ac/warwick/courses/web/controllers/ExtensionControllerTest.scala
@@ -2,10 +2,9 @@ package uk.ac.warwick.courses.web.controllers
 
 import scala.collection.JavaConversions._
 import uk.ac.warwick.courses.web.controllers.admin.ExtensionController
-import uk.ac.warwick.courses.JavaImports._
 import uk.ac.warwick.courses.services.{UserLookupService, AssignmentService}
 import uk.ac.warwick.courses.{Mockito, TestBase}
-import uk.ac.warwick.courses.commands.assignments.{ExtensionItem, EditExtensionCommand}
+import uk.ac.warwick.courses.commands.assignments.{ExtensionItem, ModifyExtensionCommand}
 import org.joda.time.DateTime
 import uk.ac.warwick.courses.data.model.forms.Extension
 
@@ -22,22 +21,25 @@ class ExtensionControllerTest extends TestBase with Mockito {
       assignment.closeDate = DateTime.parse("2012-08-15T12:00")
       assignment.extensions += new Extension("1170836")
 
-      val command = new EditExtensionCommand(assignment, currentUser)
+      val command = new ModifyExtensionCommand(assignment, currentUser)
       command.userLookup = mock[UserLookupService]
       command.extensionItems = mockExtensions
 
-      val extensions = command.copyExtensions()
-      val extensionsJson:JList[Map[String, String]] = controller.toJson("Test", extensions) toList
+      val extensions = command.copyExtensionItems()
+      val extensionsJson = controller.toJson(extensions)
       val string = json.writeValueAsString(extensionsJson)
-      string should be ("""[{"id":"1170836","expiryDate":"Thu 23rd August 2012 at 12:00","reason":"Donec a risus purus nullam.","action":"Test"}]""")
+      string should be ("""{"1170836":{"id":"1170836","status":"","expiryDate":"Thu 23rd August 2012 at 12:00","approvalComments":"Donec a risus purus nullam."}}""")
     }
   }
+
+
+
 
   def mockExtensions = {
     val extensionItem = new ExtensionItem
     extensionItem.universityId = "1170836"
     extensionItem.expiryDate = DateTime.parse("2012-08-23T12:00")
-    extensionItem.reason = "Donec a risus purus nullam."
+    extensionItem.approvalComments = "Donec a risus purus nullam."
     List(extensionItem)
   }
 }
