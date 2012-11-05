a26da84631a7d5d4885518115270d30011010e94
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2012-11-05 10:33:42 +0000

    HFC-235 - Modified zip service to include copies of the feedback template for each submission (when one is available)

--- src/main/scala/uk/ac/warwick/courses/services/ZipService.scala
+++ src/main/scala/uk/ac/warwick/courses/services/ZipService.scala
@@ -1,24 +1,19 @@
 package uk.ac.warwick.courses.services
-import java.io.Closeable
 import java.io.File
 import java.io.InputStream
 import java.util.zip.ZipEntry
 import java.util.zip.ZipInputStream
 import scala.collection.JavaConversions.asScalaBuffer
-import org.hibernate.annotations.AccessType
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.annotation.{Autowired, Value}
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.stereotype.Service
-import javax.persistence.Entity
-import uk.ac.warwick.courses.data.model.Assignment
-import uk.ac.warwick.courses.data.model.Feedback
+import uk.ac.warwick.courses.data.model.{Assignment, Feedback, Submission}
 import uk.ac.warwick.courses.helpers.Logging
 import org.apache.commons.io.input.BoundedInputStream
 import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream
 import org.apache.commons.compress.archivers.zip.ZipArchiveEntry
 import uk.ac.warwick.courses.helpers.Closeables._
-import uk.ac.warwick.courses.data.model.Submission
-import collection.JavaConversions._
+import uk.ac.warwick.courses.Features
 
 /**
  * FIXME this could generate a corrupt file if two requests tried to generate the same zip simultaneously
@@ -28,12 +23,13 @@ import collection.JavaConversions._
 class ZipService extends InitializingBean with ZipCreator with Logging {
 	@Value("${filesystem.zip.dir}") var zipDir: File = _
 	@Value("${filesystem.create.missing}") var createMissingDirectories: Boolean = _
+	@Autowired var features: Features = _
 
 	val idSplitSize = 4
 
 	logger.info("Creating ZipService")
 
-	override def afterPropertiesSet {
+	override def afterPropertiesSet() {
 		if (!zipDir.exists) {
 			if (createMissingDirectories) {
 				zipDir.mkdirs
@@ -75,9 +71,15 @@ class ZipService extends InitializingBean with ZipCreator with Logging {
 	 */
 	def getSubmissionZipItems(submission: Submission): Seq[ZipItem] = {
 		val allAttachments = submission.allAttachments
-		allAttachments map { attachment =>
+		val submissionZipItems: Seq[ZipItem] = allAttachments map { attachment =>
 			new ZipFileItem(submission.zipFileName(attachment), attachment.dataStream)
 		}
+		if (features.feedbackTemplates){
+			val feedbackSheets = generateFeedbackSheet(submission)
+			feedbackSheets ++ submissionZipItems
+		}
+		else
+			submissionZipItems
 	}
 
 	/**
@@ -94,6 +96,18 @@ class ZipService extends InitializingBean with ZipCreator with Logging {
 		getZip(resolvePathForSubmission(assignment),
 			assignment.submissions flatMap getSubmissionZipItems)
 
+	/**
+	 * Returns a sequence with a single ZipItem (the feedback template) or an empty
+	 * sequence if no feedback template exists
+	 *
+	 */
+	def generateFeedbackSheet(submission: Submission): Seq[ZipItem] = {
+		// wrap template in an option to deal with nulls
+		Option(submission.assignment.feedbackTemplate) match {
+			case Some(t) => Seq(new ZipFileItem(submission.zipFileName(t.attachment), t.attachment.dataStream))
+			case None => Seq()
+		}
+	}
 }
 
 /**
@@ -107,7 +121,7 @@ class ZipEntryInputStream(val zip: InputStream, val entry: ZipEntry)
 
 object Zips {
 
-	def each(zip: ZipInputStream)(fn: (ZipEntry) => Unit): Unit = map(zip)(fn)
+	def each(zip: ZipInputStream)(fn: (ZipEntry) => Unit) { map(zip)(fn) }
 
 	/**
 	 * Provides an iterator for ZipEntry items which will be closed when you're done with them.
@@ -124,7 +138,7 @@ object Zips {
 	def map[T](zip: ZipInputStream)(fn: (ZipEntry) => T): Seq[T] = ensureClose(zip) {
 		Iterator.continually { zip.getNextEntry }.takeWhile { _ != null }.map { (item) =>
 			val t = fn(item)
-			zip.closeEntry
+			zip.closeEntry()
 			t
 		}.toList // use toList to evaluate items now, before we actually close the stream
 	}
