7435ee2b3c8692cd224bb6cb8c8407652bdf6086
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2012-11-06 17:02:57 +0000

    HFC-262 - added unauthorised message to extension request page when disabled

--- src/main/scala/uk/ac/warwick/courses/data/model/Department.scala
+++ src/main/scala/uk/ac/warwick/courses/data/model/Department.scala
@@ -44,6 +44,8 @@ class Department extends GeneratedId with PostLoadBehaviour with Viewable with M
 	def addOwner(owner:String) = ensureOwners.addUser(owner)
 	def removeOwner(owner:String) = ensureOwners.removeUser(owner)
 
+	def canRequestExtension = allowExtensionRequests != null && allowExtensionRequests
+
 	def addFeedbackForm(form:FeedbackTemplate) = feedbackTemplates.add(form)
 	
 	// If hibernate sets owners to null, make a new empty usergroup
