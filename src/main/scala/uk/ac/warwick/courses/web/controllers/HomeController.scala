06073f3c78231455a81f335813cc0d9d4f55b4fa
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2012-11-08 11:04:11 +0000

    HFC-262 - Added a request extension button to the deadline notice that appears on the home and assignment pages for students

--- src/main/scala/uk/ac/warwick/courses/web/controllers/HomeController.scala
+++ src/main/scala/uk/ac/warwick/courses/web/controllers/HomeController.scala
@@ -1,15 +1,11 @@
 package uk.ac.warwick.courses.web.controllers
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.beans.factory.annotation.Autowired
 import uk.ac.warwick.courses.services.ModuleAndDepartmentService
-import uk.ac.warwick.userlookup.GroupService
 import uk.ac.warwick.courses.CurrentUser
 import uk.ac.warwick.userlookup.Group
 import collection.JavaConversions._
 import uk.ac.warwick.courses.data.model.Module
-import org.joda.time.DateTime
-import org.joda.time.Duration
 import uk.ac.warwick.courses.services.UserLookupService
 import uk.ac.warwick.courses.services.AssignmentService
 import uk.ac.warwick.courses.Features
@@ -39,12 +35,18 @@ import uk.ac.warwick.courses.Features
 			// exclude assignments already included in other lists.
 			val enrolledAssignmentsTrimmed = enrolledAssignments.diff(assignmentsWithFeedback).diff(assignmentsWithSubmission)
 			// adorn the enrolled assignments with extra data.
-			val enrolledAssignmentsInfo = for (assignment <- enrolledAssignmentsTrimmed) yield Map(
-			    "assignment" -> assignment,
-			    "extension" -> assignment.extensions.find(_.userId == user.apparentId),
-			    "isExtended" -> assignment.isWithinExtension(user.apparentId),
-			    "submittable" -> assignment.submittable(user.apparentId)
-			)
+			val enrolledAssignmentsInfo = for (assignment <- enrolledAssignmentsTrimmed) yield {
+				val extension = assignment.extensions.find(_.userId == user.apparentId)
+				val isExtended = assignment.isWithinExtension(user.apparentId)
+				val extensionRequested = extension.isDefined && !extension.get.isManual
+				Map(
+					"assignment" -> assignment,
+					"extension" -> extension,
+					"isExtended" -> isExtended,
+					"extensionRequested" -> extensionRequested,
+					"submittable" -> assignment.submittable(user.apparentId)
+				)
+			}
 
 			Mav("home/view",
 				"assignmentsWithFeedback" -> assignmentsWithFeedback,
