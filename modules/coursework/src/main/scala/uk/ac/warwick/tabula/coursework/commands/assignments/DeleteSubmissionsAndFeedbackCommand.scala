a93b7836f3d59b045e9fe3d18183657298059fa5
Zoe <zoe.james@warwick.ac.uk>
2012-11-28 12:14:42 +0000

    added name to confirm delete screen - HFC-411

--- modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/commands/assignments/DeleteSubmissionsAndFeedbackCommand.scala
+++ modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/commands/assignments/DeleteSubmissionsAndFeedbackCommand.scala
@@ -1,22 +1,21 @@
 package uk.ac.warwick.tabula.coursework.commands.assignments
 
+import scala.collection.JavaConversions.asScalaBuffer
+import scala.reflect.BeanProperty
+
+import org.springframework.validation.Errors
+
+import uk.ac.warwick.spring.Wire
 import uk.ac.warwick.tabula.commands.Command
 import uk.ac.warwick.tabula.commands.Description
-import org.springframework.validation.Errors
-import uk.ac.warwick.tabula.data.model.Assignment
-import uk.ac.warwick.tabula.data.model.Feedback
-import uk.ac.warwick.tabula.helpers.ArrayList
-import scala.reflect.BeanProperty
-import collection.JavaConversions._
-import org.springframework.beans.factory.annotation.Autowired
-import uk.ac.warwick.tabula.data.FeedbackDao
-import uk.ac.warwick.tabula.CurrentUser
 import uk.ac.warwick.tabula.commands.SelfValidating
-import org.springframework.beans.factory.annotation.Configurable
-import uk.ac.warwick.tabula.data.model.Submission
-import uk.ac.warwick.tabula.services.ZipService
+import uk.ac.warwick.tabula.data.FeedbackDao
+import uk.ac.warwick.tabula.data.model.Assignment
+import uk.ac.warwick.tabula.helpers.ArrayList
 import uk.ac.warwick.tabula.services.AssignmentService
-import uk.ac.warwick.spring.Wire
+import uk.ac.warwick.tabula.services.UserLookupService
+import uk.ac.warwick.tabula.services.ZipService
+import uk.ac.warwick.userlookup.User
 
 /**
  * Takes a list of student university IDs and deletes either all their submissions, or all their feedback, or both,
@@ -27,6 +26,7 @@ class DeleteSubmissionsAndFeedbackCommand(val assignment: Assignment) extends Co
 	var assignmentService = Wire.auto[AssignmentService]
 	var zipService = Wire.auto[ZipService]
 	var feedbackDao = Wire.auto[FeedbackDao]
+	var userLookup = Wire.auto[UserLookupService]
 	
     @BeanProperty var students: JList[String] = ArrayList()
     @BeanProperty var submissionOrFeedback: String = ""
@@ -77,6 +77,16 @@ class DeleteSubmissionsAndFeedbackCommand(val assignment: Assignment) extends Co
 	def validate(errors: Errors) {
 		prevalidate(errors)
 		if (!confirm) errors.rejectValue("confirm", "submissionOrFeedback.delete.confirm")
+	}
+	
+	
+	def getStudentsAsUsers(): JList[User] = {
+		val studentsAsUsers: JList[User] = ArrayList()
+		for (student <- students)  {
+			val user: User = userLookup.getUserByWarwickUniId(student)
+			studentsAsUsers.add(user)
+		}
+		studentsAsUsers
 	}
 
 	override def describe(d: Description) = d
