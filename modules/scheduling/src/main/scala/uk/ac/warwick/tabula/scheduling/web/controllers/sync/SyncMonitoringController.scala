e9d49a62a1971b8a4c30370f21dd601015f19cd6
Mat Mannion <m.mannion@warwick.ac.uk>
2013-01-07 13:47:02 +0000

    Add extra status controllers for sanity/cleanup status

--- modules/scheduling/src/main/scala/uk/ac/warwick/tabula/scheduling/web/controllers/sync/SyncMonitoringController.scala
+++ modules/scheduling/src/main/scala/uk/ac/warwick/tabula/scheduling/web/controllers/sync/SyncMonitoringController.scala
@@ -11,11 +11,15 @@ import java.io.File
 import org.springframework.util.FileCopyUtils
 import java.io.FileReader
 import org.joda.time.DateTime
+import uk.ac.warwick.tabula.scheduling.commands.CleanupUnreferencedFilesCommand
+import uk.ac.warwick.tabula.scheduling.commands.SanityCheckFilesystemCommand
 
 @Controller
 @RequestMapping(Array("/sync/nagios"))
 class SyncMonitoringController extends BaseController {
 	import SyncReplicaFilesystemCommand._
+	import CleanupUnreferencedFilesCommand._
+	import SanityCheckFilesystemCommand._
 	import SyncMonitoringController._
 	
 	var dataDir = Wire[String]("${base.data.dir}")
@@ -34,10 +38,42 @@ class SyncMonitoringController extends BaseController {
 		response.addHeader("Content-Length", allDetails.length.toString)
 		response.getWriter().write(allDetails)
 	}
+	
+	lazy val lastSanityCheckJobDetailsFile = new File(new File(dataDir), LastSanityCheckJobDetailsFilename)
+	
+	@RequestMapping(Array("/sanity"))
+	def lastSanityCheck(implicit request: HttpServletRequest, response: HttpServletResponse): Unit = {
+		val lastJobDetails = FileCopyUtils.copyToString(new FileReader(lastSanityCheckJobDetailsFile)).trim
+		val lastRun = lastJobDetails.substring(lastJobDetails.indexOf(CheckLastRunDelimiter) + CheckLastRunDelimiter.length())	
+		val minutesSinceLastRun = (new DateTime().getMillis - lastRun.toLong) / MillisInAMinute
+		
+		val allDetails = lastJobDetails.concat(",minutesSinceLastRun," + minutesSinceLastRun)
+		
+		response.addHeader("Content-Type", "text/plain")
+		response.addHeader("Content-Length", allDetails.length.toString)
+		response.getWriter().write(allDetails)
+	}
+	
+	lazy val lastCleanupJobDetailsFile = new File(new File(dataDir), LastCleanupJobDetailsFilename)
+	
+	@RequestMapping(Array("/cleanup"))
+	def lastCleanup(implicit request: HttpServletRequest, response: HttpServletResponse): Unit = {
+		val lastJobDetails = FileCopyUtils.copyToString(new FileReader(lastCleanupJobDetailsFile)).trim
+		val lastRun = lastJobDetails.substring(lastJobDetails.indexOf(CheckLastRunDelimiter) + CheckLastRunDelimiter.length())
+		val minutesSinceLastRun = (new DateTime().getMillis - lastRun.toLong) / MillisInAMinute
+		
+		val allDetails = lastJobDetails.concat(",minutesSinceLastRun," + minutesSinceLastRun)
+		
+		response.addHeader("Content-Type", "text/plain")
+		response.addHeader("Content-Length", allDetails.length.toString)
+		response.getWriter().write(allDetails)
+	}
 
 }
 
 object SyncMonitoringController {
 	val LastRunDelimiter = "lastRun,"
+	val CheckLastRunDelimiter = "lastSuccessfulRun,"
 	val MillisInASecond = 1000
+	val MillisInAMinute = MillisInASecond * 60
 }
