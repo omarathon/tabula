6eef78c2e1ca90ffd9aae05d9f7cfa787799cd42
Steve Woodward <s.woodward@warwick.ac.uk>
2013-01-21 16:22:50 +0000

    TAB-83 Using a for loop rather than pattern match for missing assignment on feedback report

--- modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/commands/departments/FeedbackReportCommand.scala
+++ modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/commands/departments/FeedbackReportCommand.scala
@@ -33,17 +33,14 @@ class FeedbackReportCommand (val department:Department) extends Command[XSSFWork
 		val workbook = new XSSFWorkbook()
 		val sheet = generateNewSheet(department, workbook) 
 		
-		for (event <- events) {
-			
-			assignmentService.getAssignmentById(event.assignmentId.get) match {
-				case Some(assignment) => 
-					val row = sheet.createRow(sheet.getLastRowNum() + 1)
-					row.createCell(0).setCellValue(assignment.module.code.toUpperCase())				
-					row.createCell(1).setCellValue(dateFormatter.print(assignment.closeDate))
-					row.createCell(2).setCellValue(dateFormatter.print(event.eventDate))
-				case None =>
-			}
-			
+		for (event <- events;
+			 assignmentId <- event.assignmentId;
+			 assignment <- assignmentService.getAssignmentById(assignmentId)) {
+
+				val row = sheet.createRow(sheet.getLastRowNum() + 1)
+				row.createCell(0).setCellValue(assignment.module.code.toUpperCase())				
+				row.createCell(1).setCellValue(dateFormatter.print(assignment.closeDate))
+				row.createCell(2).setCellValue(dateFormatter.print(event.eventDate))	
 		}
 		
 		formatWorksheet(sheet)
