1c1020c58dbc2c006d29862775b23bcd57728583
Zoe <zoe.james@warwick.ac.uk>
2013-01-23 16:56:16 +0000

    renamed a field

--- config/scripts/schema/migrations/2013-01-21 - student relationships.sql
+++ config/scripts/schema/migrations/2013-01-21 - student relationships.sql
@@ -4,7 +4,7 @@ create table StudentRelationship (
 	target_sprcode NVARCHAR2(100) NOT NULL,
 	relationship_type NVARCHAR2(50) NOT NULL,
 	agent NVARCHAR2(255) NOT NULL,
-	updated_date timestamp,
+	uploaded_date timestamp,
 	start_date timestamp,
 	end_date timestamp,
 	CONSTRAINT "MEMBERRELATIONSHIP_PK" PRIMARY KEY ("ID")
