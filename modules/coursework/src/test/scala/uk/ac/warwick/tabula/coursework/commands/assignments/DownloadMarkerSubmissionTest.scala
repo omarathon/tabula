6055c95f5919c42bf17a1f4b4c61bd7bcf54c854
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2013-01-29 14:56:59 +0000

    TAB-204 - DownloadMarkerSubmissionTest

---
+++ modules/coursework/src/test/scala/uk/ac/warwick/tabula/coursework/commands/assignments/DownloadMarkerSubmissionTest.scala
@@ -0,0 +1,41 @@
+package uk.ac.warwick.tabula.coursework.commands.assignments
+
+import collection.JavaConversions._
+import uk.ac.warwick.tabula.{Mockito, AppContextTestBase}
+import java.util.zip.ZipInputStream
+import java.io.{ByteArrayInputStream, FileInputStream}
+import uk.ac.warwick.tabula.services.Zips
+import uk.ac.warwick.tabula.data.model.{SavedSubmissionValue, FileAttachment}
+import org.mockito.Mockito._
+import org.junit.Before
+
+
+class DownloadMarkerSubmissionTest extends AppContextTestBase with Mockito with MarkingWorkflowWorld {
+
+	@Before
+	def setup {
+		val attachment = mock[FileAttachment]
+		when(attachment.length) thenReturn(None)
+		when(attachment.dataStream) thenReturn(new ByteArrayInputStream("yes".getBytes))
+		assignment.submissions.foreach{submission =>
+			submission.values.add({
+				val sv = new SavedSubmissionValue()
+				sv.attachments = Set(attachment)
+				sv
+			})
+		}
+	}
+
+	@Test
+	def downloadSubmissionsTest {
+		withUser("cuslaj"){
+			val command = new DownloadMarkersSubmissionsCommand(assignment.module, assignment, currentUser)
+			command.apply { zip =>
+				val stream = new ZipInputStream(new FileInputStream(zip.file.get))
+				val items = Zips.map(stream){item => item.getName}
+				items.size should be (3)
+			}
+		}
+	}
+
+}
