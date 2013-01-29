a8835d6948eedee78dc82b20da295680620a5939
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2013-01-29 14:56:17 +0000

    TAB-204 - Added reusable enviroment for marking workflow tests

---
+++ modules/coursework/src/test/scala/uk/ac/warwick/tabula/coursework/commands/assignments/MarkingWorkflowWorld.scala
@@ -0,0 +1,60 @@
+package uk.ac.warwick.tabula.coursework.commands.assignments
+
+import collection.JavaConversions._
+import uk.ac.warwick.tabula.data.model._
+import java.util.HashMap
+import uk.ac.warwick.tabula.AppContextTestBase
+
+// reusable environment for marking workflow tests
+trait MarkingWorkflowWorld extends AppContextTestBase{
+
+	val assignment = newDeepAssignment(moduleCode = "IN101")
+	generateSubmission(assignment, "9876004", "cusxad")
+	generateSubmission(assignment, "0270954", "cuscao")
+	generateSubmission(assignment, "9170726", "curef")
+	generateSubmission(assignment, "0672088", "cusebr")
+	generateSubmission(assignment, "0672089", "cuscav")
+	addFeedback(assignment)
+
+	val markScheme = new MarkScheme()
+	markScheme.markingMethod = SeenSecondMarking
+	markScheme.department = assignment.module.department
+	markScheme.firstMarkers = makeUserGroup("cuslaj", "cuscav")
+	markScheme.secondMarkers = makeUserGroup("cuslat", "cuday")
+	assignment.markScheme = markScheme
+
+	assignment.markerMap = new HashMap[String, UserGroup]()
+	assignment.markerMap.put("cuslaj", makeUserGroup("cusxad", "cuscao", "curef"))
+	assignment.markerMap.put("cuscav", makeUserGroup("cusebr", "cuscav"))
+	assignment.markerMap.put("cuslat", makeUserGroup("cusxad", "cuscao", "curef"))
+	assignment.markerMap.put("cuday", makeUserGroup("cusebr", "cuscav"))
+
+	def addFeedback(assignment:Assignment){
+		val feedback = assignment.submissions.map{s=>
+			val newFeedback = new Feedback
+			newFeedback.assignment = assignment
+			newFeedback.uploaderId = "cuslaj"
+			newFeedback.universityId = s.universityId
+			newFeedback.released = false
+			val fmFeedback = newFeedback.retrieveFirstMarkerFeedback
+			fmFeedback.state = ReleasedForMarking
+			newFeedback
+		}
+		assignment.feedbacks = feedback
+	}
+
+
+	def generateSubmission(assignment:Assignment, uniId: String, userCode: String) {
+		val submission = new Submission()
+		submission.assignment = assignment
+		submission.universityId = uniId
+		submission.userId = userCode
+		assignment.submissions.add(submission)
+	}
+
+	def makeUserGroup(users: String*): UserGroup = {
+		val ug = new UserGroup
+		ug.includeUsers = users
+		ug
+	}
+}
