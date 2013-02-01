1e85feb45e11d3b32705c8a19400ed7024924b53
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2013-02-01 10:48:18 +0000

    TAB-401 added unit test to test for bug

---
+++ modules/coursework/src/test/scala/uk/ac/warwick/tabula/coursework/commands/assignments/RevokeExtensionTest.scala
@@ -0,0 +1,32 @@
+package uk.ac.warwick.tabula.coursework.commands.assignments
+
+
+import extensions.DeleteExtensionCommand
+import uk.ac.warwick.tabula.{AppContextTestBase, Mockito, TestBase}
+import uk.ac.warwick.tabula.data.model.forms.Extension
+import org.joda.time.DateTime
+import org.springframework.transaction.annotation.Transactional
+
+
+class RevokeExtensionTest extends AppContextTestBase with Mockito {
+
+	@Transactional @Test
+	def revokeExtension(){
+		withUser("cuslaj") {
+			val assignment = newDeepAssignment()
+			val extension = new Extension()
+			extension.universityId = "0000001"
+			extension.assignment = assignment
+			extension.expiryDate =  new DateTime()
+			extension.reason = "I feel like it"
+			assignment.extensions add extension
+			assignment.extensions.size should be (1)
+
+			val command = new DeleteExtensionCommand(assignment.module, assignment, "0000001" ,currentUser)
+			command.apply()
+
+			assignment.extensions.size should be (0)
+		}
+	}
+
+}
