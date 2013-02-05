82d81520899b9ed70404b7c102986412f781503c
Mat Mannion <m.mannion@warwick.ac.uk>
2013-02-05 15:51:35 +0000

    TAB-431 Go to a *STUPID* version of the permissions helper, where we extract the zip into /tmp.

--- modules/home/src/test/scala/uk/ac/warwick/tabula/home/web/controllers/sysadmin/PermissionsHelperControllerTest.scala
+++ modules/home/src/test/scala/uk/ac/warwick/tabula/home/web/controllers/sysadmin/PermissionsHelperControllerTest.scala
@@ -2,23 +2,42 @@ package uk.ac.warwick.tabula.home.web.controllers.sysadmin
 import java.net.URL
 import java.net.URLConnection
 import java.net.URLStreamHandler
-
 import uk.ac.warwick.tabula.TestBase
+import java.io.File
+import java.net.URLStreamHandlerFactory
+import org.junit.Before
+import org.junit.BeforeClass
+import org.scalatest.BeforeAndAfterAll
+import java.util.jar.JarFile
+import org.springframework.util.FileCopyUtils
+import java.io.FileOutputStream
 
 class PermissionsHelperControllerTest extends TestBase {
 	
-	@Test def sillyUrlType() = {
+	@Test def sillyUrlType() {
+		URL.setURLStreamHandlerFactory(new CatchAllUrlStreamHandlerFactory)
+		
 		val urlType = new SillyJbossVfsUrlType()
 		
-		val url = new URL("vfszip", "", -1, "/package/jboss-5.1.0/server/tabula-secondary/deploy/home.war/WEB-INF/lib/common-27-SNAPSHOT.jar/", new VfsUrlHandler)
-		url.toString should be ("vfszip:/package/jboss-5.1.0/server/tabula-secondary/deploy/home.war/WEB-INF/lib/common-27-SNAPSHOT.jar/")
+		val url = new URL("vfszip:/package/jboss-5.1.0/server/tabula-primary/deploy/home.war/WEB-INF/lib/common-27-SNAPSHOT.jar/")
 		
 		urlType.matches(url) should be (true)
+	}
+	
+	@Test def argh() {		
+		val urlType = new SillyJbossVfsUrlType()
+		urlType.createDir(new URL("vfszip:/opt/jboss-5.1.0-GA/server/tabula/deploy/home.war/WEB-INF/lib/common-27-SNAPSHOT.jar/")) should not be (null)
 	}
 
 }
 
-class VfsUrlHandler extends URLStreamHandler {
+class CatchAllUrlStreamHandlerFactory extends URLStreamHandlerFactory {
+	override def createURLStreamHandler(protocol: String) = 
+		if ("vfszip".equals(protocol) || "zip".equals(protocol)) new CatchAllUrlHandler
+		else null
+}
+
+class CatchAllUrlHandler extends URLStreamHandler {
 	override def openConnection(url: URL): URLConnection = {
 		throw new UnsupportedOperationException()
 	}
