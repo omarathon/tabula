047fa58163048bdcc64e9f729fdb6c5ba7e81a95
Mat Mannion <m.mannion@warwick.ac.uk>
2013-02-25 14:46:45 +0000

    TAB-553 Unit tests for uk.ac.warwick.tabula.data.convert - also make all of these TwoWayConverters

--- modules/common/src/main/scala/uk/ac/warwick/tabula/data/convert/AssessmentGroupIdConverter.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/convert/AssessmentGroupIdConverter.scala
@@ -1,22 +1,18 @@
 package uk.ac.warwick.tabula.data.convert
-
-import org.springframework.core.convert.converter.Converter
-import org.springframework.format.Formatter
 import org.springframework.beans.factory.annotation.Autowired
-import uk.ac.warwick.tabula.services.AssignmentService
-import java.util.Locale
+
 import uk.ac.warwick.tabula.data.model.forms.AssessmentGroup
+import uk.ac.warwick.tabula.services.AssignmentService
+import uk.ac.warwick.tabula.system.TwoWayConverter
 
-
-class AssessmentGroupIdConverter extends Converter[String, AssessmentGroup] with Formatter[AssessmentGroup] {
+class AssessmentGroupIdConverter extends TwoWayConverter[String, AssessmentGroup] {
 
 	@Autowired var service: AssignmentService = _
 
 	// Converter used for binding request
-	override def convert(id: String) = service.getAssessmentGroup(id).orNull
-
+	override def convertRight(id: String) = service.getAssessmentGroup(id) orNull
+	
 	// Formatter used for generating textual value in template
-	override def parse(id: String, locale: Locale): AssessmentGroup = convert(id)
-	override def print(group: AssessmentGroup, locale: Locale): String = group.id
+	override def convertLeft(group: AssessmentGroup) = Option(group) map { _.id } orNull
 
 }
