48177893bd88623e47522ea85db1cb77d77c8349
Zoe <zoe.james@warwick.ac.uk>
2013-03-04 13:25:36 +0000

    TAB-308: don't need all those imports

--- modules/scheduling/src/main/scala/uk/ac/warwick/tabula/scheduling/services/SitsStatusesImporter.scala
+++ modules/scheduling/src/main/scala/uk/ac/warwick/tabula/scheduling/services/SitsStatusesImporter.scala
@@ -1,38 +1,15 @@
 package uk.ac.warwick.tabula.scheduling.services
 
 import java.sql.ResultSet
-import java.sql.Types
+
 import scala.collection.JavaConversions.asScalaBuffer
-import scala.collection.JavaConversions.mapAsJavaMap
-import scala.util.matching.Regex
-import org.apache.commons.lang3.text.WordUtils
-import org.springframework.beans.factory.InitializingBean
+
 import org.springframework.jdbc.`object`.MappingSqlQuery
-import org.springframework.jdbc.core.SqlParameter
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Service
+
 import javax.sql.DataSource
 import uk.ac.warwick.spring.Wire
-import uk.ac.warwick.tabula.JavaImports.JList
-import uk.ac.warwick.tabula.data.model.Address
-import uk.ac.warwick.tabula.data.model.AddressType
-import uk.ac.warwick.tabula.data.model.Member
-import uk.ac.warwick.tabula.data.model.NextOfKin
-import uk.ac.warwick.userlookup.User
-import org.springframework.stereotype.Service
-import uk.ac.warwick.tabula.scheduling.commands.imports.ImportSingleMemberCommand
-import uk.ac.warwick.tabula.data.model.MemberUserType._
-import uk.ac.warwick.tabula.data.model.MemberUserType
-import uk.ac.warwick.tabula.scheduling.commands.imports.ImportSingleStudentCommand
-import uk.ac.warwick.tabula.scheduling.commands.imports.ImportSingleStaffCommand
 import uk.ac.warwick.tabula.helpers.Logging
-import org.springframework.remoting.rmi.RmiProxyFactoryBean
-import collection.JavaConverters._
-import uk.ac.warwick.tabula.data.model.Department
-import org.joda.time.LocalDate
-import uk.ac.warwick.tabula.data.model.Gender
-import uk.ac.warwick.membership.MembershipInterfaceWrapper
-import uk.ac.warwick.tabula.data.model.SitsStatus
 import uk.ac.warwick.tabula.scheduling.commands.imports.ImportSingleSitsStatusCommand
 
 @Service
