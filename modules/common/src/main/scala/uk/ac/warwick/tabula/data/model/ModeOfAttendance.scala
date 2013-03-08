15502328332c9ac4d108cadabb37f5def83323e8
Zoe <zoe.james@warwick.ac.uk>
2013-03-08 15:00:23 +0000

    TAB-308: new stuff

---
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/ModeOfAttendance.scala
@@ -0,0 +1,38 @@
+package uk.ac.warwick.tabula.data.model
+
+import scala.reflect.BeanProperty
+import org.hibernate.annotations.Type
+import org.joda.time.DateTime
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.NamedQueries
+import javax.persistence.NamedQuery
+
+@Entity
+@NamedQueries(Array(
+	new NamedQuery(name = "modeofattendance.code", query = "select modeOfAttendance from ModeOfAttendance modeOfAttendance where code = :code")))
+class ModeOfAttendance {
+
+	def this(code: String = null, shortName: String = null, fullName: String = null) {
+		this()
+		this.code = code
+		this.shortName = shortName
+		this.fullName = fullName
+	}
+
+	@Id @BeanProperty var code: String = _
+	@BeanProperty var shortName: String = _
+	@BeanProperty var fullName: String = _
+
+	@Type(`type` = "org.joda.time.contrib.hibernate.PersistentDateTime")
+	@BeanProperty var lastUpdatedDate = DateTime.now	
+	
+	override def toString = fullName.toLowerCase()
+	
+	def fullNameToDisplay = {
+		if (code.equals("F"))
+			"full time"
+		else fullName.toLowerCase
+	}
+
+}
---
+++ modules/scheduling/src/main/scala/uk/ac/warwick/tabula/scheduling/services/ModeOfAttendanceImporter.scala
@@ -0,0 +1,59 @@
+package uk.ac.warwick.tabula.scheduling.services
+
+import java.sql.ResultSet
+import scala.collection.JavaConversions._
+import org.springframework.jdbc.`object`.MappingSqlQuery
+import org.springframework.stereotype.Service
+import javax.sql.DataSource
+import uk.ac.warwick.spring.Wire
+import uk.ac.warwick.tabula.data.SitsStatusDao
+import uk.ac.warwick.tabula.helpers.Logging
+import uk.ac.warwick.tabula.scheduling.commands.imports.ImportSingleSitsStatusCommand
+import org.apache.log4j.Logger
+import uk.ac.warwick.tabula.data.model.SitsStatus
+import uk.ac.warwick.tabula.data.Transactions._
+import uk.ac.warwick.tabula.data.model.ModeOfAttendance
+import uk.ac.warwick.tabula.data.ModeOfAttendanceDao
+import uk.ac.warwick.tabula.scheduling.commands.imports.ImportSingleModeOfAttendanceCommand
+
+@Service
+class ModeOfAttendanceImporter extends Logging {
+	import ModeOfAttendanceImporter._
+
+	var modeOfAttendanceDao = Wire.auto[ModeOfAttendanceDao]
+	
+	var sits = Wire[DataSource]("sitsDataSource")
+	
+	lazy val modeOfAttendanceQuery = new ModeOfAttendanceQuery(sits)
+	
+	var modeOfAttendanceMap = slurpModeOfAttendances()
+
+	def getModeOfAttendances(): Seq[ImportSingleModeOfAttendanceCommand] = {
+		val modeOfAttendances = modeOfAttendanceQuery.execute.toSeq
+		modeOfAttendanceMap = slurpModeOfAttendances()
+		modeOfAttendances
+	}
+	
+	def slurpModeOfAttendances(): Map[String, ModeOfAttendance] = {
+		transactional(readOnly = true) {
+			logger.debug("refreshing SITS mode of attendance map")
+
+			(for (modeOfAttendanceCode <- modeOfAttendanceDao.getAllStatusCodes; status <- modeOfAttendanceDao.getByCode(modeOfAttendanceCode)) yield {
+				(modeOfAttendanceCode, status)
+			}).toMap
+		}
+	}		
+}
+
+object ModeOfAttendanceImporter {
+		
+	val GetModeOfAttendance = """
+		select moa_code, moa_snam, moa_name from intuit.ins_moa
+		"""
+	
+	class ModeOfAttendanceQuery(ds: DataSource) extends MappingSqlQuery[ImportSingleModeOfAttendanceCommand](ds, GetModeOfAttendance) {
+		compile()
+		override def mapRow(resultSet: ResultSet, rowNumber: Int) = new ImportSingleModeOfAttendanceCommand(resultSet)
+	}
+	
+}
