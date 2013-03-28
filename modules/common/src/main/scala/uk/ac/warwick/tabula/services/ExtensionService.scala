26345bccb937328968703a010d7cdd6dbc9a30eb
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2013-03-28 11:55:49 +0000

    TAB-621 - Fix 'n extensions need approval' label on the admin homepage

--- modules/common/src/main/scala/uk/ac/warwick/tabula/services/ExtensionService.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/services/ExtensionService.scala
@@ -1,25 +1,27 @@
 package uk.ac.warwick.tabula.services
 
-import scala.collection.JavaConversions._
-
-import org.hibernate.annotations.AccessType
-import org.hibernate.criterion.{Projections, Restrictions, Order}
 import org.springframework.stereotype.Service
 
-import javax.persistence.Entity
 import uk.ac.warwick.tabula.data.Daoisms
 import uk.ac.warwick.tabula.data.model._
 import uk.ac.warwick.tabula.data.model.forms._
 import uk.ac.warwick.tabula.helpers.Logging
-import uk.ac.warwick.tabula.services._
 
 trait ExtensionService {
 	def getExtensionById(id: String): Option[Extension]
+	def countUnapprovedExtensions(assignment:Assignment): Int
 }
 
 @Service(value = "extensionService")
 class ExtensionServiceImpl extends ExtensionService with Daoisms with Logging {
-	import Restrictions._	
 
 	def getExtensionById(id: String) = getById[Extension](id)
+
+	def countUnapprovedExtensions(assignment:Assignment): Int = {
+		session.createSQLQuery("""select count(*) from extension where assignment_id = :assignmentId
+															and requestedon is not null and approved=0 and rejected=0""")
+			.setString("assignmentId", assignment.id)
+			.uniqueResult
+			.asInstanceOf[Number].intValue
+	}
 }
