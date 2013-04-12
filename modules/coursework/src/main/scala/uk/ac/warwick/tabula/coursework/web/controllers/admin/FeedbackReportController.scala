d8c088014e02e8709f9f3d1330b828c0cb997a54
Steve Woodward <s.woodward@warwick.ac.uk>
2013-04-12 10:44:21 +0100

    TAB-530 Some fixes for git horror

--- modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/web/controllers/admin/FeedbackReportController.scala
+++ modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/web/controllers/admin/FeedbackReportController.scala
@@ -2,42 +2,40 @@ package uk.ac.warwick.tabula.coursework.web.controllers.admin
 
 import org.springframework.stereotype.Controller
 import uk.ac.warwick.tabula.coursework.web.controllers.CourseworkController
-import org.springframework.beans.factory.annotation.Autowired
-import uk.ac.warwick.tabula.services.ModuleAndDepartmentService
 import uk.ac.warwick.tabula.CurrentUser
-import uk.ac.warwick.tabula.Features
-import org.springframework.web.bind.annotation.{ModelAttribute, RequestMethod, RequestMapping, PathVariable}
+import org.springframework.web.bind.annotation.{ModelAttribute, RequestMapping, PathVariable}
 import uk.ac.warwick.tabula.data.model.Department
 import scala.Array
 import org.springframework.validation.Errors
 import uk.ac.warwick.tabula.web.Mav
-import uk.ac.warwick.tabula.coursework.web.Routes
 import uk.ac.warwick.tabula.coursework.commands.departments.FeedbackReportCommand
-import org.apache.poi.xssf.usermodel.{ XSSFSheet, XSSFWorkbook }
-import org.apache.poi.ss.util.WorkbookUtil
-import org.apache.poi.ss.usermodel.{ IndexedColors, ComparisonOperator }
-import org.apache.poi.ss.util.CellRangeAddress
-import uk.ac.warwick.tabula.web.views.ExcelView
-import uk.ac.warwick.tabula.data.model.Assignment
-import uk.ac.warwick.tabula.services.AuditEventIndexService
-import uk.ac.warwick.spring.Wire
-import uk.ac.warwick.tabula.services.AssignmentService
-import uk.ac.warwick.tabula.data.model.Submission
-import java.util.ArrayList
 import org.joda.time.format.DateTimeFormat
-import org.apache.poi.xssf.usermodel.XSSFFont
-import uk.ac.warwick.tabula.data.model.AuditEvent
+import org.joda.time.DateTime
+import uk.ac.warwick.tabula.coursework.services.feedbackreport.FeedbackReport
+import uk.ac.warwick.tabula.web.views.ExcelView
 
 @Controller
 @RequestMapping(Array("/admin/department/{dept}/reports/feedback"))
 class FeedbackReportController extends CourseworkController {
 
-	@ModelAttribute def command(@PathVariable(value = "dept") dept: Department) =
-		new FeedbackReportCommand(dept)
+	@ModelAttribute def command(@PathVariable(value = "dept") dept: Department, user: CurrentUser) =
+		new FeedbackReportCommand(dept, user)
 
-	@RequestMapping(method = Array(HEAD, GET))
+	@RequestMapping(method=Array(HEAD, GET))
+	def requestReport(cmd:FeedbackReportCommand, errors:Errors):Mav = {
+		val dateFormat = DateTimeFormat.forPattern("dd-MMM-yyyy HH:mm:ss")
+		val model = Mav("admin/assignments/feedbackreport/report_range",
+			"department" -> cmd.department,
+			"startDate" -> dateFormat.print(DateTime.now.minusMonths(3)),
+			"endDate" -> dateFormat.print(DateTime.now)
+		).noLayout()
+		model
+	}
+
+	@RequestMapping(method = Array(POST), params = Array("!test"))
 	def generateReport(cmd: FeedbackReportCommand) = {
-		new ExcelView(cmd.department.name + " feedback report.xlsx", cmd.apply())
+		val jobId = cmd.apply().id
+		Mav("admin/assignments/feedbackreport/progress")
 	}
 
 }
