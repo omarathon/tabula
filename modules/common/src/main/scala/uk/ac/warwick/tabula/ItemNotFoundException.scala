6b6d49d03cf7207b0df92d0de95e5a5d8b37087a
Mat Mannion <m.mannion@warwick.ac.uk>
2013-04-16 10:18:02 +0100

    TAB-665 Send correct status codes for 403s, 404s and 500s

--- modules/common/src/main/scala/uk/ac/warwick/tabula/ItemNotFoundException.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/ItemNotFoundException.scala
@@ -1,5 +1,8 @@
 package uk.ac.warwick.tabula
 
 import uk.ac.warwick.tabula.system.exceptions.UserError
+import org.apache.http.HttpStatus
 
-class ItemNotFoundException(val item: Any) extends RuntimeException() with UserError
+class ItemNotFoundException(val item: Any) extends RuntimeException() with UserError {
+	override val statusCode = HttpStatus.SC_NOT_FOUND
+}
