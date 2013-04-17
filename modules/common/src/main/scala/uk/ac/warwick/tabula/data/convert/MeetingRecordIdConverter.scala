f591013b906062aa73a980adc40e5bcd579a9208
Nick Kaijaks <Nick.Kaijaks@warwick.ac.uk>
2013-04-17 11:13:44 +0100

    TAB-359 Revert .gitignore, include missing file

---
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/convert/MeetingRecordIdConverter.scala
@@ -0,0 +1,15 @@
+package uk.ac.warwick.tabula.data.convert
+import org.springframework.beans.factory.annotation.Autowired
+
+import uk.ac.warwick.tabula.data.model.MeetingRecord
+import uk.ac.warwick.tabula.data.MeetingRecordDao
+import uk.ac.warwick.tabula.system.TwoWayConverter
+
+class MeetingRecordIdConverter extends TwoWayConverter[String, MeetingRecord] {
+
+	@Autowired var dao: MeetingRecordDao = _
+
+	override def convertRight(id: String) = (Option(id) flatMap { dao.get(_) }).orNull
+	override def convertLeft(meetingRecord: MeetingRecord) = (Option(meetingRecord) map {_.id}).orNull
+
+}
