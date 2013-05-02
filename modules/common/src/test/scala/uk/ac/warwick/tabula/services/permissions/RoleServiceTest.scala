620caae0da686913512d46c966e7082cb840f1e2
Nick Howes <n.howes@warwick.ac.uk>
2013-05-02 12:19:33 +0100

    TAB-87 Fix test to reflect department permission cascading.

--- modules/common/src/test/scala/uk/ac/warwick/tabula/services/permissions/RoleServiceTest.scala
+++ modules/common/src/test/scala/uk/ac/warwick/tabula/services/permissions/RoleServiceTest.scala
@@ -94,8 +94,7 @@ class RoleServiceTest extends TestBase with Mockito {
 		(service.getRolesFor(currentUser, module) exists { _ == DepartmentalAdministrator(dept) }) should be (true)
 	}
 	
-	/** Test that permissions checking DOESN'T go up from a department to its parent if it has one.
-	 *  This is the current behaviour though subject to change.
+	/** Test that permissions checking DOES go up from a department to its parent if it has one.
 	 */
 	@Test def parentDepartments = withUser("cuscav", "0672089") {
 
@@ -113,7 +112,7 @@ class RoleServiceTest extends TestBase with Mockito {
 		val service = new RoleServiceImpl()
 		service.roleProviders = Array(provider)
 
-		service.getRolesFor(currentUser, insub1).toList should equal (List())
+		service.getRolesFor(currentUser, insub1).toList should equal (List(DepartmentalAdministrator(in)))
 
 	}
 
