5a40ea64d4c294254aa056ef9df960e1a52ec8c2
Zoe <zoe.james@warwick.ac.uk>
2013-07-04 15:44:40 +0100

    TAB-579 : used unique department for test

--- modules/coursework/src/test/scala/uk/ac/warwick/tabula/coursework/ApplicationTest.scala
+++ modules/coursework/src/test/scala/uk/ac/warwick/tabula/coursework/ApplicationTest.scala
@@ -32,23 +32,23 @@ import org.junit.Ignore
 
 // scalastyle:off magic.number
 class ApplicationTest extends AppContextTestBase {
-    
+
     @Autowired var annotationMapper:RequestMappingHandlerMapping =_
-    
+
     @Value("${filesystem.index.audit.dir}") var auditIndexDir:String =_
-    
+
     @Test def handlerMappings = {
     	annotationMapper.getHandlerMethods.size should not be (0)
     	for ((info,method) <- annotationMapper.getHandlerMethods()) {
-    		
+
     	}
     }
-    
-    /** 
+
+    /**
      * Check that a property in default.properties can reference
      * a property found in tabula.properties, even though the latter
      * is loaded after the former.
-     * 
+     *
      * This is important for allowing "base.data.dir" to be set in
      * tabula.properties, and default.properties using that as
      * the root directory for many other directory locations.
@@ -56,52 +56,52 @@ class ApplicationTest extends AppContextTestBase {
     @Test def defaultProperties = {
     	auditIndexDir should fullyMatch regex ("target/test-[A-Z0-9]+-tmp/index/audit")
     }
-    
+
     @Transactional @Test def hibernatePersistence = {
 	  val assignment = new Assignment
 	  assignment.name = "Cake Studies 1"
 	  assignment.academicYear = new AcademicYear(2009)
 	  session.save(assignment)
 	  assignment.id should not be (null)
-	  
+
 	  session.flush
 	  session.clear
-	  
+
 	  val fetchedAssignment = session.get(classOf[Assignment], assignment.id).asInstanceOf[Assignment]
 	  fetchedAssignment.name should be("Cake Studies 1")
 	  fetchedAssignment.academicYear should be(assignment.academicYear)
 	}
-    
+
     /*
      * A post-load event in Department makes sure that a null settings
      * property is replaced with a new empty group on load.
      */
     @Transactional @Test def departmentLoadEvent {
       val dept = new Department
-      dept.code = "ch"
+      dept.code = "gr"
       dept.settings = null
       session.save(dept)
-      
+
       val id = dept.id
-      
+
       session.flush
       session.clear
-      
+
       session.load(classOf[Department], id) match {
         case loadedDepartment:Department => loadedDepartment.settings should not be(null)
         case _ => fail("Department not found")
       }
-      
-    }    
-    
+
+    }
+
     @Transactional @Test def getModules = {
       val modules = session.createCriteria(classOf[Module]).list
       modules.size should be (3)
       modules(0).asInstanceOf[Module].department.name should be ("Computer Science")
     }
-    
+
     @Ignore @Test def getAllCommands {
     	allCommandsInSystem("uk.ac.warwick.tabula.coursework").map {clz => println(clz.getName.substring("uk.ac.warwick.tabula.coursework".length))}
     }
-    
+
 }
