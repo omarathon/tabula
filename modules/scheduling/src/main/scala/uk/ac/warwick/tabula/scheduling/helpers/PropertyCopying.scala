4a9bb93da4619fc8955942dd7769aa3951e1e7d9
Zoe <zoe.james@warwick.ac.uk>
2013-07-19 14:47:02 +0100

    TAB-924a student import tests no longer broken

--- modules/scheduling/src/main/scala/uk/ac/warwick/tabula/scheduling/helpers/PropertyCopying.scala
+++ modules/scheduling/src/main/scala/uk/ac/warwick/tabula/scheduling/helpers/PropertyCopying.scala
@@ -27,7 +27,7 @@ trait PropertyCopying extends Logging {
 
 			// null == null in Scala so this is safe for unset values
 			if (oldValue != newValue) {
-				logger.debug("Detected property change; setting value")
+				logger.debug("Detected property change for " + property + " (" + oldValue + " -> " + newValue + "); setting value")
 
 				destinationBean.setPropertyValue(property, newValue)
 				true
