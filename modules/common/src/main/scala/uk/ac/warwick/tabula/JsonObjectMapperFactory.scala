3b0eee5cd45ee230870984d206593d384bd7c905
Nick Howes <n.howes@warwick.ac.uk>
2013-07-29 12:47:10 +0100

    TAB-984

--- modules/common/src/main/scala/uk/ac/warwick/tabula/JsonObjectMapperFactory.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/JsonObjectMapperFactory.scala
@@ -1,14 +1,20 @@
 package uk.ac.warwick.tabula
-import org.springframework.beans.factory.FactoryBean
+import scala.reflect._
+
 import org.springframework.beans.factory.config.AbstractFactoryBean
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.joda.JodaModule
 
-class JsonObjectMapperFactory extends AbstractFactoryBean[ObjectMapper] {
-	override def getObjectType = classOf[ObjectMapper]
-	override def createInstance = {
+abstract class ScalaFactoryBean[A : ClassTag] extends AbstractFactoryBean[A] {
+	override def getObjectType = classTag[A].runtimeClass
+}
+
+class JsonObjectMapperFactory extends ScalaFactoryBean[ObjectMapper] {
+	override def createInstance: ObjectMapper = {
 		val mapper = new ObjectMapper
 		mapper.registerModule(DefaultScalaModule)
+		mapper.registerModule(new JodaModule)
 		mapper
 	}
 }
