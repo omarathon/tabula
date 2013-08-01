693fe496a58d9602a723930c07f93525a1da4986
Nick Kaijaks <Nick.Kaijaks@warwick.ac.uk>
2013-08-01 12:15:24 +0100

    TAB-985 Code review typo

--- modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/Member.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/Member.scala
@@ -204,8 +204,8 @@ class StudentMember extends Member with StudentProperties {
 	}
 
 	override def mostSignificantCourseDetails: Option[StudentCourseDetails] = {
-		if (studentCourseDetails == null || studentCourseDetails.isEmpty) None else
-			studentCourseDetails.asScala.find { details => details.mostSignificant != null && details.mostSignificant }
+		if (studentCourseDetails == null || studentCourseDetails.isEmpty) None
+		else studentCourseDetails.asScala.find { details => details.mostSignificant != null && details.mostSignificant }
 	}
 
 	override def hasCurrentEnrolment: Boolean = studentCourseDetails.asScala.exists(_.hasCurrentEnrolment)
