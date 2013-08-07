e1fc1cb5d9927ddcc8ded1b1584ab27401a75e4b
Sarah O'Toole <s.o-toole@warwick.ac.uk>
2013-08-07 16:06:22 +0100

    Convert an AppContext test

--- modules/coursework/src/test/scala/uk/ac/warwick/tabula/coursework/commands/assignments/AddMarksCommandTest.scala
+++ modules/coursework/src/test/scala/uk/ac/warwick/tabula/coursework/commands/assignments/AddMarksCommandTest.scala
@@ -1,48 +1,49 @@
 package uk.ac.warwick.tabula.coursework.commands.assignments
 
 import scala.collection.JavaConversions._
-import uk.ac.warwick.tabula.{AppContextTestBase, TestBase, RequestInfo, Mockito}
+import uk.ac.warwick.tabula.{TestBase, RequestInfo, Mockito}
 import uk.ac.warwick.tabula.events.EventHandling
 import org.springframework.validation.BindException
 import uk.ac.warwick.tabula.services.UserLookupService
 import org.springframework.transaction.annotation.Transactional
+import org.hibernate.classic.Session
 
 // scalastyle:off magic.number
-class AddMarksCommandTest extends AppContextTestBase with Mockito {
+class AddMarksCommandTest extends TestBase with Mockito {
 
 	EventHandling.enabled = false
-	
+
 	/**
 	 * Check that validation marks an empty mark as an invalid row
 	 * so that the apply method skips it.
 	 */
 
-	@Transactional @Test
+	@Test
 	def emptyMarkField {
 		withUser("cusebr") {
 			val currentUser = RequestInfo.fromThread.get.user
 			val assignment = newDeepAssignment()
 			val command = new AdminAddMarksCommand(assignment.module, assignment, currentUser)
 			command.userLookup = mock[UserLookupService]
-			command.userLookup.getUserByWarwickUniId("0672088") answers { id => 
+			command.userLookup.getUserByWarwickUniId("0672088") answers { id =>
 				currentUser.apparentUser
 			}
-			
+
 			val errors = new BindException(command, "command")
-			
+
 			val marks1 = command.marks.get(0)
 			marks1.universityId = "0672088"
 			marks1.actualMark = ""
-			
+
 			val marks2 = command.marks.get(1)
 			marks2.universityId = "1235"
 			marks2.actualMark = "5"
-			
+
 			command.postExtractValidation(errors)
 			command.apply()
 		}
 	}
-	
+
 	/**
 	 * Check that validation allows either mark or grade to be non-empty
 	 */
@@ -51,19 +52,21 @@ class AddMarksCommandTest extends AppContextTestBase with Mockito {
 		withUser("cusebr") {
 			val currentUser = RequestInfo.fromThread.get.user
 			val assignment = newDeepAssignment()
-			val command = new AdminAddMarksCommand(assignment.module, assignment, currentUser)
+			val command = new AdminAddMarksCommand(assignment.module, assignment, currentUser) {
+				override val session = mock[Session]
+			}
 			command.userLookup = mock[UserLookupService]
-			command.userLookup.getUserByWarwickUniId("0672088") answers { id => 
+			command.userLookup.getUserByWarwickUniId("0672088") answers { id =>
 				currentUser.apparentUser
 			}
-			
+
 			val errors = new BindException(command, "command")
-			
+
 			val marks1 = command.marks.get(0)
 			marks1.universityId = "0672088"
 			marks1.actualMark = ""
 			marks1.actualGrade = "EXCELLENT"
-			
+
 			val marks2 = command.marks.get(1)
 			marks2.universityId = "55555"
 			marks2.actualMark = "65"
@@ -84,9 +87,9 @@ class AddMarksCommandTest extends AppContextTestBase with Mockito {
 
 			command.postExtractValidation(errors)
 			command.marks.filter(_.isValid).size should be (1)
-		
+
 			command.apply()
 		}
 	}
-	
+
 }
