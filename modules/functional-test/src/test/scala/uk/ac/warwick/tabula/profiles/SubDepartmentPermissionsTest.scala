942aeb474d4b198ad22b1494ba81811e423bcfcf
Chris May <chrismay50@gmail.com>
2013-09-17 14:02:47 +0100

    TAB-779 functional test for student lists

--- modules/functional-test/src/test/scala/uk/ac/warwick/tabula/profiles/SubDepartmentPermissionsTest.scala
+++ modules/functional-test/src/test/scala/uk/ac/warwick/tabula/profiles/SubDepartmentPermissionsTest.scala
@@ -1,15 +1,8 @@
 package uk.ac.warwick.tabula.profiles
 
-import uk.ac.warwick.tabula.BrowserTest
-import org.scalatest.GivenWhenThen
-import uk.ac.warwick.tabula.home.FixturesDriver
+import uk.ac.warwick.tabula.profiles.pages.SubDepartmentFixture
 
-class SubDepartmentPermissionsTest  extends BrowserTest with GivenWhenThen with FixturesDriver{
-	val TEST_UG_ROUTE_CODE="xx123"
-	val TEST_PG_ROUTE_CODE="xp123"
-	val TEST_DEPARTMENT_CODE="xxx"
-	val TEST_UNDERGRAD_COURSE_CODE="Ux123"
-	val TEST_POSTGRAD_COURSE_CODE="Rx123"
+class SubDepartmentPermissionsTest  extends SubDepartmentFixture{
 
 	/**
 	 * These tests all assume that:
@@ -18,28 +11,12 @@ class SubDepartmentPermissionsTest  extends BrowserTest with GivenWhenThen with 
 	 *  to be displayed when you view another user's profile
 	 *
 	 *  - the DepartmentAdmin role grants the holder that permission on any student who touches the department
+	 *
 	 */
 
-
-	before{
-		Given("A department xxx,a sub-department xxx-ug, and a sub-sub department xxx-ug1 exist")
-		And("Admin1 is a departmental admin in xxx")
-		And("Admin3 is a departmental admin in xxx-ug")
-		And("Admin4 is a departmental admin in xxx-ug1")
-			go to (Path("/scheduling/fixtures/setup")) // all set up in FixturesCommand
-
-		And("student1 and student2 have a membership record with an undergraduate course")
-			createRoute(TEST_UG_ROUTE_CODE, TEST_DEPARTMENT_CODE, "UG Route")
-			createCourse(TEST_UNDERGRAD_COURSE_CODE,"Test UG Course")
-			createStudentMember(P.Student1.usercode,routeCode=TEST_UG_ROUTE_CODE, courseCode=TEST_UNDERGRAD_COURSE_CODE,deptCode = TEST_DEPARTMENT_CODE, yearOfStudy = 1)
- 		  createStudentMember(P.Student2.usercode,routeCode=TEST_UG_ROUTE_CODE, courseCode=TEST_UNDERGRAD_COURSE_CODE,deptCode = TEST_DEPARTMENT_CODE, yearOfStudy = 2)
-
-		And("student3 has a membership record with an undergraduate course")
-			createCourse(TEST_POSTGRAD_COURSE_CODE,"Test PG Course")
-		  createRoute(TEST_PG_ROUTE_CODE, TEST_DEPARTMENT_CODE, "PG Route","PG")
-			createStudentMember(P.Student3.usercode,routeCode=TEST_PG_ROUTE_CODE, courseCode=TEST_POSTGRAD_COURSE_CODE,deptCode = TEST_DEPARTMENT_CODE)
-
-	}
+		before{
+			beforeEverything()
+		}
 
 	"A departmental administrator" should "have admin rights on students in their department" in {
 
