eed945856809810e69cac4da4bc920be417edcd2
Zoe <zoe.james@warwick.ac.uk>
2013-09-25 13:45:08 +0100

    TAB-1184 replace module registration index

--- config/scripts/schema/migrations/2013-09-25 - add occurrence to module registration.sql
+++ config/scripts/schema/migrations/2013-09-25 - add occurrence to module registration.sql
@@ -2,3 +2,4 @@ alter table moduleregistration add occurrence nvarchar2(10);
 
 drop index idx_modreg_notional_key;
 
+create unique index idx_modreg_notional_key on moduleregistration(scjcode, modulecode, academicyear, cats, occurrence);
