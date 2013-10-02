048fb21a67ec19e2528863ad105f09579554d2fb
Mat Mannion <m.mannion@warwick.ac.uk>
2013-10-02 13:51:34 +0100

    Avoid pyramid of doom

--- modules/profiles/src/main/scala/uk/ac/warwick/tabula/profiles/services/docconversion/RawStudentRelationshipExtractor.scala
+++ modules/profiles/src/main/scala/uk/ac/warwick/tabula/profiles/services/docconversion/RawStudentRelationshipExtractor.scala
@@ -27,67 +27,82 @@ import uk.ac.warwick.spring.Wire
 import uk.ac.warwick.tabula.data.model.StudentRelationshipType
 
 object RawStudentRelationshipExtractor {
-	val AcceptedFileExtensions = Seq(".xlsx")
-}
-
-@Service
-class RawStudentRelationshipExtractor {
-	
 	type RowData = Map[String, String]
 	type RawStudentRelationship = (Member, Option[Member])
 	type ErrorCode = (String, String)
 	
 	type ParsedRow = (RowData, Option[RawStudentRelationship], Seq[ErrorCode])
 	
+	val AcceptedFileExtensions = Seq(".xlsx")
+}
+
+class RawStudentRelationshipRow(relationshipType: StudentRelationshipType, val rowData: Map[String, String]) {
+	import RawStudentRelationshipExtractor._
+	
 	var profileService = Wire[ProfileService]
+	
+	def extractStudent(): (Option[StudentMember], Option[ErrorCode]) = {
+		def validateCourseDetails(student: StudentMember): Option[ErrorCode] = { 
+			student.mostSignificantCourseDetails match {
+				case Some(scd) if scd.department == null => 
+					Some("student_id" -> "profiles.relationship.allocate.student.noDepartment")
+				case Some(scd) if relationshipType.readOnly(scd.department) =>
+					Some("student_id" -> "profiles.relationship.allocate.student.readOnlyDepartment")
+				case Some(scd) => None
+				case None => Some("student_id" -> "profiles.relationship.allocate.student.noCourseDetails")
+			}
+		}
+		
+		rowData("student_id") match {
+			case strStudentId if strStudentId.matches("\\d+") =>
+				val studentId = UniversityId.zeroPad(strStudentId)
+				
+				profileService.getMemberByUniversityId(studentId) match {
+					case Some(student: StudentMember) => 
+						(Some(student), validateCourseDetails(student))
+						
+					case Some(member) => // non-student member 
+						(None, Some("student_id" -> "profiles.relationship.allocate.universityId.notStudent"))
+						
+					case _ => (None, Some("student_id" -> "profiles.relationship.allocate.universityId.notMember"))
+				}
+			case _ => (None, Some("student_id" -> "profiles.relationship.allocate.universityId.badFormat"))
+		}
+	}
+	
+	def extractAgent(): (Option[Member], Option[ErrorCode]) = {
+		rowData.get("agent_id") match {
+			case Some(strAgentId) if strAgentId.hasText && strAgentId.matches("\\d+") =>
+				val agentId = UniversityId.zeroPad(strAgentId)
+				
+				profileService.getMemberByUniversityId(agentId) match {
+					case Some(member) => (Some(member), None)
+					case _ => (None, Some("agent_id" -> "profiles.relationship.allocate.universityId.notMember"))
+				}
+			case Some("ERROR:#N/A") | None => (None, None)
+			case _ => (None, Some("agent_id" -> "profiles.relationship.allocate.universityId.badFormat"))
+		}
+	}
+	
+	// Only if there is a student ID in the row
+	def isValid = rowData.contains("student_id") && rowData("student_id").hasText
+}
+
+@Service
+class RawStudentRelationshipExtractor {
+	import RawStudentRelationshipExtractor._
 
 	/**
 	 * Method for reading in a xlsx spreadsheet and converting it into a list of relationships
 	 */
-	def readXSSFExcelFile(file: InputStream, relationshipType: StudentRelationshipType): Seq[ParsedRow] = {
-		def parseStudent(row: RowData): (Option[StudentMember], Option[ErrorCode]) = {
-			row("student_id") match {
-				case strStudentId if strStudentId.matches("\\d+") =>
-					val studentId = UniversityId.zeroPad(strStudentId)
-					
-					profileService.getMemberByUniversityId(studentId) match {
-						case Some(student: StudentMember) =>
-							student.mostSignificantCourseDetails match {
-								case Some(scd) if scd.department == null => 
-									(Some(student), Some("student_id" -> "profiles.relationship.allocate.student.noDepartment"))
-								case Some(scd) if relationshipType.readOnly(scd.department) =>
-									(Some(student), Some("student_id" -> "profiles.relationship.allocate.student.readOnlyDepartment"))
-								case Some(scd) => (Some(student), None)
-								case None => (Some(student), Some("student_id" -> "profiles.relationship.allocate.student.noCourseDetails"))
-							}
-						case Some(member) => (None, Some("student_id" -> "profiles.relationship.allocate.universityId.notStudent"))
-						case _ => (None, Some("student_id" -> "profiles.relationship.allocate.universityId.notMember"))
-					}
-				case _ => (None, Some("student_id" -> "profiles.relationship.allocate.universityId.badFormat"))
-			}
-		}
-		
-		def parseAgent(row: RowData): (Option[Member], Option[ErrorCode]) = {
-			row.get("agent_id") match {
-				case Some(strAgentId) if strAgentId.hasText && strAgentId.matches("\\d+") =>
-					val agentId = UniversityId.zeroPad(strAgentId)
-					
-					profileService.getMemberByUniversityId(agentId) match {
-						case Some(member) => (Some(member), None)
-						case _ => (None, Some("agent_id" -> "profiles.relationship.allocate.universityId.notMember"))
-					}							
-				case Some("ERROR:#N/A") | None => (None, None)
-				case _ => (None, Some("agent_id" -> "profiles.relationship.allocate.universityId.badFormat"))
-			}
-		}
-		
+	def readXSSFExcelFile(file: InputStream, relationshipType: StudentRelationshipType): Seq[ParsedRow] =
 		SpreadsheetHelpers.parseXSSFExcelFile(file)
-			.filter { row => row.contains("student_id") && row("student_id").hasText } // Only if there is a student ID in the row
+			.map { rowData => new RawStudentRelationshipRow(relationshipType, rowData) }
+			.filter { _.isValid } // Ignore blank rows
 			.map { row =>
-				val (student, studentError) = parseStudent(row)
-				val (agent, agentError) = parseAgent(row)
+				val (student, studentError) = row.extractStudent()
+				val (agent, agentError) = row.extractAgent()
 				
-				(row, student.map { student => student -> agent }, Seq(studentError, agentError).flatten)
+				(row.rowData, student.map { student => (student -> agent) }, Seq(studentError, agentError).flatten)
 			}
-	}
 }
