b9a171683149972d70b255261872729fa83ef965
Chris May <chrismay50@gmail.com>
2013-10-10 12:02:11 +0100

    TAB-1249 Replace fancy-but-slow stream reader with dull-but-effective Spring alternative

--- modules/functional-test/src/test/scala/uk/ac/warwick/tabula/FileDownload.scala
+++ modules/functional-test/src/test/scala/uk/ac/warwick/tabula/FileDownload.scala
@@ -4,7 +4,8 @@ import dispatch.classic.{Request, StatusCode, url, NoLogging, thread, Http}
 import dispatch.classic.thread.ThreadSafeHttpClient
 import org.apache.http.client.params.{CookiePolicy, ClientPNames}
 import scala.util.{Failure, Success, Try}
-import java.io.InputStream
+import java.io.{Reader, InputStreamReader, InputStream}
+import org.springframework.util.FileCopyUtils
 
 class FileDownload(uri: String, credentials: Option[LoginDetails]) {
 
@@ -17,14 +18,14 @@ class FileDownload(uri: String, credentials: Option[LoginDetails]) {
 	val request = (url(uri))
 	val authorizedRequest = credentials map (cred => request.as_!(cred.usercode, cred.password)) getOrElse (request)
 
-	def streamToString(is:InputStream):String = new String(Stream.continually(is.read).takeWhile(-1 != _).map(_.toByte).toArray,"UTF-8" )
+	def streamToString(is:Reader):String = FileCopyUtils.copyToString(is)
 
 	case class Result(content:Option[String], statusCode:Option[Int])
 
 	lazy val result = {
 		// as_str barfs with an EncodingException, on feedback PDFs even though everything renders fine.
 		// manually reading the content into a UTF-8 string seems to work, so we'll stick with that for now.
-		Try(http(authorizedRequest >>{is=>streamToString(is)})) match {
+		Try(http(authorizedRequest >>~{r=>streamToString(r)})) match {
 			case Success(src) => Result(Some(src), Some(200)) // treat things like 201, 204, as 200.
 			case Failure(StatusCode(code, contents)) => Result(None, Some(code))
 			case Failure(unexpectedException)=>throw unexpectedException
