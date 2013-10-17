3d52ccb052a79ec41e912b88a373e16d2b7e20fd
Nick Kaijaks <Nick.Kaijaks@warwick.ac.uk>
2013-10-17 13:34:17 +0100

    TAB-1400 Fix freemarker error on null shortDepartment, ULS tweaks
    ULS throws on any exception in cache create, not just UserLookupException (which may not get propagated here anyway). Test coverage for caching.

--- modules/common/src/test/scala/uk/ac/warwick/tabula/MockUserLookup.scala
+++ modules/common/src/test/scala/uk/ac/warwick/tabula/MockUserLookup.scala
@@ -1,29 +1,35 @@
 package uk.ac.warwick.tabula
 import scala.collection.JavaConversions._
 import org.apache.commons.lang3.StringUtils._
-import uk.ac.warwick.tabula.services.UserLookupService
-import uk.ac.warwick.userlookup.AnonymousUser
-import uk.ac.warwick.userlookup.Group
-import uk.ac.warwick.userlookup.GroupService
-import uk.ac.warwick.userlookup.User
-import uk.ac.warwick.userlookup.UserLookupAdapter
-import uk.ac.warwick.userlookup.WebServiceTimeoutConfig
+import uk.ac.warwick.tabula.services.{UserByWarwickIdCache, UserLookupService}
+import uk.ac.warwick.userlookup._
 import uk.ac.warwick.userlookup.webgroups.GroupInfo
 import uk.ac.warwick.userlookup.webgroups.GroupNotFoundException
 import uk.ac.warwick.tabula.JavaImports._
+import scala.Some
+import scala.util.Random
+import java.security.MessageDigest
+import sun.misc.BASE64Encoder
+import uk.ac.warwick.tabula.data.model.Gender.{Female, Male}
+import uk.ac.warwick.tabula.UserFlavour.{Applicant, Anonymous, Unverified, Vanilla}
 
-class MockUserLookup(var defaultFoundUser: Boolean) extends UserLookupAdapter(null) with UserLookupService with JavaImports {
+class MockUserLookup(var defaultFoundUser: Boolean)
+	extends UserLookupAdapter(null)
+	with UserLookupService
+	with JavaImports
+	with MockUser {
+
 	def this() = {
 		this(false)
 	}
-  
+
 	var users: Map[String, User] = Map()
 	var filterUserResult: JList[User] = JArrayList()
-	
+
 	var findUsersEnabled: Boolean = false
-	
+
 	val groupService: MockGroupService = new MockGroupService
-	
+
 	override def getGroupService() = groupService
 
     def addFindUsersWithFilterResult(user: User) {
@@ -42,8 +48,8 @@ class MockUserLookup(var defaultFoundUser: Boolean) extends UserLookupAdapter(nu
     		}
     	}
     }
-    
-    override def getUserByWarwickUniId(warwickId: String) = 
+
+    override def getUserByWarwickUniId(warwickId: String) =
     	users.values.find(_.getWarwickId() == warwickId).getOrElse {
     		if (defaultFoundUser) {
     			val user = new User()
@@ -54,7 +60,7 @@ class MockUserLookup(var defaultFoundUser: Boolean) extends UserLookupAdapter(nu
     			new AnonymousUser
     		}
 		}
-    
+
     override def getUserByWarwickUniId(warwickId: String, includeDisabledLogins: Boolean) = getUserByWarwickUniId(warwickId)
 
     override def findUsersWithFilter(filterValues: JMap[String, String]) = {
@@ -69,7 +75,7 @@ class MockUserLookup(var defaultFoundUser: Boolean) extends UserLookupAdapter(nu
 
     override def getUsersByUserIds(userIds: JList[String]): JMap[String, User] = {
     	val map: Map[String, User] = (userIds.toList map { id => (id -> getUserByUserId(id) )}).toMap
-    	
+
     	map
     }
 
@@ -79,18 +85,11 @@ class MockUserLookup(var defaultFoundUser: Boolean) extends UserLookupAdapter(nu
      */
     def registerUsers(userIds: String*) {
         for (id <- userIds) {
-            val user = new User(id);
-            user.setFoundUser(true);
-            user.setFirstName(capitalize(id)+"y");
-            user.setLastName("Mc"+capitalize(id)+"erson");
-            user.setDepartment("Department of " + capitalize(id));
-            user.setShortDepartment("Dept of " + capitalize(id));
-            user.setDepartmentCode("D" + capitalize(id.substring(0, 1)));
-            user.setEmail(id + "@example.com");
+            val user = mockUser(id);
             users += (id -> user)
         }
     }
-    
+
     def registerUserObjects(u: User*) {
     	for (user <- u) {
     		users += (user.getUserId -> user)
@@ -99,11 +98,83 @@ class MockUserLookup(var defaultFoundUser: Boolean) extends UserLookupAdapter(nu
 
 }
 
+
+class MockCachingLookupService(var flavour: UserFlavour = Vanilla)
+	extends UserLookupAdapter(null)
+	with UserLookupService
+	with UserByWarwickIdCache
+	with MockUser {
+
+	private val sha = MessageDigest.getInstance("SHA-1")
+	private val b64 = new BASE64Encoder()
+
+	override def getUserByWarwickUniIdUncached(warwickId: String): User = {
+		flavour match {
+			case Unverified => new UnverifiedUser(new UserLookupException)
+			case Anonymous => new AnonymousUser()
+			case Applicant => {
+				val user = new AnonymousUser()
+				user.setWarwickId(warwickId)
+				user
+			}
+			case Vanilla => {
+				// hash WarwickId to a consistent 6 char 'usercode'. Tiny, but finite risk of collision/short usercodes.
+				val userId = b64.encode(sha.digest(warwickId.getBytes)).filter(_.isLower).take(6)
+				val user = mockUser(userId)
+				user.setWarwickId(warwickId)
+				user
+			}
+		}
+	}
+
+	override def getUserByWarwickUniId(id: String) = getUserByWarwickUniId(id, true)
+
+	override def getUserByWarwickUniId(id: String, ignored: Boolean) = UserByWarwickIdCache.get(id)
+}
+
+
+trait MockUser {
+	def mockUser(id: String): User = {
+		val user = new User(id)
+		user.setFoundUser(true)
+		user.setVerified(true)
+		user.setFirstName(capitalize(id)+"y");
+		user.setLastName("Mc"+capitalize(id)+"erson");
+		user.setDepartment("Department of " + capitalize(id));
+		user.setShortDepartment("Dept of " + capitalize(id));
+		user.setDepartmentCode("D" + capitalize(id.substring(0, 1)));
+		user.setEmail(id + "@example.com");
+		user
+	}
+}
+
+/* Vanilla is a verified user, whose session was completely retrieved from SSO.
+
+ * Anonymous means user is *not* logged into SSO, but *is*
+ * implicitly verified - as anonymous - with default properties set,
+ * including warwickId of empty string.
+ *
+ * Unverified means a UserLookupException has been raised, which should
+ * return an UnverifiedUser, a subclass of AnonymousUser;
+ * most properties should have default properties set too.
+ *
+ * Applicant is another special case of AnonymousUser, this time with
+ * a real warwickID set.
+ */
+sealed abstract class UserFlavour
+object UserFlavour {
+	case object Anonymous extends UserFlavour
+	case object Unverified extends UserFlavour
+	case object Applicant extends UserFlavour
+	case object Vanilla extends UserFlavour
+}
+
+
 class MockGroupService extends GroupService {
 	var groupMap: Map[String, Group] = Map()
 	var usersInGroup: Map[(String, String), Boolean] = Map() withDefaultValue(false)
 	var groupNamesForUserMap: Map[String, Seq[String]] = Map()
-	
+
 	override def getGroupByName(groupName: String) = groupMap.get(groupName).getOrElse {
 		throw new GroupNotFoundException(groupName)
 	}
@@ -114,14 +185,14 @@ class MockGroupService extends GroupService {
 
     override def getGroupsForUser(user: String) = ???
 
-    override def getGroupsNamesForUser(user: String) = 
+    override def getGroupsNamesForUser(user: String) =
     	groupNamesForUserMap.get(user) match {
     		case Some(groups) => groups
     		case _ => JArrayList[String]()
     	}
 
     override def getRelatedGroups(groupName: String) = ???
-    
+
     override def getUserCodesInGroup(groupName: String) = {
         try {
             getGroupByName(groupName).getUserCodes()
@@ -135,8 +206,8 @@ class MockGroupService extends GroupService {
     override def setTimeoutConfig(config: WebServiceTimeoutConfig) {}
 
     override def getGroupInfo(name: String) = new GroupInfo(getGroupByName(name).getUserCodes().size())
-    
+
     override def clearCaches() = ???
     override def getCaches() = ???
-	
+
 }
