e4a8af88ebadfea58b5d2c63f2ab5eb48a62727c
Nick Howes <n.howes@warwick.ac.uk>
2013-10-24 10:58:19 +0100

    Fix indenting

--- modules/common/src/test/scala/uk/ac/warwick/tabula/FunctionalContext.scala
+++ modules/common/src/test/scala/uk/ac/warwick/tabula/FunctionalContext.scala
@@ -4,19 +4,19 @@ import org.springframework.scala.context.function.{FunctionalConfigApplicationCo
 import uk.ac.warwick.spring.SpringConfigurer
 
 abstract class FunctionalContext extends FunctionalConfiguration {
-  bean[SpringConfigurer](name="springConfigurer") {
-    new SpringConfigurer()
-  }
+	bean[SpringConfigurer](name="springConfigurer") {
+		new SpringConfigurer()
+	}
 }
 
 trait FunctionalContextTesting {
 
-  def inContext[A <: FunctionalContext : Manifest](fn: => Unit) {
-    def use(ctx: FunctionalConfigApplicationContext, fn: => Unit) {
-      try fn
-      finally ctx.destroy()
-    }
-    use(FunctionalConfigApplicationContext[A](), fn)
-  }
+	def inContext[A <: FunctionalContext : Manifest](fn: => Unit) {
+		def use(ctx: FunctionalConfigApplicationContext, fn: => Unit) {
+			try fn
+			finally ctx.destroy()
+		}
+		use(FunctionalConfigApplicationContext[A](), fn)
+	}
 
 }
