02a09da897a9b5c5f2fa34e7015d01bac277b671
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2013-10-29 11:18:15 +0000

    TAB-1422 - Bring profiles groups section into line with the groups homepage for students

--- modules/groups/src/main/scala/uk/ac/warwick/tabula/groups/commands/ListStudentsGroupsCommand.scala
+++ modules/groups/src/main/scala/uk/ac/warwick/tabula/groups/commands/ListStudentsGroupsCommand.scala
@@ -1,34 +1,40 @@
 package uk.ac.warwick.tabula.groups.commands
 
-import uk.ac.warwick.tabula.data.model.{Member, Module}
-import uk.ac.warwick.tabula.data.model.groups.{SmallGroup, SmallGroupSet}
+import uk.ac.warwick.tabula.data.model.Member
 import uk.ac.warwick.tabula.commands.{Unaudited, ReadOnly, Command}
 import uk.ac.warwick.spring.Wire
 import uk.ac.warwick.tabula.services.SmallGroupService
 import uk.ac.warwick.tabula.permissions.Permissions
-import uk.ac.warwick.tabula.data.model.groups.SmallGroupAllocationMethod
+import uk.ac.warwick.tabula.groups.web.views.GroupsViewModel.ViewModules
+import uk.ac.warwick.tabula.groups.web.controllers.GroupsDisplayHelper
 
 
-trait ListStudentsGroupsCommand extends Applicable[Map[Module, Map[SmallGroupSet, Seq[SmallGroup]]]]
+trait ListStudentsGroupsCommand extends Applicable[ViewModules]
 
 /**
  * Gets the data for a students view of all small groups they're a member of.
  */
 
 class ListStudentsGroupsCommandImpl(member: Member)
-	extends Command[Map[Module, Map[SmallGroupSet, Seq[SmallGroup]]]]
-	with TutorHomeCommand
+	extends Command[ViewModules]
 	with ReadOnly
 	with Unaudited {
+
+	import GroupsDisplayHelper._
 
 	PermissionCheck(Permissions.Profiles.Read.SmallGroups, member)
 
 	var smallGroupService = Wire[SmallGroupService]
 
-	def applyInternal() =
-		smallGroupService.findSmallGroupsByStudent(member.asSsoUser)
-			.filter(_.groupSet.visibleToStudents)
-			.groupBy { group => group.groupSet }
-			.groupBy { case (set, groups) => set.module }
+	def applyInternal() = {
+
+		val user = member.asSsoUser
+		val memberGroupSets = smallGroupService.findSmallGroupSetsByMember(user)
+		val releasedMemberGroupSets = getGroupSetsReleasedToStudents(memberGroupSets)
+		val nonEmptyMemberViewModules = getViewModulesForStudent(releasedMemberGroupSets, getGroupsToDisplay(_, user))
+
+		ViewModules(nonEmptyMemberViewModules.sortBy(_.module.code), canManageDepartment = false)
+
+	}
 
 }
--- modules/groups/src/main/scala/uk/ac/warwick/tabula/groups/web/controllers/StudentGroupsController.scala
+++ modules/groups/src/main/scala/uk/ac/warwick/tabula/groups/web/controllers/StudentGroupsController.scala
@@ -4,10 +4,8 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{PathVariable, RequestMapping, ModelAttribute}
 import uk.ac.warwick.tabula.groups.commands.ListStudentsGroupsCommandImpl
 import uk.ac.warwick.tabula.web.Mav
-import uk.ac.warwick.tabula.data.model.groups.SmallGroupSet
 import uk.ac.warwick.tabula.data.model.groups.SmallGroupFormat.Example
 import uk.ac.warwick.tabula.data.model.Member
-import uk.ac.warwick.tabula.groups.web.views.GroupsViewModel
 
 /**
  * Displays the groups that the current user is a member of.
@@ -21,19 +19,16 @@ class StudentGroupsController extends GroupsController {
 
 	@RequestMapping(method = Array(POST, GET))
 	def listGroups(@ModelAttribute("command") command: ListStudentsGroupsCommandImpl): Mav = {
-		val mapping = command.apply()
 
-		val data = generateViewModules(mapping, GroupsViewModel.StudentAssignedToGroup)
+		val data = command.apply()
 
 		val title = {
-			val smallGroups: Seq[SmallGroupSet] = for (
-				setToGroupsMap <- mapping.values.toSeq;
-				groupSet <- setToGroupsMap.keys
-			) yield groupSet
+
+			val smallGroups = data.moduleItems.flatMap(_.setItems.map(_.set))
 
 			val formats = smallGroups.map(_.format.description).distinct
 			val pluralisedFormats = formats.map {
-				case s:String if (s == Example.description) => s + "es"
+				case s:String if s == Example.description => s + "es"
 				case s:String => s + "s"
 				case _ =>
 			}
