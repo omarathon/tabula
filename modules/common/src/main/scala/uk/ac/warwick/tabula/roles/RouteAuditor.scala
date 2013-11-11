e53e8c33e1326efbe0c33e6d17caf37edfc07d6d
Mat Mannion <m.mannion@warwick.ac.uk>
2013-11-11 15:48:33 +0000

    TAB-1514

---
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/roles/RouteAuditor.scala
@@ -0,0 +1,19 @@
+package uk.ac.warwick.tabula.roles
+
+import uk.ac.warwick.tabula.data._
+import uk.ac.warwick.tabula.permissions.Permissions._
+import uk.ac.warwick.tabula.permissions.PermissionsTarget
+import uk.ac.warwick.tabula.JavaImports
+
+case class RouteAuditor(route: model.Route) extends BuiltInRole(RouteAuditorRoleDefinition, route)
+
+case object RouteAuditorRoleDefinition extends BuiltInRoleDefinition {
+	
+	override def description = "Route Auditor"
+
+	GrantsScopedPermission( 
+		MonitoringPoints.View
+	)
+
+	def canDelegateThisRolesPermissions: JavaImports.JBoolean = true
+}
