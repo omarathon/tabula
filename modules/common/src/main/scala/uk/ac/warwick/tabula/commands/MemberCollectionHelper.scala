bfd379776c6a7702f47427e9f4207d132bd5bf55
Sarah O'Toole <s.o-toole@warwick.ac.uk>
2013-11-13 17:21:43 +0000

    TAB-1522

---
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/commands/MemberCollectionHelper.scala
@@ -0,0 +1,23 @@
+package uk.ac.warwick.tabula.commands
+
+import uk.ac.warwick.tabula.data.model.Member
+
+trait MemberCollectionHelper {
+
+	def allMembersRoutesSorted(members: Iterable[Member]) = {
+		val routes = for {
+			member <- members
+			course <- member.mostSignificantCourseDetails
+			if Option(course.route).isDefined
+		} yield course.route
+		routes.toSeq.sortBy(_.code).distinct
+	}
+
+	def allMembersYears(members: Iterable[Member]) = {
+		val years = for (
+			member <- members;
+			course <- member.mostSignificantCourseDetails)
+			yield course.latestStudentCourseYearDetails.yearOfStudy
+		years.toSeq.distinct.sorted
+	}
+}
