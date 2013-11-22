7ee65d67f12109eb5d014e8de8d9ed904fcbf2f6
Zoe <zoe.james@warwick.ac.uk>
2013-11-22 16:10:33 +0000

    TAB-1450 fixed uni id converter test

--- modules/common/src/test/scala/uk/ac/warwick/tabula/data/convert/MemberUniversityIdConverterTest.scala
+++ modules/common/src/test/scala/uk/ac/warwick/tabula/data/convert/MemberUniversityIdConverterTest.scala
@@ -8,30 +8,30 @@ import uk.ac.warwick.tabula.data.model.StaffMember
 import uk.ac.warwick.tabula.Mockito
 
 class MemberUniversityIdConverterTest extends TestBase with Mockito {
-	
+
 	val converter = new MemberUniversityIdConverter
 	var service = mock[ProfileService]
 	converter.service = service
-	
+
 	@Test def validInput {
 		val member = new StaffMember
 		member.universityId = "0672089"
-			
-		service.getMemberByUniversityId("0672089") returns (Some(member))
-		
+
+		service.getMemberByUniversityIdStaleOrFresh("0672089") returns (Some(member))
+
 		converter.convertRight("0672089") should be (member)
 	}
-	
+
 	@Test def invalidInput {
-		service.getMemberByUniversityId("20X6") returns (None)
-		
+		service.getMemberByUniversityIdStaleOrFresh("20X6") returns (None)
+
 		converter.convertRight("20X6") should be (null)
 	}
-	
+
 	@Test def formatting {
 		val member = new StaffMember
 		member.universityId = "0672089"
-		
+
 		converter.convertLeft(member) should be ("0672089")
 		converter.convertLeft(null) should be (null)
 	}
