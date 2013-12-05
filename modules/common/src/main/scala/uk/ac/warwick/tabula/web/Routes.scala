fbdd434879817bc4aa3e61c268959489062a4028
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2013-12-05 13:01:11 +0000

    TAB-269 - add warning about not so common routes

--- modules/common/src/main/scala/uk/ac/warwick/tabula/web/Routes.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/web/Routes.scala
@@ -6,6 +6,13 @@ import uk.ac.warwick.tabula.data.model.Assignment
 object Routes {
 	private def encoded(string: String) = URLEncoder.encode(string, "UTF-8")
 
+	/**
+	 * Warning! - These routes are not 'common'. They all assume that the current context is /coursework
+	 *
+	 * They need to be in common so we don't have a dependancy between coursework and common.
+	 * TODO - work out a nicer way of doing this - maybe by adding a workflow URL helper to coursework
+	 */
+
 	private def assignmentroot(assignment: Assignment) =
 		"/admin/module/%s/assignments/%s" format (encoded(assignment.module.code), assignment.id)
 
@@ -16,4 +23,5 @@ object Routes {
 	object onlineModeration {
 		def apply(assignment: Assignment) = assignmentroot(assignment) + "/marker/feedback/online/moderation"
 	}
+
 }
