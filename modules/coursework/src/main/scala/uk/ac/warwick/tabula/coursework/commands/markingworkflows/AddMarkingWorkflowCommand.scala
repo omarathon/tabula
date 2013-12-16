aae3282f47e41bdd532290df9cd8679e2b706dff
Mat Mannion <m.mannion@warwick.ac.uk>
2013-12-16 11:52:14 +0000

    TAB-1678 + Opportunistic cake-ing

--- modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/commands/markingworkflows/AddMarkingWorkflowCommand.scala
+++ modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/commands/markingworkflows/AddMarkingWorkflowCommand.scala
@@ -1,15 +1,30 @@
 package uk.ac.warwick.tabula.coursework.commands.markingworkflows
-
-import uk.ac.warwick.tabula.data.model._
+import org.springframework.validation.Errors
 import uk.ac.warwick.tabula.commands._
 import uk.ac.warwick.tabula.data.Transactions._
-import org.springframework.validation.Errors
-import uk.ac.warwick.tabula.permissions._
+import uk.ac.warwick.tabula.data.model._
 import uk.ac.warwick.tabula.data.model.MarkingMethod.{ModeratedMarking, StudentsChooseMarker, SeenSecondMarking}
+import uk.ac.warwick.tabula.permissions._
+import uk.ac.warwick.tabula.services.AssignmentServiceComponent
+import uk.ac.warwick.tabula.services.AutowiringAssignmentServiceComponent
+import uk.ac.warwick.tabula.system.permissions.PermissionsChecking
+import uk.ac.warwick.tabula.system.permissions.PermissionsCheckingMethods
+import uk.ac.warwick.tabula.system.permissions.RequiresPermissionsChecking
+import uk.ac.warwick.tabula.services.MarkingWorkflowServiceComponent
+import uk.ac.warwick.tabula.services.AutowiringMarkingWorkflowServiceComponent
 
-class AddMarkingWorkflowCommand(department: Department) extends ModifyMarkingWorkflowCommand(department) {
+object AddMarkingWorkflowCommand {
+	def apply(department: Department) =
+		new AddMarkingWorkflowCommandInternal(department)
+			with ComposableCommand[MarkingWorkflow]
+			with AddMarkingWorkflowCommandPermissions
+			with AddMarkingWorkflowCommandValidation
+			with AddMarkingWorkflowCommandDescription
+			with AutowiringMarkingWorkflowServiceComponent
+}
 
-	PermissionCheck(Permissions.MarkingWorkflow.Create, department)
+class AddMarkingWorkflowCommandInternal(department: Department) extends ModifyMarkingWorkflowCommand(department) {
+	self: MarkingWorkflowServiceComponent =>
 
 	// Copy properties to a new MarkingWorkflow, save it transactionally, return it.
 	def applyInternal() = {
@@ -21,20 +36,33 @@ class AddMarkingWorkflowCommand(department: Department) extends ModifyMarkingWor
 				case _ => throw new UnsupportedOperationException(markingMethod + " not specified")
 			}
 			this.copyTo(markingWorkflow)
-			session.save(markingWorkflow)
+			markingWorkflowService.save(markingWorkflow)
 			markingWorkflow
 		}
 	}
-	
+}
+
+trait AddMarkingWorkflowCommandValidation extends MarkingWorkflowCommandValidation {
+	self: MarkingWorkflowCommandState =>
+		
 	// For validation. Not editing an existing MarkingWorkflow so return None
 	def currentMarkingWorkflow = None
 
 	def contextSpecificValidation(errors:Errors){}
+}
 
-	override def validate(errors: Errors) {
-		super.validate(errors)
+trait AddMarkingWorkflowCommandPermissions extends RequiresPermissionsChecking with PermissionsCheckingMethods {
+	self: MarkingWorkflowCommandState =>
+		
+	override def permissionsCheck(p: PermissionsChecking) {
+		p.PermissionCheck(Permissions.MarkingWorkflow.Create, mandatory(department))
 	}
-	
+}
 
-	def describe(d: Description) = d.department(department)
+trait AddMarkingWorkflowCommandDescription extends Describable[MarkingWorkflow] {
+	self: MarkingWorkflowCommandState =>
+		
+	def describe(d: Description) {
+		d.department(department)
+	}
 }
