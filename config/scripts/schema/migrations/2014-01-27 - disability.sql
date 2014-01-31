ae467efc2a2fd9dd9d8353d7832f1e2c9eec2545
Nick Kaijaks <Nick.Kaijaks@warwick.ac.uk>
2014-01-31 16:41:55 +0000

    TAB-128 Tabula-friendly definitions

--- config/scripts/schema/migrations/2014-01-27 - disability.sql
+++ config/scripts/schema/migrations/2014-01-27 - disability.sql
@@ -5,7 +5,41 @@ alter table member add disability nvarchar2(20);
 create table disability
 (	code nvarchar2(20) not null enable,
 	shortName nvarchar2(20),
-	definition nvarchar2(300),
+	sitsDefinition nvarchar2(300),
+	tabulaDefinition nvarchar(100),
 	lastUpdatedDate timestamp(6),
   constraint disability_pk primary key (code)
 );
+
+/* set our own consistent text for disabilities on which we want to report */
+update disability set tabulaDefinition = 'Learning difficulty' where code = '01';
+update disability set tabulaDefinition = 'Blind/vision impaired' where code = '02';
+update disability set tabulaDefinition = 'Deaf/hearing impaired' where code = '03';
+update disability set tabulaDefinition = 'Mobility issues' where code = '04';
+update disability set tabulaDefinition = 'Personal care support needs' where code = '05';
+update disability set tabulaDefinition = 'Mental health condition' where code = '06';
+update disability set tabulaDefinition = 'Unseen disability' where code = '07';
+update disability set tabulaDefinition = 'Multiple disabilities' where code = '08';
+update disability set tabulaDefinition = 'Unclassified disability' where code = '09';
+update disability set tabulaDefinition = 'Autistic Spectrum Disorder' where code = '10';
+update disability set tabulaDefinition = 'Learning difficulty' where code = '11';
+update disability set tabulaDefinition = 'Unclassified disability' where code = '96';
+update disability set tabulaDefinition = 'Autistic Spectrum Disorder' where code = 'B';
+update disability set tabulaDefinition = 'Blind/vision impaired' where code = 'C';
+update disability set tabulaDefinition = 'Deaf/hearing impaired' where code = 'D';
+update disability set tabulaDefinition = 'Long standing condition' where code = 'E';
+update disability set tabulaDefinition = 'Mental health condition' where code = 'F';
+update disability set tabulaDefinition = 'Learning difficulty' where code = 'G';
+update disability set tabulaDefinition = 'Mobility issues' where code = 'H';
+update disability set tabulaDefinition = 'Unclassified disability' where code = 'I';
+update disability set tabulaDefinition = 'Multiple disabilities' where code = 'J';
+update disability set tabulaDefinition = 'Autistic Spectrum Disorder' where code = 'T';
+
+/* empty strings (deliberately not null) for things we consider to be NOT disabilities */
+update disability set tabulaDefinition = '' where code = '00';
+update disability set tabulaDefinition = '' where code = '97';
+update disability set tabulaDefinition = '' where code = '98';
+update disability set tabulaDefinition = '' where code = '99';
+update disability set tabulaDefinition = '' where code = 'A';
+update disability set tabulaDefinition = '' where code = 'N';
+update disability set tabulaDefinition = '' where code = 'W';
--- modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/Disability.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/Disability.scala
@@ -1,8 +1,7 @@
 package uk.ac.warwick.tabula.data.model
 
 import org.joda.time.DateTime
-import javax.persistence.Entity
-import javax.persistence.Id
+import javax.persistence.{Column, Entity, Id}
 
 @Entity
 class Disability {
@@ -10,14 +9,20 @@ class Disability {
 	def this(code: String = null, definition: String = null) {
 		this()
 		this.code = code
-		this.definition = definition
+		this.sitsDefinition = definition
 	}
 
 	@Id var code: String = _
 	var shortName: String = _
-	var definition: String = _
-
+	var sitsDefinition: String = _
+	var tabulaDefinition: String = _
 	var lastUpdatedDate = DateTime.now
+
+	/**
+	 * use our override out of preference, where available.
+	 * It's manually maintained in the database.
+	 */
+	def definition = Option(tabulaDefinition).getOrElse(sitsDefinition)
 
 	override def toString = definition
 }
