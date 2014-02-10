52cfe409615d92046b53d0a2a36cbd82d3fd99aa
Nick Kaijaks <Nick.Kaijaks@warwick.ac.uk>
2014-02-10 11:16:35 +0000

    TAB-882 Caked ERC

--- modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/web/controllers/ExtensionRequestController.scala
+++ modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/web/controllers/ExtensionRequestController.scala
@@ -3,35 +3,36 @@ package uk.ac.warwick.tabula.coursework.web.controllers
 import scala.collection.JavaConversions._
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{PathVariable, ModelAttribute, RequestMapping}
-import uk.ac.warwick.tabula.data.model.{Module, Assignment}
+import uk.ac.warwick.tabula.data.model.{MeetingRecordApproval, Module, Assignment}
 import uk.ac.warwick.tabula.CurrentUser
 import uk.ac.warwick.tabula.PermissionDeniedException
 import uk.ac.warwick.tabula.web.Mav
 import org.springframework.validation.Errors
-import uk.ac.warwick.tabula.coursework.commands.assignments.extensions.ExtensionRequestCommand
+import uk.ac.warwick.tabula.coursework.commands.assignments.extensions.{ExtensionRequestState, ExtensionRequestCommand}
 import uk.ac.warwick.tabula.data.model.forms.Extension
 import javax.validation.Valid
 import uk.ac.warwick.tabula.permissions._
 import uk.ac.warwick.tabula.coursework.web.Routes
+import uk.ac.warwick.tabula.commands.{Appliable, SelfValidating}
 
 @Controller
 @RequestMapping(value=Array("/module/{module}/{assignment}/extension"))
 class ExtensionRequestController extends CourseworkController{
 
 	@ModelAttribute
-	def extensionRequestCommand(@PathVariable("module") module: Module, @PathVariable("assignment") assignment:Assignment, user:CurrentUser) =
-		new ExtensionRequestCommand(module, assignment, user)
-	
-	validatesSelf[ExtensionRequestCommand]
+	def cmd(@PathVariable("module") module: Module, @PathVariable("assignment") assignment:Assignment, user:CurrentUser) =
+		ExtensionRequestCommand(module, assignment, user)
+
+	validatesSelf[SelfValidating]
 
 	// Add the common breadcrumbs to the model.
 	def crumbed(mav:Mav, module:Module)
 		= mav.crumbs(Breadcrumbs.Department(module.department), Breadcrumbs.Module(module))
 
 	@RequestMapping(method=Array(HEAD,GET))
-	def showForm(cmd:ExtensionRequestCommand):Mav = {
+	def showForm(@Valid @ModelAttribute cmd: Appliable[Extension] with ExtensionRequestState): Mav = {
 		val (assignment, module) = (cmd.assignment, cmd.module)
-		
+
 		if (!module.department.canRequestExtension) {
 			logger.info("Rejecting access to extension request screen as department does not allow extension requests")
 			throw new PermissionDeniedException(user, Permissions.Extension.MakeRequest, assignment)
@@ -58,9 +59,9 @@ class ExtensionRequestController extends CourseworkController{
 	}
 
 	@RequestMapping(method=Array(POST))
-	def persistExtensionRequest(@Valid cmd:ExtensionRequestCommand, errors: Errors):Mav = {
+	def persistExtensionRequest(@ModelAttribute cmd: Appliable[Extension] with ExtensionRequestState, errors: Errors): Mav = {
 		val (assignment, module) = (cmd.assignment, cmd.module)
-		
+
 		if(errors.hasErrors){
 			println(errors)
 			showForm(cmd)
