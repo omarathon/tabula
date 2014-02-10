84ae12e3bc6455bb81d1aec63e6a967120f68f81
Zoe <zoe.james@warwick.ac.uk>
2014-02-10 15:39:25 +0000

    TAB-1420 draw out common trait from FiltersStudents and FiltersRelationships

---
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/commands/FilterStudentsOrRelationships.scala
@@ -0,0 +1,80 @@
+package uk.ac.warwick.tabula.commands
+
+import uk.ac.warwick.tabula.data.{ScalaRestriction, ScalaOrder}
+import uk.ac.warwick.tabula.services.ProfileServiceComponent
+import scala.collection.JavaConverters._
+import uk.ac.warwick.tabula.helpers.StringUtils._
+import uk.ac.warwick.tabula.system.permissions.PermissionsCheckingMethods
+import uk.ac.warwick.tabula.data.ScalaRestriction._
+
+object FilterStudentsOrRelationships {
+
+	val MaxStudentsPerPage = 100
+	val DefaultStudentsPerPage = 50
+
+}
+
+trait FilterStudentsOrRelationships extends FiltersStudentsBase with ProfileServiceComponent with PermissionsCheckingMethods {
+	val MaxStudentsPerPage = 100
+	val DefaultStudentsPerPage = 50
+
+	def getAliasPaths(sitsTable: String): Seq[(String, String)]
+
+	protected def buildOrders(): Seq[ScalaOrder] =
+		(sortOrder.asScala ++ defaultOrder).map { underlying =>
+			underlying.getPropertyName match {
+				case r"""([^\.]+)${aliasPath}\..*""" => ScalaOrder(underlying, getAliasPaths(aliasPath) : _*)
+				case _ => ScalaOrder(underlying)
+			}
+		}
+
+
+	def courseRestriction: Option[ScalaRestriction] = startsWithIfNotEmpty(
+		"course.code", courseTypes.asScala.map { _.courseCodeChar.toString },
+		getAliasPaths("course") : _*
+	)
+
+	def attendanceRestriction: Option[ScalaRestriction] = inIfNotEmpty(
+		"studentCourseYearDetails.modeOfAttendance", modesOfAttendance.asScala,
+		getAliasPaths("studentCourseYearDetails") : _*
+	)
+
+	def courseTypeRestriction: Option[ScalaRestriction] = startsWithIfNotEmpty(
+		"course.code", courseTypes.asScala.map { _.courseCodeChar.toString },
+		getAliasPaths("course") : _*
+	)
+
+	def routeRestriction: Option[ScalaRestriction]
+
+	def yearOfStudyRestriction: Option[ScalaRestriction] = inIfNotEmpty(
+		"studentCourseYearDetails.yearOfStudy", yearsOfStudy.asScala,
+		getAliasPaths("studentCourseYearDetails") : _*
+	)
+
+	def registeredModulesRestriction: Option[ScalaRestriction] = inIfNotEmpty(
+		"moduleRegistration.module", modules.asScala,
+		getAliasPaths("moduleRegistration") : _*
+	)
+
+	def sprStatusRestriction: Option[ScalaRestriction]
+
+	def tier4Restriction: Option[ScalaRestriction] = atLeastOneIsTrue(
+		"studentCourseYearDetails.casUsed", "studentCourseYearDetails.tier4Visa", otherCriteria.contains("Tier 4 only"),
+		getAliasPaths("studentCourseYearDetails") : _*
+	)
+
+	protected def buildRestrictions(): Seq[ScalaRestriction] = {
+		Seq(
+				courseTypeRestriction,
+				routeRestriction,
+				attendanceRestriction,
+				yearOfStudyRestriction,
+				sprStatusRestriction,
+				registeredModulesRestriction,
+				tier4Restriction
+		).flatten
+	}
+
+	lazy val allYearsOfStudy: Seq[Int] = 1 to 8
+	lazy val allOtherCriteria: Seq[String] = Seq("Tier 4 only") // only one 'other' criteria so far - tier4
+}
