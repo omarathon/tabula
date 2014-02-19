46aa745545d13be70edc5dc1ec23c5a448199159
Nick Howes <n.howes@warwick.ac.uk>
2014-02-19 11:21:08 +0000

    TAB-1848 Remove scala-react

--- modules/common/src/test/scala/uk/ac/warwick/tabula/services/MaintenanceModeServiceTest.scala
+++ modules/common/src/test/scala/uk/ac/warwick/tabula/services/MaintenanceModeServiceTest.scala
@@ -1,44 +1,44 @@
 package uk.ac.warwick.tabula.services
 
 import uk.ac.warwick.tabula.TestBase
-import scala.react.Observing
+import uk.ac.warwick.tabula.Reactor
 import org.joda.time.DateTime
 
-class MaintenanceModeServiceTest extends TestBase with Observing {
+class MaintenanceModeServiceTest extends TestBase {
 	
 	val service = new MaintenanceModeServiceImpl
 	
 	@Test def enable {
 		service._enabled = false
-		
+
 		var called = false
-		observe(service.changingState) { enabled =>
+		service.changingState.observe { enabled =>
 			called should be (false)
 			enabled should be (true)
 			called = true
-			true
 		}
-		
+
 		val message = new MaintenanceModeMessage
 		message.enabled = true
 		message.until = DateTime.now.getMillis
 		message.message = "Yo momma"
 			
 		service.update(message)
-		
+
 		// so good I called it twice
 		service.update(message)
+
+		called should be (true)
 	}
 	
 	@Test def disable {
 		service._enabled = true
 		
 		var called = false
-		observe(service.changingState) { enabled =>
+		service.changingState.observe { enabled =>
 			called should be (false)
 			enabled should be (false)
 			called = true
-			true
 		}
 		
 		val message = new MaintenanceModeMessage
