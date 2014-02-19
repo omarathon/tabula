1f9771d02b2268b08eeade3f9ec7a92b3bdc1353
Mat Mannion <m.mannion@warwick.ac.uk>
2014-02-19 09:09:09 +0000

    Don't give Member-less staff from SSO the ability to see disability records

--- modules/common/src/main/scala/uk/ac/warwick/tabula/roles/StaffRole.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/roles/StaffRole.scala
@@ -6,10 +6,29 @@ import uk.ac.warwick.tabula.permissions.PermissionsSelector
 import uk.ac.warwick.tabula.data.model.StudentRelationshipType
 
 case class StaffRole(department: model.Department) extends BuiltInRole(StaffRoleDefinition, department)
+case class SSOStaffRole(department: model.Department) extends BuiltInRole(SSOStaffRoleDefinition, department)
 
 case object StaffRoleDefinition extends UnassignableBuiltInRoleDefinition {
 
 	override def description = "Staff Member"
+
+	GeneratesSubRole(SSOStaffRoleDefinition)
+
+	GrantsGlobalPermission(
+		// TAB-128 University Disability Services confirm disability status should be visible to staff generally
+		Profiles.Read.Disability
+	)
+}
+
+/**
+ * In the case of users not having a member record, they may get SSOStaffRoleDefinition because
+ * they are staff=true in SSO. Since staff=true is given to PGRs who would normally be a student,
+ * we need to be more careful about exposing sensitive information in this role. If you're not sure,
+ * put it in StaffRoleDefinition above.
+ */
+case object SSOStaffRoleDefinition extends UnassignableBuiltInRoleDefinition {
+
+	override def description = "Staff Member (SSO)"
 
 	GrantsScopelessPermission(
 		UserPicker,
@@ -23,10 +42,7 @@ case object StaffRoleDefinition extends UnassignableBuiltInRoleDefinition {
 		// TAB-1619
 		Profiles.Read.Usercode,
 		Profiles.Read.StudentCourseDetails.Core,
-		Profiles.StudentRelationship.Read(PermissionsSelector.Any[StudentRelationshipType]),
-
-		// TAB-128 University Disability Services confirm disability status should be visible to staff generally
-		Profiles.Read.Disability
+		Profiles.StudentRelationship.Read(PermissionsSelector.Any[StudentRelationshipType])
 	)
 
 	GrantsScopedPermission(
@@ -36,4 +52,5 @@ case object StaffRoleDefinition extends UnassignableBuiltInRoleDefinition {
 		Profiles.Read.Timetable,
 		Profiles.Read.SmallGroups
 	)
+
 }
