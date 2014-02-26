dee8e832cee32ec2cf3744d2081e4234e39c879b
Nick Howes <n.howes@warwick.ac.uk>
2014-02-26 16:57:49 +0000

    Reactor comments, test

--- modules/common/src/main/scala/uk/ac/warwick/tabula/Reactor.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/Reactor.scala
@@ -7,9 +7,34 @@ package uk.ac.warwick.tabula
 object Reactor {
 	def EventSource[A]() = new EventSource[A]
 
+	/* An observable event source.
+
+		object Service {
+			val stateChanged = new EventSource[Boolean]
+
+			def activate() {
+				stateChanged.emit(true)
+			}
+		}
+
+		Service.stateChanged.observe {
+			active => println("Service active: " + active)
+		}
+
+		Service.activate()
+	 */
 	class EventSource[A]() {
 		type Observer = (A) => Unit
 		var observers = List[Observer]()
+
+		/**
+		 * Observe events from this event source.
+		 * WARNING - exceptions thrown in the observer
+		 * will not be caught here, so it will caught
+		 * the event source to fail. You might want to
+		 * catch any exceptions that you wouldn't want
+		 * to effect the operation of the event source.
+		 */
 		def observe(ob: Observer) {
 			observers = (ob :: observers)
 		}
