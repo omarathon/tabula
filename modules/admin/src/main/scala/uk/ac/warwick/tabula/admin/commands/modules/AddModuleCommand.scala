956f5b8d5629e93871f3a986744e6c7dc07fa5b9
Mat Mannion <m.mannion@warwick.ac.uk>
2014-02-27 16:19:21 +0000

    Test for AddModuleCommand

--- modules/admin/src/main/scala/uk/ac/warwick/tabula/admin/commands/modules/AddModuleCommand.scala
+++ modules/admin/src/main/scala/uk/ac/warwick/tabula/admin/commands/modules/AddModuleCommand.scala
@@ -1,57 +1,84 @@
 package uk.ac.warwick.tabula.admin.commands.modules
-import uk.ac.warwick.tabula._
+
 import uk.ac.warwick.tabula.data.Transactions._
-import uk.ac.warwick.tabula.commands.{Description, SelfValidating}
-import uk.ac.warwick.tabula.data.Daoisms
+import uk.ac.warwick.tabula.commands._
 import uk.ac.warwick.tabula.data.model._
 import collection.JavaConversions._
-import reflect.BeanProperty
 import org.springframework.validation.{ ValidationUtils, Errors }
 import uk.ac.warwick.tabula.permissions._
-import uk.ac.warwick.tabula.commands.Command
+import uk.ac.warwick.tabula.system.permissions.{PermissionsChecking, PermissionsCheckingMethods, RequiresPermissionsChecking}
+import uk.ac.warwick.tabula.services.{AutowiringModuleAndDepartmentServiceComponent, ModuleAndDepartmentServiceComponent}
 
+object AddModuleCommand {
+	def apply(department: Department) =
+		new AddModuleCommandInternal(department)
+				with ComposableCommand[Module]
+				with AddModuleCommandPermissions
+				with AddModuleCommandValidation
+				with AddModuleCommandDescription
+				with AutowiringModuleAndDepartmentServiceComponent
+}
 
-class AddModuleCommand(@BeanProperty val department: Department) extends Command[Module] with Daoisms with SelfValidating {
-	
-	PermissionCheck(Permissions.Module.Create, department)
-
-	var code: String = _
-	var name: String = _
-
-	def sanitisedCode = Option(code).map(_.toLowerCase).orNull
+class AddModuleCommandInternal(val department: Department) extends CommandInternal[Module] with AddModuleCommandState {
+	self: ModuleAndDepartmentServiceComponent =>
 
 	def applyInternal() = transactional() {
 		val module = new Module()
 		module.department = department
 		module.name = name
 		module.code = sanitisedCode
-		session.save(module)
+
+		moduleAndDepartmentService.saveOrUpdate(module)
 		module
 	}
 
+}
+
+trait AddModuleCommandValidation extends SelfValidating {
+	self: AddModuleCommandState with ModuleAndDepartmentServiceComponent =>
+
 	def validate(errors: Errors) {
-		//TODO proper department checks before we open this up to non-sysadmins
+		// TODO proper department checks before we open this up to non-sysadmins
 
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "code", "NotEmpty")
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "NotEmpty")
 
-		ValidationUtils.rejectIfEmpty(errors, "department", "NotEmpty")
-
 		// check for duplicate name or code
-		if (department != null) {
-			if (code != null && department.modules.exists { _.code == code.toLowerCase }) {
-				errors.rejectValue("code", "code.duplicate.module", Array(code.toUpperCase()), "")
+		if (!errors.hasErrors) {
+			if (moduleAndDepartmentService.getModuleByCode(sanitisedCode).isDefined) {
+				errors.rejectValue("code", "code.duplicate.module", Array(code.toUpperCase), "")
 			}
-			if (name != null && department.modules.exists { _.name equalsIgnoreCase name }) {
+			if (department.modules.exists { _.name equalsIgnoreCase name }) {
 				errors.rejectValue("name", "name.duplicate.module", Array(name), "")
 			}
 		}
-
 	}
+}
 
-	def describe(d: Description) = d
-		.department(department)
-		.property("moduleCode", sanitisedCode)
-		.property("moduleName", name)
+trait AddModuleCommandState {
+	def department: Department
 
+	var code: String = _
+	var name: String = _
+
+	def sanitisedCode = Option(code).map(_.toLowerCase.trim).orNull
+}
+
+trait AddModuleCommandPermissions extends RequiresPermissionsChecking with PermissionsCheckingMethods {
+	self: AddModuleCommandState =>
+
+	override def permissionsCheck(p: PermissionsChecking) {
+		p.PermissionCheck(Permissions.Module.Create, mandatory(department))
+	}
+}
+
+trait AddModuleCommandDescription extends Describable[Module] {
+	self: AddModuleCommandState =>
+
+	def describe(d: Description) =
+		d.department(department)
+		 .properties(
+				"moduleCode" -> sanitisedCode,
+				"moduleName" -> name
+			)
 }
