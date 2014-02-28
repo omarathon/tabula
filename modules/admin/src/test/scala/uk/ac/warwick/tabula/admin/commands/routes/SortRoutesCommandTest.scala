d08ea16405e4ed213ea8d980fed18ca3425941fa
Mat Mannion <m.mannion@warwick.ac.uk>
2014-02-28 09:34:53 +0000

    Cake and tests for SortRoutesCommand

---
+++ modules/admin/src/test/scala/uk/ac/warwick/tabula/admin/commands/routes/SortRoutesCommandTest.scala
@@ -0,0 +1,209 @@
+package uk.ac.warwick.tabula.admin.commands.routes
+
+import uk.ac.warwick.tabula.JavaImports._
+import scala.collection.JavaConverters._
+import uk.ac.warwick.tabula.{Fixtures, TestBase, Mockito}
+import uk.ac.warwick.tabula.services.{ModuleAndDepartmentService, ModuleAndDepartmentServiceComponent}
+import uk.ac.warwick.tabula.commands.{Describable, SelfValidating, Appliable, DescriptionImpl, GroupsObjects}
+import uk.ac.warwick.tabula.data.model.{Route, Department}
+import org.springframework.validation.BindException
+import uk.ac.warwick.tabula.system.permissions.PermissionsChecking
+import uk.ac.warwick.tabula.permissions.Permissions
+
+class SortRoutesCommandTest extends TestBase with Mockito {
+
+	trait CommandTestSupport extends SortRoutesCommandState with ModuleAndDepartmentServiceComponent {
+		val moduleAndDepartmentService = mock[ModuleAndDepartmentService]
+	}
+
+	trait SortRoutesWorld {
+		val department = Fixtures.department("in", "IT Services")
+		val ugDepartment = Fixtures.department("in-ug", "ITS Undergraduates")
+		val pgDepartment = Fixtures.department("in-pg", "ITS Postgraduates")
+
+		department.children.add(ugDepartment)
+		department.children.add(pgDepartment)
+
+		ugDepartment.parent = department
+		pgDepartment.parent = department
+
+		val route1 = Fixtures.route("in101")
+		val route2 = Fixtures.route("in102")
+		val route3 = Fixtures.route("in103")
+		val route4 = Fixtures.route("in104")
+		val route5 = Fixtures.route("in105")
+		val route6 = Fixtures.route("in106")
+		val route7 = Fixtures.route("in107")
+
+		department.routes.add(route1)
+		department.routes.add(route2)
+
+		// Mix up the order here to make sure sort() works
+		ugDepartment.routes.add(route4)
+		ugDepartment.routes.add(route5)
+		ugDepartment.routes.add(route3)
+
+		pgDepartment.routes.add(route6)
+		pgDepartment.routes.add(route7)
+	}
+
+	trait Fixture extends SortRoutesWorld {
+		val command = new SortRoutesCommandInternal(department) with CommandTestSupport
+	}
+
+	@Test def init { new Fixture {
+		command.department should be (department)
+		command.departments.toSet should be (Set(department, ugDepartment, pgDepartment))
+	}}
+
+	@Test def populateAndSort { new Fixture {
+		command.mapping.asScala should be ('empty)
+
+		command.populate()
+		command.mapping.asScala.mapValues(_.asScala.toSeq) should be (Map(
+			department -> Seq(route1, route2),
+			ugDepartment -> Seq(route4, route5, route3), // wrong order until .sort() is called
+			pgDepartment -> Seq(route6, route7)
+		))
+
+		command.sort()
+		command.mapping.asScala.mapValues(_.asScala.toSeq) should be (Map(
+			department -> Seq(route1, route2),
+			ugDepartment -> Seq(route3, route4, route5),
+			pgDepartment -> Seq(route6, route7)
+		))
+	}}
+
+	@Test def mappingByCode { new Fixture {
+		command.mappingByCode should be ('empty)
+
+		command.populate()
+		command.sort()
+
+		// Mapping by code is also sorted
+		command.mappingByCode.mapValues(_.asScala.toSeq) should be (Map(
+			"in" -> Seq(route1, route2),
+			"in-ug" -> Seq(route3, route4, route5),
+			"in-pg" -> Seq(route6, route7)
+		))
+	}}
+
+	@Test def apply { new Fixture {
+		command.populate()
+		command.sort()
+
+		// Move route1 into in-ug and route2 into in-pg
+		command.mapping.get(ugDepartment).add(route1)
+		command.mapping.get(pgDepartment).add(route2)
+		command.mapping.get(department).clear()
+
+		command.applyInternal()
+
+		route1.department should be (ugDepartment)
+		route2.department should be (pgDepartment)
+
+		department.routes.asScala should be ('empty)
+		ugDepartment.routes.asScala.toSet should be (Set(route1, route3, route4, route5))
+		pgDepartment.routes.asScala.toSet should be (Set(route2, route6, route7))
+
+		there was one (command.moduleAndDepartmentService).save(department)
+		there was one (command.moduleAndDepartmentService).save(ugDepartment)
+		there was one (command.moduleAndDepartmentService).save(pgDepartment)
+	}}
+
+	trait ValidationFixture extends SortRoutesWorld {
+		val d = department
+		val command = new SortRoutesCommandValidation with CommandTestSupport with SortRoutesCommandGrouping {
+			val department = d
+		}
+
+		command.populate()
+		command.sort()
+	}
+
+	@Test def validateNoErrors { new ValidationFixture {
+		val errors = new BindException(command, "command")
+		command.validate(errors)
+
+		errors.hasErrors should be (false)
+	}}
+
+	@Test def rejectUnrelatedDepartments { new ValidationFixture {
+		command.mapping.put(Fixtures.department("other"), JArrayList())
+
+		val errors = new BindException(command, "command")
+		command.validate(errors)
+
+		errors.getErrorCount should be (1)
+		errors.getGlobalError.getCodes should contain ("sortRoutes.departments.invalid")
+	}}
+
+	@Test def rejectUnrelatedRoutes { new ValidationFixture {
+		command.mapping.get(department).add(Fixtures.route("cs205"))
+
+		val errors = new BindException(command, "command")
+		command.validate(errors)
+
+		errors.getErrorCount should be (1)
+		errors.getGlobalError.getCodes should contain ("sortRoutes.routes.invalid")
+	}}
+
+	@Test def rejectOrphanRoutes { new ValidationFixture {
+		command.mapping.get(department).remove(route1)
+
+		val errors = new BindException(command, "command")
+		command.validate(errors)
+
+		errors.getErrorCount should be (1)
+		errors.getGlobalError.getCodes should contain ("sortRoutes.routes.invalid")
+	}}
+
+	@Test
+	def permissionsRequireCreateRouteOnDepartment {
+		val dept = Fixtures.department("in")
+
+		val command = new SortRoutesCommandPermissions with CommandTestSupport {
+			val department = dept
+			def sort() = ???
+			def populate() = ???
+		}
+
+		val checking = mock[PermissionsChecking]
+		command.permissionsCheck(checking)
+		there was one(checking).PermissionCheck(Permissions.Department.ArrangeRoutes, dept)
+	}
+
+	@Test
+	def describe {
+		val dept = Fixtures.department("in")
+
+		val command = new SortRoutesCommandDescription with CommandTestSupport {
+			val eventName: String = "test"
+			val department = dept
+			def sort() = ???
+			def populate() = ???
+		}
+
+		val d = new DescriptionImpl
+		command.describe(d)
+
+		d.allProperties should be (Map(
+			"department" -> "in"
+		))
+	}
+
+	@Test
+	def glueEverythingTogether() {
+		val department = Fixtures.department("in", "IT Services")
+		val command = SortRoutesCommand(department)
+
+		command should be (anInstanceOf[Appliable[Unit]])
+		command should be (anInstanceOf[GroupsObjects[Route, Department]])
+		command should be (anInstanceOf[SortRoutesCommandState])
+		command should be (anInstanceOf[SortRoutesCommandPermissions])
+		command should be (anInstanceOf[SelfValidating])
+		command should be (anInstanceOf[SortRoutesCommandValidation])
+		command should be (anInstanceOf[Describable[Unit]])
+	}
+
+}
