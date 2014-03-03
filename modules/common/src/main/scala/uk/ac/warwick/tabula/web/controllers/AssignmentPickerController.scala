b6f3198fdcfa26e53ce97ff0c5617a811a55a0af
Sarah O'Toole <s.o-toole@warwick.ac.uk>
2014-03-03 14:05:59 +0000

    TAB-743 Assignment picker

---
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/web/controllers/AssignmentPickerController.scala
@@ -0,0 +1,55 @@
+package uk.ac.warwick.tabula.web.controllers
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.{RequestMapping, ModelAttribute}
+import uk.ac.warwick.tabula.commands.{Appliable, Command, ReadOnly, Unaudited, CommandInternal}
+import uk.ac.warwick.tabula.data.model.Assignment
+import uk.ac.warwick.tabula.services.{AutowiringAssignmentServiceComponent, AssignmentServiceComponent}
+import uk.ac.warwick.tabula.system.permissions.Public
+import uk.ac.warwick.tabula.web.views.JSONView
+
+
+@Controller
+@RequestMapping(value = Array("/api/assignmentpicker/query"))
+class AssignmentPickerController extends BaseController {
+
+	@ModelAttribute("command")
+	def command = AssignmentPickerCommand()
+
+	@RequestMapping
+	def query(@ModelAttribute("command")cmd: Appliable[Seq[Assignment]]) = {
+		val assignments = cmd.apply()
+		Mav(
+			new JSONView(
+				assignments.map(assignment => Map(
+					"id" -> assignment.id,
+					"name" -> assignment.name,
+					"module" -> assignment.module.code,
+					"department" -> assignment.module.department.name)
+				)
+			)
+		)
+	}
+
+}
+
+class AssignmentPickerCommand extends CommandInternal[Seq[Assignment]] {
+
+	self: AssignmentServiceComponent =>
+
+	var query: String = _
+
+	def applyInternal() = {
+		if (query.isEmpty) {
+			Seq()
+		} else {
+			assignmentService.findAssignmentsByNameOrModule(query)
+		}
+	}
+
+}
+
+object AssignmentPickerCommand {
+	def apply() = new AssignmentPickerCommand with Command[Seq[Assignment]] with AutowiringAssignmentServiceComponent
+	with ReadOnly with Unaudited with Public
+}
