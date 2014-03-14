a3f80380a3e6c4cdef85f73157fe5799d1ea401b
Mat Mannion <m.mannion@warwick.ac.uk>
2014-03-14 16:01:12 +0000

    TAB-2067

--- modules/common/src/main/scala/uk/ac/warwick/tabula/data/MeetingRecordDao.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/MeetingRecordDao.scala
@@ -1,7 +1,7 @@
 package uk.ac.warwick.tabula.data
 
 import org.springframework.stereotype.Repository
-import uk.ac.warwick.tabula.data.model.{AbstractMeetingRecord, ScheduledMeetingRecord, MeetingRecordApproval, MeetingRecord, StudentRelationship, Member}
+import uk.ac.warwick.tabula.data.model._
 import org.hibernate.criterion.{Restrictions,Order}
 import scala.collection.JavaConversions._
 import uk.ac.warwick.spring.Wire
@@ -45,14 +45,18 @@ class MeetingRecordDaoImpl extends MeetingRecordDao with Daoisms {
 
 	private def addMeetingRecordListRestrictions[A](criteria: ScalaCriteria[A], rel: Set[StudentRelationship], currentUser: Member) = {
 		criteria.add(Restrictions.in("relationship", rel))
-			// and only pick records where deleted = 0 or the current user id is the creator id
-			// - so that no-one can see records created and deleted by someone else
-			.add(Restrictions.disjunction()
+
+		// and only pick records where deleted = 0 or the current user id is the creator id
+		// - so that no-one can see records created and deleted by someone else
+		currentUser match {
+			case _: RuntimeMember => criteria.add(is("deleted", false))
+			case currentUser => criteria.add(Restrictions.disjunction()
 				.add(is("deleted", false))
 				.add(is("creator", currentUser))
 			)
-			.addOrder(Order.desc("meetingDate"))
-			.addOrder(Order.desc("lastUpdatedDate"))
+		}
+
+		criteria.addOrder(Order.desc("meetingDate")).addOrder(Order.desc("lastUpdatedDate"))
 	}
 
 	def list(rel: StudentRelationship): Seq[MeetingRecord] = {
