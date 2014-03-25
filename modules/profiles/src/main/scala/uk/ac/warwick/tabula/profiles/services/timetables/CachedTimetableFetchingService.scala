2fd3f7562712ba10b8f886a2a37699f666a75c3e
Mat Mannion <mat.mannion@gmail.com>
2014-03-25 14:27:45 +0000

    Ensure cache keys are serializable

--- modules/profiles/src/main/scala/uk/ac/warwick/tabula/profiles/services/timetables/CachedTimetableFetchingService.scala
+++ modules/profiles/src/main/scala/uk/ac/warwick/tabula/profiles/services/timetables/CachedTimetableFetchingService.scala
@@ -2,12 +2,11 @@ package uk.ac.warwick.tabula.profiles.services.timetables
 
 import uk.ac.warwick.util.cache.{CacheEntryFactory, Caches}
 import uk.ac.warwick.util.cache.Caches.CacheStrategy
-import java.util
 import scala.collection.JavaConverters._
-import uk.ac.warwick.tabula.JavaImports.JMap
+import uk.ac.warwick.tabula.JavaImports._
 import uk.ac.warwick.tabula.timetables.TimetableEvent
 import uk.ac.warwick.tabula.services.permissions.AutowiringCacheStrategyComponent
-
+import uk.ac.warwick.tabula.profiles.services.timetables.TimetableCacheKey._
 
 /**
  * A wrapper around a TimetableFetchingService that stores the resulting TimetableEvents in an ehCache disk-backed
@@ -16,30 +15,20 @@ import uk.ac.warwick.tabula.services.permissions.AutowiringCacheStrategyComponen
  */
 class CachedTimetableFetchingService(delegate:TimetableFetchingService) extends TimetableFetchingService with AutowiringCacheStrategyComponent {
 
-	val CacheExpiryTime = 1000*60*60*6 // 6 hours in ms
-
-	sealed trait TimetableCacheKey extends Serializable{
-		val id:String
-	}
-	case class StudentKey(val id:String) extends TimetableCacheKey
-	case class StaffKey(val id:String) extends TimetableCacheKey
-	case class CourseKey(val id:String) extends TimetableCacheKey
-	case class RoomKey(val id:String) extends TimetableCacheKey
-	case class ModuleKey(val id:String) extends TimetableCacheKey
+	val CacheExpiryTime = 1000 * 60 * 60 * 6 // 6 hours in ms
 
 	/**
 	 * Yukkiness Ahead.
 	 *
 	 * Caches requre their payload to be serializable, but Scala's Seq trait isn't marked as serializable. Most of
-	 * the Seq implementations (Vector and LinkedList in particular,which are the defaults returned by Seq() and List() )
+	 * the Seq implementations (Vector and LinkedList in particular, which are the defaults returned by Seq() and List() )
 	 * _are_ serializable, so it should be safe to match the Seqs to "Seq with Serializable" and assume it will never fail.
-	 *
 	 */
 	type EventList = Seq[TimetableEvent] with java.io.Serializable
 
-	val cacheEntryFactory = new CacheEntryFactory[TimetableCacheKey,EventList] {
+	val cacheEntryFactory = new CacheEntryFactory[TimetableCacheKey, EventList] {
 
-	 private def toEventList(events: Seq[TimetableEvent]):EventList = {
+	 private def toEventList(events: Seq[TimetableEvent]): EventList = {
 			events match {
 				// can't use "case v: EventList" because the type inference engine in 2.10 can't cope.
 				case v: Seq[TimetableEvent] with java.io.Serializable => v
@@ -48,19 +37,20 @@ class CachedTimetableFetchingService(delegate:TimetableFetchingService) extends 
 		}
 
 		def create(key:TimetableCacheKey):EventList = toEventList(key match {
-			case StudentKey(id)=>delegate.getTimetableForStudent(id)
-			case StaffKey(id)=>delegate.getTimetableForStaff(id)
-			case CourseKey(id)=>delegate.getTimetableForCourse(id)
-			case RoomKey(id)=>delegate.getTimetableForRoom(id)
-			case ModuleKey(id)=>delegate.getTimetableForModule(id)
+			case StudentKey(id) => delegate.getTimetableForStudent(id)
+			case StaffKey(id)   => delegate.getTimetableForStaff(id)
+			case CourseKey(id)  => delegate.getTimetableForCourse(id)
+			case RoomKey(id)    => delegate.getTimetableForRoom(id)
+			case ModuleKey(id)  => delegate.getTimetableForModule(id)
 		})
 
-		def create(keys: util.List[TimetableCacheKey]): util.Map[TimetableCacheKey, EventList] = {
+		def create(keys: JList[TimetableCacheKey]): JMap[TimetableCacheKey, EventList] = {
 			JMap(keys.asScala.map(id => (id, create(id))): _*)
 		}
 		def isSupportsMultiLookups: Boolean = true
 		def shouldBeCached(response: EventList): Boolean = true
 	}
+
 	// rather than using 5 little caches, use one big one with a composite key
 	lazy val timetableCache = {
 		Caches.newCache("SyllabusPlusTimetables", cacheEntryFactory, CacheExpiryTime, cacheStrategy)
@@ -73,3 +63,13 @@ class CachedTimetableFetchingService(delegate:TimetableFetchingService) extends 
 	def getTimetableForStaff(universityId: String): Seq[TimetableEvent] = timetableCache.get(StaffKey(universityId))
 }
 
+sealed trait TimetableCacheKey extends Serializable {
+	val id: String
+}
+object TimetableCacheKey {
+	case class StudentKey(val id: String) extends TimetableCacheKey
+	case class StaffKey(val id: String) extends TimetableCacheKey
+	case class CourseKey(val id: String) extends TimetableCacheKey
+	case class RoomKey(val id: String) extends TimetableCacheKey
+	case class ModuleKey(val id: String) extends TimetableCacheKey
+}
