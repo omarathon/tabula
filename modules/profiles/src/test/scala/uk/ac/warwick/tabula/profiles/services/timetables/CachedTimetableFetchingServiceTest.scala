426802dbc53ae6fe7e6fb09df5d81c42591beabc
Mat Mannion <mat.mannion@gmail.com>
2014-03-25 15:15:43 +0000

    TAB-2104 Fix tests

--- modules/profiles/src/test/scala/uk/ac/warwick/tabula/profiles/services/timetables/CachedTimetableFetchingServiceTest.scala
+++ modules/profiles/src/test/scala/uk/ac/warwick/tabula/profiles/services/timetables/CachedTimetableFetchingServiceTest.scala
@@ -3,9 +3,10 @@ package uk.ac.warwick.tabula.profiles.services.timetables
 import uk.ac.warwick.tabula.{AcademicYear, Mockito, TestBase}
 import uk.ac.warwick.tabula.data.model.groups.DayOfWeek
 import org.joda.time.LocalTime
-import uk.ac.warwick.util.cache.Caches
+import uk.ac.warwick.util.cache.{HashMapCacheStore, Caches}
 import uk.ac.warwick.tabula.timetables.{TimetableEventType, TimetableEvent}
 import net.spy.memcached.transcoders.SerializingTranscoder
+import org.junit.Before
 
 class CachedTimetableFetchingServiceTest  extends TestBase with Mockito{
 
@@ -16,12 +17,12 @@ class CachedTimetableFetchingServiceTest  extends TestBase with Mockito{
 		val delegate = mock[TimetableFetchingService]
 
 		delegate.getTimetableForStudent(studentId) returns studentEvents
-		
-		Caches.resetEhCacheCheck()
-		System.setProperty("warwick.ehcache.disk.store.dir", createTemporaryDirectory().getAbsolutePath)
-		System.setProperty("warwick.ehcache.config", "/ehcache-config.xml")
 
 		val cache = new CachedTimetableFetchingService(delegate)
+	}
+
+	@Before def clearCaches {
+		HashMapCacheStore.clearAll()
 	}
 
 	@Test
@@ -32,13 +33,13 @@ class CachedTimetableFetchingServiceTest  extends TestBase with Mockito{
 
 	@Test
 	def repeatedRequestsAreCached(){new Fixture {
-		cache.getTimetableForStudent(studentId)  should be(studentEvents)
-		cache.getTimetableForStudent(studentId)  should be(studentEvents)
-		there was one (delegate).getTimetableForStudent(studentId)
+		cache.getTimetableForStudent(studentId) should be(studentEvents)
+		cache.getTimetableForStudent(studentId) should be(studentEvents)
+		there was one(delegate).getTimetableForStudent(studentId)
 	}}
 
 	@Test
-	def keyTypesAreDiscriminated(){new Fixture {
+	def keyTypesAreDiscriminated() { new Fixture {
 		// deliberately use the student ID to look up some staff events. The cache key should be the ID + the type of
 		// request (staff, student, room, etc) so we should get different results back for student and staff
 
