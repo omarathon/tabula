1dfbf6dc48b8fda7e9a4af39078f2c05bb1dee09
Mat Mannion <m.mannion@warwick.ac.uk>
2014-04-15 12:06:44 +0100

    TAB-2156

--- modules/scheduling/src/main/scala/uk/ac/warwick/tabula/dev/web/controllers/FixturesController.scala
+++ modules/scheduling/src/main/scala/uk/ac/warwick/tabula/dev/web/controllers/FixturesController.scala
@@ -2,7 +2,6 @@ package uk.ac.warwick.tabula.dev.web.controllers
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.ModelAttribute
 import uk.ac.warwick.tabula.dev.web.commands.FixturesCommand
 
 @Controller
