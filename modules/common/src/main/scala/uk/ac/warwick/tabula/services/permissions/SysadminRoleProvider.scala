6e9bfb29f6d80239800ffa35fdd8dac15ac674e5
Nick Howes <n.howes@warwick.ac.uk>
2014-04-23 12:46:37 +0100

    TAB-2191

--- modules/common/src/main/scala/uk/ac/warwick/tabula/services/permissions/SysadminRoleProvider.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/services/permissions/SysadminRoleProvider.scala
@@ -3,25 +3,9 @@ package uk.ac.warwick.tabula.services.permissions
 import org.springframework.stereotype.Component
 
 import uk.ac.warwick.spring.Wire
-import uk.ac.warwick.tabula.CurrentUser
-import uk.ac.warwick.tabula.data.model._
-import uk.ac.warwick.tabula.roles.Role
 import uk.ac.warwick.tabula.roles.Sysadmin
-import uk.ac.warwick.tabula.services.UserLookupService
-import uk.ac.warwick.tabula.helpers.StringUtils._
 
 @Component
-class SysadminRoleProvider extends ScopelessRoleProvider {
-	
-	var userLookup = Wire.auto[UserLookupService]
-	var adminGroup: String = Wire.property("${permissions.admin.group}")
-	
-	def groupService = userLookup.getGroupService
-
-	def getRolesFor(user: CurrentUser): Stream[Role] =
-		if (user.realId.hasText && groupService.isUserInGroup(user.realId, adminGroup)) Stream(Sysadmin())
-		else Stream.empty
-		
-	def rolesProvided = Set(classOf[Sysadmin])
-	
+class SysadminRoleProvider extends WebgroupRoleProvider(Sysadmin()) {
+	override var webgroup: String = Wire.property("${permissions.admin.group}")
 }
