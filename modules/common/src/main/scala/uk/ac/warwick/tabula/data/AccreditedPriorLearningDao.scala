045e6216bf83ec51f023b0487152875a188eb285
Zoe <zoe.james@warwick.ac.uk>
2014-04-23 11:38:33 +0100

    TAB-2112 removed unnecessary code

--- modules/common/src/main/scala/uk/ac/warwick/tabula/data/AccreditedPriorLearningDao.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/AccreditedPriorLearningDao.scala
@@ -16,7 +16,6 @@ trait AutowiringAccreditedPriorLearningDaoComponent extends AccreditedPriorLearn
 trait AccreditedPriorLearningDao {
 	def saveOrUpdate(accreditedPriorLearning: AccreditedPriorLearning)
 	def getByNotionalKey(studentCourseDetails: StudentCourseDetails, award: Award, sequenceNumber: Integer): Option[AccreditedPriorLearning]
-	def getByUsercodesAndYear(userCodes: Seq[String], academicYear: AcademicYear) : Seq[AccreditedPriorLearning]
 }
 
 @Repository
@@ -24,28 +23,11 @@ class AccreditedPriorLearningDaoImpl extends AccreditedPriorLearningDao with Dao
 
 	def saveOrUpdate(accreditedPriorLearning: AccreditedPriorLearning) = session.saveOrUpdate(accreditedPriorLearning)
 
-	def getByNotionalKey(
-												studentCourseDetails: StudentCourseDetails,
-												award: Award,
-												sequenceNumber: Integer
-												) =
+	def getByNotionalKey(studentCourseDetails: StudentCourseDetails, award: Award, sequenceNumber: Integer) =
 		session.newCriteria[AccreditedPriorLearning]
 			.add(is("studentCourseDetails", studentCourseDetails))
 			.add(is("award", award))
 			.add(is("sequenceNumber", sequenceNumber))
 			.uniqueResult
 
-	def getByUsercodesAndYear(userCodes: Seq[String], academicYear: AcademicYear) : Seq[AccreditedPriorLearning] = {
-		val query = session.newQuery[AccreditedPriorLearning]("""
-				select distinct mr
-					from AccreditedPriorLearning mr
-					where academicYear = :academicYear
-					and studentCourseDetails.missingFromImportSince is null
-					and studentCourseDetails.student.userId in :usercodes
-																										 """)
-			.setString("academicYear", academicYear.getStoreValue.toString)
-
-		query.setParameterList("usercodes", userCodes)
-			.seq
-	}
 }
