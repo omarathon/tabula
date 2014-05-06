bd8763ce87154e96a717d0d4a4004b6edefd99b5
Mat Mannion <m.mannion@warwick.ac.uk>
2014-05-06 10:53:22 +0100

    Revert "TAB-2221"
    This reverts commit 34a3caface0a92b1288a80f7ede647e2613520ce.

--- modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/Notification.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/Notification.scala
@@ -2,7 +2,6 @@ package uk.ac.warwick.tabula.data.model
 
 import scala.collection.JavaConverters._
 import javax.persistence._
-import javax.persistence.CascadeType._
 import org.joda.time.DateTime
 
 import uk.ac.warwick.tabula.JavaImports._
@@ -101,8 +100,7 @@ abstract class Notification[A >: Null <: ToEntityReference, B]
 	@Type(`type`="uk.ac.warwick.tabula.data.model.SSOUserType")
 	final var agent: User = null // the actor in open social activity speak
 
-	@OneToMany(fetch=FetchType.LAZY, targetEntity=classOf[EntityReference[_]], cascade=Array(ALL))
-	@JoinColumn(name = "notification_id")
+	@OneToMany(mappedBy="notification", fetch=FetchType.LAZY, targetEntity=classOf[EntityReference[_]], cascade=Array(CascadeType.ALL))
 	var items: JList[EntityReference[A]] = JArrayList()
 
 	def entities = items.asScala.map { _.entity }.toSeq
@@ -116,7 +114,7 @@ abstract class Notification[A >: Null <: ToEntityReference, B]
 	// The priority, or if it is null then the default value of Info.
 	def priorityOrDefault = Option(priority).getOrElse(NotificationPriority.Info)
 
-	@OneToMany(mappedBy="notification", fetch=FetchType.LAZY, cascade=Array(ALL))
+	@OneToMany(mappedBy="notification", fetch=FetchType.LAZY, cascade=Array(CascadeType.ALL))
 	var recipientNotificationInfos: JList[RecipientNotificationInfo] = JArrayList()
 
 	// when performing operations on recipientNotificationInfos you should use this to fetch a users info.
