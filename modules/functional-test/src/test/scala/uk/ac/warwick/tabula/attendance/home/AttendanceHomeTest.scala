23945d02463a901b2be1aa438fe80ae4c7609950
Mat Mannion <m.mannion@warwick.ac.uk>
2014-05-16 09:37:31 +0100

    Rename attendance monitoring profile page

--- modules/functional-test/src/test/scala/uk/ac/warwick/tabula/attendance/home/AttendanceHomeTest.scala
+++ modules/functional-test/src/test/scala/uk/ac/warwick/tabula/attendance/home/AttendanceHomeTest.scala
@@ -19,7 +19,7 @@ class AttendanceHomeTest extends AttendanceFixture with GivenWhenThen{
 
 		Then("I am redirected to /attendance/{P.Student1.warwickId}/2013")
 		eventually(currentUrl should include(s"/attendance/profile/${P.Student1.warwickId}/2013"))
-		pageSource should include("My Attendance Monitoring")
+		pageSource should include("My Monitoring Points")
 	}
 
 	"A Member of staff" should "see the monitoring points home page" in {
