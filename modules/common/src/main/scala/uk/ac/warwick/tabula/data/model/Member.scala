140bb553bc908677740828a2da8c54bcdf08a704
Mat Mannion <m.mannion@warwick.ac.uk>
2014-05-19 10:28:43 +0100

    Fix permissions button from student profile

--- modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/Member.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/Member.scala
@@ -134,6 +134,7 @@ abstract class Member extends MemberProperties with ToString with HibernateVersi
 
 	def permissionsParents: Stream[PermissionsTarget] = touchedDepartments
 	override def humanReadableId = fullName.getOrElse(toString())
+	override final def urlCategory = "member"
 
 	@OneToMany(mappedBy="scope", fetch = FetchType.LAZY, cascade = Array(CascadeType.ALL))
 	@ForeignKey(name="none")
