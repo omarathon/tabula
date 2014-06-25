696935d594f9fab649f734daf4180d4c05d636eb
Nick Howes <n.howes@warwick.ac.uk>
2014-06-25 14:18:22 +0100

    TAB-2440 Keep all functional test users signed in at once, and remember their sessions.
    Should speed things up a bit.

--- modules/functional-test/src/test/scala/uk/ac/warwick/tabula/WebsignonMethods.scala
+++ modules/functional-test/src/test/scala/uk/ac/warwick/tabula/WebsignonMethods.scala
@@ -1,9 +1,11 @@
 package uk.ac.warwick.tabula
 
+import collection.JavaConverters._
+
 import org.scalatest.selenium.WebBrowser
 import org.scalatest.Assertions
 import org.scalatest.matchers.ShouldMatchers
-import org.openqa.selenium.WebDriver
+import org.openqa.selenium.{Cookie, WebDriver}
 import org.scalatest.concurrent.Eventually
 import org.scalatest.time.SpanSugar._
 
@@ -18,13 +20,37 @@ object WebsignonMethods {
 			case _ => "couldn't parse anything useful from the HTML"
 		}
 	}
+
+	val sessions = new SessionCache
+}
+
+/**
+ * Maintains a JVM-level cache of each user's cookies, so signing in
+ * as a user should be faster after the first time in a run - we can
+ * just switch their cookies back in, simulating each user being online
+ * at the same time in different browsers.
+ */
+class SessionCache {
+	private var map = Map[String, Set[Cookie]]()
+	def retrieve(usercode: String, webDriver: WebDriver) {
+		map.get(usercode).foreach { cookies =>
+			cookies.foreach { cookie =>
+				webDriver.manage().deleteCookieNamed(cookie.getName)
+				webDriver.manage().addCookie(cookie)
+			}
+		}
+	}
+
+	def store(usercode: String, webDriver: WebDriver) {
+		map += (usercode -> webDriver.manage().getCookies.asScala.toSet)
+	}
 }
 
 trait WebsignonMethods extends ShouldMatchers  with Eventually{
 	import WebBrowser._ // include methods like "go to"
 	implicit val webDriver: WebDriver // let the trait know this will be implemented
 	
-	// nested objects so we can say "signIn.as(user) to (url)".
+	// nested objects so we can say "signIn as(user) to (url)".
 	// with Java punctuation it would be "(signIn.as(user)).to(url)". 
 	//
 	// (currently requires that the user's first name is the usercode, to check signed-in-ness)
@@ -36,7 +62,11 @@ trait WebsignonMethods extends ShouldMatchers  with Eventually{
     case class SigningInPhase(details: LoginDetails) {
 
 			def to(url: String) {
+				// Sets session cookies if this user's logged in once before.
+				WebsignonMethods.sessions.retrieve(details.usercode, webDriver)
+
         go to (url)
+
         // FIXME doesn't handle being signed in as another user
         // TODO doesn't check that SSO sends you back to the right page
         // FIXME requires firstName==usercode
@@ -45,8 +75,9 @@ trait WebsignonMethods extends ShouldMatchers  with Eventually{
         } else {
 
           if (pageSource contains ("Signed in as ")) {
-						// signed in as someone else; sign out first
-						click on linkText("Sign out")
+						// signed in as someone else; clear cookies (but don't sign that user out, we might use them later)
+						webDriver.manage().deleteAllCookies()
+						reloadPage()
 					}else if (pageTitle startsWith("Sign in - Access refused")){
 						//signed in as someone else who doesn't have permissions to view the page
 						//  - follow the "sign in as another user" link
@@ -78,7 +109,8 @@ trait WebsignonMethods extends ShouldMatchers  with Eventually{
 						go to url
 					}
           if (pageSource contains ("Signed in as " + details.usercode)) {
-            // NOW we're done
+            // NOW we're done. Store cookies for quick retrieval in other tests
+						WebsignonMethods.sessions.store(details.usercode, webDriver)
           } else if (pageSource contains ("Access refused")) {
             Assertions.fail("Signed in as " + details.description + " but access refused to " + url)
           } else {
