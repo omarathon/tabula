3cb7964a0f8f4748ab2308dc49bf65438af5b2ac
Matthew Jones <Matthew.Jones@warwick.ac.uk>
2014-07-15 13:37:59 +0100

    TAB-2481

--- modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/groups/SmallGroupEventOccurrence.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/groups/SmallGroupEventOccurrence.scala
@@ -1,12 +1,14 @@
 package uk.ac.warwick.tabula.data.model.groups
 
 import javax.persistence._
+
 import org.hibernate.annotations.{AccessType, BatchSize}
-import uk.ac.warwick.tabula.data.model.{UserGroup, GeneratedId}
-import uk.ac.warwick.tabula.permissions.PermissionsTarget
+import org.joda.time.LocalDate
+import uk.ac.warwick.spring.Wire
 import uk.ac.warwick.tabula.JavaImports._
-import javax.persistence.CascadeType._
-import java.lang.annotation.Annotation
+import uk.ac.warwick.tabula.data.model.GeneratedId
+import uk.ac.warwick.tabula.permissions.PermissionsTarget
+import uk.ac.warwick.tabula.services.TermService
 
 @AccessType("field")
 @Entity
@@ -14,6 +16,9 @@ import java.lang.annotation.Annotation
 	new UniqueConstraint(columnNames = Array("event_id", "week"))
 ))
 class SmallGroupEventOccurrence extends GeneratedId with PermissionsTarget with Serializable {
+
+	@transient var termService = Wire[TermService]
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name="event_id")
 	var event: SmallGroupEvent = _
@@ -25,6 +30,18 @@ class SmallGroupEventOccurrence extends GeneratedId with PermissionsTarget with 
 	var attendance: JSet[SmallGroupEventAttendance] = JHashSet()
 
 	def permissionsParents = Stream(event)
+
+	def date: Option[LocalDate] = {
+		event.day match {
+			case null => None
+			case d: DayOfWeek =>
+				val weeksForYear = termService.getAcademicWeeksForYear(event.group.groupSet.academicYear.dateInTermOne).toMap
+				def weekNumberToDate(weekNumber: Int, dayOfWeek: DayOfWeek) =
+					weeksForYear(weekNumber).getStart.withDayOfWeek(dayOfWeek.jodaDayOfWeek)
+
+				Option(weekNumberToDate(week, event.day).toLocalDate)
+		}
+	}
 
 }
 
