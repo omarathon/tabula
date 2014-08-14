bee12845c292e5a517e30887747ae0b749dd6ed6
Sarah O'Toole <sarah.o-toole@warwick.ac.uk>
2014-08-14 11:52:59 +0100

    TAB-2469

--- modules/common/src/main/scala/uk/ac/warwick/tabula/services/SecurityService.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/services/SecurityService.scala
@@ -141,7 +141,7 @@ class SecurityService extends Logging with RequestLevelCaching[(CurrentUser, Per
 	def can(user: CurrentUser, permission: Permission, scope: PermissionsTarget) = _can(user, permission, Option(scope), false)
 
 	def canForAny(user: CurrentUser, permission: Permission, scopes: Seq[PermissionsTarget]): Boolean  = {
-		scopes.exists(scope => _can(user, permission, Option(scope), false))
+		scopes.exists(scope => _can(user, permission, Option(scope), canDelegate = false))
 	}
 
 	def canDelegate(user: CurrentUser, permission: ScopelessPermission ) = _can(user, permission, None, true)
