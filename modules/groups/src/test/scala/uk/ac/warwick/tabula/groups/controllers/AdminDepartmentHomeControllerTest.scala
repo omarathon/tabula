d2dc621e30b1204d0f8051cd196b89f1e2f2bb14
Matthew Jones <Matthew.Jones@warwick.ac.uk>
2014-09-02 13:10:45 +0100

    Fix build

--- modules/groups/src/test/scala/uk/ac/warwick/tabula/groups/controllers/AdminDepartmentHomeControllerTest.scala
+++ modules/groups/src/test/scala/uk/ac/warwick/tabula/groups/controllers/AdminDepartmentHomeControllerTest.scala
@@ -1,16 +1,15 @@
 package uk.ac.warwick.tabula.groups.controllers
 
-import uk.ac.warwick.tabula.groups.commands.admin.AdminSmallGroupsHomeInformation
-import uk.ac.warwick.tabula.groups.web.views.GroupsViewModel
-import uk.ac.warwick.tabula.groups.web.views.GroupsViewModel.ViewSetWithProgress
-import uk.ac.warwick.tabula.{Mockito, CurrentUser, TestBase}
-import uk.ac.warwick.tabula.groups.web.controllers.admin.{AdminDepartmentHomeController}
-import uk.ac.warwick.tabula.groups.SmallGroupFixture
 import org.mockito.Mockito._
 import uk.ac.warwick.tabula.commands.Appliable
+import uk.ac.warwick.tabula.groups.SmallGroupFixture
+import uk.ac.warwick.tabula.groups.commands.admin.{AdminSmallGroupsHomeCommandState, AdminSmallGroupsHomeInformation}
+import uk.ac.warwick.tabula.groups.web.controllers.admin.AdminDepartmentHomeController
+import uk.ac.warwick.tabula.groups.web.views.GroupsViewModel
+import uk.ac.warwick.tabula.groups.web.views.GroupsViewModel.ViewSetWithProgress
+import uk.ac.warwick.tabula.permissions.{Permission, PermissionsTarget}
 import uk.ac.warwick.tabula.services.SecurityService
-import uk.ac.warwick.tabula.permissions.Permission
-import uk.ac.warwick.tabula.permissions.PermissionsTarget
+import uk.ac.warwick.tabula.{CurrentUser, Mockito, TestBase}
 
 import scala.collection.immutable.ListMap
 
@@ -19,7 +18,7 @@ class AdminDepartmentHomeControllerTest extends TestBase with Mockito{
   def createController = {
     val controller = new AdminDepartmentHomeController()
     controller.securityService = mock[SecurityService]
-    when(controller.securityService.can(any[CurrentUser], any[Permission], any[PermissionsTarget])).thenReturn(true)
+    when(controller.securityService.can(any[CurrentUser], any[Permission], any[PermissionsTarget])).thenReturn{true}
     controller
   }
 
@@ -29,11 +28,11 @@ class AdminDepartmentHomeControllerTest extends TestBase with Mockito{
       groupSet1.releasedToStudents = true
       groupSet1.releasedToTutors = false
 
-      val cmd = mock[Appliable[AdminSmallGroupsHomeInformation]]
-      when(cmd.apply()).thenReturn(AdminSmallGroupsHomeInformation(false, Seq(groupSet1.module), Seq(ViewSetWithProgress(groupSet1, Nil, GroupsViewModel.Tutor, null, None, ListMap())), Nil))
+      val cmd = mock[Appliable[AdminSmallGroupsHomeInformation] with AdminSmallGroupsHomeCommandState]
+      when(cmd.apply()).thenReturn(AdminSmallGroupsHomeInformation(canAdminDepartment = false, Seq(groupSet1.module), Seq(ViewSetWithProgress(groupSet1, Nil, GroupsViewModel.Tutor, null, None, ListMap())), Nil))
       val mav = createController.adminDepartment(cmd, department, currentUser)
       mav.map.get("hasUnreleasedGroupsets") match{
-        case Some(v: Boolean) => v should be (true)
+        case Some(v: Boolean) => v should be {true}
         case _ => fail()
       }
     }}
@@ -46,11 +45,11 @@ class AdminDepartmentHomeControllerTest extends TestBase with Mockito{
       groupSet1.releasedToStudents = true
       groupSet1.releasedToTutors = true
 
-      val cmd = mock[Appliable[AdminSmallGroupsHomeInformation]]
-      when(cmd.apply()).thenReturn(AdminSmallGroupsHomeInformation(false, Seq(groupSet1.module), Seq(ViewSetWithProgress(groupSet1, Nil, GroupsViewModel.Tutor, null, None, ListMap())), Nil))
+      val cmd = mock[Appliable[AdminSmallGroupsHomeInformation] with AdminSmallGroupsHomeCommandState]
+      when(cmd.apply()).thenReturn(AdminSmallGroupsHomeInformation(canAdminDepartment = false, Seq(groupSet1.module), Seq(ViewSetWithProgress(groupSet1, Nil, GroupsViewModel.Tutor, null, None, ListMap())), Nil))
       val mav = createController.adminDepartment(cmd, department, currentUser)
       mav.map.get("hasUnreleasedGroupsets") match {
-        case Some(v: Boolean) => v should be (false)
+        case Some(v: Boolean) => v should be {false}
         case _ => fail()
       }
 
