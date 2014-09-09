e038e82ab0bdc88a7a8ebe8c465e7de4bf78ea7e
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2014-09-09 12:33:08 +0100

    TAB-2489 - Add extra tests and fix code to pass

--- modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/meetingrecord/ScheduledMeetingRecordInviteeNotification.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/meetingrecord/ScheduledMeetingRecordInviteeNotification.scala
@@ -16,7 +16,10 @@ class ScheduledMeetingRecordInviteeNotification
 	def FreemarkerTemplate = "/WEB-INF/freemarker/notifications/meetingrecord/scheduled_meeting_record_invitee_notification.ftl"
 	def title = s"Meeting $verb"
 
-	def actor = if (agent.getWarwickId == meeting.relationship.studentId || agent == meeting.relationship.agentMember) {
+	def isStudent = agent.getWarwickId == meeting.relationship.studentId
+	def isAgent = meeting.relationship.agentMember.exists(_.universityId == agent.getWarwickId)
+
+	def actor = if (isStudent || isAgent) {
 		agent
 	} else {
 		meeting.relationship.agentMember.map(_.asSsoUser).getOrElse(throw new IllegalStateException("Relationship has no agent"))
