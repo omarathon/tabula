3c2cb2d9c67b38d554df54ae3642b127dd470aa2
Matthew Jones <Matthew.Jones@warwick.ac.uk>
2014-10-15 16:48:38 +0100

    TAB-2871 - Replace ACTB in test

--- modules/coursework/src/test/scala/uk/ac/warwick/tabula/coursework/commands/markerfeedback/DownloadMarkerFeedbackTest.scala
+++ modules/coursework/src/test/scala/uk/ac/warwick/tabula/coursework/commands/markerfeedback/DownloadMarkerFeedbackTest.scala
@@ -1,18 +1,19 @@
 package uk.ac.warwick.tabula.coursework.commands.markerfeedback
 
-import collection.JavaConversions._
-import uk.ac.warwick.tabula.{Mockito, AppContextTestBase}
-import org.junit.Before
-import uk.ac.warwick.tabula.data.model.MarkingState._
-import uk.ac.warwick.tabula.data.model.{FirstFeedback, FileAttachment}
-import java.io.{FileInputStream, ByteArrayInputStream}
-import uk.ac.warwick.tabula.coursework.commands.feedback.{DownloadMarkersFeedbackForPositionCommand, AdminGetSingleMarkerFeedbackCommand}
+import java.io.{ByteArrayInputStream, FileInputStream, FileOutputStream}
 import java.util.zip.ZipInputStream
-import uk.ac.warwick.tabula.services.{UserLookupService, AutowiringZipServiceComponent, Zips}
-import org.springframework.util.FileCopyUtils
-import java.io.FileOutputStream
 
-class DownloadMarkerFeedbackTest extends AppContextTestBase with MarkingWorkflowWorld with Mockito {
+import org.junit.Before
+import org.springframework.util.FileCopyUtils
+import uk.ac.warwick.tabula.coursework.commands.feedback.{AdminGetSingleMarkerFeedbackCommand, DownloadMarkersFeedbackForPositionCommand}
+import uk.ac.warwick.tabula.data.model.MarkingState._
+import uk.ac.warwick.tabula.data.model.{FileAttachment, FirstFeedback}
+import uk.ac.warwick.tabula.services.{AutowiringZipServiceComponent, Zips}
+import uk.ac.warwick.tabula.{Mockito, TestBase}
+
+import scala.collection.JavaConversions._
+
+class DownloadMarkerFeedbackTest extends TestBase with MarkingWorkflowWorld with Mockito {
 
 	@Before
 	def setup() {
@@ -32,27 +33,25 @@ class DownloadMarkerFeedbackTest extends AppContextTestBase with MarkingWorkflow
 	}
 
 	@Test
-	def downloadSingle()= transactional{ts=>
-		withUser("cuslaj"){
-			val markerFeedback = assignment.getMarkerFeedback("9876004", currentUser.apparentUser, FirstFeedback)
-			val command = new AdminGetSingleMarkerFeedbackCommand(assignment.module, assignment, markerFeedback.get)
-			val renderable = command.apply()
-			val stream = new ZipInputStream(new FileInputStream(renderable.file.get))
-			val items = Zips.map(stream){item => item.getName}
-			items.size should be (1)
-		}
-	}
+	def downloadSingle() { withUser("cuslaj"){
+		val markerFeedback = assignment.getMarkerFeedback("9876004", currentUser.apparentUser, FirstFeedback)
+		val command = new AdminGetSingleMarkerFeedbackCommand(assignment.module, assignment, markerFeedback.get)
+		command.zipService = zipService
+		val renderable = command.applyInternal()
+		val stream = new ZipInputStream(new FileInputStream(renderable.file.get))
+		val items = Zips.map(stream){item => item.getName}
+		items.size should be (1)
+	}}
 
 	@Test
-	def downloadAll()= transactional{ts=>
-		withUser("cuslat"){
-			val command = new DownloadMarkersFeedbackForPositionCommand(assignment.module, assignment, currentUser, FirstFeedback) with AutowiringZipServiceComponent
-			assignment.markingWorkflow.userLookup = mockUserLookup
-			val renderable = command.applyInternal()
-			val stream = new ZipInputStream(new FileInputStream(renderable.file.get))
-			val items = Zips.map(stream){item => item.getName}
-			items.size should be (3)
-		}
-	}
+	def downloadAll() { withUser("cuslat"){
+		val command = new DownloadMarkersFeedbackForPositionCommand(assignment.module, assignment, currentUser, FirstFeedback) with AutowiringZipServiceComponent
+		assignment.markingWorkflow.userLookup = mockUserLookup
+		command.zipService = zipService
+		val renderable = command.applyInternal()
+		val stream = new ZipInputStream(new FileInputStream(renderable.file.get))
+		val items = Zips.map(stream){item => item.getName}
+		items.size should be (3)
+	}}
 
 }
