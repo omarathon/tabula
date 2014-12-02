353e6f100d92cafcf7e836776e8a4d436c333d5e
Sarah O'Toole <sarah.o-toole@warwick.ac.uk>
2014-12-02 14:32:55 +0000

    TAB-2673 - Add test

---
+++ modules/profiles/src/test/scala/uk/ac/warwick/tabula/profiles/commands/ViewModuleRegistrationsCommandTest.scala
@@ -0,0 +1,27 @@
+package uk.ac.warwick.tabula.profiles.commands
+
+import uk.ac.warwick.tabula.{AcademicYear, Mockito, TestBase}
+import uk.ac.warwick.tabula.data.model.{StudentCourseDetails, StudentCourseYearDetails, StudentMember}
+
+import uk.ac.warwick.tabula.system.permissions.PermissionsChecking
+import uk.ac.warwick.tabula.permissions.Permissions.Profiles
+
+class ViewModuleRegistrationsCommandTest extends TestBase with Mockito {
+
+	val testStudent = new StudentMember
+	val scd = new StudentCourseDetails(testStudent, "student")
+	val scyd = new StudentCourseYearDetails(scd, 1, AcademicYear(2014))
+
+	@Test
+	def requiresModuleRegistrationCoreReadPermissions() {
+		val perms = new ViewModuleRegistrationsCommandPermissions with ViewModuleRegistrationsCommandState {
+			val student = testStudent
+			val studentCourseYearDetails = scyd
+		}
+
+		val checking = mock[PermissionsChecking]
+		perms.permissionsCheck(checking)
+		there was one(checking).PermissionCheck(Profiles.Read.ModuleRegistration.Core, testStudent)
+	}
+
+}
