9f056b7b65772539a9ffd51e765c536ec9f94cec
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2015-01-27 14:18:22 +0000

    TAB-3196 - Fix empty reduceLeft

--- modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/web/controllers/admin/ListMarkerFeedbackController.scala
+++ modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/web/controllers/admin/ListMarkerFeedbackController.scala
@@ -31,7 +31,8 @@ class ListMarkerFeedbackController extends CourseworkController {
 			val markerFeedback = command.apply()
 			val feedbackItems = markerFeedback.flatMap(_.feedbackItems)
 
-			val maxFeedbackCount = feedbackItems.map(_.feedbacks.size).reduceOption(_ max _).getOrElse(0)
+			val feedbackCounts: Seq[Int] = feedbackItems.map(_.feedbacks.size)
+			val maxFeedbackCount = feedbackCounts.foldLeft(0)(_ max _)
 			val hasFirstMarkerFeedback = maxFeedbackCount > 1
 			val hasSecondMarkerFeedback = maxFeedbackCount > 2
 
