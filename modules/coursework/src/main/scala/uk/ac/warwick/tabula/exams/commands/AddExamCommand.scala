dcbef2e32dae89da897489bc831f731cae2226d5
Sarah O'Toole <sarah.o-toole@warwick.ac.uk>
2015-02-18 16:12:42 +0000

    TAB-3274

--- modules/coursework/src/main/scala/uk/ac/warwick/tabula/exams/commands/AddExamCommand.scala
+++ modules/coursework/src/main/scala/uk/ac/warwick/tabula/exams/commands/AddExamCommand.scala
@@ -5,6 +5,7 @@ import uk.ac.warwick.tabula.data.model.{Exam, Module}
 import uk.ac.warwick.tabula.system.permissions.{PermissionsChecking, PermissionsCheckingMethods, RequiresPermissionsChecking}
 import uk.ac.warwick.tabula.permissions.Permissions
 import uk.ac.warwick.tabula.services.{ExamServiceComponent, AutowiringExamServiceComponent}
+import org.springframework.validation.Errors
 
 object AddExamCommand  {
 	def apply(module: Module) =
@@ -13,15 +14,20 @@ object AddExamCommand  {
 			with AddExamPermissions
 			with AddExamCommandState
 			with AddExamCommandDescription
+			with AddExamValidation
 			with AutowiringExamServiceComponent
+			with CurrentSITSAcademicYear
+
 }
 
 class AddExamCommandInternal(val module: Module) extends CommandInternal[Exam] with AddExamCommandState {
 
-	self: ExamServiceComponent =>
+	self: ExamServiceComponent with CurrentSITSAcademicYear =>
 
 	override def applyInternal() = {
 		val exam = new Exam
+		exam.name = name
+		exam.academicYear = academicYear
 		examService.saveOrUpdate(exam)
 		exam
 	}
@@ -40,6 +46,9 @@ trait AddExamPermissions extends RequiresPermissionsChecking with PermissionsChe
 
 trait AddExamCommandState {
 	def module: Module
+
+	// bind variables
+	var name: String = _
 }
 
 trait AddExamCommandDescription extends Describable[Exam] {
@@ -48,4 +57,16 @@ trait AddExamCommandDescription extends Describable[Exam] {
 	def describe(d: Description) {
 		d.module(module)
 	}
+}
+
+trait AddExamValidation extends SelfValidating {
+
+	self: AddExamCommandState =>
+
+	override def validate(errors: Errors) {
+		if (name == null) {
+			errors.rejectValue("name", "exam.name.empty")
+		}
+	}
+
 }
