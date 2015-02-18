17d05e96eeeddcda5641ac76d279317d8a8dce52
Sarah O'Toole <sarah.o-toole@warwick.ac.uk>
2015-02-18 16:43:09 +0000

    TAB-3274

--- modules/coursework/src/main/scala/uk/ac/warwick/tabula/exams/web/controllers/admin/AddExamController.scala
+++ modules/coursework/src/main/scala/uk/ac/warwick/tabula/exams/web/controllers/admin/AddExamController.scala
@@ -2,18 +2,34 @@ package uk.ac.warwick.tabula.exams.web.controllers.admin
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{ModelAttribute, PathVariable, RequestMapping}
-import uk.ac.warwick.tabula.data.model.Module
-import uk.ac.warwick.tabula.exams.commands.AddExamCommand
+import uk.ac.warwick.tabula.data.model.{Exam, Module}
+import uk.ac.warwick.tabula.exams.commands.{AddExamCommandState, AddExamCommand}
 import uk.ac.warwick.tabula.web.controllers.BaseController
+import javax.validation.Valid
+import org.springframework.validation.Errors
+import uk.ac.warwick.tabula.commands.Appliable
+import uk.ac.warwick.tabula.coursework.web.Routes
 
 @Controller
 @RequestMapping(value = Array("/exams/admin/module/{module}/exam/new"))
 class AddExamController extends BaseController {
 
+	type AddExamCommand = Appliable[Exam] with AddExamCommandState
+
 	@ModelAttribute("command")
 	def command(@PathVariable("module") module: Module) = AddExamCommand(mandatory(module))
 
 	@RequestMapping(method = Array(HEAD, GET))
-	def showForm() = Mav("exams/admin/module/new")
+	def showForm(@ModelAttribute("command") cmd: AddExamCommand) = Mav("exams/admin/module/new")
+
+	@RequestMapping(method = Array(POST))
+	def submit(@Valid @ModelAttribute("command") cmd: AddExamCommand, errors: Errors) = {
+		if (errors.hasErrors) {
+			showForm(cmd)
+		} else {
+			cmd.apply()
+			Redirect(Routes.home)
+		}
+	}
 
 }
