b03250c5e6c21d5ce81d5a4ee407de93bd1f0f85
Paul Strapps <paul.strapps@warwick.ac.uk>
2015-02-19 09:23:09 +0000

    TAB-3274 populated thw academic year drop down for create exam

--- modules/coursework/src/main/scala/uk/ac/warwick/tabula/exams/web/controllers/admin/AddExamController.scala
+++ modules/coursework/src/main/scala/uk/ac/warwick/tabula/exams/web/controllers/admin/AddExamController.scala
@@ -1,7 +1,12 @@
 package uk.ac.warwick.tabula.exams.web.controllers.admin
 
+
+import scala.collection.JavaConverters._
+import org.joda.time.DateTime
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{ModelAttribute, PathVariable, RequestMapping}
+import uk.ac.warwick.tabula.AcademicYear
+import uk.ac.warwick.tabula.JavaImports._
 import uk.ac.warwick.tabula.data.model.{Exam, Module}
 import uk.ac.warwick.tabula.exams.commands.{AddExamCommandState, AddExamCommand}
 import uk.ac.warwick.tabula.web.controllers.BaseController
@@ -17,6 +22,10 @@ class AddExamController extends BaseController {
 	type AddExamCommand = Appliable[Exam] with AddExamCommandState
 
 	validatesSelf[SelfValidating]
+
+	@ModelAttribute("academicYearChoices") def academicYearChoices: JList[AcademicYear] = {
+		AcademicYear.guessSITSAcademicYearByDate(DateTime.now).yearsSurrounding(2, 2).asJava
+	}
 
 	@ModelAttribute("command")
 	def command(@PathVariable("module") module: Module) = AddExamCommand(mandatory(module))
