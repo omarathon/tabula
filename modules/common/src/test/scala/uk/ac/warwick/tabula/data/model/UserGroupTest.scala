396965b584be687ea79578ce95dadfbb8f4d6d85
Matthew Jones <Matthew.Jones@warwick.ac.uk>
2015-02-26 16:46:06 +0000

    TAB-3343 - Fix tests

--- modules/common/src/test/scala/uk/ac/warwick/tabula/data/model/UserGroupTest.scala
+++ modules/common/src/test/scala/uk/ac/warwick/tabula/data/model/UserGroupTest.scala
@@ -1,18 +1,24 @@
 package uk.ac.warwick.tabula.data.model
 
+import org.hibernate.{Session, SessionFactory}
+import uk.ac.warwick.tabula.JavaImports.{JArrayList, JHashMap, JList}
 import uk.ac.warwick.tabula._
-import collection.JavaConversions._
-import collection.JavaConverters._
-import JavaImports.{JArrayList,JList,JHashMap}
-import uk.ac.warwick.userlookup.{User, GroupImpl}
 import uk.ac.warwick.tabula.services.UserLookupService
+import uk.ac.warwick.userlookup.{GroupImpl, User}
+
+import scala.collection.JavaConverters._
 
 class UserGroupTest extends PersistenceTestBase  with Mockito{
 
-	@Test def membership {
+	val mockSessionFactory = smartMock[SessionFactory]
+	val mockSession = smartMock[Session]
+	mockSessionFactory.getCurrentSession returns mockSession
+	mockSessionFactory.openSession() returns mockSession
+
+	@Test def membership() {
 		transactional { t =>
 			var group = UserGroup.ofUsercodes
-			
+			group.sessionFactory = mockSessionFactory
 			// users that can't be changed (i.e. as imported from upstream)
 			group.staticUserIds = Seq( "exoman", "eggdog" )
 			// users added manually
@@ -20,8 +26,8 @@ class UserGroupTest extends PersistenceTestBase  with Mockito{
 			group.addUserId("menace")
 			
 			session.saveOrUpdate(group)
-			session.flush
-			session.clear
+			session.flush()
+			session.clear()
 			
 			group = session.get(classOf[UserGroup], group.id).asInstanceOf[UserGroup]
 			
@@ -32,11 +38,11 @@ class UserGroupTest extends PersistenceTestBase  with Mockito{
 			group.includedUserIds should (contain ("superhat") and contain ("menace"))
 			
 			group.excludeUserId("eggdog") // poor eggdog.
-			group.includesUserId("whoareyou") should be (false)
-			group.includesUserId("exoman") should be (true)
-			group.includesUserId("eggdog") should be (false)
-			group.includesUserId("superhat") should be (true)
-			group.includesUserId("menace") should be (true)
+			group.includesUserId("whoareyou") should be {false}
+			group.includesUserId("exoman") should be {true}
+			group.includesUserId("eggdog") should be {false}
+			group.includesUserId("superhat") should be {true}
+			group.includesUserId("menace") should be {true}
 			
 			/* check that members works and is consistent.
 			 * At time of writing, staticIncludeUsers would get
@@ -48,10 +54,11 @@ class UserGroupTest extends PersistenceTestBase  with Mockito{
 		}
 	} 
 	
-	@Test def withWebgroup {
+	@Test def withWebgroup() {
 		val userLookup = new MockUserLookup
 		
 		val group = UserGroup.ofUsercodes
+		group.sessionFactory = mockSessionFactory
 		group.userLookup = userLookup
 		
 		group.addUserId("cuscav")
@@ -68,8 +75,9 @@ class UserGroupTest extends PersistenceTestBase  with Mockito{
 		group.members should be (Seq("sb_systemtest", "cuscav", "curef", "cusebr"))
 	}
 	
-	@Test def copy {
+	@Test def copy() {
 		val group = UserGroup.ofUsercodes
+		group.sessionFactory = mockSessionFactory
 		group.addUserId("cuscav")
 		group.addUserId("curef")
 		group.excludeUserId("cusmab") // we don't like Steve
@@ -77,9 +85,10 @@ class UserGroupTest extends PersistenceTestBase  with Mockito{
 		group.baseWebgroup = "in-elab"
 			
 		val group2 = UserGroup.ofUsercodes
+		group2.sessionFactory = mockSessionFactory
 		group2.copyFrom(group)
 		
-		group.eq(group2) should be (false)
+		group.eq(group2) should be {false}
 		
 		group2.includedUserIds should be (group.includedUserIds)
 		group2.excludedUserIds should be (group.excludedUserIds)
