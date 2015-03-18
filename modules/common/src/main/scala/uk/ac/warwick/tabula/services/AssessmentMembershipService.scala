1149205631debc4f569e5e151c1b16b82c9e4f7b
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2015-03-18 14:56:01 +0000

    TAB-3389 logging

--- modules/common/src/main/scala/uk/ac/warwick/tabula/services/AssessmentMembershipService.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/services/AssessmentMembershipService.scala
@@ -107,6 +107,10 @@ class AssessmentMembershipServiceImpl
 
 	def replaceMembers(template: UpstreamAssessmentGroup, registrations: Seq[UpstreamModuleRegistration]) = {
 		if (debugEnabled) debugReplace(template, registrations.map(_.universityId))
+
+		//TODO - remove this
+		logger.info("Setting %d members in group %s" format (registrations.map(_.universityId).size, template.toString))
+		
 		getUpstreamAssessmentGroup(template).map { group =>
 			group.members.knownType.replaceStaticUsers(registrations)
 			group
