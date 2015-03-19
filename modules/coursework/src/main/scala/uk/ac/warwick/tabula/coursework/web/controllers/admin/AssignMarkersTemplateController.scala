dcd480b9f8db4deaa52a0c61529b6e97836a1bf4
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2015-03-19 17:17:56 +0000

    TAB-3052 - Fix entity reference for spreadsheet download

--- modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/web/controllers/admin/AssignMarkersTemplateController.scala
+++ modules/coursework/src/main/scala/uk/ac/warwick/tabula/coursework/web/controllers/admin/AssignMarkersTemplateController.scala
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{PathVariable, ModelAttribute, RequestMapping}
 import uk.ac.warwick.tabula.commands.Appliable
 import uk.ac.warwick.tabula.coursework.commands.assignments.AssignMarkersTemplateCommand
-import uk.ac.warwick.tabula.data.model.Assignment
+import uk.ac.warwick.tabula.data.model.{Exam, Assignment}
 import javax.validation.Valid
 import uk.ac.warwick.tabula.web.views.ExcelView
 
@@ -29,7 +29,7 @@ class ExamAssignMarkersTemplateController {
 
 
 	@ModelAttribute("command")
-	def command(@PathVariable("assignment") assignment: Assignment) = AssignMarkersTemplateCommand(assignment)
+	def command(@PathVariable("exam") exam: Exam) = AssignMarkersTemplateCommand(exam)
 
 	@RequestMapping
 	def getTemplate(@Valid @ModelAttribute("command") cmd: Appliable[ExcelView]) = {
