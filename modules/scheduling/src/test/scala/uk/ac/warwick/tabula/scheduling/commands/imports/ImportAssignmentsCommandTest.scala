f06bd225bec79e686bb83b89da74576a8598fbc6
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2015-04-15 08:34:29 +0100

    TAB-3457 - Update tests so that we check replaceMembers is never called with an empty list for unseen groups

--- modules/scheduling/src/test/scala/uk/ac/warwick/tabula/scheduling/commands/imports/ImportAssignmentsCommandTest.scala
+++ modules/scheduling/src/test/scala/uk/ac/warwick/tabula/scheduling/commands/imports/ImportAssignmentsCommandTest.scala
@@ -117,6 +117,9 @@ class ImportAssignmentsCommandTest extends FlatSpec with Matchers with Mockito {
 				)
 			))
 
+			// The bug is that we don't update any group we don't have moduleregistrations for.
+			verify(membershipService, times(0)).replaceMembers(anArgThat(hasModuleCode("HI900-30")), isEq(Nil))
+
 		}
 	}
 
