7543227b019f5c8962558dfb9eb4e98ba73c6f6f
Paul Strapps <paul.strapps@warwick.ac.uk>
2015-04-28 11:23:04 +0100

    TAB-3421 - added export for XML and Excel

--- modules/coursework/src/main/scala/uk/ac/warwick/tabula/exams/web/controllers/admin/ExamExports.scala
+++ modules/coursework/src/main/scala/uk/ac/warwick/tabula/exams/web/controllers/admin/ExamExports.scala
@@ -1,77 +1,53 @@
 package uk.ac.warwick.tabula.exams.web.controllers.admin
 
+import org.apache.commons.lang3.text.WordUtils
+import org.apache.poi.hssf.usermodel.HSSFDataFormat
+import org.apache.poi.ss.util.WorkbookUtil
+import org.apache.poi.xssf.usermodel.{XSSFSheet, XSSFWorkbook}
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
-import uk.ac.warwick.spring.Wire
 import uk.ac.warwick.tabula.AcademicYear
 import uk.ac.warwick.tabula.coursework.commands.assignments.SubmissionAndFeedbackCommand.Student
-import uk.ac.warwick.tabula.data.model.{Exam, ExamFeedback, Module}
+import uk.ac.warwick.tabula.data.model._
 import uk.ac.warwick.tabula.exams.commands.ViewExamCommandResult
 import uk.ac.warwick.util.csv.CSVLineWriter
 
+import scala.xml.{Elem, Null, UnprefixedAttribute}
+
 trait ExamExports {
 
-	class CSVBuilder(val items:Seq[Student], val results:ViewExamCommandResult, val exam: Exam, val module: Module, val academicYear: AcademicYear)
-		extends CSVLineWriter[Student] with ExamCsvExport {
-
-		var topLevelUrl: String = Wire.property("${toplevel.url}")
+class CSVBuilder(val students:Seq[Student], val results:ViewExamCommandResult, val exam: Exam, val module: Module, val academicYear: AcademicYear)
+		extends CSVLineWriter[Student] with ExamHeaderInformation with ItemData with formatContent{
 
 		def getNoOfColumns(item:Student) = headers.size
-		def getColumn(item:Student, i:Int) = formatData(itemData(item).get(headers(i)))
-
-		def itemData(student: Student) = {
-			val warwickId = student.user.getWarwickId
-			val seatNumber = results.seatNumberMap.get(student.user)
-			var hasFeedback:Boolean = false
-			var feedback = new ExamFeedback
-
-			if (!results.feedbackMap.get(student.user).isEmpty) {
-				hasFeedback = true;
-				feedback  = results.feedbackMap.get(student.user).get.get
-			}
-
-			val hasSitsStatus =  hasFeedback && results.sitsStatusMap.get(feedback).size > 0
-
-			var studentName = ""
-			if (module.adminDepartment.showStudentName) {
-				studentName = student.user.getFullName
-			} else {
-				studentName = warwickId
-			}
-
-			var data:Map[String, Any] = Map();
-			data += (SEAT_NUMBER -> seatNumber)
-			data += (STUDENT -> studentName)
-			if (hasFeedback) {
-				data += (ORIGINAL_MARK -> feedback.actualMark.get)
-				data += (ORIGINAL_GRADE -> feedback.actualGrade.get)
-				data += (ADJUSTED_MARK -> feedback.latestPrivateOrNonPrivateAdjustment.get.mark)
-				data += (ADJUSTED_GRADE -> feedback.latestPrivateOrNonPrivateAdjustment.get.grade)
-				if (hasSitsStatus) {
-					val sitsStatus = results.sitsStatusMap.get(feedback).get.get
-					data += (SITS_UPLOAD_STATUS -> sitsStatus.status.description)
-					data += (SITS_UPLOAD_DATE -> sitsStatus.dateOfUpload)
-					data += (SITS_UPLOAD_MARK -> sitsStatus.actualMarkLastUploaded)
-					data += (SITS_UPLOAD_GRADE -> sitsStatus.actualGradeLastUploaded)
-				}
-			}
-			data
-		}
-
-		protected def formatData(data: Option[Any]) = data match {
-			case Some(date: DateTime) => DateTimeFormat.forPattern("HH:mm:ss dd/MM/yyyy").print(date)
-			case Some(b: Boolean) => b.toString.toLowerCase
-			case Some(i: Int) => i.toString
-			case Some(s: String) => s
-			case Some(Some(s: String)) => s
-			case Some(Some(Some(i:Int))) => i.toString
-			case None => ""
-			case _ => ""
-		}
+		def getColumn(item:Student, i:Int) = formatData(getItemData(item, results, module).get(headers(i)))
 	}
 }
 
-trait ExamCsvExport {
+trait formatContent {
+
+	protected def formatData(data: Option[Any]) = data match {
+		case Some(date: DateTime) => DateTimeFormat.forPattern("HH:mm:ss dd/MM/yyyy").print(date)
+		case Some(b: Boolean) => b.toString.toLowerCase
+		case Some(i: Int) => i.toString
+		case Some(s: String) => s
+		case Some(Some(s: String)) => s
+		case Some(Some(Some(i:Int))) => i.toString
+		case None => ""
+		case _ => ""
+	}
+
+	def toXMLString(value: Option[Any]): String = value match {
+		case Some(o: Option[Any]) => toXMLString(o)
+		case Some(b: Boolean) => b.toString.toLowerCase
+		case Some(i: Int) => i.toString
+		case Some(s: String) => s
+		case Some(other) => other.toString
+		case None => ""
+	}
+}
+
+trait ExamHeaderInformation {
 
 	val SEAT_NUMBER: String = "Seat number"
 	val STUDENT: String = "Student"
@@ -98,3 +74,176 @@ trait ExamCsvExport {
 	)
 }
 
+trait ItemData extends ExamHeaderInformation {
+	def getItemData(student: Student,  results:ViewExamCommandResult, module: Module) = {
+
+		val feedback:ExamFeedback = studentFeedback(results, student)
+		val hasFeedback = studentHasFeedback(results, student)
+		val hasSitsStatus = studentHasSitsFeedack(results, student)
+
+		var data:Map[String, Any] = Map();
+
+		data += (SEAT_NUMBER -> results.seatNumberMap.get(student.user))
+		data += (STUDENT -> studentName(module, student))
+		if (hasFeedback) {
+			data += (ORIGINAL_MARK -> feedback.actualMark.get)
+			data += (ORIGINAL_GRADE -> feedback.actualGrade.get)
+			data += (ADJUSTED_MARK -> feedback.latestPrivateOrNonPrivateAdjustment.get.mark)
+			data += (ADJUSTED_GRADE -> feedback.latestPrivateOrNonPrivateAdjustment.get.grade)
+			if (hasSitsStatus) {
+				val sitsStatus = studentSitsFeedback(results, student)
+				data += (SITS_UPLOAD_STATUS -> sitsStatus.status.description)
+				data += (SITS_UPLOAD_DATE -> sitsStatus.dateOfUpload)
+				data += (SITS_UPLOAD_MARK -> sitsStatus.actualMarkLastUploaded)
+				data += (SITS_UPLOAD_GRADE -> sitsStatus.actualGradeLastUploaded)
+			}
+		}
+		data
+	}
+
+	def studentName(module: Module, student: Student): String = {
+		if (module.adminDepartment.showStudentName) {
+			return student.user.getFullName
+		} else {
+			return student.user.getWarwickId
+		}
+	}
+
+	def studentHasFeedback(results:ViewExamCommandResult, student:Student): Boolean = {
+		!results.feedbackMap.get(student.user).isEmpty
+	}
+
+	def studentFeedback(results:ViewExamCommandResult, student:Student): ExamFeedback = {
+		if (studentHasFeedback(results, student)) {
+			return results.feedbackMap.get(student.user).get.get
+		} else {
+			return new ExamFeedback
+		}
+	}
+
+	def studentHasSitsFeedack(results:ViewExamCommandResult, student:Student) : Boolean = {
+		val feedback = studentFeedback(results, student)
+		studentHasFeedback(results, student) && results.sitsStatusMap.get(feedback).size > 0
+	}
+
+	def studentSitsFeedback(results:ViewExamCommandResult, student:Student) : FeedbackForSits = {
+		val feedback = studentFeedback(results, student)
+		if (studentHasSitsFeedack(results, student)) {
+			return results.sitsStatusMap.get(feedback).get.get
+		} else {
+			return new FeedbackForSits
+		}
+	}
+}
+
+
+class XMLBuilder(val students:Seq[Student], val results:ViewExamCommandResult, val exam: Exam, val module: Module, val academicYear: AcademicYear)
+	extends ItemData with formatContent {
+
+	// Pimp XML elements to allow a map mutator for attributes
+	implicit class PimpedElement(elem: Elem) {
+		def %(attrs:Map[String,Any]) = {
+			val seq = for( (n,v) <- attrs ) yield new UnprefixedAttribute(n, toXMLString(Some(v)), Null)
+			(elem /: seq) ( _ % _ )
+		}
+	}
+
+	def toXML  = {
+		<exam>
+			<name>{exam.name}</name>
+			<module-code>{ module.code }</module-code>
+			<module-name>{ module.name }</module-name>
+			<department-name>{ module.adminDepartment.name }</department-name>
+			<department-code>{ module.adminDepartment.code }</department-code>
+			<academic-year>{academicYear}</academic-year>
+			<students>
+					{ students map studentXML }
+			</students>
+		</exam>
+	}
+
+	def studentXML(student: Student) = {
+
+		val feedback:ExamFeedback = studentFeedback(results, student)
+		val hasFeedback = studentHasFeedback(results, student)
+		val hasSitsStatus = studentHasSitsFeedack(results, student)
+		val sitsStatus = studentSitsFeedback(results, student)
+		<student>
+			<university-number>{ student.user.getWarwickId }</university-number>
+			<student-name>{if (module.adminDepartment.showStudentName){ student.user.getFullName } }</student-name>
+			<seat-number>{ results.seatNumberMap.get(student.user).get.getOrElse("") }</seat-number>
+			<original-mark>{if (hasFeedback) {feedback.actualMark.getOrElse("")} }</original-mark>
+			<original-grade>{if (hasFeedback) {feedback.actualGrade.getOrElse("")} }</original-grade>
+			<adjusted-mark>{if (hasFeedback) {feedback.latestPrivateOrNonPrivateAdjustment.get.mark} }</adjusted-mark>
+			<adjusted-grade>{if (hasFeedback) {feedback.latestPrivateOrNonPrivateAdjustment.get.mark} }</adjusted-grade>
+			<sits-upload-status>{if (hasSitsStatus) {sitsStatus.status.description} }</sits-upload-status>
+			<sits-upload-date>{if (hasSitsStatus) {sitsStatus.dateOfUpload} }</sits-upload-date>
+			<sits-upload-mark>{if (hasSitsStatus) {sitsStatus.actualMarkLastUploaded} }</sits-upload-mark>
+			<sits-upload-grade>{if (hasSitsStatus) {sitsStatus.actualGradeLastUploaded} }</sits-upload-grade>
+		</student>
+	}
+}
+
+class ExcelBuilder(val students: Seq[Student], val results:ViewExamCommandResult, val module: Module)
+	extends ExamHeaderInformation with ItemData with formatContent {
+
+	def toXLSX = {
+		val workbook = new XSSFWorkbook()
+		val sheet = generateNewSheet(workbook)
+
+		students foreach { addRow(sheet)(_) }
+
+		formatWorksheet(sheet)
+		workbook
+	}
+
+	def generateNewSheet(workbook: XSSFWorkbook) = {
+		val sheet = workbook.createSheet(module.code.toUpperCase + " - " + safeAssignmentName)
+
+		def formatHeader(header: String) =
+			WordUtils.capitalizeFully(header.replace('-', ' '))
+
+		// add header row
+		val headerRow = sheet.createRow(0)
+		headers.zipWithIndex foreach {
+			case (header, index) => headerRow.createCell(index).setCellValue(formatHeader(header))
+		}
+		sheet
+	}
+
+	def addRow(sheet: XSSFSheet)(student: Student) {
+		val plainCellStyle = {
+			val cs = sheet.getWorkbook.createCellStyle()
+			cs.setDataFormat(HSSFDataFormat.getBuiltinFormat("@"))
+			cs
+		}
+
+		val row = sheet.createRow(sheet.getLastRowNum + 1)
+		headers.zipWithIndex foreach {
+			case (header, index) =>
+				val cell = row.createCell(index)
+
+				if (index == 0) {
+					// University IDs have leading zeros and Excel would normally remove them.
+					// Set a manual data format to remove this possibility
+					cell.setCellStyle(plainCellStyle)
+				}
+				cell.setCellValue(formatData(getItemData(student,results, module).get(header)))
+		}
+	}
+
+	def formatWorksheet(sheet: XSSFSheet) = {
+		(0 to headers.size) foreach sheet.autoSizeColumn
+	}
+
+	// trim the assignment name down to 20 characters. Excel sheet names must be 31 chars or less so
+	val trimmedModuleName = {
+		if (module.name.length > 20)
+			module.name.substring(0, 20)
+		else
+			module.name
+	}
+
+	// util to replace unsafe characters with spaces
+	val safeAssignmentName = WorkbookUtil.createSafeSheetName(trimmedModuleName)
+}
--- modules/coursework/src/main/scala/uk/ac/warwick/tabula/exams/web/controllers/admin/ExportExamsController.scala
+++ modules/coursework/src/main/scala/uk/ac/warwick/tabula/exams/web/controllers/admin/ExportExamsController.scala
@@ -7,18 +7,16 @@ import org.springframework.web.bind.annotation.{PathVariable, RequestMapping}
 import uk.ac.warwick.tabula.AcademicYear
 import uk.ac.warwick.tabula.coursework.commands.assignments.SubmissionAndFeedbackCommand.Student
 import uk.ac.warwick.tabula.data.model.{Exam, Module}
-import uk.ac.warwick.tabula.exams.commands.ViewExamCommand
+import uk.ac.warwick.tabula.exams.commands.{ViewExamCommand, ViewExamCommandResult}
 import uk.ac.warwick.tabula.exams.web.controllers.ExamsController
-import uk.ac.warwick.tabula.web.views.CSVView
+import uk.ac.warwick.tabula.web.views.{CSVView, ExcelView}
 import uk.ac.warwick.util.csv.GoodCsvDocument
 
 import scala.collection.mutable
 
 @Controller
 @RequestMapping(Array("/admin/module/{module}/{academicYear}/exams/{exam}"))
-class ExportExamsController extends ExamsController with ExamExports {
-
-
+class ExportExamsController extends ExamsController with ExamExports with requestData {
 
 	@RequestMapping(Array("/export.csv"))
 	def csv (
@@ -27,15 +25,7 @@ class ExportExamsController extends ExamsController with ExamExports {
 		@PathVariable academicYear: AcademicYear
 	) = {
 
-		val command = ViewExamCommand(module, academicYear, exam)
-		val results = command.apply();
-
-		val students = mutable.MutableList[Student]()
-		for (user <- results.students) {
-			students += new Student(user, null, null, null, null, null)
-		}
-
-
+		val (results, students) = getData(module,exam,academicYear)
 		val writer = new StringWriter
 		val csvBuilder = new CSVBuilder(students, results, exam, module, academicYear)
 		val doc = new GoodCsvDocument(csvBuilder, null)
@@ -49,19 +39,41 @@ class ExportExamsController extends ExamsController with ExamExports {
 		new CSVView(module.code + "-" + exam.name + ".csv", writer.toString)
 	}
 
-//	@RequestMapping(Array("/export.xml"))
-//	def xml(
-//		@PathVariable module: Module,
-//		@PathVariable exam: Exam,
-//		@PathVariable academicYear: AcademicYear
-//	) = {
-//		val command = ViewExamCommand(module, academicYear, exam)
-//		val results = command.apply();
-//
-//		val items = results.students
-//
-//		new XMLBuilder(items, assignment, module).toXML
-//	}
+	@RequestMapping(Array("/export.xml"))
+	def xml(
+		@PathVariable module: Module,
+		@PathVariable exam: Exam,
+		@PathVariable academicYear: AcademicYear
+	) = {
 
+		val (results, students) = getData(module,exam,academicYear)
+		new XMLBuilder(students, results, exam, module, academicYear).toXML
+	}
 
+	@RequestMapping(Array("/export.xlsx"))
+	def toXLSX(
+		@PathVariable module: Module,
+		@PathVariable exam: Exam,
+		@PathVariable academicYear: AcademicYear
+	) = {
+
+		val (results, students) = getData(module,exam,academicYear)
+		val workbook = new ExcelBuilder(students, results, module).toXLSX
+		new ExcelView(module.code + "-" + exam.name + ".xlsx", workbook)
+	}
+}
+
+trait requestData {
+
+	def getData(module: Module, exam: Exam, academicYear: AcademicYear) : (ViewExamCommandResult, mutable.MutableList[Student]) = {
+
+		val command = ViewExamCommand(module, academicYear, exam)
+		val results = command.apply();
+
+		val students = mutable.MutableList[Student]()
+		for (user <- results.students) {
+			students += new Student(user, null, null, null, null, null)
+		}
+		(results, students)
+	}
 }
