4046071619c02f1d5c75fa735e36b665f5ead523
Sarah O'Toole <sarah.o-toole@warwick.ac.uk>
2015-06-25 11:06:42 +0100

    TAB-3621

--- modules/common/src/main/scala/uk/ac/warwick/tabula/data/convert/FileAttachmentTokenIdConverter.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/convert/FileAttachmentTokenIdConverter.scala
@@ -8,4 +8,5 @@ import uk.ac.warwick.tabula.data.Daoisms
 class FileAttachmentTokenIdConverter extends TwoWayConverter[String, FileAttachmentToken] with Daoisms {
 
 	override def convertRight(id: String) = getById[FileAttachmentToken](id).orNull
-	override def convertLeft(token: FileAttachmentToken) = (Option(token) map {_.id}).orNull}
+	override def convertLeft(token: FileAttachmentToken) = (Option(token) map {_.id}).orNull
+}
