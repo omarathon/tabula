b237d70d67ab4084c4a176b96c09c86f1edaf160
Sarah O'Toole <sarah.o-toole@warwick.ac.uk>
2015-06-25 11:14:35 +0100

    Revert "TAB-3621"
    This reverts commit 4046071619c02f1d5c75fa735e36b665f5ead523.

--- modules/common/src/main/scala/uk/ac/warwick/tabula/data/convert/FileAttachmentTokenIdConverter.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/convert/FileAttachmentTokenIdConverter.scala
@@ -8,5 +8,4 @@ import uk.ac.warwick.tabula.data.Daoisms
 class FileAttachmentTokenIdConverter extends TwoWayConverter[String, FileAttachmentToken] with Daoisms {
 
 	override def convertRight(id: String) = getById[FileAttachmentToken](id).orNull
-	override def convertLeft(token: FileAttachmentToken) = (Option(token) map {_.id}).orNull
-}
+	override def convertLeft(token: FileAttachmentToken) = (Option(token) map {_.id}).orNull}
