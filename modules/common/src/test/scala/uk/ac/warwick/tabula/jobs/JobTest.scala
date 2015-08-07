0fcb4e17f39b2fd0ca0d5ce857033ea28dae9724
Sarah O'Toole <sarah.o-toole@warwick.ac.uk>
2015-08-07 16:02:45 +0100

    TAB-3674 - Rename test classes so they get picked up by maven

--- modules/common/src/test/scala/uk/ac/warwick/tabula/jobs/JobTest.scala
+++ modules/common/src/test/scala/uk/ac/warwick/tabula/jobs/JobTest.scala
@@ -1,8 +1,6 @@
 package uk.ac.warwick.tabula.jobs
 
-import org.junit.Test
 import uk.ac.warwick.tabula.TestBase
-import uk.ac.warwick.tabula.services.jobs._
 import org.junit.Before
 import uk.ac.warwick.tabula.Mockito
 import uk.ac.warwick.tabula.CurrentUser
@@ -11,28 +9,28 @@ import uk.ac.warwick.userlookup.User
 import uk.ac.warwick.tabula.data.Transactions
 
 // scalastyle:off magic.number
-class JobTests extends TestBase with Mockito with JobTestHelp {
+class JobTest extends TestBase with Mockito with JobTestHelp {
 
 	val testingJob = new TestingJob
 	override def createJobs = Array[Job](testingJob)
 
-	@Before def setup {
+	@Before def setup() {
 		dao.clear
 		val realUser = new User("real")
 		val fakeUser = new User("apparent")
 		currentUser = new CurrentUser(realUser, fakeUser)
 	}
 
-	@Test def testingJobTest {
+	@Test def testingJobTest() {
 		Transactions.disable {
 			dao.findOutstandingInstances(10).size should be (0)
 			val id = service.add(Some(currentUser), TestingJob("Magic")).id
 			dao.findOutstandingInstances(10).size should be (1)
 			val myInstance = service.getInstance(id).get
-			myInstance.started should be (false)
-			service.run
-			myInstance.finished should be (true)
-			myInstance.succeeded should be (true)
+			myInstance.started should be (right = false)
+			service.run()
+			myInstance.finished should be (right = true)
+			myInstance.succeeded should be (right = true)
 			dao.findOutstandingInstances(10).size should be (0)
 		}
 	}
