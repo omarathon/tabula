db3831c2a237f497ad9539cb5c03f2ab4ad6f47b
Sarah O'Toole <sarah.o-toole@warwick.ac.uk>
2015-09-04 11:15:30 +0100

    TAB-3561 - LTI conformance testing

---
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/commands/coursework/turnitinlti/TurnitinLtiConformanceOutcomesServiceCommand.scala
@@ -0,0 +1,202 @@
+package uk.ac.warwick.tabula.commands.coursework.turnitinlti
+
+import org.springframework.validation.Errors
+import uk.ac.warwick.tabula.commands._
+import uk.ac.warwick.tabula.data.model.Assignment
+import uk.ac.warwick.tabula.helpers.Logging
+import uk.ac.warwick.tabula.system.permissions.PubliclyVisiblePermissions
+import uk.ac.warwick.tabula.services.{AssessmentServiceComponent, AutowiringAssessmentServiceComponent}
+import scala.xml.Elem
+
+
+object TurnitinLtiConformanceOutcomesServiceCommand {
+	def apply(assignment: Assignment) =
+		new TurnitinLtiConformanceOutcomesServiceCommandInternal(assignment)
+			with ComposableCommand[TurnitinLtiConformanceOutcomesServiceCommandResponse]
+			with TurnitinLtiConformanceOutcomesServiceValidation
+			with AutowiringAssessmentServiceComponent
+			with Unaudited with ReadOnly with Logging with PubliclyVisiblePermissions
+			with TurnitinLtiConformanceOutcomesServiceCommandState
+		}
+
+abstract class TurnitinLtiConformanceOutcomesServiceCommandInternal(val assignment: Assignment)
+	extends CommandInternal[TurnitinLtiConformanceOutcomesServiceCommandResponse]
+		with TurnitinLtiConformanceOutcomesServiceRequest with AssessmentServiceComponent {
+
+	self: Logging with TurnitinLtiConformanceOutcomesServiceCommandState =>
+
+	def applyInternal(): TurnitinLtiConformanceOutcomesServiceCommandResponse = {
+
+		logger.info("body: " + body)
+
+		val incomingXml = scala.xml.XML.loadString(body)
+
+		val replaceResultRequest = (incomingXml \\ "replaceResultRequest").text
+		val readResultRequest = (incomingXml \\ "readResultRequest").text
+
+		if (replaceResultRequest.nonEmpty) applyReplaceResultRequest(incomingXml)
+		else if (readResultRequest.nonEmpty) applyReadResultRequest(incomingXml)
+		else applyDeleteResultRequest(incomingXml)
+
+	}
+
+	private def applyDeleteResultRequest(incomingXml: Elem): TurnitinLtiConformanceOutcomesServiceCommandResponse = {
+		val sourcedId = (incomingXml \\ "sourcedId").text
+
+		val assignment = assessmentService.getAssignmentById(sourcedId).get
+
+		val imsx_codeMajor = {
+			if ((incomingXml \\ "deleteResultRequest").text.nonEmpty) {
+				assignment.turnitinId = ""
+				"success"
+			}
+			else "unsupported"
+		}
+
+		val messageIdentifier = (incomingXml \\ "imsx_messageIdentifier").text
+
+		val xmlresponsestring = s"""
+			<?xml version="1.0" encoding="utf-8"?>
+			<imsx_POXEnvelopeResponse xmlns = "http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0">
+				<imsx_POXHeader>
+					<imsx_POXResponseHeaderInfo>
+						<imsx_version>V1.0</imsx_version>
+						<imsx_messageIdentifier>$messageIdentifier-message</imsx_messageIdentifier>
+						<imsx_statusInfo>
+							<imsx_codeMajor>$imsx_codeMajor</imsx_codeMajor>
+							<imsx_severity>status</imsx_severity>
+							<imsx_messageRefIdentifier>$messageIdentifier</imsx_messageRefIdentifier>
+							<imsx_operationRefIdentifier>deleteResult</imsx_operationRefIdentifier>
+						</imsx_statusInfo>
+					</imsx_POXResponseHeaderInfo>
+				</imsx_POXHeader>
+				<imsx_POXBody>
+					<deleteResultResponse/>
+				</imsx_POXBody>
+			</imsx_POXEnvelopeResponse>
+			"""
+
+		TurnitinLtiConformanceOutcomesServiceCommandResponse(xmlresponsestring)
+	}
+
+	private def applyReadResultRequest(incomingXml: Elem): TurnitinLtiConformanceOutcomesServiceCommandResponse = {
+		val sourcedId = (incomingXml \\ "sourcedId").text
+
+		val assignment = assessmentService.getAssignmentById(sourcedId).get
+		val score = {
+			if (assignment.turnitinId !=null && assignment.turnitinId.nonEmpty) assignment.turnitinId
+			else ""
+		}
+
+		val imsx_codeMajor = {
+			if ((incomingXml \\ "readResultRequest").text.nonEmpty) "success"
+			else "unsupported"
+		}
+
+		val messageIdentifier = (incomingXml \\ "imsx_messageIdentifier").text
+		val version = (incomingXml \\ "imsx_version").text
+
+		val xmlresponsestring = s"""
+			<?xml version="1.0" encoding="utf-8"?>
+			<imsx_POXEnvelopeResponse xmlns = "http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0">
+				<imsx_POXHeader>
+					<imsx_POXResponseHeaderInfo>
+						<imsx_version>$version</imsx_version>
+						<imsx_messageIdentifier>$messageIdentifier-message</imsx_messageIdentifier>
+						<imsx_statusInfo>
+							<imsx_codeMajor>$imsx_codeMajor</imsx_codeMajor>
+							<imsx_severity>status</imsx_severity>
+							<imsx_description>Result read</imsx_description>
+							<imsx_messageRefIdentifier>$messageIdentifier</imsx_messageRefIdentifier>
+							<imsx_operationRefIdentifier>readResult</imsx_operationRefIdentifier>
+						</imsx_statusInfo>
+					</imsx_POXResponseHeaderInfo>
+				</imsx_POXHeader>
+				<imsx_POXBody>
+					<readResultResponse>
+						<result>
+							<resultScore>
+								<language>en</language>
+								<textString>$score</textString>
+							</resultScore>
+						</result>
+					</readResultResponse>
+				</imsx_POXBody>
+			</imsx_POXEnvelopeResponse>
+			"""
+
+		TurnitinLtiConformanceOutcomesServiceCommandResponse(xmlresponsestring)
+	}
+
+	private def applyReplaceResultRequest(incomingXml: Elem): TurnitinLtiConformanceOutcomesServiceCommandResponse = {
+		val sourcedId = (incomingXml \\ "sourcedId").text
+
+		val assignment = assessmentService.getAssignmentById(sourcedId).get
+
+		val score = (incomingXml \\ "textString").text
+		val messageIdentifier = (incomingXml \\ "imsx_messageIdentifier").text
+		val version = (incomingXml \\ "imsx_version").text
+
+		val (imsx_codeMajor, score_double) =
+			try {
+				score.toDouble match {
+					case s if 0<=s && s<= 1 => {
+						assignment.turnitinId = score
+						("success", score)
+					}
+					case s if 0>s || s>1  => ("failure", -1)
+					case _ => ("unsupported", -1)
+				}} catch {
+				case e: NumberFormatException => ("failure", -1)
+			}
+
+		val xmlresponsestring = s"""
+			<?xml version="1.0" encoding="utf-8"?>
+			<imsx_POXEnvelopeResponse xmlns = "http://www.imsglobal.org/services/ltiv1p1/xsd/imsoms_v1p0">
+				<imsx_POXHeader>
+					<imsx_POXResponseHeaderInfo>
+						<imsx_version>${version}</imsx_version>
+						<imsx_messageIdentifier>${messageIdentifier}-message</imsx_messageIdentifier>
+						<imsx_statusInfo>
+							<imsx_codeMajor>${imsx_codeMajor}</imsx_codeMajor>
+							<imsx_severity>status</imsx_severity>
+							<imsx_description>Score for ${sourcedId} is now ${score_double}</imsx_description>
+							<imsx_messageRefIdentifier>${messageIdentifier}</imsx_messageRefIdentifier>
+							<imsx_operationRefIdentifier>replaceResult</imsx_operationRefIdentifier>
+						</imsx_statusInfo>
+					</imsx_POXResponseHeaderInfo>
+				</imsx_POXHeader>
+				<imsx_POXBody>
+					<replaceResultResponse/>
+				</imsx_POXBody>
+			</imsx_POXEnvelopeResponse>
+			"""
+
+		TurnitinLtiConformanceOutcomesServiceCommandResponse(xmlresponsestring)
+	}
+
+}
+
+trait TurnitinLtiConformanceOutcomesServiceState {
+	val assignment: Assignment
+}
+
+trait TurnitinLtiConformanceOutcomesServiceRequest extends TurnitinLtiConformanceOutcomesServiceState
+	with CurrentSITSAcademicYear
+
+
+trait TurnitinLtiConformanceOutcomesServiceValidation extends SelfValidating {
+	self: TurnitinLtiConformanceOutcomesServiceRequest =>
+
+	override def validate(errors: Errors) {
+		if (assignment == null) {
+			errors.rejectValue("assignment", "NotEmpty")
+		}
+	}
+}
+
+trait TurnitinLtiConformanceOutcomesServiceCommandState {
+	var body: String = _
+}
+
+case class TurnitinLtiConformanceOutcomesServiceCommandResponse(xmlString: String)
