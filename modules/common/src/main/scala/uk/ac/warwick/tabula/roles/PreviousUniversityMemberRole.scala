d62968b173cd3845f6e9dd7044decf74643e31a3
Matthew Jones <Matthew.Jones@warwick.ac.uk>
2015-11-09 16:38:56 +0000

    TAB-3805

---
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/roles/PreviousUniversityMemberRole.scala
@@ -0,0 +1,52 @@
+package uk.ac.warwick.tabula.roles
+
+import uk.ac.warwick.tabula.data._
+import uk.ac.warwick.tabula.permissions.Permissions._
+import uk.ac.warwick.tabula.permissions.PermissionsSelector
+import uk.ac.warwick.tabula.data.model.StudentRelationshipType
+
+case class PreviousUniversityMemberRole(member: model.Member) extends BuiltInRole(PreviousUniversityMemberRoleDefinition, member)
+
+case object PreviousUniversityMemberRoleDefinition extends UnassignableBuiltInRoleDefinition {
+
+	override def description = "Previous University Member"
+
+	GrantsScopedPermission(
+		Profiles.Read.Core,
+		Profiles.Read.NextOfKin,
+		Profiles.Read.HomeAndTermTimeAddresses,
+		Profiles.Read.TelephoneNumber,
+		Profiles.Read.MobileNumber,
+		Profiles.Read.Usercode,
+		Profiles.Read.StudentCourseDetails.Core,
+		Profiles.Read.StudentCourseDetails.Status,
+		Profiles.Read.SmallGroups,
+		Profiles.Read.Coursework,
+		Profiles.Read.Timetable,
+		Profiles.Read.TimetablePrivateFeed,
+		Profiles.Read.Disability,
+
+		MemberNotes.Read,
+		Profiles.Read.ModuleRegistration.Core,
+		Profiles.Read.ModuleRegistration.Results,
+
+    Profiles.Read.RelationshipStudents(PermissionsSelector.Any[StudentRelationshipType]),
+
+    Profiles.StudentRelationship.Read(PermissionsSelector.Any[StudentRelationshipType]),
+
+    Profiles.MeetingRecord.Read(PermissionsSelector.Any[StudentRelationshipType]),
+    Profiles.MeetingRecord.ReadDetails(PermissionsSelector.Any[StudentRelationshipType]),
+
+		MonitoringPoints.View,
+		SmallGroupEvents.ViewRegister,
+
+		// Can read own coursework info
+		Submission.Read,
+		AssignmentFeedback.Read,
+		Extension.Read
+	)
+
+	GrantsScopelessPermission(
+		UserPicker
+	)
+}
--- modules/common/src/main/scala/uk/ac/warwick/tabula/roles/UniversityMemberRole.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/roles/UniversityMemberRole.scala
@@ -4,6 +4,7 @@ import uk.ac.warwick.tabula.data._
 import uk.ac.warwick.tabula.permissions.Permissions._
 import uk.ac.warwick.tabula.permissions.PermissionsSelector
 import uk.ac.warwick.tabula.data.model.StudentRelationshipType
+import uk.ac.warwick.tabula.roles.ModuleManagerRoleDefinition._
 
 case class UniversityMemberRole(member: model.Member) extends BuiltInRole(UniversityMemberRoleDefinition, member)
 
@@ -11,46 +12,16 @@ case object UniversityMemberRoleDefinition extends UnassignableBuiltInRoleDefini
 
 	override def description = "University Member"
 
+	GeneratesSubRole(PreviousUniversityMemberRoleDefinition)
+
 	// As per discussion in TAB-753, anyone at the University can see anyone else's core information
 	GrantsGlobalPermission(
 		Profiles.Read.Core
 	)
 
 	GrantsScopedPermission(
-		Profiles.Read.NextOfKin,
-		Profiles.Read.HomeAndTermTimeAddresses,
-		Profiles.Read.TelephoneNumber,
-		Profiles.Read.MobileNumber,
-		Profiles.Read.Usercode,
-		Profiles.Read.StudentCourseDetails.Core,
-		Profiles.Read.StudentCourseDetails.Status,
-		Profiles.Read.SmallGroups,
-		Profiles.Read.Coursework,
-		Profiles.Read.Timetable,
-		Profiles.Read.TimetablePrivateFeed,
-		Profiles.Read.Disability,
-
-		MemberNotes.Read,
-		Profiles.Read.ModuleRegistration.Core,
-		Profiles.Read.ModuleRegistration.Results,
-
-    Profiles.Read.RelationshipStudents(PermissionsSelector.Any[StudentRelationshipType]),
-
-    Profiles.StudentRelationship.Read(PermissionsSelector.Any[StudentRelationshipType]),
-
-    Profiles.MeetingRecord.Read(PermissionsSelector.Any[StudentRelationshipType]),
-    Profiles.MeetingRecord.ReadDetails(PermissionsSelector.Any[StudentRelationshipType]),
     Profiles.MeetingRecord.Manage(PermissionsSelector.Any[StudentRelationshipType]),
-
-		Profiles.ScheduledMeetingRecord.Manage(PermissionsSelector.Any[StudentRelationshipType]),
-
-		MonitoringPoints.View,
-		SmallGroupEvents.ViewRegister,
-
-		// Can read own coursework info
-		Submission.Read,
-		AssignmentFeedback.Read,
-		Extension.Read
+		Profiles.ScheduledMeetingRecord.Manage(PermissionsSelector.Any[StudentRelationshipType])
 	)
 
 	GrantsScopelessPermission(
