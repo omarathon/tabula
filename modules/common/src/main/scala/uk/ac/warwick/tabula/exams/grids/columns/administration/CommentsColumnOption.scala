e03a9b5b778fdb690aa6d5d78195403394ba25c8
Matthew Jones <matthew.jones@warwick.ac.uk>
2016-01-14 12:24:13 +0000

    TAB-4007 - Fix merge

--- modules/common/src/main/scala/uk/ac/warwick/tabula/exams/grids/columns/administration/CommentsColumnOption.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/exams/grids/columns/administration/CommentsColumnOption.scala
@@ -2,9 +2,8 @@ package uk.ac.warwick.tabula.exams.grids.columns.administration
 
 import org.apache.poi.xssf.usermodel.{XSSFCellStyle, XSSFRow}
 import org.springframework.stereotype.Component
-import uk.ac.warwick.tabula.commands.exams.grids.GenerateExamGridExporter
-import uk.ac.warwick.tabula.data.model.StudentCourseYearDetails
-import uk.ac.warwick.tabula.exams.grids.columns.{HasExamGridColumnCategory, ExamGridColumn, ExamGridColumnOption}
+import uk.ac.warwick.tabula.commands.exams.grids.{GenerateExamGridEntity, GenerateExamGridExporter}
+import uk.ac.warwick.tabula.exams.grids.columns.{ExamGridColumn, ExamGridColumnOption, HasExamGridColumnCategory}
 
 @Component
 class CommentsColumnOption extends ExamGridColumnOption {
@@ -13,19 +12,19 @@ class CommentsColumnOption extends ExamGridColumnOption {
 
 	override val sortOrder: Int = 20
 
-	case class Column(scyds: Seq[StudentCourseYearDetails]) extends ExamGridColumn(scyds) with HasExamGridColumnCategory {
+	case class Column(entities: Seq[GenerateExamGridEntity]) extends ExamGridColumn(entities) with HasExamGridColumnCategory {
 
 		override val title: String = "Comments"
 
 		override val category: String = "Administration"
 
 		override def render: Map[String, String] =
-			scyds.map(scyd => scyd.id -> "").toMap
+			entities.map(entity => entity.id -> "").toMap
 
 		override def renderExcelCell(
 			row: XSSFRow,
 			index: Int,
-			scyd: StudentCourseYearDetails,
+			entity: GenerateExamGridEntity,
 			cellStyleMap: Map[GenerateExamGridExporter.Style, XSSFCellStyle]
 		): Unit = {
 			row.createCell(index)
@@ -33,6 +32,6 @@ class CommentsColumnOption extends ExamGridColumnOption {
 
 	}
 
-	override def getColumns(scyds: Seq[StudentCourseYearDetails]): Seq[ExamGridColumn] = Seq(Column(scyds))
+	override def getColumns(entities: Seq[GenerateExamGridEntity]): Seq[ExamGridColumn] = Seq(Column(entities))
 
 }
