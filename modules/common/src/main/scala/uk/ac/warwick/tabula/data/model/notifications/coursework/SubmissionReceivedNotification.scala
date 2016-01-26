6ad5f26f98e850de623a9d719b5cd5d0e21e1aee
Mat Mannion <m.mannion@warwick.ac.uk>
2016-01-26 11:24:53 +0000

    Cope with a null marker usercode for submission received notifications

--- modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/coursework/SubmissionReceivedNotification.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/coursework/SubmissionReceivedNotification.scala
@@ -82,8 +82,11 @@ class SubmissionReceivedNotification extends SubmissionNotification {
 
 		// Contact the current marker, if there is one, and the submission has already been released
 		val feedback = assignment.findFeedback(submission.universityId)
-		val currentMarker = if (assignment.hasWorkflow && feedback.isDefined && feedback.exists(_.isPlaceholder)) {
-			feedback.get.getCurrentWorkflowFeedback.map(mf => Seq(mf.getMarkerUser)).getOrElse(Seq())
+		val currentMarker = if (assignment.hasWorkflow && feedback.exists(_.isPlaceholder)) {
+			feedback.flatMap { f => f.getCurrentWorkflowFeedback }
+				.filter { _.getMarkerUsercode.isDefined }
+				.map { _.getMarkerUser }
+				.toSeq
 		} else {
 			Seq()
 		}
