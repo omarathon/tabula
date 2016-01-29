0af882e8b16237888aa27e32f4a8ce025747bda5
Mat Mannion <m.mannion@warwick.ac.uk>
2016-01-29 17:45:55 +0000

    Make ObjectStorageMigration job much faster

--- modules/scheduling/src/main/scala/uk/ac/warwick/tabula/scheduling/commands/ObjectStorageMigrationCommand.scala
+++ modules/scheduling/src/main/scala/uk/ac/warwick/tabula/scheduling/commands/ObjectStorageMigrationCommand.scala
@@ -24,7 +24,7 @@ object ObjectStorageMigrationCommand {
 			with Unaudited with ReadOnly
 }
 
-class ObjectStorageMigrationCommandInternal extends CommandInternal[Set[String]] with Logging {
+class ObjectStorageMigrationCommandInternal extends CommandInternal[Set[String]] with TaskBenchmarking {
 	self: ObjectStorageServiceComponent with FileDaoComponent with FileHasherComponent =>
 
 	override def applyInternal(): Set[String] = objectStorageService match {
@@ -32,19 +32,24 @@ class ObjectStorageMigrationCommandInternal extends CommandInternal[Set[String]]
 			val defaultStore = legacyAware.defaultService
 			val legacyStore = legacyAware.legacyService
 
-			val allKeys = fileDao.getAllFileIds(None)
+			val legacyKeys = benchmarkTask("Get all FileAttachment IDs") { fileDao.getAllFileIds(None) }
+			val defaultKeys = benchmarkTask("Get all object store keys") { defaultStore.listKeys().toSet }
 
-			allKeys.toStream.filterNot(defaultStore.keyExists).take(BatchSize).flatMap { key =>
-				for {
-					metadata <- legacyStore.metadata(key)
-					dataStream <- legacyStore.fetch(key)
-					fileHash <- legacyStore.fetch(key).map(fileHasher.hash) // Intentionally a different stream to above
-				} yield {
-					logger.info(s"Migrating blob (size: ${metadata.contentLength} bytes) for key $key to default store")
-					defaultStore.push(key, dataStream, metadata.copy(fileHash = Some(fileHash)))
-					key
+			val keysToMigrate = legacyKeys -- defaultKeys
+
+			benchmarkTask(s"Migrate $BatchSize keys") {
+				keysToMigrate.take(BatchSize).flatMap { key =>
+					for {
+						metadata <- legacyStore.metadata(key)
+						dataStream <- legacyStore.fetch(key)
+						fileHash <- legacyStore.fetch(key).map(fileHasher.hash) // Intentionally a different stream to above
+					} yield {
+						logger.info(s"Migrating blob (size: ${metadata.contentLength} bytes) for key $key to default store")
+						defaultStore.push(key, dataStream, metadata.copy(fileHash = Some(fileHash)))
+						key
+					}
 				}
-			}.toSet
+			}
 		case _ =>
 			logger.warn("No legacy aware object storage service found - can this be removed?")
 			Set.empty
