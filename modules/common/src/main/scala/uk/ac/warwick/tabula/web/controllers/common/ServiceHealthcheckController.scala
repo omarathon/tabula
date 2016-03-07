ebdcf40e17d3d9fe2859a30f939f410743ccd171
Mat Mannion <m.mannion@warwick.ac.uk>
2016-03-07 13:32:58 +0000

    Don't return service healthchecks that are stale

--- modules/common/src/main/scala/uk/ac/warwick/tabula/web/controllers/common/ServiceHealthcheckController.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/web/controllers/common/ServiceHealthcheckController.scala
@@ -1,5 +1,6 @@
 package uk.ac.warwick.tabula.web.controllers.common
 
+import org.joda.time.DateTime
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.{HttpStatus, MediaType, ResponseEntity}
 import org.springframework.stereotype.Controller
@@ -15,8 +16,11 @@ class ServiceHealthcheckController {
 
 	@RequestMapping(value = Array("/service/healthcheck"), produces = Array(MediaType.APPLICATION_JSON_VALUE))
 	@ResponseBody def healthcheck: ResponseEntity[String] = {
+		// We don't return any checks older than 15 minutes, which will force the monitoring check into UNKNOWN
+		val fifteenMinutesAgo = DateTime.now.minusMinutes(15)
+
 		val json = Map(
-			"data" -> healthcheckProviders.flatMap(_.latest).map(_.asMap)
+			"data" -> healthcheckProviders.flatMap(_.latest).filter(_.testedAt.isAfter(fifteenMinutesAgo)).map(_.asMap)
 		)
 
 		new ResponseEntity[String](JsonObjectMapperFactory.instance.writeValueAsString(json), HttpStatus.OK)
