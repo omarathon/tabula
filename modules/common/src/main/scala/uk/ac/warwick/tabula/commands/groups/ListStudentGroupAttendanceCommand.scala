e3eeb9e07148e2d3e54e132b1edc7dab748ebbc9
Archana Mathur <archana.mathur@warwick.ac.uk>
2016-05-10 10:49:35 +0100

    TAB-4306 - Display attendance record only when published

--- modules/common/src/main/scala/uk/ac/warwick/tabula/commands/groups/ListStudentGroupAttendanceCommand.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/commands/groups/ListStudentGroupAttendanceCommand.scala
@@ -78,11 +78,12 @@ class ListStudentGroupAttendanceCommandInternal(val member: Member, val academic
 
 		val attendance = (groupByTerm(allInstances).mapValues { instances =>
 			val groups = SortedMap(instances.groupBy { case ((event, _), _) => event.group }.toSeq:_*)
-			groups.mapValues { instances =>
-				SortedMap(instances.groupBy { case ((_, week), _) => week }.toSeq:_*).mapValues { instances =>
-					attendanceForStudent(instances, isLate(user))(user)
-				}
-			}.filter(hasExpectedAttendanceForGroup)
+			groups.filterKeys { smallGroup =>
+				smallGroup.groupSet.visibleToStudents }.mapValues { instances =>
+					SortedMap(instances.groupBy { case ((_, week), _) => week }.toSeq:_*).mapValues { instances =>
+						attendanceForStudent(instances, isLate(user))(user)
+					}
+				}.filter(hasExpectedAttendanceForGroup)
 		}).filterNot { case (term, attendance) => attendance.isEmpty }
 
 		val missedCountByTerm = attendance.mapValues { groups =>
