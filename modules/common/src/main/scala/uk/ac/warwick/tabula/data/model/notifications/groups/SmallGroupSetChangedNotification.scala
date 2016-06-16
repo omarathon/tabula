4a3608a724ce4e5ee87052b4c2b6027687134203
Archana Mathur <archana.mathur@warwick.ac.uk>
2016-06-16 10:02:31 +0100

    TAB-3820- Old group size info added in the notification email

--- modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/groups/SmallGroupSetChangedNotification.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/groups/SmallGroupSetChangedNotification.scala
@@ -20,12 +20,15 @@ abstract class SmallGroupSetChangedNotification(recipientRole: UserRoleOnGroup)
 
 	def title = "%s: Your %s allocation has changed".format(target.entity.module.code.toUpperCase, target.entity.format.description.toLowerCase)
 
+	@transient val oldSmallGroupSizes = StringMapSetting("oldSmallGroupSizes", Map())
+
+
 	def content =
 		FreemarkerModel(SmallGroupSetChangedNotification.templateLocation, Map(
 			"groups" -> entities,
 			"groupSet" -> target.entity,
 			"profileUrl" -> url
-		))
+		) ++ extraModel)
 
 	def url: String = {
 		recipientRole match {
@@ -36,6 +39,7 @@ abstract class SmallGroupSetChangedNotification(recipientRole: UserRoleOnGroup)
 
 	def urlTitle = "view this small group"
 
+	def extraModel: Map[String, Any] = Map()
 }
 
 @Entity
@@ -44,7 +48,17 @@ class SmallGroupSetChangedStudentNotification extends SmallGroupSetChangedNotifi
 
 @Entity
 @DiscriminatorValue(value="SmallGroupSetChangedTutor")
-class SmallGroupSetChangedTutorNotification extends SmallGroupSetChangedNotification(UserRoleOnGroup.Tutor)
+class SmallGroupSetChangedTutorNotification extends SmallGroupSetChangedNotification(UserRoleOnGroup.Tutor) {
+
+	private def changedGroupInfo = {
+		entities.map { newSmallGroup =>
+			(newSmallGroup, oldSmallGroupSizes.value.get(newSmallGroup.id).getOrElse("0")) }
+		}
+
+	override  def extraModel = Map(
+		"groupsWithOldSizeInfo" -> changedGroupInfo
+	)
+}
 
 sealed trait UserRoleOnGroup
 object UserRoleOnGroup {
--- modules/common/src/main/webapp/WEB-INF/freemarker/notifications/groups/small_group_modified_notification.ftl
+++ modules/common/src/main/webapp/WEB-INF/freemarker/notifications/groups/small_group_modified_notification.ftl
@@ -1,5 +1,13 @@
 Your small teaching group allocation for ${groupSet.module.code?upper_case} - "${groupSet.module.name?trim}" has been updated
 
+<#if groupsWithOldSizeInfo??>
+<#list groupsWithOldSizeInfo as changedGroupInfo>
+<#assign newGroup = changedGroupInfo._1() />
+<#assign oldGroupSize = changedGroupInfo._2() />
+${newGroup.name} has changed  from - ${oldGroupSize} to ${newGroup.students.members?size}
+</#list>
+</#if>
+
 ${groupSet.name} ${groupSet.format.description} for ${groupSet.module.code?upper_case} - ${groupSet.module.name}
 
 <#list groups as group>
