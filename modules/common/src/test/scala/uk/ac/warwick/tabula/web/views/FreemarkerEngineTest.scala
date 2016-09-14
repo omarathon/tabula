f374e740a24d64921b26c69220b794aa292d5490
Sarah O'Toole <s.o-toole@warwick.ac.uk>
2016-09-14 16:36:03 +0100

    TAB-4536 - fix tests

--- modules/common/src/test/scala/uk/ac/warwick/tabula/web/views/FreemarkerEngineTest.scala
+++ modules/common/src/test/scala/uk/ac/warwick/tabula/web/views/FreemarkerEngineTest.scala
@@ -1,22 +1,20 @@
 package uk.ac.warwick.tabula.web.views
-import uk.ac.warwick.tabula.TestBase
-import org.junit.Test
-import freemarker.cache.ClassTemplateLoader
+import uk.ac.warwick.tabula.AppContextTestBase
 import java.io.StringWriter
-import collection.JavaConversions._
+
 import org.junit.Before
 import freemarker.template.Configuration
 import uk.ac.warwick.tabula.data.model.Department
-import org.junit.Ignore
 import org.joda.time.Duration
+
 import scala.util.Properties
 import uk.ac.warwick.tabula.web.Routes
 
-class FreemarkerEngineTest extends TestBase {
+class FreemarkerEngineTest extends AppContextTestBase {
 	var configuration:Configuration = _
 
-	@Before def setup {
-		configuration = newFreemarkerConfiguration
+	@Before def setup() {
+		configuration = newFreemarkerConfiguration()
 	}
 
 	def render(template:String, map:Map[String,Any]) = {
@@ -25,7 +23,7 @@ class FreemarkerEngineTest extends TestBase {
 		writer.toString
 	}
 
-	@Test def plain {
+	@Test def plain() {
 		val num:Option[Duration] = Some(Duration.standardSeconds(1))
 		val output = render("plain.ftl", Map(
 				"specifiedValue" -> "Specified value.",
@@ -38,14 +36,14 @@ class FreemarkerEngineTest extends TestBase {
 	 * Accessing the Routes object from FTL now works as this test demonstrates,
 	 * but at the time of writing we don't use it. It could replace the routes.ftl
 	 */
-	@Test def routes {
+	@Test def routes() {
 		val department = new Department
 		department.code = "la"
 		val output = render("renderroutes.ftl", Map("Routes"->Routes, "department" -> department))
 		output should be ("The path to department LA is /coursework/admin/department/la/")
 	}
 
-	@Test def someAndNon {
+	@Test def someAndNon() {
 		val output = render("somenone.ftl", Map(
 			"some" -> Some("CERBERUS"),
 			"none" -> None
@@ -53,7 +51,7 @@ class FreemarkerEngineTest extends TestBase {
 		output should be ("Robot=CERBERUS;")
 	}
 
-  @Test def usesMacros{
+  @Test def usesMacros() {
     val output=render("uses_macros.ftl",Map("count"->3))
     output should be ("First line"+Properties.lineSeparator+"3 wombats")
   }
