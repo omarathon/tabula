2221a072e7fa53d6f9c969df85b6885fd326ceb8
Matthew Jones <matthew.jones@warwick.ac.uk>
2017-03-06 10:52:54 +0000

    TAB-4930

--- modules/common/src/main/scala/uk/ac/warwick/tabula/web/controllers/common/AppVersionController.scala
+++ modules/common/src/main/scala/uk/ac/warwick/tabula/web/controllers/common/AppVersionController.scala
@@ -9,11 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping
 class AppVersionController {
 
 	@Value("${build.time}") var buildTime: String = _
-  @Value("${git.commit}") var gitSha:String= _
-	@Value("${git.describe}") var gitDesc:String = _
 
 	@RequestMapping(Array("/version"))
-	def showTime = new ResponseEntity("%s - %s - %s".format(buildTime, gitSha, gitDesc), plainTextHeaders, HttpStatus.OK)
+	def showTime = new ResponseEntity(buildTime, plainTextHeaders, HttpStatus.OK)
 
 	def plainTextHeaders = new HttpHeaders {
 		setContentType(MediaType.TEXT_PLAIN)
