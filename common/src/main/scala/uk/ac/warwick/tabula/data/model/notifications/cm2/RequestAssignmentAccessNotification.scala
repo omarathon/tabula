2ffa3a00c923b4d1894cd2249126c21626f52be4
Cerys Lewis <C.Lewis.3@warwick.ac.uk>
2017-05-02 15:48:18 +0100

    TAB-5002 - Request access

--- common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/RequestAssignmentAccessNotification.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/RequestAssignmentAccessNotification.scala
@@ -1,8 +1,8 @@
-package uk.ac.warwick.tabula.data.model.notifications.coursework
+package uk.ac.warwick.tabula.data.model.notifications.cm2
 
 import javax.persistence.{DiscriminatorValue, Entity}
 
-import uk.ac.warwick.tabula.coursework.web.Routes
+import uk.ac.warwick.tabula.cm2.web.Routes
 import uk.ac.warwick.tabula.data.model.NotificationPriority.Warning
 import uk.ac.warwick.tabula.data.model._
 import uk.ac.warwick.tabula.helpers.StringUtils._
@@ -24,7 +24,7 @@ class RequestAssignmentAccessNotification
 		"student" -> agent)
 	)
 
-	def url: String = Routes.admin.assignment.edit(assignment)
+	def url: String = Routes.admin.assignment.editAssignmentDetails(assignment)
 	def urlTitle = "review which students are enrolled on the assignment"
 
 	def recipients: Seq[User] = assignment.module.adminDepartment.owners.users
---
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/coursework/OldRequestAssignmentAccessNotification.scala
@@ -0,0 +1,35 @@
+package uk.ac.warwick.tabula.data.model.notifications.coursework
+
+import javax.persistence.{DiscriminatorValue, Entity}
+
+import uk.ac.warwick.tabula.coursework.web.Routes
+import uk.ac.warwick.tabula.data.model.NotificationPriority.Warning
+import uk.ac.warwick.tabula.data.model._
+import uk.ac.warwick.tabula.helpers.StringUtils._
+import uk.ac.warwick.userlookup.User
+
+@Entity
+@DiscriminatorValue("OldRequestAssignmentAccess")
+class OldRequestAssignmentAccessNotification
+	extends Notification[Assignment, Unit]
+	with SingleItemNotification[Assignment] with AllCompletedActionRequiredNotification {
+
+	def assignment: Assignment = item.entity
+
+	def verb = "request"
+	def title: String = "%s: Access requested for \"%s\"".format(assignment.module.code.toUpperCase, assignment.name)
+
+	def content = FreemarkerModel("/WEB-INF/freemarker/emails/requestassignmentaccess.ftl", Map(
+		"assignment" -> assignment,
+		"student" -> agent)
+	)
+
+	def url: String = Routes.admin.assignment.edit(assignment)
+	def urlTitle = "review which students are enrolled on the assignment"
+
+	def recipients: Seq[User] = assignment.module.adminDepartment.owners.users
+		.filter(admin => admin.isFoundUser && admin.getEmail.hasText).toSeq
+
+	priority = Warning
+
+}
