1e6a73f4855d76ec7e4bdbc89016f7e3a3186093
Mat Mannion <m.mannion@warwick.ac.uk>
2017-05-11 15:15:12 +0100

    Re-work to fix tests

--- common/src/main/scala/uk/ac/warwick/tabula/data/model/forms/FormattedHtml.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/forms/FormattedHtml.scala
@@ -1,12 +1,12 @@
 package uk.ac.warwick.tabula.data.model.forms
 
-import uk.ac.warwick.tabula.web.views.MarkdownRendererImpl
+import uk.ac.warwick.tabula.web.views.MarkdownRenderer
 
-trait FormattedHtml extends MarkdownRendererImpl {
+trait FormattedHtml {
 	/**
 	 * Return a formatted version of the text that can be inserted
 	 * WITHOUT escaping.
 	 */
-	def formattedHtml(string: Option[String]): String = string.map { s => renderMarkdown(scala.xml.Utility.escape(s)) }.getOrElse("")
+	def formattedHtml(string: Option[String]): String = string.map { s => MarkdownRenderer.renderMarkdown(scala.xml.Utility.escape(s)) }.getOrElse("")
 	def formattedHtml(string: String): String = formattedHtml(Option(string))
 }
--- common/src/main/scala/uk/ac/warwick/tabula/web/views/MarkdownRenderer.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/web/views/MarkdownRenderer.scala
@@ -3,7 +3,6 @@ package uk.ac.warwick.tabula.web.views
 import com.vladsch.flexmark.ext.autolink.AutolinkExtension
 import com.vladsch.flexmark.html.HtmlRenderer
 import com.vladsch.flexmark.parser.Parser
-
 import uk.ac.warwick.tabula.JavaImports._
 
 trait MarkdownRenderer {
@@ -11,9 +10,13 @@ trait MarkdownRenderer {
 }
 
 trait MarkdownRendererImpl extends MarkdownRenderer {
+	override def renderMarkdown(source: String): String = MarkdownRenderer.renderMarkdown(source)
+}
+
+object MarkdownRenderer {
 	private val markdownParser = Parser.builder().extensions(JList(AutolinkExtension.create())).build()
 	private val markdownRenderer = HtmlRenderer.builder().build()
 
-	override def renderMarkdown(source: String): String =
+	def renderMarkdown(source: String): String =
 		markdownRenderer.render(markdownParser.parse(source))
 }
