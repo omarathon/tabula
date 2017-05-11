f1d1519cdb722a0560a3eac2e79c86027c0f6e0d
Mat Mannion <m.mannion@warwick.ac.uk>
2017-05-11 12:54:51 +0100

    Allow markdown in formatted HTML fields

--- common/src/main/scala/uk/ac/warwick/tabula/data/model/forms/FormattedHtml.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/forms/FormattedHtml.scala
@@ -1,17 +1,12 @@
 package uk.ac.warwick.tabula.data.model.forms
 
-import scala.xml.NodeSeq
+import uk.ac.warwick.tabula.web.views.MarkdownRendererImpl
 
-trait FormattedHtml {
+trait FormattedHtml extends MarkdownRendererImpl {
 	/**
 	 * Return a formatted version of the text that can be inserted
 	 * WITHOUT escaping.
 	 */
-	def formattedHtml(string: Option[String]): String = string map { raw =>
-		val Splitter = """\s*\n(\s*\n)+\s*""".r // two+ newlines, with whitespace
-		val nodes = Splitter.split(raw).map { p => <p>{ p }</p> }
-		(NodeSeq fromSeq nodes).toString
-	} getOrElse ("")
-
+	def formattedHtml(string: Option[String]): String = string.map { s => renderMarkdown(scala.xml.Utility.escape(s)) }.getOrElse("")
 	def formattedHtml(string: String): String = formattedHtml(Option(string))
 }
