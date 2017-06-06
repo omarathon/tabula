2d2a29aa094782ff6dd15d7369ecfdb44947a9b5
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2017-06-06 12:01:59 +0100

    TAB-5187 - Add headers to prevent ajax caching in IE11

--- common/src/main/scala/uk/ac/warwick/tabula/web/controllers/BaseController.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/web/controllers/BaseController.scala
@@ -7,9 +7,11 @@ import org.springframework.context.MessageSource
 import org.springframework.stereotype.Controller
 import org.springframework.validation.Validator
 import org.springframework.web.bind.WebDataBinder
-import org.springframework.web.bind.annotation.InitBinder
+import org.springframework.web.bind.annotation.{InitBinder, ModelAttribute}
 import javax.annotation.Resource
-import uk.ac.warwick.tabula.{PermissionDeniedException, CurrentUser, ItemNotFoundException, RequestInfo}
+import javax.servlet.http.HttpServletResponse
+
+import uk.ac.warwick.tabula.{CurrentUser, ItemNotFoundException, PermissionDeniedException, RequestInfo}
 import uk.ac.warwick.tabula.data.Daoisms
 import uk.ac.warwick.tabula.events.EventHandling
 import uk.ac.warwick.tabula.helpers.Logging
@@ -181,4 +183,14 @@ abstract class BaseController extends ControllerMethods
 	 */
 	def binding[A](binder: WebDataBinder, target: A) {}
 
+}
+
+trait PreventCaching {
+	self: BaseController =>
+
+	@ModelAttribute def preventCaching(response: HttpServletResponse): Unit = {
+		response.setHeader("Cache-control", "no-cache, no-store")
+		response.setHeader("Pragma", "no-cache")
+		response.setHeader("Expires", "0")
+	}
 }
