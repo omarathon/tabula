4032131a930e3a2b55c24563f9bfcb9c3784ef70
Archana Mathur <Archana.Mathur@warwick.ac.uk>
2017-06-20 14:18:09 +0100

    Merge pull request #1459 in TAB/tabula from TAB-5229 to develop
    Squashed commit of the following:
    
    commit 512e8498ccd9fee84204415bb527207cf89eea89
    Author: Archana Mathur <archana.mathur@warwick.ac.uk>
    Date:   Tue Jun 20 11:33:12 2017 +0100
    
        TAB-5229 - changes based on review
    
    commit 470b766ead2409b7acdf32b0fb5d4db66d844198
    Author: Archana Mathur <archana.mathur@warwick.ac.uk>
    Date:   Mon Jun 19 10:08:40 2017 +0100
    
        Merge branch 'develop' into TAB-5229
    
    commit 1e913e2514cff751ff9c5f19f00234b1061ad6d1
    Author: Archana Mathur <archana.mathur@warwick.ac.uk>
    Date:   Mon Jun 19 10:07:11 2017 +0100
    
        TAB-5229 - CM1 workflow names appended with CM1 in filter dropdown
    
    commit 0d4cca2164af83c762f161e73e608a97d0acd37e
    Author: Archana Mathur <archana.mathur@warwick.ac.uk>
    Date:   Tue Jun 13 12:01:56 2017 +0100
    
        TAB-5229 - Impl CM2 workflow filters (changed name)
    
    commit 940ff78607e9504c96f2b7a7670b5162478d90b8
    Author: Archana Mathur <archana.mathur@warwick.ac.uk>
    Date:   Tue Jun 13 11:36:13 2017 +0100
    
        TAB-5229 - Impl CM2 workflow filters

--- common/src/main/scala/uk/ac/warwick/tabula/data/convert/AssignmentInfoFilterConverter.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/convert/AssignmentInfoFilterConverter.scala
@@ -3,6 +3,7 @@ package uk.ac.warwick.tabula.data.convert
 import org.springframework.beans.factory.annotation.Autowired
 import uk.ac.warwick.tabula.commands.cm2.assignments.{AssignmentInfoFilter, AssignmentInfoFilters}
 import uk.ac.warwick.tabula.data.model.MarkingMethod
+import uk.ac.warwick.tabula.data.model.markingworkflow.MarkingWorkflowType
 import uk.ac.warwick.tabula.helpers.StringUtils._
 import uk.ac.warwick.tabula.services.ModuleAndDepartmentService
 import uk.ac.warwick.tabula.system.TwoWayConverter
@@ -18,11 +19,15 @@ class AssignmentInfoFilterConverter extends TwoWayConverter[String, AssignmentIn
 			AssignmentInfoFilters.Module(moduleAndDepartmentService.getModuleByCode(sanitise(moduleCode)).getOrElse {
 				moduleAndDepartmentService.getModuleById(moduleCode).orNull
 			})
-		case _ => Try(MarkingMethod.fromCode(source)).map(AssignmentInfoFilters.WorkflowType.apply)
-			.getOrElse(AssignmentInfoFilters.of(source))
+		case _ => Try(MarkingMethod.fromCode(source)).map(AssignmentInfoFilters.WorkflowType.apply).orElse(
+			Try(MarkingWorkflowType.fromCode(source)).map(AssignmentInfoFilters.CM2WorkflowType.apply)
+		).getOrElse(AssignmentInfoFilters.of(source))
+
 	}
 
-	override def convertLeft(source: AssignmentInfoFilter): String = Option(source).map { _.getName }.orNull
+	override def convertLeft(source: AssignmentInfoFilter): String = Option(source).map {
+		_.getName
+	}.orNull
 
 	private def sanitise(code: String): String = {
 		if (code == null) throw new IllegalArgumentException
