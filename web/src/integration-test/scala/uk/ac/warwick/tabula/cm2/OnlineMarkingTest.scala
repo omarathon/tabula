7b2c7563d174b4ea2233856bd3bf87ee5d7b62b6
Archana Mathur <archana.mathur@warwick.ac.uk>
2017-07-19 15:59:09 +0100

    TAB-5412 - revert changes to online marker test.

--- web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/OnlineMarkingTest.scala
+++ web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/OnlineMarkingTest.scala
@@ -19,8 +19,7 @@ class OnlineMarkingTest  extends BrowserTest with CourseworkFixtures with GivenW
 
 
 	"Marker" should "be able to mark work and send to next step" in {
-		addModuleManagers("xxx01", assistants= Seq(P.Marker2.usercode))
-		as(P.Marker2) {
+		as(P.Marker1) {
 			When("I click to mark the assignment")
 			eventuallyAjax({
 				val mark = partialLinkText("Mark").webElement
