96649060e44cb96c67617fb063d2857883e135c1
Cerys Lewis <C.Lewis.3@warwick.ac.uk>
2017-08-15 17:10:12 +0100

    TAB-5454 - Delete Assignment Test

--- web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/DeleteAssignmentTest.scala
+++ web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/DeleteAssignmentTest.scala
@@ -8,8 +8,11 @@ import scala.collection.JavaConverters._
 class DeleteAssignmentTest extends BrowserTest with CourseworkFixtures {
 
 	private def openAssignmentsScreen(): Unit = {
+
 		When("I go the admin page")
-		click on linkText("Test Services")
+		if (!currentUrl.contains("/department/xxx/20")) {
+			click on linkText("Test Services")
+		}
 
 		// make sure we are looking at the latest academic year
 		val yearNav = findAll(cssSelector(".navbar-tertiary .navbar-nav")).next().underlying
@@ -20,23 +23,32 @@ class DeleteAssignmentTest extends BrowserTest with CourseworkFixtures {
 		val moduleBlock = id("main").webElement.findElements(By.cssSelector("h4.with-button")).get(1)
 		val arrow = moduleBlock.findElement(By.cssSelector(".fa-chevron-right"))
 		click on arrow
-
+		
 		Then("The  module should expand")
 		eventually(timeout(45.seconds), interval(300.millis)) ({
 			And("I should find an assignment with no submissions")
-			val noSubmissionsAssignmentsSize = id("main").webElement.findElements(By.xpath("//*[contains(text(),'No Submissions Assignment CM2')]")).size()
-			noSubmissionsAssignmentsSize should be(1)
+			val premarkedAssignmentsSize = id("main").webElement.findElements(By.xpath("//*[contains(text(),'Premarked assignment CM2')]")).size()
+			premarkedAssignmentsSize should be(1)
 		})
 	}
 
 	private def checkUndeletableAssignment(): Unit = {
 
+		val editBtn = id("main").webElement.findElements(By.cssSelector("a.btn-xs")).get(4)
+		click on editBtn
+
+		eventually {
+			Then("I should reach the edit page")
+			currentUrl should include("/edit")
+		}
+
+		cssSelector("p.alert-info").webElement.getText should include ("It's not possible to delete this assignment because it has submissions and/or feedback is published.")
+
 	}
 
 	private def deleteAssignment(): Unit = {
 
-		val noSubmissionsAssignmentsSpan = id("main").webElement.findElements(By.cssSelector("div.pull-right")).asScala.find({_.findElement(By.xpath("//a[contains(text(),'No Submissions Assignment CM2')]")).isDisplayed}).get
-		val editBtn = noSubmissionsAssignmentsSpan.findElement(By.cssSelector("a.btn-xs"))
+		val editBtn = id("main").webElement.findElements(By.cssSelector("a.btn-xs")).get(5)
 		click on editBtn
 
 		eventually {
@@ -45,8 +57,8 @@ class DeleteAssignmentTest extends BrowserTest with CourseworkFixtures {
 		}
 
 		When("I click on the delete button")
-		cssSelector("btn-danger").webElement.getText should be ("delete")
-		click on cssSelector("btn-danger")
+		cssSelector(".btn-danger").webElement.getText should be ("delete")
+		click on cssSelector(".btn-danger")
 
 		When("Then I should go to the confirmation page")
 		eventually {
@@ -55,14 +67,14 @@ class DeleteAssignmentTest extends BrowserTest with CourseworkFixtures {
 		}
 
 		And("The delete button should be disabled")
-		cssSelector("btn-danger").webElement.isEnabled should be (false)
+		cssSelector(".btn-danger").webElement.isEnabled should be (false)
 
 		When("When I click on the confirmation checkbox")
 		click on id("confirmCheck").webElement
 		eventually {
 			Then("The delete button should be enabled")
-			cssSelector("btn-danger").webElement.isEnabled should be(true)
-			click on cssSelector("btn-danger")
+			cssSelector(".btn-danger").webElement.isEnabled should be(true)
+			click on cssSelector(".btn-danger")
 		}
 		eventuallyAjax {
 			Then("I should reach the delete page")
@@ -72,14 +84,15 @@ class DeleteAssignmentTest extends BrowserTest with CourseworkFixtures {
 		Then("I should find not an assignment with no assignments")
 		val noSubmissionsAssignmentsSize = id("main").webElement.findElements(By.xpath("//*[contains(text(),'No Submissions Assignment CM2')]")).size()
 		noSubmissionsAssignmentsSize should be (0)
+
 	}
 
 	"Department admin" should "be able to delete an assignment" in {
 		withAssignment("xxx02", "No Submissions Assignment CM2"){ assignmentId => }
 
 			openAssignmentsScreen()
-			checkUndeletableAssignment()
 			deleteAssignment()
-
+			openAssignmentsScreen()
+			checkUndeletableAssignment()
 	}
 }
