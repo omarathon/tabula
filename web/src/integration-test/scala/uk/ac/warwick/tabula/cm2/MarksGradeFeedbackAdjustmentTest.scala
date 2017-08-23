89ef05c0e9599c080b333ec86783d720a0294380
Cerys Lewis <C.Lewis.3@warwick.ac.uk>
2017-08-23 15:51:51 +0100

    TAB-5454 - Feedback Adjustment Test

--- web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/MarksGradeFeedbackAdjustmentTest.scala
+++ web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/MarksGradeFeedbackAdjustmentTest.scala
@@ -1,7 +1,6 @@
 package uk.ac.warwick.tabula.cm2
 
 import org.openqa.selenium.By
-import org.openqa.selenium.support.ui.Select
 import org.scalatest.GivenWhenThen
 import uk.ac.warwick.tabula.BrowserTest
 
@@ -30,29 +29,30 @@ class MarksGradeFeedbackAdjustmentTest extends BrowserTest with CourseworkFixtur
 			click on linkText("Fully featured assignment")
 			currentUrl should endWith(assignmentId+"/summary")
 
-			click on partialLinkText("Feedback")
-
-			And("I should see the Add marks menu option")
-			val addMarksLink = partialLinkText("Add marks").webElement
-			eventually {
-				addMarksLink.isDisplayed should be {
-					true
-				}
-			}
-			When("I click the Add marks link")
-			click on addMarksLink
-
-			Then("I should reach the Submit marks and feedback settings page")
-			currentUrl should include("/marks")
-			And("Tab option Web Form should be available")
-			linkText("Web Form").webElement.isDisplayed should be(true)
-
-			click on linkText("Web Form")
-			currentUrl should include("/marks#webform")
 		}
 	}
 
 	private def addMarksGrades(): Unit = {
+
+		click on partialLinkText("Feedback")
+
+		And("I should see the Add marks menu option")
+		val addMarksLink = partialLinkText("Add marks").webElement
+		eventually {
+			addMarksLink.isDisplayed should be {
+				true
+			}
+		}
+		When("I click the Add marks link")
+		click on addMarksLink
+
+		Then("I should reach the Submit marks and feedback settings page")
+		currentUrl should include("/marks")
+		And("Tab option Web Form should be available")
+		linkText("Web Form").webElement.isDisplayed should be(true)
+
+		click on linkText("Web Form")
+		currentUrl should include("/marks#webform")
 
 		var markPercentage = id("main").webElement.findElement(By.name("marks[0].actualMark"))
 		markPercentage.sendKeys("68")
@@ -63,10 +63,63 @@ class MarksGradeFeedbackAdjustmentTest extends BrowserTest with CourseworkFixtur
 		var feedbackComment = id("main").webElement.findElement(By.name("marks[0].feedbackComment"))
 		feedbackComment.sendKeys("Interesting essay")
 
+		var saveBtn = id("main").webElement.findElements(By.cssSelector(".btn-primary")).get(1)
+		click on saveBtn
+
+		id("main").webElement.findElement(By.cssSelector("form#command p")).getText should equal("I've received your files and I found marks and feedback for 1 students.")
+
+		var confirmBtn = id("main").webElement.findElements(By.cssSelector(".btn-primary")).get(0)
+		click on confirmBtn
+
 	}
 
-	private def changeMarksGrades(): Unit = {}
+	private def changeMarksGrades(): Unit = {
 
+		click on partialLinkText("Feedback")
+
+		And("I should see the Add marks menu option")
+		val addMarksLink = partialLinkText("Add marks").webElement
+		eventually {
+			addMarksLink.isDisplayed should be {
+				true
+			}
+		}
+		When("I click the Add marks link")
+		click on addMarksLink
+
+		Then("I should reach the Submit marks and feedback settings page")
+		currentUrl should include("/marks")
+		And("Tab option Web Form should be available")
+		linkText("Web Form").webElement.isDisplayed should be(true)
+
+		click on linkText("Web Form")
+		currentUrl should include("/marks#webform")
+
+		var markPercentage2 = id("main").webElement.findElement(By.name("marks[0].actualMark"))
+		markPercentage2.clear()
+		markPercentage2.sendKeys("71")
+
+		var grade2 = id("main").webElement.findElement(By.name("marks[0].actualGrade"))
+		grade2.clear()
+		grade2.sendKeys("2.1")
+
+		var feedbackComment2 = id("main").webElement.findElement(By.name("marks[0].feedbackComment"))
+		feedbackComment2.clear()
+		feedbackComment2.sendKeys("Very good")
+
+		var saveBtn2 = id("main").webElement.findElements(By.cssSelector(".btn-primary")).get(1)
+		click on saveBtn2
+
+		id("main").webElement.findElement(By.cssSelector("form#command p")).getText should equal("I've received your files and I found marks and feedback for 1 students.")
+
+		id("main").webElement.findElement(By.cssSelector("span.warning")).getText should equal("Feedback and/or marks have already been uploaded for this student. This will be overwritten when you click confirm")
+
+		var confirmBtn2 = id("main").webElement.findElements(By.cssSelector(".btn-primary")).get(0)
+		click on confirmBtn2
+
+		id("main").webElement.findElements(By.cssSelector("td.action-col")).get(1).getText should equal("Feedback needs publishing")
+
+	}
 
 	"Department admin" should "be able to adjust marks and grades for assignment" in as(P.Admin1) {
 
