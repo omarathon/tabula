520e387f0550d28097109510906c0f60fcc11a04
Cerys Lewis <C.Lewis.3@warwick.ac.uk>
2017-08-23 16:26:26 +0100

    TAB-5454 - Feedback Adjustment Test

--- web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/MarksGradeFeedbackAdjustmentTest.scala
+++ web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/MarksGradeFeedbackAdjustmentTest.scala
@@ -34,9 +34,10 @@ class MarksGradeFeedbackAdjustmentTest extends BrowserTest with CourseworkFixtur
 
 	private def addMarksGrades(): Unit = {
 
+		When("I click the Feedback menu section")
 		click on partialLinkText("Feedback")
 
-		And("I should see the Add marks menu option")
+		Then("I should see the Add marks menu option")
 		val addMarksLink = partialLinkText("Add marks").webElement
 		eventually {
 			addMarksLink.isDisplayed should be {
@@ -54,6 +55,7 @@ class MarksGradeFeedbackAdjustmentTest extends BrowserTest with CourseworkFixtur
 		click on linkText("Web Form")
 		currentUrl should include("/marks#webform")
 
+		When("I create marks, grades and feedback")
 		var markPercentage = id("main").webElement.findElement(By.name("marks[0].actualMark"))
 		markPercentage.sendKeys("68")
 
@@ -63,21 +65,27 @@ class MarksGradeFeedbackAdjustmentTest extends BrowserTest with CourseworkFixtur
 		var feedbackComment = id("main").webElement.findElement(By.name("marks[0].feedbackComment"))
 		feedbackComment.sendKeys("Interesting essay")
 
+		And("I save them")
 		var saveBtn = id("main").webElement.findElements(By.cssSelector(".btn-primary")).get(1)
 		click on saveBtn
 
+		Then("The marks, grades and feedback should be received")
 		id("main").webElement.findElement(By.cssSelector("form#command p")).getText should equal("I've received your files and I found marks and feedback for 1 students.")
 
 		var confirmBtn = id("main").webElement.findElements(By.cssSelector(".btn-primary")).get(0)
 		click on confirmBtn
 
+		Then("The user assignemnt should be updated")
+		id("main").webElement.findElements(By.cssSelector("td.action-col")).get(1).getText should equal("Feedback needs publishing")
+
 	}
 
 	private def changeMarksGrades(): Unit = {
 
+		When("I click the Feedback menu section")
 		click on partialLinkText("Feedback")
 
-		And("I should see the Add marks menu option")
+		Then("I should see the Add marks menu option")
 		val addMarksLink = partialLinkText("Add marks").webElement
 		eventually {
 			addMarksLink.isDisplayed should be {
@@ -89,12 +97,14 @@ class MarksGradeFeedbackAdjustmentTest extends BrowserTest with CourseworkFixtur
 
 		Then("I should reach the Submit marks and feedback settings page")
 		currentUrl should include("/marks")
+
 		And("Tab option Web Form should be available")
 		linkText("Web Form").webElement.isDisplayed should be(true)
 
 		click on linkText("Web Form")
 		currentUrl should include("/marks#webform")
 
+		When("I create marks, grades and feedback")
 		var markPercentage2 = id("main").webElement.findElement(By.name("marks[0].actualMark"))
 		markPercentage2.clear()
 		markPercentage2.sendKeys("71")
@@ -107,16 +117,20 @@ class MarksGradeFeedbackAdjustmentTest extends BrowserTest with CourseworkFixtur
 		feedbackComment2.clear()
 		feedbackComment2.sendKeys("Very good")
 
+		And("I save them")
 		var saveBtn2 = id("main").webElement.findElements(By.cssSelector(".btn-primary")).get(1)
 		click on saveBtn2
 
+		Then("The marks, grades and feedback should be received")
 		id("main").webElement.findElement(By.cssSelector("form#command p")).getText should equal("I've received your files and I found marks and feedback for 1 students.")
 
+		And("The user should be warned this will replace the existing marks,grades and feedback")
 		id("main").webElement.findElement(By.cssSelector("span.warning")).getText should equal("Feedback and/or marks have already been uploaded for this student. This will be overwritten when you click confirm")
 
 		var confirmBtn2 = id("main").webElement.findElements(By.cssSelector(".btn-primary")).get(0)
 		click on confirmBtn2
 
+		Then("The user assignemnt should be updated")
 		id("main").webElement.findElements(By.cssSelector("td.action-col")).get(1).getText should equal("Feedback needs publishing")
 
 	}
