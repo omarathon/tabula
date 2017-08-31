7cfcdf314d23994c90a29986b3f48259ac075dc5
Cerys Lewis <C.Lewis.3@warwick.ac.uk>
2017-08-31 14:39:15 +0100

    TAB-5452 - Replace Marker Reusable Workflow Test

--- web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/ReplaceMarkerReusableWorkflowTest.scala
+++ web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/ReplaceMarkerReusableWorkflowTest.scala
@@ -11,10 +11,13 @@ class ReplaceMarkerReusableWorkflowTest extends BrowserTest with CourseworkFixtu
 	private val currentYear = AcademicYear.guessSITSAcademicYearByDate(DateTime.now)
 
 	private def openModifyMarkerScreen(): Unit =	{
-		withAssignment("xxx02", "Moderated marking-4C", loggedUser = P.ModuleManager1) { _ =>
 
+		if(!currentUrl.contains("/department/xxx")){
 			When("I go the admin page")
 			click on linkText("Test Services")
+		}
+
+		if(!currentUrl.contains("/department/xxx/$currentYear/markingworkflows")) {
 
 			Then("I should be able to click on the Marking workflows option")
 			val toolbar = findAll(className("dept-toolbar")).next().underlying
@@ -27,44 +30,92 @@ class ReplaceMarkerReusableWorkflowTest extends BrowserTest with CourseworkFixtu
 				Then(s"I should be on the ${currentYear.previous} version of the page")
 				currentUrl should include(s"/${currentYear.previous.startYear}/markingworkflows")
 			}
-
+			// only for single use?
 			val addToYear = id("main").webElement.findElement(By.linkText(s"Add to $currentYear"))
 			click on addToYear
-
-			eventuallyAjax {
-				Then("I should reach the marking workflows page")
-				currentUrl should include(s"/${currentYear.startYear}/markingworkflows")
-			}
-
-			val currentMarker = id("main").webElement.findElements(By.tagName("td")).get(2).getText == "Marker: tabula-functest-marker1 user"
-			currentMarker should be(true)
-
-			val modifyBtn = id("main").webElement.findElement(By.cssSelector("td a.btn-default"))
-			click on modifyBtn
-
-			eventuallyAjax {
-				Then("I should reach the modify workflows options page")
-				currentUrl should include("/edit")
-			}
-
-			val replaceLink = id("main").webElement.findElement(By.partialLinkText("Replace marker"))
-			click on replaceLink
-
-			eventuallyAjax {
-				Then("I should reach the modify workflows page")
-				currentUrl should include("/replace")
-			}
 		}
+
+		eventuallyAjax {
+			Then("I should reach the marking workflows page")
+			currentUrl should include(s"/${currentYear.startYear}/markingworkflows")
+		}
+
 	}
 
-	private def replaceMarker(): Unit = {
+	private def replaceIncompleteMarker(): Unit = {
+
+		val currentMarker = id("main").webElement.findElements(By.tagName("td")).get(2).getText == "Marker: tabula-functest-marker1 user"
+		currentMarker should be(true)
+
+		val modifyBtn = id("main").webElement.findElement(By.cssSelector("td a.btn-default"))
+		click on modifyBtn
+
+		eventuallyAjax {
+			Then("I should reach the modify workflows options page")
+			currentUrl should include("/edit")
+		}
+
+		val replaceLink = id("main").webElement.findElement(By.partialLinkText("Replace marker"))
+		click on replaceLink
+
+		eventuallyAjax {
+			Then("I should reach the modify workflows page")
+			currentUrl should include("/replace")
+		}
+
+		val markerToReplace2 = id("oldMarker").webElement
+		click on markerToReplace2
+
+		val oldMarker2 = markerToReplace2.findElements(By.tagName("option")).get(1)
+		oldMarker2.isDisplayed should be (true)
+		click on oldMarker2
+
+		val newMarker = id("main").webElement.findElement(By.cssSelector("input[name=newMarker]"))
+		newMarker.sendKeys("tabula-functest-marker2")
+
+		val saveButton = cssSelector("input.btn-primary")
+		click on saveButton
+
+		eventuallyAjax {
+			Then("I should reach the modify workflows page again")
+			currentUrl should include(s"/xxx/${currentYear.startYear}/markingworkflows")
+		}
+
+		val addedMarker = id("main").webElement.findElements(By.tagName("td")).get(2).getText == "Marker: tabula-functest-marker2 user"
+		addedMarker should be (true)
+
+	}
+
+	private def replaceCompletedMarker(): Unit = {
+
+		val currentMarker = id("main").webElement.findElements(By.tagName("td")).get(6).getText == "Marker: tabula-functest-marker1 user"
+		currentMarker should be(true)
+
+		val modifyBtn = id("main").webElement.findElement(By.cssSelector("td a.btn-default"))
+		click on modifyBtn
+
+		eventuallyAjax {
+			Then("I should reach the modify workflows options page")
+			currentUrl should include("/edit")
+		}
+
+		val replaceLink = id("main").webElement.findElement(By.partialLinkText("Replace marker"))
+		click on replaceLink
+
+		eventuallyAjax {
+			Then("I should reach the modify workflows page")
+			currentUrl should include("/replace")
+		}
+
 
 		val markerToReplace = id("oldMarker").webElement
 		click on markerToReplace
 
 		val oldMarker = markerToReplace.findElement(By.cssSelector("option[value=tabula-functest-marker1]"))
-		oldMarker.isDisplayed should be (true)
-		click on oldMarker
+		eventuallyAjax {
+			oldMarker.isDisplayed should be (true)
+			click on oldMarker
+		}
 
 		val newMarker = id("main").webElement.findElement(By.cssSelector("input[name=newMarker]"))
 		newMarker.sendKeys("tabula-functest-marker2")
@@ -85,6 +136,9 @@ class ReplaceMarkerReusableWorkflowTest extends BrowserTest with CourseworkFixtu
 	"Department admin" should "be able to modify markers in reusable workflows" in as(P.Admin1) {
 
 		openModifyMarkerScreen()
+		replaceIncompleteMarker()
+		openModifyMarkerScreen()
+		replaceCompletedMarker()
 
 	}
 }
