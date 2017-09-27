453d78e6be1d443f25833dba14e1db1a7bcfdc6b
Cerys Lewis <C.Lewis.3@warwick.ac.uk>
2017-09-27 10:51:18 +0100

    TAB-5458 Bulk Mark Adjustment Test

--- web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/AdjustMarksInBulkTest.scala
+++ web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/AdjustMarksInBulkTest.scala
@@ -14,23 +14,27 @@ class AdjustMarksInBulkTest extends BrowserTest with CourseworkFixtures {
 			click on linkText("Test Services")
 
 			eventually {
-				And("ensure I'm looking at this year")
+				And("ensure I'm looking at the current year")
 				loadCurrentAcademicYearTab()
 
+				Then("I should get to the department page")
 				currentUrl.contains("/department/xxx") should be(true)
 			}
 
 			eventually {
+				When("I expand the the module")
 				val testModule = id("main").webElement.findElements(By.cssSelector(".fa-chevron-right")).get(0)
 				click on testModule
 			}
 
 			eventuallyAjax {
+				And("click on the cm2 assignment")
 				val cm2Assignment = id("main").webElement.findElements(By.cssSelector("h5.assignment-name a")).get(0)
 				click on cm2Assignment
 			}
 
 			eventuallyAjax {
+				Then("I should get to the summary page")
 				currentUrl.contains("/summary") should be(true)
 			}
 
@@ -38,23 +42,31 @@ class AdjustMarksInBulkTest extends BrowserTest with CourseworkFixtures {
 			val feedbackDropdown = id("main").webElement.findElements(By.cssSelector("a.dropdown-toggle")).get(5)
 			click on feedbackDropdown
 
+			Then("The adjustments link is disabled")
 			val adjustmentsLinkHolder = id("main").webElement.findElements(By.cssSelector("li")).get(27)
 			adjustmentsLinkHolder.getAttribute("class")contains("disabled") should be(true)
 
+			When("I select the all student checkbox")
 			val selectAll = id("main").webElement.findElements(By.cssSelector(".collection-check-all")).get(0)
 			click on selectAll
 
+			And("Select the feeback dropdown")
 			click on feedbackDropdown
 
+			Then("The adjustments link is enabled")
 			adjustmentsLinkHolder.getAttribute("class") contains ("disabled") should be(false)
 			val adjustmentsLink = id("main").webElement.findElements(By.cssSelector(".dropdown-menu li .form-post")).get(4)
 			adjustmentsLink.isDisplayed should be (true)
+
+			When("I click on the adjustments link")
 			click on adjustmentsLink
 
+			Then("I am taken to the adjustments page")
 			eventually {
 				currentUrl.contains("/feedback/adjustments") should be(true)
 			}
 
+			And("I click on the bulk adjust buttons")
 			val bulkAdjustButton = id("main").webElement.findElements(By.cssSelector("div.pull-right a")).get(0)
 			bulkAdjustButton.isEnabled should be (true)
 			click on bulkAdjustButton
@@ -63,18 +75,28 @@ class AdjustMarksInBulkTest extends BrowserTest with CourseworkFixtures {
 
 	private def adjustMarks(): Unit = {
 
-	}
+		When("I upload an adjustments file")
+		ifPhantomJSDriver(
+			operation = { d =>
+				// This hangs forever for some reason in PhantomJS if you use the normal pressKeys method
+				d.executePhantomJS("var page = this; page.uploadFile('input[type=file]', '" + getClass.getResource("/adjustments.xlsx").getFile + "');")
+			},
+			otherwise = { _ =>
+				click on find(cssSelector("input[type=file]")).get
+				pressKeys(getClass.getResource("/adjustments.xlsx").getFile)
+			}
+		)
 
-	private def uncheckStudents(): Unit = {
+		And("Click on the upload button")
+		click on cssSelector("button.btn-primary").webElement
+		Then("summary confirmations should be displayed")
+		cssSelector("input#privateAdjustment1").webElement.isEnabled should be (true)
 
-		val student1Checkbox = id("main").webElement.findElements(By.cssSelector(".collection-checkbox")).get(0)
-		if(student1Checkbox.isSelected){
-			click on student1Checkbox
-		}
-		val student2Checkbox = id("main").webElement.findElements(By.cssSelector(".collection-checkbox")).get(1)
-		if(student2Checkbox.isSelected){
-			click on student2Checkbox
-		}
+		When("I click on the confirm button")
+		click on cssSelector(".btn-primary").webElement
+
+		Then("I am returned to the summary page")
+		currentUrl.contains("/summary") should be(true)
 
 	}
 
@@ -82,7 +104,6 @@ class AdjustMarksInBulkTest extends BrowserTest with CourseworkFixtures {
 
 		openBulkAdjustmentScreen()
 		adjustMarks()
-		//uncheckStudents()
 
 	}
 }
