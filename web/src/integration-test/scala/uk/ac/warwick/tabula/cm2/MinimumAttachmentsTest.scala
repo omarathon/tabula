8cfa0a3a66e126281df4b519ce2c225d3279d897
Cerys Lewis <C.Lewis.3@warwick.ac.uk>
2017-10-03 11:42:50 +0100

    Merge pull request #1558 in TAB/tabula from TAB-5590 to develop
    Squashed commit of the following:
    
    commit 99ff01d53c8ab31c1d8d94e981859239cded0bb4
    Author: Cerys Lewis <C.Lewis.3@warwick.ac.uk>
    Date:   Tue Oct 3 11:14:21 2017 +0100
    
        TAB-5590 Apply Minimum No of Attachments to Assignment Test
    
    commit 93d4ea77218cdf84bc36e010529b5a69f8cc8489
    Author: Cerys Lewis <C.Lewis.3@warwick.ac.uk>
    Date:   Mon Oct 2 12:38:05 2017 +0100
    
        TAB-5590 Apply Mimimum No of Attatchments to Assigment Test
    
    commit d6877ec1b4b53fd0592aa63cd5b63f4c4e5b69da
    Author: Cerys Lewis <C.Lewis.3@warwick.ac.uk>
    Date:   Fri Sep 29 11:19:14 2017 +0100
    
        TAB-5590 Apply Mimimum No of Attatchments to Assigment Test
    
    commit 242b56ccc66161fa77ef1a5b778814144aff65ad
    Merge: 5da8d09 9bc5e7b
    Author: Cerys Lewis <C.Lewis.3@warwick.ac.uk>
    Date:   Thu Sep 28 10:50:53 2017 +0100
    
        Merge branch 'TAB-5458' into TAB-5590
    
    commit 5da8d09bbe2997b36a4427654322033df4fd3bf4
    Merge: b2842ac 0588190
    Author: Cerys Lewis <C.Lewis.3@warwick.ac.uk>
    Date:   Thu Sep 28 10:47:44 2017 +0100
    
        O
        Merge branch 'develop' into TAB-5590
    
    commit b2842ac7ba78f31e8ec1a90d56a44bfb743574c1
    Author: Cerys Lewis <C.Lewis.3@warwick.ac.uk>
    Date:   Thu Sep 28 10:08:47 2017 +0100
    
        TAB-5590 Apply Mimimum No of Attatchments to Assigment Test
    
    commit 9bc5e7b6af9defee9252986621ce9c51b6b38f35
    Merge: 453d78e ec6219d
    Author: Cerys Lewis <C.Lewis.3@warwick.ac.uk>
    Date:   Wed Sep 27 12:06:08 2017 +0100
    
        Merge branch 'develop' into TAB-5458
    
    commit 453d78e6be1d443f25833dba14e1db1a7bcfdc6b
    Author: Cerys Lewis <C.Lewis.3@warwick.ac.uk>
    Date:   Wed Sep 27 10:51:18 2017 +0100
    
        TAB-5458 Bulk Mark Adjustment Test
    
    commit 8f7b1f5bb50a48b3a2dafaac8d826f8ef214b3ff
    Author: Cerys Lewis <C.Lewis.3@warwick.ac.uk>
    Date:   Tue Sep 26 16:26:30 2017 +0100
    
        TAB-5458 Bulk Mark Adjustment Test
    
    commit 49758bb3de3079bcb26ec3a76331cc87505641fe
    Merge: 7ce6b82 8dbbfae
    Author: Cerys Lewis <C.Lewis.3@warwick.ac.uk>
    Date:   Mon Sep 25 16:01:27 2017 +0100
    
        Merge branch 'develop' into TAB-5458
    
    commit 7ce6b822a98662456777dcaa29985ea4a280efe8
    Author: Cerys Lewis <C.Lewis.3@warwick.ac.uk>
    Date:   Fri Sep 22 10:49:15 2017 +0100
    
        TAB-5456 Download Submission (Marker) Test

---
+++ web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/MinimumAttachmentsTest.scala
@@ -0,0 +1,123 @@
+package uk.ac.warwick.tabula.cm2
+
+import org.openqa.selenium.By
+import uk.ac.warwick.tabula.BrowserTest
+
+class MinimumAttachmentsTest extends BrowserTest with CourseworkFixtures {
+
+	private def openEditOptionsScreen(): Unit = {
+
+		When("I expand module XXX02")
+		val moduleBlock = id("main").webElement.findElements(By.cssSelector("h4.with-button")).get(1)
+		val arrow = moduleBlock.findElement(By.cssSelector(".fa-chevron-right"))
+		click on arrow
+
+		Then("The  module should expand")
+		eventually {
+			And("I should find a button to edit the CM2 assignment")
+			val editAssignmentBtn = id("main").webElement.findElements(By.xpath("//*[contains(text(),'Edit assignment')]")).get(1)
+			click on editAssignmentBtn
+		}
+
+		eventually {
+			And("I should be able to select the Options tab")
+			val optionsLink = id("main").webElement.findElements(By.xpath("//*[contains(text(),'Options')]")).get(0)
+			click on optionsLink
+		}
+
+	}
+
+	private def changeAttachmentsNo(): Unit = {
+		click on id("minimumFileAttachmentLimit")
+		click on id("main").webElement.findElements(By.xpath("//option[@value='2']")).get(0)
+
+		id("minimumFileAttachmentLimit").webElement.getAttribute("value") should be ("2")
+
+		click on id("fileAttachmentLimit")
+		click on id("main").webElement.findElements(By.xpath("//option[@value='5']")).get(1)
+
+		id("fileAttachmentLimit").webElement.getAttribute("value") should be ("5")
+
+		val saveAndExitBtn = id("main").webElement.findElements(By.cssSelector(".btn-primary")).get(1)
+		click on saveAndExitBtn
+
+		eventually {
+			currentUrl should include ("/summary")
+		}
+	}
+
+	private def addAttachments(): Unit = {
+
+		click on linkText("Coursework Management")
+		currentUrl should include ("/coursework/")
+
+		eventually {
+			val cm2AssignmentLink = id("main").webElement.findElements(By.xpath("//*[contains(text(),'Premarked assignment CM2')]")).get(0)
+			click on cm2AssignmentLink
+		}
+
+		When("I upload a file for submission")
+		ifPhantomJSDriver(
+			operation = { d =>
+				// This hangs forever for some reason in PhantomJS if you use the normal pressKeys method
+				d.executePhantomJS("var page = this; page.uploadFile('input[type=file]', '" + getClass.getResource("/file1.txt").getFile + "');")
+			},
+			otherwise = { _ =>
+				click on find(cssSelector("input[type=file]")).get
+				pressKeys(getClass.getResource("/file1.txt").getFile)
+			}
+		)
+
+		And("confirm it is my own work")
+		if(!id("plagiarismDeclaration").webElement.isSelected){
+			click on id("plagiarismDeclaration").webElement
+		}
+
+		id("plagiarismDeclaration").webElement.isSelected should be (true)
+
+		And("press submit")
+		click on id("main").webElement.findElements(By.cssSelector(".btn-primary")).get(0)
+
+		eventually {
+			Then("an error around file numbers will be shown")
+			val errorMsg = cssSelector(".has-error").webElement.findElements(By.xpath("//*[contains(text(),'You need to at least submit 2 files.')]")).get(0)
+			errorMsg.isDisplayed should be (true)
+		}
+
+		When("I upload another file")
+		ifPhantomJSDriver(
+			operation = { d =>
+				// This hangs forever for some reason in PhantomJS if you use the normal pressKeys method
+				d.executePhantomJS("var page = this; page.uploadFile('input[type=file]', '" + getClass.getResource("/file2.txt").getFile + "');")
+			},
+			otherwise = { _ =>
+				click on find(cssSelector("input[type=file]")).get
+				pressKeys(getClass.getResource("/file2.txt").getFile)
+			}
+		)
+
+		And("press submit")
+		click on id("main").webElement.findElements(By.cssSelector(".btn-primary")).get(0)
+
+		eventually{
+			Then("an error around file numbers is no longer shown")
+			val fileCountErrorMsg = cssSelector(".has-error").webElement.findElements(By.xpath("//*[contains(text(),'You need to at least submit 2 files.')]")).size()
+			fileCountErrorMsg should be (0)
+		}
+
+
+	}
+
+	"Department admin" should "be able to be able to specify the minimum number of attachents for an assignment" in as(P.Admin1) {
+		openAdminPage()
+		loadCurrentAcademicYearTab()
+		openEditOptionsScreen()
+		changeAttachmentsNo()
+
+		as(P.Student1){
+			addAttachments()
+		}
+	}
+
+}
+
