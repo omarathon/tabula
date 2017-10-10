179cf5b1fb07e5c0b08aaed311939487f6365e72
Cerys Lewis <C.Lewis.3@warwick.ac.uk>
2017-10-10 17:14:48 +0100

    TAB-5469 - Change to plagiarism wording - fix for testing

--- web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/CourseworkAssignmentSubmissionTest.scala
+++ web/src/integration-test/scala/uk/ac/warwick/tabula/cm2/CourseworkAssignmentSubmissionTest.scala
@@ -4,15 +4,48 @@ import uk.ac.warwick.tabula.BrowserTest
 
 class CourseworkAssignmentSubmissionTest extends BrowserTest with CourseworkFixtures {
 
+	def options() = {
+		singleSel("minimumFileAttachmentLimit").value = "2"
+		singleSel("fileAttachmentLimit").value = "3"
+	}
+
 	// TAB-413, TAB-415
 	"Student" should "be able to submit assignment after validation errors without re-uploading file" in {
-		withAssignment("xxx01", "Fully featured assignment") { assignmentId =>
+
+		withAssignment("xxx01", "Min 2 attachments", optionSettings = options) { assignmentId =>
+
 			as(P.Student1) {
-				click on linkText("Fully featured assignment")
+				click on linkText("Min 2 attachments")
 				currentUrl should endWith(assignmentId)
 
 				click on find(cssSelector("input[type=file]")).get
-				pressKeys(getClass.getResource("/file1.txt").getFile)
+				ifPhantomJSDriver(
+					operation = { d =>
+						// This hangs forever for some reason in PhantomJS if you use the normal pressKeys method
+						d.executePhantomJS("var page = this; page.uploadFile('input[type=file]', '" + getClass.getResource("/file1.txt").getFile + "');")
+					},
+					otherwise = { _ =>
+						click on find(cssSelector("input[type=file]")).get
+						pressKeys(getClass.getResource("/file1.txt").getFile)
+					}
+				)
+				// Don't upload the second file yet
+				submit()
+
+				pageSource contains "Thanks, we've received your submission." should be (false)
+				pageSource contains "You need to at least submit 2 files" should be {true}
+
+				click on find(cssSelector("input[type=file]")).get
+				ifPhantomJSDriver(
+					operation = { d =>
+						// This hangs forever for some reason in PhantomJS if you use the normal pressKeys method
+						d.executePhantomJS("var page = this; page.uploadFile('input[type=file]', '" + getClass.getResource("/file2.txt").getFile + "');")
+					},
+					otherwise = { _ =>
+						click on find(cssSelector("input[type=file]")).get
+						pressKeys(getClass.getResource("/file2.txt").getFile)
+					}
+				)
 
 				submit()
 
@@ -31,11 +64,6 @@ class CourseworkAssignmentSubmissionTest extends BrowserTest with CourseworkFixt
 	}
 
 	"Student" should "see a validation error when submitting less than the minimum number of files" in {
-
-		def options() = {
-			singleSel("minimumFileAttachmentLimit").value = "2"
-			singleSel("fileAttachmentLimit").value = "3"
-		}
 
 		withAssignment("xxx01", "Min 2 attachments", optionSettings = options) { assignmentId =>
 			submitAssignment(P.Student1, "Min 2 attachments", assignmentId, "/file1.txt")
