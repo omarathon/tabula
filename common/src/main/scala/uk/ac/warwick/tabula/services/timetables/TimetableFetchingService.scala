e571b0ec5cf61fd7cba7a039c07d15d12b4c15cd
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2017-10-11 09:34:27 +0100

    TAB-5622 - whitespace crime and comment

--- common/src/main/scala/uk/ac/warwick/tabula/services/timetables/TimetableFetchingService.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/services/timetables/TimetableFetchingService.scala
@@ -156,6 +156,7 @@ class CombinedTimetableFetchingService(services: PartialTimetableFetchingService
 		events.map(events => events.groupBy { event => (event.year, event.day, event.startTime, event.endTime, event.weekRanges,
 			event.eventType, event.parent.shortName, event.location, event.staff) }
 			.mapValues {
+				// values are Seq so List cons (::) never matches
 				case event +: Nil => event
 				case groupedEvents =>
 					val event = groupedEvents.head
