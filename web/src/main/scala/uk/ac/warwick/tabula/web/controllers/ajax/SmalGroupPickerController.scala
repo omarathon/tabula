bc7febab7f3d7ecc130c517b533cec464431e6a6
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2017-10-23 16:23:57 +0100

    TAB-5657

--- web/src/main/scala/uk/ac/warwick/tabula/web/controllers/ajax/SmalGroupPickerController.scala
+++ web/src/main/scala/uk/ac/warwick/tabula/web/controllers/ajax/SmalGroupPickerController.scala
@@ -1,7 +1,9 @@
 package uk.ac.warwick.tabula.web.controllers.ajax
 
+import org.joda.time.DateTime
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{ModelAttribute, RequestMapping}
+import uk.ac.warwick.tabula.AcademicYear
 import uk.ac.warwick.tabula.commands._
 import uk.ac.warwick.tabula.data.model.groups.SmallGroup
 import uk.ac.warwick.tabula.helpers.StringUtils._
@@ -10,6 +12,7 @@ import uk.ac.warwick.tabula.system.permissions.Public
 import uk.ac.warwick.tabula.web.Mav
 import uk.ac.warwick.tabula.web.controllers.BaseController
 import uk.ac.warwick.tabula.web.views.JSONView
+import scala.util.Try
 
 @Controller
 @RequestMapping(value = Array("/ajax/smallgrouppicker/query"))
@@ -45,12 +48,14 @@ class SmallGroupPickerCommand extends CommandInternal[Seq[SmallGroup]] {
 	self: SmallGroupServiceComponent =>
 
 	var query: String = _
+	var academicYear: String = _
 
 	def applyInternal(): Seq[SmallGroup] = {
 		if (!query.hasText) {
 			Seq()
 		} else {
-			smallGroupService.findSmallGroupsByNameOrModule(query)
+			val year = Try(AcademicYear(academicYear.toInt)).getOrElse(AcademicYear.guessSITSAcademicYearByDate(DateTime.now))
+			smallGroupService.findSmallGroupsByNameOrModule(query, year)
 		}
 	}
 
