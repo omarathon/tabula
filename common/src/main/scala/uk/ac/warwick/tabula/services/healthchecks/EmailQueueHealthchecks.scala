305d311c08384bb036f2d1e047c594868b4e80c8
Archana Mathur <archana.mathur@warwick.ac.uk>
2017-11-14 16:37:29 +0000

    TAB-5683 - oops removed extra coma

--- common/src/main/scala/uk/ac/warwick/tabula/services/healthchecks/EmailQueueHealthchecks.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/services/healthchecks/EmailQueueHealthchecks.scala
@@ -75,7 +75,7 @@ class EmailOldestUnsentItemHealthcheck extends ServiceHealthcheckProvider {
 			message = s"Last sent email $recentSentEmail minute${if (recentSentEmail == 1) "" else "s"} old, Oldest unsent email $oldestUnsentEmail minute${if (oldestUnsentEmail == 1) "" else "s"} old, (warning: $WarningThreshold, critical: $ErrorThreshold)",
 			performanceData = Seq(
 				ServiceHealthcheck.PerformanceData("oldest_unsent", oldestUnsentEmail, WarningThreshold, ErrorThreshold),
-				ServiceHealthcheck.PerformanceData("last_sent", recentSentEmail, WarningThreshold, ErrorThreshold),
+				ServiceHealthcheck.PerformanceData("last_sent", recentSentEmail, WarningThreshold, ErrorThreshold)
 			)
 		))
 	}
