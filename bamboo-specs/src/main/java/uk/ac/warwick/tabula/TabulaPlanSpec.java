f92be512ef9d7b73c5e2fb5d67187612550f2042
Mat Mannion <M.Mannion@warwick.ac.uk>
2017-11-15 16:09:20 +0000

    Fix right deployment source plan

--- bamboo-specs/src/main/java/uk/ac/warwick/tabula/TabulaPlanSpec.java
+++ bamboo-specs/src/main/java/uk/ac/warwick/tabula/TabulaPlanSpec.java
@@ -107,7 +107,7 @@ public class TabulaPlanSpec extends AbstractWarwickBuildSpec {
     @Override
     protected Collection<Deployment> deployments() {
         return Collections.singleton(
-            deployment(PROJECT, "BUILD", "Tabula")
+            deployment(PROJECT, "ALL", "Tabula")
                 .autoTomcatEnvironment("Development", "tabula-dev.warwick.ac.uk", "tabula", "dev", SLACK_CHANNEL)
                 .autoTomcatEnvironment("Test", "tabula-test.warwick.ac.uk", "tabula", "test", SLACK_CHANNEL)
                 .tomcatEnvironment("Sandbox", "tabula-sandbox.warwick.ac.uk", "tabula", "sandbox", env -> env
