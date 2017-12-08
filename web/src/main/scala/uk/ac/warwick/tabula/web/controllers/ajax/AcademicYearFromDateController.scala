d36df1d0321c37d925a4200e77fa22b58e4e534e
Mat Mannion <M.Mannion@warwick.ac.uk>
2017-12-08 14:25:16 +0000

    Don't throw as severe an exception when passed an invalid date when creating a meeting

--- web/src/main/scala/uk/ac/warwick/tabula/web/controllers/ajax/AcademicYearFromDateController.scala
+++ web/src/main/scala/uk/ac/warwick/tabula/web/controllers/ajax/AcademicYearFromDateController.scala
@@ -3,10 +3,12 @@ package uk.ac.warwick.tabula.web.controllers.ajax
 import org.joda.time.LocalDate
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{RequestMapping, RequestParam}
-import uk.ac.warwick.tabula.AcademicYear
+import uk.ac.warwick.tabula.{AcademicYear, ItemNotFoundException}
 import uk.ac.warwick.tabula.web.Mav
 import uk.ac.warwick.tabula.web.controllers.BaseController
 import uk.ac.warwick.tabula.web.views.JSONView
+
+import scala.util.{Failure, Success, Try}
 
 /**
 	* Returns the start date (inclusive) and end date (exclusive, the first datetime NOT in the provided academic year)
@@ -17,12 +19,13 @@ import uk.ac.warwick.tabula.web.views.JSONView
 class AcademicYearFromDateController extends BaseController {
 
 	@RequestMapping
-	def result(@RequestParam date: LocalDate): Mav = {
-		val academicYear = AcademicYear.forDate(date)
-		Mav(new JSONView(Map(
-			"startYear" -> academicYear.startYear,
-			"string" -> academicYear.toString
-		)))
-	}
+	def result(@RequestParam date: LocalDate): Mav =
+		Try(AcademicYear.forDate(date)) match {
+			case Success(academicYear) => Mav(new JSONView(Map(
+				"startYear" -> academicYear.startYear,
+				"string" -> academicYear.toString
+			)))
+			case Failure(e) => throw new ItemNotFoundException(date, s"Couldn't find an academic year for $date: $e")
+		}
 
 }
