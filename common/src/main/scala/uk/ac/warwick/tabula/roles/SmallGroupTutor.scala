1506dd8871e382495a3e1237209e860ce5bffc2b
Mat Mannion <M.Mannion@warwick.ac.uk>
2018-01-05 14:45:37 +0000

    TAB-5745 Remove SmallGroup from permissions parents of StudentMember
    This implements the behaviour described in a 3-year old FIXME in StudentMember, noting that because a SmallGroup doesn't completely contain a StudentMember, it's incorrect for it to be a PermissionsParent of one and has strange behaviour (as well as performance implications). I can't see any way that the change made would make performance _worse_, but it won't necessarily make it better because every permissions lookup against an individual will still require listing all of the small groups that they are a member of, and checking that the current user isn't a tutor for any events in those groups.

--- common/src/main/scala/uk/ac/warwick/tabula/roles/SmallGroupTutor.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/roles/SmallGroupTutor.scala
@@ -1,17 +1,17 @@
 package uk.ac.warwick.tabula.roles
 
+import uk.ac.warwick.tabula.data.model.groups.{SmallGroup, SmallGroupEvent}
+import uk.ac.warwick.tabula.data.model.{StudentMember, StudentRelationshipType}
 import uk.ac.warwick.tabula.permissions.Permissions._
-import uk.ac.warwick.tabula.data.model.groups.{SmallGroupEvent, SmallGroup}
 import uk.ac.warwick.tabula.permissions.PermissionsSelector
-import uk.ac.warwick.tabula.data.model.StudentRelationshipType
-import uk.ac.warwick.tabula.roles.SmallGroupTutorRoleDefinition._
 
 case class SmallGroupEventTutor(smallGroupEvent: SmallGroupEvent) extends BuiltInRole(SmallGroupEventTutorRoleDefinition, smallGroupEvent)
 case class SmallGroupTutor(smallGroup: SmallGroup) extends BuiltInRole(SmallGroupTutorRoleDefinition, smallGroup)
+case class SmallGroupMembersTutor(student: StudentMember) extends BuiltInRole(SmallGroupMembersTutorRoleDefinition, student)
 
 case object SmallGroupEventTutorRoleDefinition extends UnassignableBuiltInRoleDefinition {
 
-	override def description = "SmallGroupEventTutor"
+	override def description = "Small group event tutor"
 
 	GrantsScopedPermission(
 		SmallGroupEvents.Register
@@ -21,13 +21,21 @@ case object SmallGroupEventTutorRoleDefinition extends UnassignableBuiltInRoleDe
 
 case object SmallGroupTutorRoleDefinition extends UnassignableBuiltInRoleDefinition {
 
-	override def description = "SmallGroupTutor"
+	override def description = "Small group tutor"
 
 	GrantsScopedPermission(
 		SmallGroups.Read,
 		SmallGroups.ReadMembership,
-		SmallGroupEvents.ViewRegister,
+		SmallGroupEvents.ViewRegister
+	)
 
+}
+
+case object SmallGroupMembersTutorRoleDefinition extends UnassignableBuiltInRoleDefinition {
+
+	override def description = "Small group member's tutor"
+
+	GrantsScopedPermission(
 		// Extra permissions on any students in these groups for their profile
 		Profiles.Read.Usercode,
 		Profiles.Read.StudentCourseDetails.Core,
--- common/src/main/scala/uk/ac/warwick/tabula/services/permissions/SmallGroupTutorRoleProvider.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/services/permissions/SmallGroupTutorRoleProvider.scala
@@ -1,28 +1,51 @@
 package uk.ac.warwick.tabula.services.permissions
 
 import org.springframework.stereotype.Component
+import uk.ac.warwick.spring.Wire
 import uk.ac.warwick.tabula.CurrentUser
+import uk.ac.warwick.tabula.commands.TaskBenchmarking
+import uk.ac.warwick.tabula.data.model.StudentMember
+import uk.ac.warwick.tabula.data.model.groups.{SmallGroup, SmallGroupEvent}
+import uk.ac.warwick.tabula.helpers.MutablePromise
+import uk.ac.warwick.tabula.helpers.Promises.promise
 import uk.ac.warwick.tabula.permissions.PermissionsTarget
 import uk.ac.warwick.tabula.roles._
-import uk.ac.warwick.tabula.data.model.groups.{SmallGroup, SmallGroupEvent}
-import uk.ac.warwick.tabula.commands.TaskBenchmarking
+import uk.ac.warwick.tabula.services.SmallGroupService
 
 @Component
-class SmallGroupTutorRoleProvider extends RoleProvider with TaskBenchmarking {
+class SmallGroupTutorRoleProvider extends RoleProvider
+	with CustomRolesForAdminDepartments
+	with TaskBenchmarking {
+
+	val smallGroupService: MutablePromise[SmallGroupService] = promise { Wire[SmallGroupService] }
 
 	override def getRolesFor(user: CurrentUser, scope: PermissionsTarget): Stream[Role] = benchmarkTask("Get roles for SmallGroupTutorRoleProvider") {
 		scope match {
 			case event: SmallGroupEvent => getRoles(user, Seq(event))
 			case group: SmallGroup => getRoles(user, group.events)
+
+			case student: StudentMember =>
+				// TAB-5745
+				val studentGroups = smallGroupService.get.findSmallGroupsByStudent(student.asSsoUser)
+				val isTutor =
+					studentGroups.exists { group =>
+						group.groupSet.releasedToTutors && group.events.exists(_.tutors.includesUser(user.apparentUser))
+					}
+
+				if (isTutor) {
+					val custom = customRoles(student, SmallGroupMembersTutorRoleDefinition)
+					Stream(custom.headOption.getOrElse(SmallGroupMembersTutor(student)))
+				} else Stream.empty
+
 			case _ => Stream.empty
 		}
 	}
 
-	private def getRoles(user: CurrentUser, events: Seq[SmallGroupEvent]) = {
+	private def getRoles(user: CurrentUser, events: Iterable[SmallGroupEvent]) = {
 		val validEvents =
 			events.toStream
-				.filter { _.tutors.includesUser(user.apparentUser) }
 				.filter { _.group.groupSet.releasedToTutors }
+				.filter { _.tutors.includesUser(user.apparentUser) }
 
 		val eventRoles = validEvents.map { event =>
 			customRoleFor(event.group.groupSet.module.adminDepartment)(SmallGroupEventTutorRoleDefinition, event).getOrElse(SmallGroupEventTutor(event))
@@ -35,6 +58,6 @@ class SmallGroupTutorRoleProvider extends RoleProvider with TaskBenchmarking {
 		eventRoles #::: groupRoles
 	}
 
-	def rolesProvided = Set(classOf[SmallGroupTutor], classOf[SmallGroupEventTutor])
+	def rolesProvided = Set(classOf[SmallGroupTutor], classOf[SmallGroupEventTutor], classOf[SmallGroupMembersTutor])
 
 }
