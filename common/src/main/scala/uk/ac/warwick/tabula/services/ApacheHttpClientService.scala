0b5b9d7e58d095db891d458f14f4001f7bfb70f3
Alec Cursley <a.cursley@warwick.ac.uk>
2018-01-31 10:40:41 +0000

    TAB-5907 Use new WAI2Go API for location fetching

---
+++ common/src/main/scala/uk/ac/warwick/tabula/services/ApacheHttpClientService.scala
@@ -0,0 +1,40 @@
+package uk.ac.warwick.tabula.services
+
+import org.apache.http.client.config.RequestConfig
+import org.apache.http.impl.client.{CloseableHttpClient, HttpClients}
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+import uk.ac.warwick.spring.Wire
+import uk.ac.warwick.tabula.ScalaFactoryBean
+
+@Service
+class ApacheHttpClientFactoryBean extends ScalaFactoryBean[CloseableHttpClient] {
+	@Value("${httpclient.connectTimeout:20000}") var connectTimeout: Int = _
+	@Value("${httpclient.socketTimeout:20000}") var socketTimeout: Int = _
+	@Value("${httpclient.maxConnections:250}") var maxConnections: Int = _
+	@Value("${httpclient.maxConnectionsPerRoute:100}") var maxConnectionsPerRoute: Int = _
+
+	override def createInstance(): CloseableHttpClient = HttpClients.custom()
+		.setConnectionManager({
+			val manager = new PoolingHttpClientConnectionManager()
+			manager.setMaxTotal(maxConnections)
+			manager.setDefaultMaxPerRoute(maxConnectionsPerRoute)
+			manager
+		})
+		.setDefaultRequestConfig(RequestConfig.custom()
+			.setConnectTimeout(connectTimeout)
+			.setSocketTimeout(socketTimeout)
+			.build())
+		.build()
+
+	override def destroyInstance(instance: CloseableHttpClient): Unit = instance.close()
+}
+
+trait ApacheHttpClientComponent {
+	def httpClient: CloseableHttpClient
+}
+
+trait AutowiringApacheHttpClientComponent extends ApacheHttpClientComponent {
+	var httpClient: CloseableHttpClient = Wire[CloseableHttpClient]
+}
--- common/src/main/scala/uk/ac/warwick/tabula/services/timetables/LocationFetchingService.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/services/timetables/LocationFetchingService.scala
@@ -1,11 +1,15 @@
 package uk.ac.warwick.tabula.services.timetables
 
-import dispatch.classic._
+import org.apache.commons.io.IOUtils
+import org.apache.http.HttpEntity
+import org.apache.http.client.methods.{CloseableHttpResponse, HttpGet}
+import org.apache.http.client.utils.{HttpClientUtils, URIBuilder}
+import org.apache.http.util.EntityUtils
 import uk.ac.warwick.tabula.JavaImports._
 import uk.ac.warwick.tabula.data.model.{Location, MapLocation, NamedLocation}
 import uk.ac.warwick.tabula.helpers.Logging
 import uk.ac.warwick.tabula.services.permissions.AutowiringCacheStrategyComponent
-import uk.ac.warwick.tabula.services.{AutowiringDispatchHttpClientComponent, DispatchHttpClientComponent}
+import uk.ac.warwick.tabula.services.{ApacheHttpClientComponent, AutowiringApacheHttpClientComponent}
 import uk.ac.warwick.util.cache.{Cache, CacheEntryFactory, Caches}
 import uk.ac.warwick.util.web.Uri
 
@@ -46,7 +50,8 @@ class CachedLocationFetchingService(delegate: LocationFetchingService) extends L
 trait WAI2GoConfiguration {
 	def baseUri: Uri
 	def cached: Boolean
-	def defaultProperties: Map[String, String]
+	def defaultHeaders: Map[String, String]
+	def defaultParameters: Map[String, String]
 	def queryParameter: String
 }
 
@@ -58,14 +63,21 @@ trait AutowiringWAI2GoConfigurationComponent extends WAI2GoConfigurationComponen
 	val wai2GoConfiguration = new AutowiringWAI2GoConfiguration
 	class AutowiringWAI2GoConfiguration extends WAI2GoConfiguration {
 		val cached = true
-		val baseUri: Uri = Uri.parse("https://campus.warwick.ac.uk/warwick/API/getAC")
-		val defaultProperties = Map(
-			"mlim" -> "2",
-			"limit" -> "2",
-			"type" -> "lb",
-			"key" -> "$2a$11$ra7c/DofvF6yZuQhl.SBEuLrA8k2fvyt.WlJ8bbI.Asd1OyT.N2JS"
+
+		val projectId: String = "1"
+		val clientId: String = "3"
+
+		val baseUri: Uri = Uri.parse(s"https://campus-cms.warwick.ac.uk/api/v1/projects/$projectId/autocomplete.json")
+		val defaultHeaders: Map[String, String] = Map(
+			"Authorization" -> "Token 3a08c5091e5e477faa6ea90e4ae3e6c3",
+			"Accept" -> "application/json"
 		)
-		val queryParameter = "q"
+		val defaultParameters: Map[String, String] = Map(
+			"client_id" -> clientId,
+			"exact_limit" -> "2",
+			"limit" -> "2"
+		)
+		val queryParameter = "term"
 	}
 }
 
@@ -80,31 +92,49 @@ trait WAI2GoHttpLocationFetchingServiceComponent extends LocationFetchingService
 }
 
 private class WAI2GoHttpLocationFetchingService(config: WAI2GoConfiguration) extends LocationFetchingService with Logging {
-	self: DispatchHttpClientComponent =>
-
-	// a dispatch response handler which reads JSON from the response and parses it into a list of TimetableEvents
-	// the timetable response doesn't include its year, so we pass that in separately.
-	def handler: (Map[String, Seq[String]], Request) => Handler[Seq[WAI2GoLocation]] = { (headers: Map[String,Seq[String]], req: dispatch.classic.Request) =>
-		req >- { (rawJSON) =>
-			JSON.parseFull(rawJSON) match {
-				case Some(locations: Seq[Map[String, Any]] @unchecked) => locations.flatMap(WAI2GoLocation.fromProperties)
-				case _ => Nil
-			}
-		}
-	}
+	self: ApacheHttpClientComponent =>
 
 	def locationFor(name: String): Location = {
-		val req = url(config.baseUri.toString) <<? (config.defaultProperties ++ Map(config.queryParameter -> name))
-
-		// Execute the request
 		logger.info(s"Requesting location info for $name")
-		Try(httpClient.when(_==200)(req >:+ handler)) match {
+
+		val uriBuilder = new URIBuilder(config.baseUri.toJavaUri)
+		uriBuilder.addParameter(config.queryParameter, name)
+		config.defaultParameters.foreach { case (n, v) => uriBuilder.addParameter(n, v) }
+
+		val get = new HttpGet(uriBuilder.build())
+		config.defaultHeaders.foreach { case (n, v) => get.addHeader(n, v) }
+
+		var response: CloseableHttpResponse = null
+		var entity: HttpEntity = null
+
+		val result: Try[Seq[WAI2GoLocation]] = try {
+			response = httpClient.execute(get)
+
+			if (response.getStatusLine.getStatusCode == 200) {
+				entity = response.getEntity
+				val json = IOUtils.toString(entity.getContent)
+
+				JSON.parseFull(json) match {
+					case Some(locations: Seq[Map[String, Any]]@unchecked) => Success(locations.flatMap(WAI2GoLocation.fromProperties))
+					case _ => Success(Nil)
+				}
+			} else {
+				throw new RuntimeException(s"Got unexpected response status ${response.getStatusLine.toString} from the WAI2Go location service")
+			}
+		} catch {
+			case e: Exception => Failure(e)
+		} finally {
+			EntityUtils.consumeQuietly(entity)
+			HttpClientUtils.closeQuietly(response)
+		}
+
+		result match {
 			case Success(locations)
 				if locations.size == 1 =>
 					MapLocation(locations.head.name, locations.head.locationId)
 
 			case Success(locations) =>
-				logger.info(s"Multiple locations returned for $name, returning NamedLocation")
+				logger.info(s"Multiple locations (or no locations) returned for $name, returning NamedLocation")
 				NamedLocation(name)
 
 			case Failure(ex) =>
@@ -116,7 +146,7 @@ private class WAI2GoHttpLocationFetchingService(config: WAI2GoConfiguration) ext
 
 object WAI2GoHttpLocationFetchingService {
 	def apply(config: WAI2GoConfiguration): LocationFetchingService = {
-		val service = new WAI2GoHttpLocationFetchingService(config) with AutowiringDispatchHttpClientComponent
+		val service = new WAI2GoHttpLocationFetchingService(config) with AutowiringApacheHttpClientComponent
 
 		if (config.cached) {
 			new CachedLocationFetchingService(service)
@@ -126,15 +156,16 @@ object WAI2GoHttpLocationFetchingService {
 	}
 }
 
-case class WAI2GoLocation(name: String, subLocation: String, locationId: String)
+case class WAI2GoLocation(name: String, building: String, floor: String, locationId: String)
 object WAI2GoLocation {
 	def fromProperties(properties: Map[String, Any]): Option[WAI2GoLocation] = {
-		val name = properties.get("name").collect { case s: String => s }
-		val locationId = properties.get("lid").collect { case s: String => s }
-		val subLocation = properties.get("sub").collect { case s: String => s }
+		val name = properties.get("value").collect { case s: String => s }
+		val locationId = properties.get("w2gid").collect { case x => x.toString }
+		val building = properties.get("building").collect { case s: String => s }
+		val floor = properties.get("floor").collect { case s: String => s }
 
 		if (name.nonEmpty && locationId.nonEmpty) {
-			Some(WAI2GoLocation(name.get, subLocation.getOrElse(""), locationId.get))
+			Some(WAI2GoLocation(name.get, building.getOrElse(""), floor.getOrElse(""), locationId.get))
 		} else {
 			None
 		}
