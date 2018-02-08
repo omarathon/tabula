5c709f1916b2bf3561ca6e3815a1e3d016a7723a
Archana Mathur <archana.mathur@warwick.ac.uk>
2018-02-08 12:49:47 +0000

    TAB-5769 - Set default bulk feature flag to false

--- common/src/main/scala/uk/ac/warwick/tabula/Features.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/Features.scala
@@ -147,7 +147,7 @@ abstract class Features {
 	@Value("${features.openEndedReminderDateCM2:false}") var openEndedReminderDateCM2: Boolean = defaults.openEndedReminderDateCM2
 	@Value("${features.redirectCM1:true}") var redirectCM1: Boolean = defaults.redirectCM1
 	@Value("${features.moderationSelector:true}") var moderationSelector: Boolean = defaults.moderationSelector
-	@Value("${features.bulkModeration:true}") var bulkModeration: Boolean = defaults.bulkModeration
+	@Value("${features.bulkModeration:false}") var bulkModeration: Boolean = defaults.bulkModeration
 
 
 
