a7152f000641bed72d8dd0fd201c5330f9d01364
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2018-03-05 15:09:03 +0000

    TAB-5699 Treat a pass as 100 and a fail as 0 when calculating the Weighted Mean Module Mark

--- common/src/main/scala/uk/ac/warwick/tabula/services/ModuleRegistrationService.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/services/ModuleRegistrationService.scala
@@ -72,12 +72,19 @@ abstract class AbstractModuleRegistrationService extends ModuleRegistrationServi
 
 	def weightedMeanYearMark(moduleRegistrations: Seq[ModuleRegistration], markOverrides: Map[Module, BigDecimal]): Either[String, BigDecimal] = {
 		val nonNullReplacedMarksAndCats: Seq[(BigDecimal, BigDecimal)] = moduleRegistrations.map(mr => {
-			val mark: BigDecimal = markOverrides.getOrElse(mr.module, mr.firstDefinedMark.map(mark => BigDecimal(mark)).orNull)
+			// TAB-5699 - for pass-fail modules a pass is a 100 and a fail is a 0
+			val mrMark: Option[JBigDecimal] =
+				if(mr.passFail) mr.firstDefinedGrade match {
+					case Some("P") => Some(new JBigDecimal("100"))
+					case Some("F") => Some(new JBigDecimal("0"))
+					case _ => None
+				}
+				else mr.firstDefinedMark
+			val mark: BigDecimal = markOverrides.getOrElse(mr.module, mrMark.map(mark => BigDecimal(mark)).orNull)
 			val cats: BigDecimal = Option(mr.cats).map(c => BigDecimal(c)).orNull
 			(mark, cats)
 		}).filter{case(mark, cats) => mark != null & cats != null}
-		// FIXME - TAB-5699 - Assumes that pass-fail modules are ignored when calculating weightedMeanYearMark. If that is the case remove this warning. Otherwise update code
-		if (nonNullReplacedMarksAndCats.nonEmpty && nonNullReplacedMarksAndCats.size == moduleRegistrations.filterNot(_.passFail).size) {
+		if (nonNullReplacedMarksAndCats.nonEmpty && nonNullReplacedMarksAndCats.size == moduleRegistrations.size) {
 			Right(
 				(nonNullReplacedMarksAndCats.map{case(mark, cats) => mark * cats}.sum / nonNullReplacedMarksAndCats.map{case(_, cats) => cats}.sum)
 					.setScale(1, RoundingMode.HALF_UP)
