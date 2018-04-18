3139fecb78ae980f82105aa59b604032eea6a733
Mat Mannion <M.Mannion@warwick.ac.uk>
2018-04-18 15:28:35 +0100

    TAB-6108 Migrate from dispatch to Apache HTTP Client for exam timetable fetcher

--- common/src/main/scala/uk/ac/warwick/tabula/helpers/ApacheHttpClientUtils.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/helpers/ApacheHttpClientUtils.scala
@@ -1,17 +1,26 @@
 package uk.ac.warwick.tabula.helpers
 
+import java.io.InputStreamReader
 import java.net.URI
 import java.nio.charset.StandardCharsets
 
+import dispatch.classic.Handler
+import javax.xml.parsers.SAXParserFactory
 import org.apache.commons.codec.binary.Base64
+import org.apache.commons.io.IOUtils
 import org.apache.http.auth.{AuthScope, Credentials}
+import org.apache.http.client.ResponseHandler
 import org.apache.http.client.protocol.HttpClientContext
 import org.apache.http.client.utils.URIUtils
+import org.apache.http.entity.ContentType
 import org.apache.http.impl.auth.BasicScheme
-import org.apache.http.impl.client.{BasicAuthCache, BasicCredentialsProvider}
+import org.apache.http.impl.client.{AbstractResponseHandler, BasicAuthCache, BasicCredentialsProvider}
 import org.apache.http.impl.conn.DefaultSchemePortResolver
 import org.apache.http.message.BasicHeader
-import org.apache.http.{Header, HttpHost}
+import org.apache.http.util.EntityUtils
+import org.apache.http.{Header, HttpEntity, HttpHost}
+
+import scala.xml.XML
 
 trait ApacheHttpClientUtils {
 	def preemptiveBasicAuthContext(uri: URI, credentials: Credentials): HttpClientContext = {
@@ -39,6 +48,29 @@ trait ApacheHttpClientUtils {
 
 		new BasicHeader("Authorization", s"Basic $encodedCredentials")
 	}
+
+	def xmlResponseHandler[A](block: xml.Elem => A): ResponseHandler[A] =
+		new AbstractResponseHandler[A] {
+			override def handleEntity(entity: HttpEntity): A = {
+				val in = entity.getContent
+				try {
+					val charset = ContentType.getLenientOrDefault(entity).getCharset
+					val reader = new InputStreamReader(in, charset)
+					val xml = XML.withSAXParser(Handler.saxParserFactory.newSAXParser).load(reader)
+					block(xml)
+				} finally {
+					IOUtils.closeQuietly(in)
+					EntityUtils.consumeQuietly(entity)
+				}
+			}
+		}
 }
 
-object ApacheHttpClientUtils extends ApacheHttpClientUtils
+object ApacheHttpClientUtils extends ApacheHttpClientUtils {
+	// retain factory to use with XML.load; its newInstance method is not thread-safe
+	lazy val saxParserFactory: SAXParserFactory = {
+		val spf = SAXParserFactory.newInstance()
+		spf.setNamespaceAware(false)
+		spf
+	}
+}
