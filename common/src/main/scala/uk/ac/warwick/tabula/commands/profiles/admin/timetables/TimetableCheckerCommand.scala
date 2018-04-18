cd5e00efb4ca22a0065680cd354f18bcefb5c816
Mat Mannion <M.Mannion@warwick.ac.uk>
2018-04-18 14:11:31 +0100

    TAB-6108 Migrate from dispatch to Apache HTTP Client for the Timetable Checker

--- common/src/main/scala/uk/ac/warwick/tabula/commands/profiles/admin/timetables/TimetableCheckerCommand.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/commands/profiles/admin/timetables/TimetableCheckerCommand.scala
@@ -1,58 +1,90 @@
 package uk.ac.warwick.tabula.commands.profiles.admin.timetables
 
+import java.io.IOException
+
 import com.fasterxml.jackson.databind.ObjectMapper
-import dispatch.classic.url
+import org.apache.http.auth.{AuthScope, UsernamePasswordCredentials}
+import org.apache.http.client.ResponseHandler
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.protocol.HttpClientContext
+import org.apache.http.client.utils.{URIBuilder, URIUtils}
+import org.apache.http.impl.auth.BasicScheme
+import org.apache.http.impl.client.{BasicAuthCache, BasicCredentialsProvider}
+import org.apache.http.impl.conn.DefaultSchemePortResolver
+import org.apache.http.util.EntityUtils
+import org.apache.http.{HttpHost, HttpResponse, HttpStatus}
 import uk.ac.warwick.tabula.commands._
 import uk.ac.warwick.tabula.permissions.Permissions
 import uk.ac.warwick.tabula.services.timetables._
-import uk.ac.warwick.tabula.services.{AutowiringDispatchHttpClientComponent, DispatchHttpClientComponent}
+import uk.ac.warwick.tabula.services.{ApacheHttpClientComponent, AutowiringApacheHttpClientComponent}
 import uk.ac.warwick.tabula.system.permissions.{PermissionsChecking, RequiresPermissionsChecking}
 import uk.ac.warwick.tabula.{JsonObjectMapperFactory, RequestFailedException}
 
 import scala.util.{Failure, Success, Try}
 
 object TimetableCheckerCommand {
+	type Result = String
+	type Command = Appliable[Result]
 
-	def apply() =
+	def apply(): Command =
 		new TimetableCheckerCommandInternal()
-			with ComposableCommand[Unit]
+			with ComposableCommand[String]
 			with TimetableCheckerCommandPermissions
 			with AutowiringCelcatConfigurationComponent
-			with AutowiringDispatchHttpClientComponent
+			with AutowiringApacheHttpClientComponent
 			with Unaudited
 }
 
-class TimetableCheckerCommandInternal() extends CommandInternal[Unit] with TimetableCheckerCommandState {
+class TimetableCheckerCommandInternal() extends CommandInternal[String] with TimetableCheckerCommandRequest {
 	self: CelcatConfigurationComponent
-	with DispatchHttpClientComponent =>
-	def applyInternal(): Unit = {
+		with ApacheHttpClientComponent =>
 
-		val jsonMapper: ObjectMapper = new JsonObjectMapperFactory().createInstance
+	val jsonMapper: ObjectMapper = JsonObjectMapperFactory.instance
 
+	def applyInternal(): String = {
 		val wbsConfiguration: CelcatDepartmentConfiguration = celcatConfiguration.wbsConfiguration
 
-		def handler = { (headers: Map[String, Seq[String]], req: dispatch.classic.Request) =>
-			req >- { (jsonString) =>
-				val jsonObject = if (jsonMapper != null) jsonMapper.readValue(jsonString, classOf[List[Map[String, Any]]])
-				jsonMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject)
-			}
+		val uriBuilder = new URIBuilder(wbsConfiguration.baseUri)
+		uriBuilder.setPath(s"/$warwickUniId")
+		uriBuilder.addParameter("forcebasic", "true")
+
+		val uri = uriBuilder.build()
+		val host = new HttpHost(uriBuilder.getHost, DefaultSchemePortResolver.INSTANCE.resolve(URIUtils.extractHost(uri)), uriBuilder.getScheme)
+
+		val credsProvider = new BasicCredentialsProvider
+		credsProvider.setCredentials(new AuthScope(host), new UsernamePasswordCredentials(wbsConfiguration.credentials.username, wbsConfiguration.credentials.password))
+
+		// Create AuthCache instance
+		val authCache = new BasicAuthCache
+		// Generate BASIC scheme object and add it to the local auth cache
+		val basicAuth = new BasicScheme
+		authCache.put(host, basicAuth)
+
+		// Add AuthCache to the execution context
+		val context = HttpClientContext.create
+		context.setCredentialsProvider(credsProvider)
+		context.setAuthCache(authCache)
+
+		val req = new HttpGet(uri)
+
+		def handler: ResponseHandler[String] = { response: HttpResponse =>
+			if (response.getStatusLine.getStatusCode != HttpStatus.SC_OK)
+				throw new IOException(s"Received invalid status code: ${response.getStatusLine.getStatusCode}")
+
+			val jsonString = EntityUtils.toString(response.getEntity)
+			val jsonObject = if (jsonMapper != null) jsonMapper.readValue(jsonString, classOf[List[Map[String, Any]]])
+			jsonMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject)
 		}
 
-		val req =
-			(url(wbsConfiguration.baseUri) / warwickUniId <<? Map("forcebasic" -> "true"))
-				.as_!(wbsConfiguration.credentials.username, wbsConfiguration.credentials.password)
-
-		Try(httpClient.when(_ == 200)(req >:+ handler))
-		match {
-			case Success(jsonData) => wbsFeed = jsonData
+		Try(httpClient.execute(req, handler)) match {
+			case Success(jsonData) => jsonData
 			case Failure(e) => throw new RequestFailedException("The WBS timetabling service could not be reached", e)
 		}
 	}
 }
 
-trait TimetableCheckerCommandState {
+trait TimetableCheckerCommandRequest {
 	var warwickUniId: String = _
-	var wbsFeed: String = _
 }
 
 trait TimetableCheckerCommandPermissions extends RequiresPermissionsChecking {
--- web/src/main/scala/uk/ac/warwick/tabula/web/controllers/profiles/admin/timetables/TimetableCheckerController.scala
+++ web/src/main/scala/uk/ac/warwick/tabula/web/controllers/profiles/admin/timetables/TimetableCheckerController.scala
@@ -3,29 +3,29 @@ package uk.ac.warwick.tabula.web.controllers.profiles.admin.timetables
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{ModelAttribute, RequestMapping}
 import uk.ac.warwick.tabula.AcademicYear
-import uk.ac.warwick.tabula.commands.Appliable
 import uk.ac.warwick.tabula.commands.profiles.admin.timetables.TimetableCheckerCommand
 import uk.ac.warwick.tabula.web.Mav
 import uk.ac.warwick.tabula.web.controllers.profiles.ProfilesController
 
 @Controller
-@RequestMapping(value=Array("/profiles/admin/timetablechecker"))
+@RequestMapping(value = Array("/profiles/admin/timetablechecker"))
 class TimetableCheckerController extends ProfilesController {
 
-	type TimetableCheckerCommand = Appliable[Unit]
-
 	@ModelAttribute("command")
-	def command(): TimetableCheckerCommand = TimetableCheckerCommand()
+	def command(): TimetableCheckerCommand.Command = TimetableCheckerCommand()
 
 	@RequestMapping(method=Array(GET, HEAD))
-	def showForm(@ModelAttribute("command") cmd: TimetableCheckerCommand):Mav = {
+	def showForm(@ModelAttribute("command") cmd: TimetableCheckerCommand.Command): Mav = {
 		Mav("profiles/admin/timetablechecker")
 	}
 
 	@RequestMapping(method=Array(POST))
-	def submit(@ModelAttribute("command") command: TimetableCheckerCommand): Mav = {
-		command.apply()
+	def submit(@ModelAttribute("command") command: TimetableCheckerCommand.Command): Mav = {
+		val wbsFeed = command.apply()
 
-		Mav("profiles/admin/timetablechecker_results", "academicYear" -> AcademicYear.now().getLabel.replace("/", ""))
+		Mav("profiles/admin/timetablechecker_results",
+			"academicYear" -> AcademicYear.now().getLabel.replace("/", ""),
+			"wbsFeed" -> wbsFeed
+		)
 	}
 }
