cc04abdec87809f81db44e0d20fc819932f766e7
Mat Mannion <M.Mannion@warwick.ac.uk>
2018-07-09 13:46:40 +0100

    TAB-6355 PR comments: 'manually added students' -> 'manually-added students'

--- common/src/main/webapp/WEB-INF/freemarker/emails/manual_membership_warning.ftl
+++ common/src/main/webapp/WEB-INF/freemarker/emails/manual_membership_warning.ftl
@@ -1,8 +1,8 @@
-There are manually added students in<#if numAssignments gt 0> <@fmt.p numAssignments "assignment"/></#if><#if numAssignments gt 0 && numSmallGroupSets gt 0> and</#if><#if numSmallGroupSets gt 0> <@fmt.p numSmallGroupSets "small group set"/></#if> in ${department.name}:
+There are manually-added students in<#if numAssignments gt 0> <@fmt.p numAssignments "assignment"/></#if><#if numAssignments gt 0 && numSmallGroupSets gt 0> and</#if><#if numSmallGroupSets gt 0> <@fmt.p numSmallGroupSets "small group set"/></#if> in ${department.name}:
 
 It's important that SITS holds up-to-date information on which students take each module.
 
 Please take the following action:
 
 1. Update the membership of the<#if numAssignments gt 0> <@fmt.p number=numAssignments singular="assignment" shownumber=false/></#if><#if numAssignments gt 0 && numSmallGroupSets gt 0> and</#if><#if numSmallGroupSets gt 0> <@fmt.p number=numSmallGroupSets singular="small group set" shownumber=false/></#if> in SITS.
-2. Once the membership in SITS is accurate, remove any manually added students from the<#if numAssignments gt 0> <@fmt.p number=numAssignments singular="assignment" shownumber=false/></#if><#if numAssignments gt 0 && numSmallGroupSets gt 0> and</#if><#if numSmallGroupSets gt 0> <@fmt.p number=numSmallGroupSets singular="small group set" shownumber=false/></#if> in Tabula.
+2. Once the membership in SITS is accurate, remove any manually-added students from the<#if numAssignments gt 0> <@fmt.p number=numAssignments singular="assignment" shownumber=false/></#if><#if numAssignments gt 0 && numSmallGroupSets gt 0> and</#if><#if numSmallGroupSets gt 0> <@fmt.p number=numSmallGroupSets singular="small group set" shownumber=false/></#if> in Tabula.
--- common/src/test/scala/uk/ac/warwick/tabula/data/model/notifications/ManualMembershipWarningNotificationTest.scala
+++ common/src/test/scala/uk/ac/warwick/tabula/data/model/notifications/ManualMembershipWarningNotificationTest.scala
@@ -61,35 +61,35 @@ class ManualMembershipWarningNotificationTest extends TestBase with Mockito with
 	@Test
 	def title(): Unit = {
 		new SingleAssignmentFixture {
-			notification.title should be ("1 assignment in IT Services has manually added students")
+			notification.title should be ("1 assignment in IT Services has manually-added students")
 		}
 
 		new SingleSmallGroupSetFixture {
-			notification.title should be ("1 small group set in IT Services has manually added students")
+			notification.title should be ("1 small group set in IT Services has manually-added students")
 		}
 
 		new SingleAssignmentFixture with SingleSmallGroupSetFixture {
-			notification.title should be ("1 assignment and 1 small group set in IT Services have manually added students")
+			notification.title should be ("1 assignment and 1 small group set in IT Services have manually-added students")
 		}
 
 		new MultipleAssignmentsFixture {
-			notification.title should be ("3 assignments in IT Services have manually added students")
+			notification.title should be ("3 assignments in IT Services have manually-added students")
 		}
 
 		new MultipleSmallGroupSetsFixture {
-			notification.title should be ("3 small group sets in IT Services have manually added students")
+			notification.title should be ("3 small group sets in IT Services have manually-added students")
 		}
 
 		new MultipleAssignmentsFixture with MultipleSmallGroupSetsFixture {
-			notification.title should be ("3 assignments and 3 small group sets in IT Services have manually added students")
+			notification.title should be ("3 assignments and 3 small group sets in IT Services have manually-added students")
 		}
 
 		new SingleAssignmentFixture with MultipleSmallGroupSetsFixture {
-			notification.title should be ("1 assignment and 3 small group sets in IT Services have manually added students")
+			notification.title should be ("1 assignment and 3 small group sets in IT Services have manually-added students")
 		}
 
 		new MultipleAssignmentsFixture with SingleSmallGroupSetFixture {
-			notification.title should be ("3 assignments and 1 small group set in IT Services have manually added students")
+			notification.title should be ("3 assignments and 1 small group set in IT Services have manually-added students")
 		}
 	}
 
@@ -97,58 +97,58 @@ class ManualMembershipWarningNotificationTest extends TestBase with Mockito with
 	def content(): Unit = {
 		new SingleAssignmentFixture {
 			val content: String = renderToString(freeMarkerConfig.getTemplate(notification.content.template), notification.content.model)
-			content should startWith ("There are manually added students in 1 assignment in IT Services:")
+			content should startWith ("There are manually-added students in 1 assignment in IT Services:")
 			content should include ("Update the membership of the assignment in SITS")
-			content should include ("remove any manually added students from the assignment in Tabula")
+			content should include ("remove any manually-added students from the assignment in Tabula")
 		}
 
 		new SingleSmallGroupSetFixture {
 			val content: String = renderToString(freeMarkerConfig.getTemplate(notification.content.template), notification.content.model)
-			content should startWith ("There are manually added students in 1 small group set in IT Services:")
+			content should startWith ("There are manually-added students in 1 small group set in IT Services:")
 			content should include ("Update the membership of the small group set in SITS")
-			content should include ("remove any manually added students from the small group set in Tabula")
+			content should include ("remove any manually-added students from the small group set in Tabula")
 		}
 
 		new SingleAssignmentFixture with SingleSmallGroupSetFixture {
 			val content: String = renderToString(freeMarkerConfig.getTemplate(notification.content.template), notification.content.model)
-			content should startWith ("There are manually added students in 1 assignment and 1 small group set in IT Services:")
+			content should startWith ("There are manually-added students in 1 assignment and 1 small group set in IT Services:")
 			content should include ("Update the membership of the assignment and small group set in SITS")
-			content should include ("remove any manually added students from the assignment and small group set in Tabula")
+			content should include ("remove any manually-added students from the assignment and small group set in Tabula")
 		}
 
 		new MultipleAssignmentsFixture {
 			val content: String = renderToString(freeMarkerConfig.getTemplate(notification.content.template), notification.content.model)
-			content should startWith ("There are manually added students in 3 assignments in IT Services:")
+			content should startWith ("There are manually-added students in 3 assignments in IT Services:")
 			content should include ("Update the membership of the assignments in SITS")
-			content should include ("remove any manually added students from the assignments in Tabula")
+			content should include ("remove any manually-added students from the assignments in Tabula")
 		}
 
 		new MultipleSmallGroupSetsFixture {
 			val content: String = renderToString(freeMarkerConfig.getTemplate(notification.content.template), notification.content.model)
-			content should startWith ("There are manually added students in 3 small group sets in IT Services:")
+			content should startWith ("There are manually-added students in 3 small group sets in IT Services:")
 			content should include ("Update the membership of the small group sets in SITS")
-			content should include ("remove any manually added students from the small group sets in Tabula")
+			content should include ("remove any manually-added students from the small group sets in Tabula")
 		}
 
 		new MultipleAssignmentsFixture with MultipleSmallGroupSetsFixture {
 			val content: String = renderToString(freeMarkerConfig.getTemplate(notification.content.template), notification.content.model)
-			content should startWith ("There are manually added students in 3 assignments and 3 small group sets in IT Services:")
+			content should startWith ("There are manually-added students in 3 assignments and 3 small group sets in IT Services:")
 			content should include ("Update the membership of the assignments and small group sets in SITS")
-			content should include ("remove any manually added students from the assignments and small group sets in Tabula")
+			content should include ("remove any manually-added students from the assignments and small group sets in Tabula")
 		}
 
 		new SingleAssignmentFixture with MultipleSmallGroupSetsFixture {
 			val content: String = renderToString(freeMarkerConfig.getTemplate(notification.content.template), notification.content.model)
-			content should startWith ("There are manually added students in 1 assignment and 3 small group sets in IT Services:")
+			content should startWith ("There are manually-added students in 1 assignment and 3 small group sets in IT Services:")
 			content should include ("Update the membership of the assignment and small group sets in SITS")
-			content should include ("remove any manually added students from the assignment and small group sets in Tabula")
+			content should include ("remove any manually-added students from the assignment and small group sets in Tabula")
 		}
 
 		new MultipleAssignmentsFixture with SingleSmallGroupSetFixture {
 			val content: String = renderToString(freeMarkerConfig.getTemplate(notification.content.template), notification.content.model)
-			content should startWith ("There are manually added students in 3 assignments and 1 small group set in IT Services:")
+			content should startWith ("There are manually-added students in 3 assignments and 1 small group set in IT Services:")
 			content should include ("Update the membership of the assignments and small group set in SITS")
-			content should include ("remove any manually added students from the assignments and small group set in Tabula")
+			content should include ("remove any manually-added students from the assignments and small group set in Tabula")
 		}
 	}
 
