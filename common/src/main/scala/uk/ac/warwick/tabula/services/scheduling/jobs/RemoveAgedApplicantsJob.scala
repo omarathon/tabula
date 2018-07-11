7a9f77022ba0b51b778361664b1a99e9ab2a8be3
Kai Lan <k.lan@warwick.ac.uk>
2018-07-11 15:28:21 +0100

    TAB-6197 scheduler job for removed aged applicants WIP

---
+++ common/src/main/scala/uk/ac/warwick/tabula/services/scheduling/jobs/RemoveAgedApplicantsJob.scala
@@ -0,0 +1,18 @@
+package uk.ac.warwick.tabula.services.scheduling.jobs
+
+import org.quartz.{DisallowConcurrentExecution, JobExecutionContext}
+import org.springframework.beans.factory.config.BeanDefinition
+import org.springframework.context.annotation.{Profile, Scope}
+import org.springframework.stereotype.Component
+import uk.ac.warwick.tabula.services.scheduling.AutowiredJobBean
+
+@Component
+@Profile(Array("scheduling"))
+@DisallowConcurrentExecution
+@Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
+class RemoveAgedApplicantsJob extends AutowiredJobBean {
+	override def executeInternal(context: JobExecutionContext): Unit = {
+
+		???
+	}
+}
