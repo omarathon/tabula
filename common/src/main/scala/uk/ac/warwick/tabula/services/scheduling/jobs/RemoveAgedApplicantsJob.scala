d7203bf36e5eb1552dcd9c4f21a6fc100d26992c
Kai Lan <k.lan@warwick.ac.uk>
2018-07-12 11:40:28 +0100

    TAB-6197 basic shape of the job WIP

--- common/src/main/scala/uk/ac/warwick/tabula/services/scheduling/jobs/RemoveAgedApplicantsJob.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/services/scheduling/jobs/RemoveAgedApplicantsJob.scala
@@ -4,6 +4,7 @@ import org.quartz.{DisallowConcurrentExecution, JobExecutionContext}
 import org.springframework.beans.factory.config.BeanDefinition
 import org.springframework.context.annotation.{Profile, Scope}
 import org.springframework.stereotype.Component
+import uk.ac.warwick.tabula.commands.scheduling.RemoveAgedApplicantsCommand
 import uk.ac.warwick.tabula.services.scheduling.AutowiredJobBean
 
 @Component
@@ -14,11 +15,8 @@ class RemoveAgedApplicantsJob extends AutowiredJobBean {
 	override def executeInternal(context: JobExecutionContext): Unit = {
 		if (features.schedulingRemoveAgedApplicantsJob) {
 			exceptionResolver.reportExceptions {
-
-
+				RemoveAgedApplicantsCommand.apply()
 			}
 		}
-
-		???
 	}
 }
