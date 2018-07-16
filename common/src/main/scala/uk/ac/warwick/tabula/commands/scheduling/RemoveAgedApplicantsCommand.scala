bf9db131080404bbff257242f13c1ba688246258
Kai Lan <k.lan@warwick.ac.uk>
2018-07-16 09:17:19 +0100

    TAB-6197 use the new function

--- common/src/main/scala/uk/ac/warwick/tabula/commands/scheduling/RemoveAgedApplicantsCommand.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/commands/scheduling/RemoveAgedApplicantsCommand.scala
@@ -3,7 +3,7 @@ package uk.ac.warwick.tabula.commands.scheduling
 import org.joda.time.DateTime
 import uk.ac.warwick.tabula.commands.CommandInternal
 import uk.ac.warwick.tabula.data.{AutowiringMemberDaoComponent, MemberDaoComponent}
-import uk.ac.warwick.tabula.data.model.MemberUserType
+import uk.ac.warwick.tabula.data.model.{ApplicantMember, MemberUserType}
 import uk.ac.warwick.tabula.permissions.Permissions
 import uk.ac.warwick.tabula.services.permissions.{AutowiringPermissionsServiceComponent, PermissionsServiceComponent}
 import uk.ac.warwick.tabula.system.permissions.{PermissionsChecking, PermissionsCheckingMethods, RequiresPermissionsChecking}
@@ -21,9 +21,8 @@ class RemoveAgedApplicantsCommandInternal extends CommandInternal[Unit] {
 	self: PermissionsServiceComponent with MemberDaoComponent =>
 
 	override protected def applyInternal(): Unit = {
-		memberDao.getMissingSince(
-			from = DateTime.now().minusMonths(2),
-			memberUserType = MemberUserType.Applicant
+		memberDao.getMissingBefore[ApplicantMember](
+			from = DateTime.now().minusMonths(2)
 		).flatMap { universityId =>
 			memberDao.getByUniversityId(
 				universityId = universityId,
