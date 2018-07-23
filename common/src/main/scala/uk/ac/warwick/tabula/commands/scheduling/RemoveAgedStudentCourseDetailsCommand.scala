91237b3b013656921a6306e2daf045303534f049
Kai Lan <k.lan@warwick.ac.uk>
2018-07-23 14:30:28 +0100

    TAB-6199 wip

---
+++ common/src/main/scala/uk/ac/warwick/tabula/commands/scheduling/RemoveAgedStudentCourseDetailsCommand.scala
@@ -0,0 +1,49 @@
+package uk.ac.warwick.tabula.commands.scheduling
+
+import org.joda.time.DateTime
+import uk.ac.warwick.tabula.commands.{CommandInternal, ComposableCommand, Describable, Description}
+import uk.ac.warwick.tabula.data.{AutowiringStudentCourseDetailsDaoComponent, StudentCourseDetailsDaoComponent}
+import uk.ac.warwick.tabula.helpers.Logging
+import uk.ac.warwick.tabula.permissions.Permissions
+import uk.ac.warwick.tabula.services.permissions.{AutowiringPermissionsServiceComponent, PermissionsService, PermissionsServiceComponent}
+import uk.ac.warwick.tabula.system.permissions.{PermissionsChecking, PermissionsCheckingMethods, RequiresPermissionsChecking}
+
+object RemoveAgedStudentCourseDetailsCommand {
+	def apply() =
+		new RemoveAgedStudentCourseDetailsCommandInternal
+			with ComposableCommand[Seq[String]]
+			with AutowiringPermissionsServiceComponent
+			with AutowiringStudentCourseDetailsDaoComponent
+			with RemoveAgedStudentCourseDetailsCommandPermission
+			with RemoveAgedStudentCourseDetailsCommandDescription
+}
+
+class RemoveAgedStudentCourseDetailsCommandInternal
+	extends CommandInternal[Seq[String]]
+		with Logging {
+	self: PermissionsServiceComponent
+		with StudentCourseDetailsDaoComponent =>
+	override protected def applyInternal(): Seq[String] = {
+		studentCourseDetailsDao
+			.getByEndDateBefore(DateTime.now.minusYears(7))
+			.filter(_.missingFromImportSince != null)
+			.filter(_.missingFromImportSince.isBefore(DateTime.now().minusYears(1)))
+			.map(studentCourseDetailsDao.delete)
+	}
+}
+
+trait RemoveAgedStudentCourseDetailsCommandPermission
+	extends RequiresPermissionsChecking
+		with PermissionsCheckingMethods {
+	override def permissionsCheck(p: PermissionsChecking) {
+		p.PermissionCheck(Permissions.ImportSystemData)
+	}
+}
+
+trait RemoveAgedStudentCourseDetailsCommandDescription extends Describable[Seq[String]] {
+	override def describe(d: Description) {}
+
+	override def describeResult(d: Description, result: Seq[String]): Unit = {
+		d.studentIds(result)
+	}
+}
