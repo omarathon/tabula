88c408425c825f99b7b8491eb31386209b0f4e73
Kai Lan <k.lan@warwick.ac.uk>
2018-07-30 08:33:51 +0100

    TAB-6199 job for removing scyd

---
+++ common/src/main/scala/uk/ac/warwick/tabula/services/scheduling/jobs/RemoveAgedStudentCourseYearDetailsJob.scala
@@ -0,0 +1,22 @@
+package uk.ac.warwick.tabula.services.scheduling.jobs
+
+import org.quartz.{DisallowConcurrentExecution, JobExecutionContext}
+import org.springframework.beans.factory.config.BeanDefinition
+import org.springframework.context.annotation.{Profile, Scope}
+import org.springframework.stereotype.Component
+import uk.ac.warwick.tabula.commands.scheduling.RemoveAgedStudentCourseDetailsCommand
+import uk.ac.warwick.tabula.services.scheduling.AutowiredJobBean
+
+@Component
+@Profile(Array("scheduling"))
+@DisallowConcurrentExecution
+@Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
+class RemoveAgedStudentCourseYearDetailsJob extends AutowiredJobBean {
+	override def executeInternal(context: JobExecutionContext): Unit = {
+		if (features.removeAgedStudentCourseYearDetailsJob) {
+			exceptionResolver.reportExceptions {
+				RemoveAgedStudentCourseDetailsCommand().apply()
+			}
+		}
+	}
+}
