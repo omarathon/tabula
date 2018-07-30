e5faa3231becfa3bd7528200192381c87fc6b715
Kai Lan <k.lan@warwick.ac.uk>
2018-07-30 08:33:32 +0100

    TAB-6199 job for removing aged mod reg

---
+++ common/src/main/scala/uk/ac/warwick/tabula/services/scheduling/jobs/RemoveAgedModuleRegistrationJob.scala
@@ -0,0 +1,22 @@
+package uk.ac.warwick.tabula.services.scheduling.jobs
+
+import org.quartz.{DisallowConcurrentExecution, JobExecutionContext}
+import org.springframework.beans.factory.config.BeanDefinition
+import org.springframework.context.annotation.{Profile, Scope}
+import org.springframework.stereotype.Component
+import uk.ac.warwick.tabula.commands.scheduling.RemoveAgedModuleRegistrationCommand
+import uk.ac.warwick.tabula.services.scheduling.AutowiredJobBean
+
+@Component
+@Profile(Array("scheduling"))
+@DisallowConcurrentExecution
+@Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
+class RemoveAgedModuleRegistrationJob extends AutowiredJobBean {
+	override def executeInternal(context: JobExecutionContext): Unit = {
+		if (features.removeAgedModuleRegistrationJob) {
+			exceptionResolver.reportExceptions {
+				RemoveAgedModuleRegistrationCommand().apply()
+			}
+		}
+	}
+}
