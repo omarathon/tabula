54fa22177616e0102c994b4b620d29ddb430cdb8
Kai Lan <k.lan@warwick.ac.uk>
2018-09-06 15:06:46 +0100

    TAB-6403 controller for parsing markdown to html

---
+++ web/src/main/scala/uk/ac/warwick/tabula/web/controllers/home/MarkdownParsingController.scala
@@ -0,0 +1,26 @@
+package uk.ac.warwick.tabula.web.controllers.home
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestParam
+import uk.ac.warwick.tabula.CurrentUser
+import uk.ac.warwick.tabula.web.Mav
+import uk.ac.warwick.tabula.web.controllers.BaseController
+import uk.ac.warwick.tabula.web.views.{JSONView, MarkdownRenderer, MarkdownRendererImpl}
+
+
+@Controller
+class MarkdownParsingController extends BaseController with MarkdownRendererImpl {
+
+	self: MarkdownRenderer =>
+
+	@RequestMapping(value = Array("/markdown/toHtml"), method = Array(POST), produces = Array("application/json"))
+	def toHtml(@RequestParam markdownString: String, user: CurrentUser): Mav = {
+		if (user.exists) {
+			Mav(new JSONView(Map("html" -> renderMarkdown(markdownString))))
+		} else {
+			Mav(new JSONView(Map.empty))
+		}
+	}
+
+
+}
