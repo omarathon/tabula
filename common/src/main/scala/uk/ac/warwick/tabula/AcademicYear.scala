59636e2eed6aa68b832c3f053e8b748cb22f01ab
Kai Lan <k.lan@warwick.ac.uk>
2018-09-10 17:27:59 +0100

    TAB-6476 remove extendedAcademicYearCrossover we are now always 61 weeks

--- common/src/main/scala/uk/ac/warwick/tabula/AcademicYear.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/AcademicYear.scala
@@ -86,26 +86,13 @@ object AcademicYear {
 	// An implicit for the UserType to create instances
 	implicit val factory: (JInteger) => AcademicYear = (year: JInteger) => AcademicYear(year)
 
-	// The year at which we stop having ExtendedAcademicYears and just have AcademicYears
-	val extendedAcademicYearCrossover: Int = 2018
-
-	def apply(startYear: Int): AcademicYear =
-		if (startYear < extendedAcademicYearCrossover) JExtendedAcademicYear.starting(startYear)
-		else JAcademicYear.starting(startYear)
+	def apply(startYear: Int): AcademicYear = JAcademicYear.starting(startYear)
 
 	def starting(startYear: Int): AcademicYear = apply(startYear)
 
-	def parse(string: String): AcademicYear = {
-		val year = JAcademicYear.parse(string)
-		if (year.getStartYear < extendedAcademicYearCrossover) JExtendedAcademicYear.starting(year.getStartYear)
-		else year
-	}
+	def parse(string: String): AcademicYear = JAcademicYear.parse(string)
 
-	def forDate(now: LocalDate): AcademicYear = {
-		val year = JAcademicYear.forDate(now.asJava)
-		if (year.getStartYear < extendedAcademicYearCrossover) JExtendedAcademicYear.starting(year.getStartYear)
-		else year
-	}
+	def forDate(now: LocalDate): AcademicYear = JAcademicYear.forDate(now.asJava)
 
 	def forDate(now: DateTime): AcademicYear = forDate(now.toLocalDate)
 	def now(): AcademicYear = forDate(DateTime.now())
