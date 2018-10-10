163999b874b4b4d84a2bbfb485afc325da1833e8
Kai Lan <k.lan@warwick.ac.uk>
2018-10-10 09:48:50 +0100

    wip

--- common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/ReleaseToMarkerNotificationHelper.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/ReleaseToMarkerNotificationHelper.scala
@@ -1,33 +1,25 @@
 package uk.ac.warwick.tabula.data.model.notifications.cm2
 
+import uk.ac.warwick.tabula.data.model.markingworkflow.MarkingWorkflowStage
 import uk.ac.warwick.tabula.data.model.{Assignment, FirstMarkersMap, SecondMarkersMap}
+import uk.ac.warwick.tabula.services.CM2MarkingWorkflowService.Student
 import uk.ac.warwick.tabula.services.CM2MarkingWorkflowServiceComponent
 import uk.ac.warwick.userlookup.User
 
 import scala.collection.JavaConverters._
 
-class ReleaseToMarkerNotificationHelper(assignment: Assignment) {
+class ReleaseToMarkerNotificationHelper(assignment: Assignment, recipient: User) {
 
 	self: CM2MarkingWorkflowServiceComponent =>
 
-	val allMarkers: Seq[User] = assignment.cm2MarkerAllocations.map(_.marker)
+	// all
+	val allStudents: Seq[Student] = cm2MarkingWorkflowService.getAllStudentsForMarker(assignment, recipient)
 
-	val firstMarkerMaps: Seq[FirstMarkersMap] = assignment.firstMarkers.asScala
-
-	val secondMarkerMaps: Seq[SecondMarkersMap] = assignment.secondMarkers.asScala
-
-	def firstMarkers: Seq[User] = {
-		allMarkers.filter(markerUser => firstMarkerMaps.map(_.marker_id).contains(markerUser.getUserId))
-		assignment.cm2MarkerAllocations
-		assignment.cm2MarkingWorkflow.workflowType
-
-		val sss = assignment.cm2MarkingWorkflow.workflowType.allStages.map { stage =>
-			stage -> cm2MarkingWorkflowService.getMarkerAllocations(assignment, stage).getOrElse(Nil, Nil)
+	def studentsAtStages: Seq[(MarkingWorkflowStage, Set[Student])] = {
+		assignment.cm2MarkingWorkflow.workflowType.allStages.map { stage =>
+			stage -> cm2MarkingWorkflowService.getMarkerAllocations(assignment, stage).map {
+				case (marker, students) => (marker, students.filter(allStudents.contains(_)))
+			}.getOrElse(recipient, Set.empty)
 		}
-
-		???
 	}
-
-	def secondMarkers: Seq[User] = allMarkers.filter(markerUser => secondMarkerMaps.map(_.marker_id).contains(markerUser.getUserId))
-
 }
