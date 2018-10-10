ba47d6a95d3d5a9bc2bf65ed03a8de5c6d6dd698
Kai Lan <k.lan@warwick.ac.uk>
2018-10-10 08:38:18 +0100

    TAB-5992 wip

--- common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/ReleaseToMarkerNotificationHelper.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/ReleaseToMarkerNotificationHelper.scala
@@ -1,11 +1,14 @@
 package uk.ac.warwick.tabula.data.model.notifications.cm2
 
 import uk.ac.warwick.tabula.data.model.{Assignment, FirstMarkersMap, SecondMarkersMap}
+import uk.ac.warwick.tabula.services.CM2MarkingWorkflowServiceComponent
 import uk.ac.warwick.userlookup.User
 
 import scala.collection.JavaConverters._
 
 class ReleaseToMarkerNotificationHelper(assignment: Assignment) {
+
+	self: CM2MarkingWorkflowServiceComponent =>
 
 	val allMarkers: Seq[User] = assignment.cm2MarkerAllocations.map(_.marker)
 
@@ -13,7 +16,17 @@ class ReleaseToMarkerNotificationHelper(assignment: Assignment) {
 
 	val secondMarkerMaps: Seq[SecondMarkersMap] = assignment.secondMarkers.asScala
 
-	def firstMarkers: Seq[User] = allMarkers.filter(markerUser => firstMarkerMaps.map(_.marker_id).contains(markerUser.getUserId))
+	def firstMarkers: Seq[User] = {
+		allMarkers.filter(markerUser => firstMarkerMaps.map(_.marker_id).contains(markerUser.getUserId))
+		assignment.cm2MarkerAllocations
+		assignment.cm2MarkingWorkflow.workflowType
+
+		val sss = assignment.cm2MarkingWorkflow.workflowType.allStages.map { stage =>
+			stage -> cm2MarkingWorkflowService.getMarkerAllocations(assignment, stage).getOrElse(Nil, Nil)
+		}
+
+		???
+	}
 
 	def secondMarkers: Seq[User] = allMarkers.filter(markerUser => secondMarkerMaps.map(_.marker_id).contains(markerUser.getUserId))
 
