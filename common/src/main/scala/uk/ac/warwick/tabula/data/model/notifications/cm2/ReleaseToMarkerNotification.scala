8c031b06f982f617b263fd3b5053ce459504218f
Kai Lan <k.lan@warwick.ac.uk>
2018-10-12 14:41:33 +0100

    TAB-5992 simplify the thing a bit

--- common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/ReleaseToMarkerNotification.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/ReleaseToMarkerNotification.scala
@@ -18,47 +18,35 @@ object ReleaseToMarkerNotification {
 @DiscriminatorValue("CM2ReleaseToMarker")
 class ReleaseToMarkerNotification
 	extends NotificationWithTarget[MarkerFeedback, Assignment]
-	with SingleRecipientNotification
-	with UserIdRecipientNotification
-	with AutowiringUserLookupComponent
-	with Logging
-	with AutowiringCM2MarkingWorkflowServiceComponent
-	with AllCompletedActionRequiredNotification {
+		with SingleRecipientNotification
+		with UserIdRecipientNotification
+		with AutowiringUserLookupComponent
+		with Logging
+		with AutowiringCM2MarkingWorkflowServiceComponent
+		with AllCompletedActionRequiredNotification {
 
 	val helper: ReleaseToMarkerNotificationHelper = new ReleaseToMarkerNotificationHelper(assignment, recipient, cm2MarkingWorkflowService)
 
 	def workflowVerb: String = items.asScala.headOption.map(_.entity.stage.verb).getOrElse(MarkingWorkflowStage.DefaultVerb)
 
 	def verb = "released"
+
 	def assignment: Assignment = target.entity
 
 	def title: String = s"${assignment.module.code.toUpperCase}: ${assignment.name} has been released for marking"
 
-	def submissionsCnt = if (assignment.cm2Assignment) {
-		val markerStudents = 	assignment.cm2MarkerAllocations.filter(_.marker == recipient).flatMap(_.students.map(_.getUserId)).distinct
-		markerStudents.count{ stu => assignment.submissions.asScala.exists( s => s.usercode == stu && s.submitted == true) }
-	} else {
-		assignment.getMarkersSubmissions(recipient).distinct.size
-	}
-
-	def allocatedStudents = if (assignment.cm2Assignment) {
-		assignment.cm2MarkerAllocations.filter(_.marker == recipient).flatMap(_.students).distinct.size
-	} else {
-		assignment.markingWorkflow.getMarkersStudents(assignment, recipient).distinct.size
-	}
-
 	def content = FreemarkerModel(ReleaseToMarkerNotification.templateLocation,
-		if(assignment.collectSubmissions) {
+		if (assignment.collectSubmissions) {
 			Map(
 				"assignment" -> assignment,
-				"numAllocated" -> allocatedStudents,
+				"numAllocated" -> helper.studentsAllocatedToThisMarker.size,
 				"studentsAtStagesCount" -> helper.studentsAtStagesCount,
 				"numReleasedFeedbacks" -> items.size,
-				"numReleasedSubmissionsFeedbacks" -> submissionsCnt,
-				"numReleasedNoSubmissionsFeedbacks" -> (allocatedStudents - submissionsCnt),
+				"numReleasedSubmissionsFeedbacks" -> helper.submissionsCount,
+				"numReleasedNoSubmissionsFeedbacks" -> (helper.studentsAllocatedToThisMarker.size - helper.submissionsCount),
 				"workflowVerb" -> workflowVerb
 			)
-		}else{
+		} else {
 			Map(
 				"assignment" -> assignment,
 				"numReleasedFeedbacks" -> items.size,
@@ -68,6 +56,7 @@ class ReleaseToMarkerNotification
 	)
 
 	def url: String = Routes.admin.assignment.markerFeedback(assignment, recipient)
+
 	def urlTitle = s"${workflowVerb} the assignment '${assignment.module.code.toUpperCase} - ${assignment.name}'"
 
 	priority = Warning
--- common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/ReleaseToMarkerNotificationHelper.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/ReleaseToMarkerNotificationHelper.scala
@@ -5,23 +5,30 @@ import uk.ac.warwick.tabula.data.model.Assignment
 import uk.ac.warwick.tabula.services.CM2MarkingWorkflowService.Student
 import uk.ac.warwick.tabula.services.CM2MarkingWorkflowService
 import uk.ac.warwick.userlookup.User
+import scala.collection.JavaConverters._
 
 case class StudentAtStagesCount(stageName: String, count: Int)
 
 class ReleaseToMarkerNotificationHelper(assignment: Assignment, recipient: User, cm2MarkingWorkflowService: CM2MarkingWorkflowService) {
 
 	// all students assigned to recipient (marker) for this assignment
-	val studentsAllocatedToThisMarker: Set[Student] = cm2MarkingWorkflowService.getAllStudentsForMarker(assignment, recipient).toSet
+	val studentsAllocatedToThisMarker: Set[Student] = if (assignment.cm2Assignment) {
+		cm2MarkingWorkflowService.getAllStudentsForMarker(assignment, recipient).toSet
+	} else {
+		assignment.markingWorkflow.getMarkersStudents(assignment, recipient).distinct.toSet
+	}
 
 	// students at each stage that's assigned to the recipient for this assignment
 	def studentsAtStages: Seq[(MarkingWorkflowStage, Set[Student])] = {
-		assignment.cm2MarkingWorkflow.workflowType.allStages.map { stage =>
-			stage -> {
-				val allocationsForAllMarkers = cm2MarkingWorkflowService.getMarkerAllocations(assignment, stage)
-				val allocationsForThisMarker = allocationsForAllMarkers.get(recipient)
-				allocationsForThisMarker.map(_.intersect(studentsAllocatedToThisMarker)).getOrElse(Set.empty)
+		if (assignment.cm2Assignment) {
+			assignment.cm2MarkingWorkflow.workflowType.allStages.map { stage =>
+				stage -> {
+					val allocationsForAllMarkers = cm2MarkingWorkflowService.getMarkerAllocations(assignment, stage)
+					val allocationsForThisMarker = allocationsForAllMarkers.get(recipient)
+					allocationsForThisMarker.map(_.intersect(studentsAllocatedToThisMarker)).getOrElse(Set.empty)
+				}
 			}
-		}
+		} else Seq.empty
 	}
 
 	def studentsAtStagesCount: Seq[StudentAtStagesCount] = {
@@ -29,4 +36,11 @@ class ReleaseToMarkerNotificationHelper(assignment: Assignment, recipient: User,
 			case (stage, students) => StudentAtStagesCount(stage.description, students.size)
 		}
 	}
+
+	def submissionsCount: Int = if (assignment.cm2Assignment) {
+		assignment.cm2MarkerAllocations
+			.filter(_.marker == recipient)
+			.flatMap(_.students.map(_.getUserId))
+			.distinct.count { stu => assignment.submissions.asScala.exists(s => s.usercode == stu && s.submitted) }
+	} else assignment.getMarkersSubmissions(recipient).distinct.size
 }
