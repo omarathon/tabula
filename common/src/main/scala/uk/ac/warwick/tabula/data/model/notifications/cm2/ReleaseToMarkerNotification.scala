acd211daabe0ca6988dd95ac64cda167a542fa8f
Kai Lan <k.lan@warwick.ac.uk>
2018-10-12 15:52:48 +0100

    TAB-5992 tidy up a bit, so it's easier to test

--- common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/ReleaseToMarkerNotification.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/ReleaseToMarkerNotification.scala
@@ -1,6 +1,7 @@
 package uk.ac.warwick.tabula.data.model.notifications.cm2
 
 import javax.persistence.{DiscriminatorValue, Entity}
+import uk.ac.warwick.tabula.JavaImports.JList
 import uk.ac.warwick.tabula.cm2.web.Routes
 import uk.ac.warwick.tabula.data.model.NotificationPriority.Warning
 import uk.ac.warwick.tabula.data.model.markingworkflow.MarkingWorkflowStage
@@ -11,7 +12,39 @@ import uk.ac.warwick.tabula.services.{AutowiringCM2MarkingWorkflowServiceCompone
 import scala.collection.JavaConverters._
 
 object ReleaseToMarkerNotification {
-	val templateLocation = "/WEB-INF/freemarker/emails/released_to_marker_notification.ftl"
+	val templateLocation: String = "/WEB-INF/freemarker/emails/released_to_marker_notification.ftl"
+
+	def renderCollectSubmissions(
+		assignment: Assignment,
+		numAllocated: Int,
+		studentsAtStagesCount: Seq[StudentAtStagesCount],
+		numReleasedFeedbacks: Int,
+		numReleasedSubmissionsFeedbacks: Int,
+		numReleasedNoSubmissionsFeedbacks: Int,
+		workflowVerb: String,
+	): FreemarkerModel = FreemarkerModel(templateLocation,
+		Map(
+			"assignment" -> assignment,
+			"numAllocated" -> numAllocated,
+			"studentsAtStagesCount" -> studentsAtStagesCount,
+			"numReleasedFeedbacks" -> numReleasedFeedbacks,
+			"numReleasedSubmissionsFeedbacks" -> numReleasedSubmissionsFeedbacks,
+			"numReleasedNoSubmissionsFeedbacks" -> numReleasedNoSubmissionsFeedbacks,
+			"workflowVerb" -> workflowVerb
+		)
+	)
+
+	def renderNoCollectingSubmissions(
+		assignment: Assignment,
+		numReleasedFeedbacks: Int,
+		workflowVerb: String
+	): FreemarkerModel = {
+		FreemarkerModel(templateLocation, Map(
+			"assignment" -> assignment,
+			"numReleasedFeedbacks" -> numReleasedFeedbacks,
+			"workflowVerb" -> workflowVerb
+		))
+	}
 }
 
 @Entity
@@ -25,7 +58,7 @@ class ReleaseToMarkerNotification
 		with AutowiringCM2MarkingWorkflowServiceComponent
 		with AllCompletedActionRequiredNotification {
 
-	val helper: ReleaseToMarkerNotificationHelper = new ReleaseToMarkerNotificationHelper(assignment, recipient, cm2MarkingWorkflowService)
+	lazy val helper: ReleaseToMarkerNotificationHelper = new ReleaseToMarkerNotificationHelper(assignment, recipient, cm2MarkingWorkflowService)
 
 	def workflowVerb: String = items.asScala.headOption.map(_.entity.stage.verb).getOrElse(MarkingWorkflowStage.DefaultVerb)
 
@@ -35,25 +68,23 @@ class ReleaseToMarkerNotification
 
 	def title: String = s"${assignment.module.code.toUpperCase}: ${assignment.name} has been released for marking"
 
-	def content = FreemarkerModel(ReleaseToMarkerNotification.templateLocation,
-		if (assignment.collectSubmissions) {
-			Map(
-				"assignment" -> assignment,
-				"numAllocated" -> helper.studentsAllocatedToThisMarker.size,
-				"studentsAtStagesCount" -> helper.studentsAtStagesCount,
-				"numReleasedFeedbacks" -> items.size,
-				"numReleasedSubmissionsFeedbacks" -> helper.submissionsCount,
-				"numReleasedNoSubmissionsFeedbacks" -> (helper.studentsAllocatedToThisMarker.size - helper.submissionsCount),
-				"workflowVerb" -> workflowVerb
-			)
-		} else {
-			Map(
-				"assignment" -> assignment,
-				"numReleasedFeedbacks" -> items.size,
-				"workflowVerb" -> workflowVerb
-			)
-		}
-	)
+	def content: FreemarkerModel = if (assignment.collectSubmissions) {
+		ReleaseToMarkerNotification.renderCollectSubmissions(
+			assignment = assignment,
+			numAllocated = helper.studentsAllocatedToThisMarker.size,
+			studentsAtStagesCount = helper.studentsAtStagesCount,
+			numReleasedFeedbacks = items.size,
+			numReleasedSubmissionsFeedbacks = helper.submissionsCount,
+			numReleasedNoSubmissionsFeedbacks = helper.studentsAllocatedToThisMarker.size - helper.submissionsCount,
+			workflowVerb = workflowVerb
+		)
+	} else {
+		ReleaseToMarkerNotification.renderNoCollectingSubmissions(
+			assignment = assignment,
+			numReleasedFeedbacks = items.size,
+			workflowVerb = workflowVerb
+		)
+	}
 
 	def url: String = Routes.admin.assignment.markerFeedback(assignment, recipient)
 
