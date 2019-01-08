af1ca9bbbb45ee372a20f98eb6698ef994bff0e9
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2019-01-08 09:54:03 +0000

    TAB-6823 - chosenRelationships shouldn't be filtered when that leaves no matches

--- web/src/main/scala/uk/ac/warwick/tabula/web/controllers/profiles/relationships/meetings/MeetingRecordControllerHelper.scala
+++ web/src/main/scala/uk/ac/warwick/tabula/web/controllers/profiles/relationships/meetings/MeetingRecordControllerHelper.scala
@@ -11,9 +11,10 @@ trait MeetingRecordControllerHelper {
 	): Seq[StudentRelationship] = {
 		// Go through the relationships for this SPR code and find one where the current user is the agent.
 		// and also the correct relationType
-		// we do not default to anyone if there's no match
-		manageableRelationships
+		val filteredRelationships = manageableRelationships
 			.filter(rel => rel.agentMember.map(_.universityId).contains(currentUser.universityId))
 			.filter(_.relationshipType == relationshipType)
+
+		if(filteredRelationships.isEmpty) manageableRelationships else filteredRelationships
 	}
 }
