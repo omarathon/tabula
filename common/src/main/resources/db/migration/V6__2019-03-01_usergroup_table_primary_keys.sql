7dedfc582f76b4c6637596e5ded9d1c728451c11
Mat Mannion <M.Mannion@warwick.ac.uk>
2019-03-01 16:36:22 +0000

    Fix migration 6

--- common/src/main/resources/db/migration/V6__2019-03-01_usergroup_table_primary_keys.sql
+++ common/src/main/resources/db/migration/V6__2019-03-01_usergroup_table_primary_keys.sql
@@ -2,6 +2,18 @@ delete from usergroupinclude where usercode is null;
 delete from usergroupexclude where usercode is null;
 delete from usergroupstatic where usercode is null;
 
+delete from usergroupinclude ug1
+            using usergroupinclude ug2
+where ug1.ctid < ug2.ctid and ug1.group_id = ug2.group_id and ug1.usercode = ug2.usercode;
+
+delete from usergroupexclude ug1
+            using usergroupexclude ug2
+where ug1.ctid < ug2.ctid and ug1.group_id = ug2.group_id and ug1.usercode = ug2.usercode;
+
+delete from usergroupstatic ug1
+            using usergroupstatic ug2
+where ug1.ctid < ug2.ctid and ug1.group_id = ug2.group_id and ug1.usercode = ug2.usercode;
+
 alter table usergroupinclude alter column usercode set not null;
 alter table usergroupexclude alter column usercode set not null;
 alter table usergroupstatic alter column usercode set not null;
