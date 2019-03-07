c3e7956bb66c9941218b871fee7628c98dcf9516
Mat Mannion <M.Mannion@warwick.ac.uk>
2019-03-07 09:28:48 +0000

    Functional test fixes

--- common/src/main/scala/uk/ac/warwick/tabula/commands/UpdatesStudentMembership.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/commands/UpdatesStudentMembership.scala
@@ -183,8 +183,11 @@ trait UpdatesStudentMembership {
 
 	/** get UAGs, populated with membership, from the currently stored assessmentGroups */
 	def linkedUpstreamAssessmentGroups: Seq[UpstreamAssessmentGroupInfo] =
-		if (assessmentGroups == null) Seq()
-		else assessmentMembershipService.getUpstreamAssessmentGroupInfo(assessmentGroups.asScala, academicYear)
+		if (assessmentGroups == null)
+			Nil
+		else
+			// Can't use the method on assessmentMembershipService as these may be transient
+			assessmentGroups.asScala.flatMap { _.toUpstreamAssessmentGroupInfo(academicYear) }
 
 	/**
 	 * Returns a sequence of MembershipItems
--- web/src/main/assets/static/js/id7/render.js
+++ web/src/main/assets/static/js/id7/render.js
@@ -17,8 +17,8 @@ require('jquery-ui/ui/widgets/sortable');
 require('jquery-caret-plugin/dist/jquery.caret');
 require('fixed-header-table/jquery.fixedheadertable');
 require('../../libs/jquery-tablesorter/jquery.tablesorter'); // TODO rewrite code to be compatible with npm-published version
-require('bootstrap-datetime-picker/js/bootstrap-datetimepicker');
-require('x-editable/dist/bootstrap3-editable/js/bootstrap-editable');
+require('../../libs/bootstrap-datetimepicker/js/bootstrap-datetimepicker'); // TODO this is a patched version I can't find in npm
+require('../../libs/bootstrap3-editable/js/bootstrap-editable'); // TODO bizarrely npm only has 1.5.1 and we use 1.5.3, we also patch for fa icons
 
 window.Spinner = require('spin.js/spin').Spinner;
 
