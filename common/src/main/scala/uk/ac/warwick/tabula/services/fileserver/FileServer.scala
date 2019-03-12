5858311bd962d3c93f345057a2f0237ca670557a
Mat Mannion <M.Mannion@warwick.ac.uk>
2019-03-12 10:39:31 +0000

    Whitespace crimes

--- common/src/main/scala/uk/ac/warwick/tabula/services/fileserver/FileServer.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/services/fileserver/FileServer.scala
@@ -1,6 +1,6 @@
 package uk.ac.warwick.tabula.services.fileserver
-import java.util
-import java.util.function.IntConsumer
+
+import java.util.{BitSet => JBitSet}
 
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import org.apache.tika.mime.MediaType
@@ -12,230 +12,230 @@ import uk.ac.warwick.tabula.{AutowiringFeaturesComponent, FeaturesComponent}
 @Service
 class FileServer extends StreamsFiles with AutowiringFeaturesComponent {
 
-	/**
-	 * Serves a RenderableFile out to an HTTP response.
-	 */
-	def serve(file: RenderableFile, fileName: Option[String] = None)(implicit request: HttpServletRequest, out: HttpServletResponse) {
-		/*
-		 * There's no consistent standard for encoding in the optional
-		 * "filename" attribute of Content-Disposition, so you should stick
-		 * to the only reliable method of specifying the filename which
-		 * is to put it as the last part of the URL path.
-		 */
-		val dispositionHeader = fileName match {
-			case Some(name) => "attachment;filename=\"" + name + "\""
-			case _ => "attachment"
-		}
+  /**
+    * Serves a RenderableFile out to an HTTP response.
+    */
+  def serve(file: RenderableFile, fileName: Option[String] = None)(implicit request: HttpServletRequest, out: HttpServletResponse) {
+    /*
+     * There's no consistent standard for encoding in the optional
+     * "filename" attribute of Content-Disposition, so you should stick
+     * to the only reliable method of specifying the filename which
+     * is to put it as the last part of the URL path.
+     */
+    val dispositionHeader = fileName match {
+      case Some(name) => "attachment;filename=\"" + name + "\""
+      case _ => "attachment"
+    }
 
-		out.addHeader("Content-Disposition", dispositionHeader)
+    out.addHeader("Content-Disposition", dispositionHeader)
 
-		stream(file)
-	}
+    stream(file)
+  }
 }
 
 /**
- * Sets up the appropriate response headers and streams a renderable file
- *
- * We don't support using the content disposition header here as it's an unreliable way of setting the filename
- */
+  * Sets up the appropriate response headers and streams a renderable file
+  *
+  * We don't support using the content disposition header here as it's an unreliable way of setting the filename
+  */
 trait StreamsFiles {
-	self: FeaturesComponent =>
+  self: FeaturesComponent =>
 
-	def stream(file: RenderableFile)(implicit request: HttpServletRequest, out: HttpServletResponse) {
-		val inStream = file.inputStream
+  def stream(file: RenderableFile)(implicit request: HttpServletRequest, out: HttpServletResponse) {
+    val inStream = file.inputStream
 
-		out.setHeader("Content-Type", file.contentType)
+    out.setHeader("Content-Type", file.contentType)
 
-		file.suggestedFilename.foreach { filename =>
-			val builder = new StringBuilder
-			builder.append(if (isServeInline(MediaType.parse(file.contentType))) "inline" else "attachment")
-			builder.append("; ")
-			encodeToBuilder("filename", filename, builder)
-			out.setHeader("Content-Disposition", builder.toString)
-		}
+    file.suggestedFilename.foreach { filename =>
+      val builder = new StringBuilder
+      builder.append(if (isServeInline(MediaType.parse(file.contentType))) "inline" else "attachment")
+      builder.append("; ")
+      encodeToBuilder("filename", filename, builder)
+      out.setHeader("Content-Disposition", builder.toString)
+    }
 
-		// Restrictive CSP, just enough for Firefox/Chrome's PDF viewer to work
-		out.setHeader("Content-Security-Policy", "default-src 'none'; img-src 'self'; object-src 'self'; plugin-types application/pdf; style-src 'unsafe-inline'")
+    // Restrictive CSP, just enough for Firefox/Chrome's PDF viewer to work
+    out.setHeader("Content-Security-Policy", "default-src 'none'; img-src 'self'; object-src 'self'; plugin-types application/pdf; style-src 'unsafe-inline'")
 
-		handleCaching(file, request, out)
+    handleCaching(file, request, out)
 
-		if (request.getMethod.toUpperCase != "HEAD") {
-			file.contentLength.foreach { length => out.setHeader("Content-Length", length.toString) }
-			Option(inStream).foreach { FileCopyUtils.copy(_, out.getOutputStream) }
-		} else {
-			file.contentLength.foreach { length => out.setHeader("Content-Length", length.toString) }
-		}
-	}
+    if (request.getMethod.toUpperCase != "HEAD") {
+      file.contentLength.foreach { length => out.setHeader("Content-Length", length.toString) }
+      Option(inStream).foreach { FileCopyUtils.copy(_, out.getOutputStream) }
+    } else {
+      file.contentLength.foreach { length => out.setHeader("Content-Length", length.toString) }
+    }
+  }
 
-	private lazy val serveInlineMimeTypes: Set[MediaType] = Set(
-		"text/plain",
-		"application/pdf",
-		"application/postscript",
-		"image/*",
-		"audio/*",
-		"video/*",
-		"application/msword",
-		"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-		"application/vnd.ms-powerpoint",
-		"application/vnd.openxmlformats-officedocument.presentationml.presentation",
-		"application/vnd.openxmlformats-officedocument.presentationml.slideshow",
-		"application/vnd.ms-excel",
-		"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-		"application/zip",
-		"application/rtf",
-		"text/calendar",
-		"message/rfc822"
-	).map(MediaType.parse)
+  private lazy val serveInlineMimeTypes: Set[MediaType] = Set(
+    "text/plain",
+    "application/pdf",
+    "application/postscript",
+    "image/*",
+    "audio/*",
+    "video/*",
+    "application/msword",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/vnd.ms-powerpoint",
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    "application/vnd.openxmlformats-officedocument.presentationml.slideshow",
+    "application/vnd.ms-excel",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/zip",
+    "application/rtf",
+    "text/calendar",
+    "message/rfc822"
+  ).map(MediaType.parse)
 
-	private def isServeInline(mediaType: MediaType): Boolean =
-		serveInlineMimeTypes.exists { serveInline =>
-			serveInline == mediaType || (serveInline.getSubtype == "*" && serveInline.getType == mediaType.getType)
-		}
+  private def isServeInline(mediaType: MediaType): Boolean =
+    serveInlineMimeTypes.exists { serveInline =>
+      serveInline == mediaType || (serveInline.getSubtype == "*" && serveInline.getType == mediaType.getType)
+    }
 
-	// Very simplistic cache headers - needs turbocharging with TAB-929
-	private def handleCaching(file: RenderableFile, request: HttpServletRequest, out: HttpServletResponse) {
-		out.setHeader("Cache-Control", "private")
-		for (expires <- file.cachePolicy.expires) {
-			out.setDateHeader("Expires", (DateTime.now plus expires).getMillis)
-		}
-	}
+  // Very simplistic cache headers - needs turbocharging with TAB-929
+  private def handleCaching(file: RenderableFile, request: HttpServletRequest, out: HttpServletResponse) {
+    out.setHeader("Cache-Control", "private")
+    for (expires <- file.cachePolicy.expires) {
+      out.setDateHeader("Expires", (DateTime.now plus expires).getMillis)
+    }
+  }
 
-	private def charSeqToBitSet(chars: Seq[Char]): util.BitSet = {
-		val ints: Seq[Int] = chars.map(_.toInt)
-		val max = ints.fold(0)(Math.max)
-		assert(max <= 256) // We should only be dealing with 7 or 8 bit chars
-		val bitSet = new util.BitSet(max)
-		ints.foreach(bitSet.set)
-		bitSet
-	}
+  private def charSeqToBitSet(chars: Seq[Char]): JBitSet = {
+    val ints: Seq[Int] = chars.map(_.toInt)
+    val max = ints.fold(0)(Math.max)
+    assert(max <= 256) // We should only be dealing with 7 or 8 bit chars
+    val bitSet = new JBitSet(max)
+    ints.foreach(bitSet.set)
+    bitSet
+  }
 
-	// From https://tools.ietf.org/html/rfc2616#section-2.2
-	//
-	//   separators     = "(" | ")" | "<" | ">" | "@"
-	//                  | "," | ";" | ":" | "\" | <">
-	//                  | "/" | "[" | "]" | "?" | "="
-	//                  | "{" | "}" | SP | HT
-	//
-	// Rich: We exclude <">, "\" since they can be used for quoting/escaping and HT since it is
-	// rarely used and seems like it should be escaped.
-	private val Separators: Seq[Char] = Seq('(', ')', '<', '>', '@', ',', ';', ':', '/', '[', ']', '?', '=', '{', '}', ' ')
+  // From https://tools.ietf.org/html/rfc2616#section-2.2
+  //
+  //   separators     = "(" | ")" | "<" | ">" | "@"
+  //                  | "," | ";" | ":" | "\" | <">
+  //                  | "/" | "[" | "]" | "?" | "="
+  //                  | "{" | "}" | SP | HT
+  //
+  // Rich: We exclude <">, "\" since they can be used for quoting/escaping and HT since it is
+  // rarely used and seems like it should be escaped.
+  private val Separators: Seq[Char] = Seq('(', ')', '<', '>', '@', ',', ';', ':', '/', '[', ']', '?', '=', '{', '}', ' ')
 
-	private val AlphaNum: Seq[Char] = ('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')
+  private val AlphaNum: Seq[Char] = ('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')
 
-	// From https://tools.ietf.org/html/rfc5987#section-3.2.1:
-	//
-	// attr-char     = ALPHA / DIGIT
-	// / "!" / "#" / "$" / "&" / "+" / "-" / "."
-	// / "^" / "_" / "`" / "|" / "~"
-	// ; token except ( "*" / "'" / "%" )
-	private val AttrCharPunctuation: Seq[Char] = Seq('!', '#', '$', '&', '+', '-', '.', '^', '_', '`', '|', '~')
+  // From https://tools.ietf.org/html/rfc5987#section-3.2.1:
+  //
+  // attr-char     = ALPHA / DIGIT
+  // / "!" / "#" / "$" / "&" / "+" / "-" / "."
+  // / "^" / "_" / "`" / "|" / "~"
+  // ; token except ( "*" / "'" / "%" )
+  private val AttrCharPunctuation: Seq[Char] = Seq('!', '#', '$', '&', '+', '-', '.', '^', '_', '`', '|', '~')
 
-	/**
-		* A subset of the 'qdtext' defined in https://tools.ietf.org/html/rfc2616#section-2.2. These are the
-		* characters which can be inside a 'quoted-string' parameter value. These should form a
-		* superset of the [[AttrChar]] set defined below. We exclude some characters which are technically
-		* valid, but might be problematic, e.g. "\" and "%" could be treated as escape characters by some
-		* clients. We can be conservative because we can express these characters clearly as an extended
-		* parameter.
-		*/
-	private val PartialQuotedText: util.BitSet = charSeqToBitSet(
-		AlphaNum ++ AttrCharPunctuation ++
-			// we include 'separators' plus some chars excluded from 'attr-char'
-			Separators ++ Seq('*', '\''))
+  /**
+    * A subset of the 'qdtext' defined in https://tools.ietf.org/html/rfc2616#section-2.2. These are the
+    * characters which can be inside a 'quoted-string' parameter value. These should form a
+    * superset of the [[AttrChar]] set defined below. We exclude some characters which are technically
+    * valid, but might be problematic, e.g. "\" and "%" could be treated as escape characters by some
+    * clients. We can be conservative because we can express these characters clearly as an extended
+    * parameter.
+    */
+  private val PartialQuotedText: JBitSet = charSeqToBitSet(
+    AlphaNum ++ AttrCharPunctuation ++
+      // we include 'separators' plus some chars excluded from 'attr-char'
+      Separators ++ Seq('*', '\''))
 
-	/**
-		* The 'attr-char' values defined in https://tools.ietf.org/html/rfc5987#section-3.2.1. Should be a
-		* subset of [[PartialQuotedText]] defined above.
-		*/
-	private val AttrChar: util.BitSet = charSeqToBitSet(AlphaNum ++ AttrCharPunctuation)
+  /**
+    * The 'attr-char' values defined in https://tools.ietf.org/html/rfc5987#section-3.2.1. Should be a
+    * subset of [[PartialQuotedText]] defined above.
+    */
+  private val AttrChar: JBitSet = charSeqToBitSet(AlphaNum ++ AttrCharPunctuation)
 
-	private val PlaceholderChar: Char = '?'
+  private val PlaceholderChar: Char = '?'
 
-	/**
-		* Grokked from play.core.utils.HttpHeaderParameterEncoding
-		*
-		* Render a parameter name and value, handling character set issues as
-		* recommended in RFC5987.
-		*
-		* Examples:
-		* [[
+  /**
+    * Grokked from play.core.utils.HttpHeaderParameterEncoding
+    *
+    * Render a parameter name and value, handling character set issues as
+    * recommended in RFC5987.
+    *
+    * Examples:
+    * [[
     * render("filename", "foo.txt") ==> "filename=foo.txt"
-		* render("filename", "naïve.txt") ==> "filename=na_ve.txt; filename*=utf8''na%C3%AFve.txt"
-		* ]]
-		*/
-	private def encodeToBuilder(name: String, value: String, builder: StringBuilder): Unit = {
-		// This flag gets set if we encounter extended characters when rendering the
-		// regular parameter value.
-		var hasExtendedChars = false
+    * render("filename", "naïve.txt") ==> "filename=na_ve.txt; filename*=utf8''na%C3%AFve.txt"
+    * ]]
+    */
+  private def encodeToBuilder(name: String, value: String, builder: StringBuilder): Unit = {
+    // This flag gets set if we encounter extended characters when rendering the
+    // regular parameter value.
+    var hasExtendedChars = false
 
-		// Render ASCII parameter
-		// E.g. naïve.txt --> "filename=na_ve.txt"
+    // Render ASCII parameter
+    // E.g. naïve.txt --> "filename=na_ve.txt"
 
-		builder.append(name)
-		builder.append("=\"")
+    builder.append(name)
+    builder.append("=\"")
 
-		// Iterate over code points here, because we only want one
-		// ASCII character or placeholder per logical character. If
-		// we use the value's encoded bytes or chars then we might
-		// end up with multiple placeholders per logical character.
-		value.codePoints().forEach { codePoint =>
-			// We could support a wider range of characters here by using
-			// the 'token' or 'quoted printable' encoding, however it's
-			// simpler to use the subset of characters that is also valid
-			// for extended attributes.
-			if (codePoint >= 0 && codePoint <= 255 && PartialQuotedText.get(codePoint)) {
-				builder.append(codePoint.toChar)
-			} else {
-				// Set flag because we need to render an extended parameter.
-				hasExtendedChars = true
-				// Render a placeholder instead of the unsupported character.
-				builder.append(PlaceholderChar)
-			}
-		}
+    // Iterate over code points here, because we only want one
+    // ASCII character or placeholder per logical character. If
+    // we use the value's encoded bytes or chars then we might
+    // end up with multiple placeholders per logical character.
+    value.codePoints().forEach { codePoint =>
+      // We could support a wider range of characters here by using
+      // the 'token' or 'quoted printable' encoding, however it's
+      // simpler to use the subset of characters that is also valid
+      // for extended attributes.
+      if (codePoint >= 0 && codePoint <= 255 && PartialQuotedText.get(codePoint)) {
+        builder.append(codePoint.toChar)
+      } else {
+        // Set flag because we need to render an extended parameter.
+        hasExtendedChars = true
+        // Render a placeholder instead of the unsupported character.
+        builder.append(PlaceholderChar)
+      }
+    }
 
-		builder.append('"')
+    builder.append('"')
 
-		// Optionally render extended, UTF-8 encoded parameter
-		// E.g. naïve.txt --> "; filename*=utf8''na%C3%AFve.txt"
-		//
-		// Renders both regular and extended parameters, as suggested by:
-		// - https://tools.ietf.org/html/rfc5987#section-4.2
-		// - https://tools.ietf.org/html/rfc6266#section-4.3 (for Content-Disposition filename parameter)
+    // Optionally render extended, UTF-8 encoded parameter
+    // E.g. naïve.txt --> "; filename*=utf8''na%C3%AFve.txt"
+    //
+    // Renders both regular and extended parameters, as suggested by:
+    // - https://tools.ietf.org/html/rfc5987#section-4.2
+    // - https://tools.ietf.org/html/rfc6266#section-4.3 (for Content-Disposition filename parameter)
 
-		if (hasExtendedChars) {
-			def hexDigit(x: Int): Char = (if (x < 10) x + '0' else x - 10 + 'a').toChar
+    if (hasExtendedChars) {
+      def hexDigit(x: Int): Char = (if (x < 10) x + '0' else x - 10 + 'a').toChar
 
-			// From https://tools.ietf.org/html/rfc5987#section-3.2.1:
-			//
-			// Producers MUST use either the "UTF-8" ([RFC3629]) or the "ISO-8859-1"
-			// ([ISO-8859-1]) character set.  Extension character sets (mime-
+      // From https://tools.ietf.org/html/rfc5987#section-3.2.1:
+      //
+      // Producers MUST use either the "UTF-8" ([RFC3629]) or the "ISO-8859-1"
+      // ([ISO-8859-1]) character set.  Extension character sets (mime-
 
-			val CharacterSetName = "utf-8"
+      val CharacterSetName = "utf-8"
 
-			builder.append("; ")
-			builder.append(name)
+      builder.append("; ")
+      builder.append(name)
 
-			builder.append("*=")
-			builder.append(CharacterSetName)
-			builder.append("''")
+      builder.append("*=")
+      builder.append(CharacterSetName)
+      builder.append("''")
 
-			// From https://tools.ietf.org/html/rfc5987#section-3.2.1:
-			//
-			// Inside the value part, characters not contained in attr-char are
-			// encoded into an octet sequence using the specified character set.
-			// That octet sequence is then percent-encoded as specified in Section
-			// 2.1 of [RFC3986].
+      // From https://tools.ietf.org/html/rfc5987#section-3.2.1:
+      //
+      // Inside the value part, characters not contained in attr-char are
+      // encoded into an octet sequence using the specified character set.
+      // That octet sequence is then percent-encoded as specified in Section
+      // 2.1 of [RFC3986].
 
-			val bytes = value.getBytes(CharacterSetName)
-			for (b <- bytes) {
-				if (AttrChar.get(b & 0xFF)) {
-					builder.append(b.toChar)
-				} else {
-					builder.append('%')
-					builder.append(hexDigit((b >> 4) & 0xF))
-					builder.append(hexDigit(b & 0xF))
-				}
-			}
-		}
-	}
+      val bytes = value.getBytes(CharacterSetName)
+      for (b <- bytes) {
+        if (AttrChar.get(b & 0xFF)) {
+          builder.append(b.toChar)
+        } else {
+          builder.append('%')
+          builder.append(hexDigit((b >> 4) & 0xF))
+          builder.append(hexDigit(b & 0xF))
+        }
+      }
+    }
+  }
 }
