3d845fb5cf379f2a015917f8fe0d84e07693f5ff
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2019-04-02 16:50:43 +0100

    TAB-7030 - Include all marker names on the bulk adjustment spreadsheet

--- common/src/main/scala/uk/ac/warwick/tabula/commands/exams/exams/BulkAdjustmentTemplateCommand.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/commands/exams/exams/BulkAdjustmentTemplateCommand.scala
@@ -3,7 +3,8 @@ package uk.ac.warwick.tabula.commands.exams.exams
 import org.apache.poi.xssf.streaming.SXSSFWorkbook
 import uk.ac.warwick.tabula.commands._
 import uk.ac.warwick.tabula.data.HibernateHelpers
-import uk.ac.warwick.tabula.data.model.{Assessment, Assignment, Exam}
+import uk.ac.warwick.tabula.data.model.{Assessment, Assignment, Exam, Feedback}
+import uk.ac.warwick.tabula.helpers.cm2.WorkflowItems
 import uk.ac.warwick.tabula.permissions.Permissions
 import uk.ac.warwick.tabula.services.{AssessmentMembershipServiceComponent, AutowiringAssessmentMembershipServiceComponent}
 import uk.ac.warwick.tabula.system.permissions.{PermissionsChecking, PermissionsCheckingMethods, RequiresPermissionsChecking}
@@ -33,20 +34,38 @@ class BulkAdjustmentTemplateCommandInternal(val assessment: Assessment) extends 
     val sheet = workbook.createSheet("Marks")
     sheet.trackAllColumnsForAutoSizing()
 
+    val markerFields: Seq[String] = assessment match {
+      case a: Assignment if a.markingWorkflow != null => Seq("first-marker", "second-marker")
+      case a: Assignment if a.cm2MarkingWorkflow != null => a.cm2MarkingWorkflow.allocationOrder
+      case _ => Seq()
+    }
+
     // add header row
     val header = sheet.createRow(0)
     header.createCell(0).setCellValue(BulkAdjustmentCommand.StudentIdHeader)
-    header.createCell(1).setCellValue("Original mark")
-    header.createCell(2).setCellValue("Original grade")
-    header.createCell(3).setCellValue("Previous adjusted mark (if any)")
-    header.createCell(4).setCellValue("Previous adjusted grade (if any)")
-    header.createCell(5).setCellValue(BulkAdjustmentCommand.MarkHeader)
-    header.createCell(6).setCellValue(BulkAdjustmentCommand.GradeHeader)
-    header.createCell(7).setCellValue(BulkAdjustmentCommand.ReasonHeader)
-    header.createCell(8).setCellValue(BulkAdjustmentCommand.CommentsHeader)
+    markerFields.zipWithIndex.foreach{case (mf, i) => header.createCell(i+1).setCellValue(mf) }
+    header.createCell(markerFields.size + 1).setCellValue("Original mark")
+    header.createCell(markerFields.size + 2).setCellValue("Original grade")
+    header.createCell(markerFields.size + 3).setCellValue("Previous adjusted mark (if any)")
+    header.createCell(markerFields.size + 4).setCellValue("Previous adjusted grade (if any)")
+    header.createCell(markerFields.size + 5).setCellValue(BulkAdjustmentCommand.MarkHeader)
+    header.createCell(markerFields.size + 6).setCellValue(BulkAdjustmentCommand.GradeHeader)
+    header.createCell(markerFields.size + 7).setCellValue(BulkAdjustmentCommand.ReasonHeader)
+    header.createCell(markerFields.size + 8).setCellValue(BulkAdjustmentCommand.CommentsHeader)
 
     val memberOrder = assessmentMembershipService.determineMembershipUsers(assessment)
       .zipWithIndex.toMap.map { case (user, order) => user.getWarwickId -> order }
+
+    def markerData(feedback: Feedback): Seq[String] = assessment match {
+      case a: Assignment if a.markingWorkflow != null => Seq(
+        a.getStudentsFirstMarker(feedback.studentIdentifier).map(_.getFullName).getOrElse(""),
+        a.getStudentsSecondMarker(feedback.studentIdentifier).map(_.getFullName).getOrElse("")
+      )
+      case a: Assignment if a.cm2MarkingWorkflow != null => a.cm2MarkingWorkflow.allocationOrder.map(role => {
+        feedback.feedbackMarkerByAllocationName(role).map(_.getFullName).getOrElse("")
+      })
+      case _ => Seq()
+    }
 
     assessment.fullFeedback
       .sortBy(f => {
@@ -55,11 +74,13 @@ class BulkAdjustmentTemplateCommandInternal(val assessment: Assessment) extends 
       })
       .foreach(f => {
         val row = sheet.createRow(sheet.getLastRowNum + 1)
+        val md = markerData(f)
         row.createCell(0).setCellValue(f.studentIdentifier)
-        row.createCell(1).setCellValue(f.actualMark.map(_.toString).getOrElse(""))
-        row.createCell(2).setCellValue(f.actualGrade.getOrElse(""))
-        row.createCell(3).setCellValue(f.latestPrivateOrNonPrivateAdjustment.map(_.mark.toString).getOrElse(""))
-        row.createCell(4).setCellValue(
+        md.zipWithIndex.foreach {case (mf, i) => row.createCell(i+1).setCellValue(mf)}
+        row.createCell(md.size + 1).setCellValue(f.actualMark.map(_.toString).getOrElse(""))
+        row.createCell(md.size + 2).setCellValue(f.actualGrade.getOrElse(""))
+        row.createCell(md.size + 3).setCellValue(f.latestPrivateOrNonPrivateAdjustment.map(_.mark.toString).getOrElse(""))
+        row.createCell(md.size + 4).setCellValue(
           f.latestPrivateOrNonPrivateAdjustment.flatMap(_.grade.map(_.toString)).getOrElse("")
         )
       })
