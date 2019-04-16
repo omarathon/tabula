d1f90be4755f629abf03c7cc11c92533dffc3222
Mat Mannion <M.Mannion@warwick.ac.uk>
2019-04-16 16:19:20 +0100

    MitCircsSubmissionService dao calls need wrapping in transactional()

--- common/src/main/scala/uk/ac/warwick/tabula/services/mitcircs/MitCircsSubmissionService.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/services/mitcircs/MitCircsSubmissionService.scala
@@ -1,6 +1,7 @@
 package uk.ac.warwick.tabula.services.mitcircs
 
 import org.springframework.stereotype.Service
+import uk.ac.warwick.tabula.data.Transactions._
 import uk.ac.warwick.spring.Wire
 import uk.ac.warwick.tabula.data.model.{Department, StudentMember}
 import uk.ac.warwick.tabula.data.model.mitcircs.MitigatingCircumstancesSubmission
@@ -27,11 +28,25 @@ abstract class AbstractMitCircsSubmissionService extends MitCircsSubmissionServi
 
 @Service("mitCircsSubmissionService")
 class MitCircsSubmissionServiceImpl extends AbstractMitCircsSubmissionService with AutowiringMitCircsSubmissionDaoComponent {
-  def getById(id: String): Option[MitigatingCircumstancesSubmission] = mitCircsSubmissionDao.getById(id)
-  def getByKey(key: Long): Option[MitigatingCircumstancesSubmission] = mitCircsSubmissionDao.getByKey(key)
-  def saveOrUpdate(submission: MitigatingCircumstancesSubmission): MitigatingCircumstancesSubmission = mitCircsSubmissionDao.saveOrUpdate(submission)
-  def submissionsForStudent(studentMember: StudentMember): Seq[MitigatingCircumstancesSubmission] = mitCircsSubmissionDao.submissionsForStudent(studentMember)
-  def submissionsForDepartment(department: Department): Seq[MitigatingCircumstancesSubmission] = mitCircsSubmissionDao.submissionsForDepartment(department)
+  def getById(id: String): Option[MitigatingCircumstancesSubmission] = transactional(readOnly = true) {
+    mitCircsSubmissionDao.getById(id)
+  }
+
+  def getByKey(key: Long): Option[MitigatingCircumstancesSubmission] = transactional(readOnly = true) {
+    mitCircsSubmissionDao.getByKey(key)
+  }
+
+  def saveOrUpdate(submission: MitigatingCircumstancesSubmission): MitigatingCircumstancesSubmission = transactional() {
+    mitCircsSubmissionDao.saveOrUpdate(submission)
+  }
+
+  def submissionsForStudent(studentMember: StudentMember): Seq[MitigatingCircumstancesSubmission] = transactional(readOnly = true) {
+    mitCircsSubmissionDao.submissionsForStudent(studentMember)
+  }
+
+  def submissionsForDepartment(department: Department): Seq[MitigatingCircumstancesSubmission] = transactional(readOnly = true) {
+    mitCircsSubmissionDao.submissionsForDepartment(department)
+  }
 }
 
 trait MitCircsSubmissionServiceComponent {
