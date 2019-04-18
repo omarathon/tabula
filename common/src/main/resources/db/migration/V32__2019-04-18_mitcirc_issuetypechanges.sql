dfa991c1a1953e38dc378cbecf6d8ef9eaa2a2c3
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2019-04-18 10:36:16 +0100

    TAB-7079 - Form tweaks

---
+++ common/src/main/resources/db/migration/V32__2019-04-18_mitcirc_issuetypechanges.sql
@@ -0,0 +1,3 @@
+update mitigatingcircumstancessubmission set issuetypes = array_replace(issuetypes, 'SeriousMedical', 'Accident');
+update mitigatingcircumstancessubmission set contacts = array_replace(contacts, 'DeanOfStudents', 'FacultySeniorTutor');
+update mitigatingcircumstancessubmission set contacts = array_replace(contacts, 'DirectorOfStudentSupport', 'DirectorOfStudies');
