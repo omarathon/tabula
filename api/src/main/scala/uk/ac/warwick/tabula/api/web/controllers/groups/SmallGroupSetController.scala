414c12a0fa7295f478cb63aad9499d3b63f69743
Alec Cursley <a.cursley@warwick.ac.uk>
2019-05-30 12:42:52 +0100

    TAB-7104 API to allocate students to small groups

--- api/src/main/scala/uk/ac/warwick/tabula/api/web/controllers/groups/SmallGroupSetController.scala
+++ api/src/main/scala/uk/ac/warwick/tabula/api/web/controllers/groups/SmallGroupSetController.scala
@@ -1,24 +1,30 @@
 package uk.ac.warwick.tabula.api.web.controllers.groups
 
 import javax.validation.Valid
-
-import org.springframework.http.MediaType
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.stereotype.Controller
 import org.springframework.validation.Errors
 import org.springframework.web.bind.annotation._
+import uk.ac.warwick.tabula.CurrentUser
+import uk.ac.warwick.tabula.JavaImports.{JList, JMap}
+import uk.ac.warwick.tabula.api.commands.JsonApiRequest
 import uk.ac.warwick.tabula.api.web.controllers.ApiController
 import uk.ac.warwick.tabula.api.web.controllers.groups.SmallGroupSetController.EditSmallGroupSetCommand
 import uk.ac.warwick.tabula.api.web.helpers.SmallGroupSetToJsonConverter
 import uk.ac.warwick.tabula.commands.groups.admin._
-import uk.ac.warwick.tabula.commands.{Appliable, SelfValidating, ViewViewableCommand}
+import uk.ac.warwick.tabula.commands.{Appliable, GroupsObjects, SelfValidating, ViewViewableCommand}
+import uk.ac.warwick.tabula.data.AutowiringSmallGroupDaoComponent
 import uk.ac.warwick.tabula.data.model._
-import uk.ac.warwick.tabula.data.model.groups.SmallGroupSet
+import uk.ac.warwick.tabula.data.model.groups.{SmallGroup, SmallGroupSet}
 import uk.ac.warwick.tabula.groups.web.views.GroupsViewModel
 import uk.ac.warwick.tabula.groups.web.views.GroupsViewModel.{ViewGroup, ViewSet}
 import uk.ac.warwick.tabula.permissions.Permissions
+import uk.ac.warwick.tabula.services.AutowiringUserLookupComponent
 import uk.ac.warwick.tabula.web.Mav
 import uk.ac.warwick.tabula.web.views.{JSONErrorView, JSONView}
+import uk.ac.warwick.userlookup.User
 
+import scala.beans.BeanProperty
 import scala.collection.JavaConverters._
 
 object SmallGroupSetController {
@@ -87,7 +93,7 @@ trait EditSmallGroupSetApi {
   def editCommand(@PathVariable module: Module, @PathVariable smallGroupSet: SmallGroupSet): EditSmallGroupSetCommand =
     ModifySmallGroupSetCommand.edit(module, smallGroupSet)
 
-  @RequestMapping(method = Array(PUT), consumes = Array(MediaType.APPLICATION_JSON_VALUE), produces = Array("application/json"))
+  @RequestMapping(method = Array(PUT), consumes = Array(APPLICATION_JSON_VALUE), produces = Array("application/json"))
   def edit(@RequestBody request: EditSmallGroupSetRequest, @ModelAttribute("editCommand") command: EditSmallGroupSetCommand, errors: Errors): Mav = {
     request.copyTo(command, errors)
     globalValidator.validate(command, errors)
@@ -135,5 +141,68 @@ trait DeleteSmallGroupSetApi {
         "status" -> "ok"
       )))
     }
+  }
+}
+
+@Controller
+@RequestMapping(Array("/v1/module/{module}/groups/{smallGroupSet}/allocations"))
+class AllocateStudentsToSmallGroupsControllerForApi extends SmallGroupSetController with AllocateStudentsToSmallGroupsApi
+
+trait AllocateStudentsToSmallGroupsApi {
+  self: SmallGroupSetController =>
+
+  type SmallGroupAllocationCommand = Appliable[SmallGroupSet] with GroupsObjects[User, SmallGroup] with SelfValidating
+
+  @ModelAttribute(name = "allocateCommand", binding = false)
+  def allocateCommand(@PathVariable module: Module, @PathVariable smallGroupSet: SmallGroupSet, currentUser: CurrentUser): SmallGroupAllocationCommand = {
+    AllocateStudentsToGroupsCommand.apply(module, smallGroupSet, currentUser)
+  }
+
+  @PutMapping(consumes = Array(APPLICATION_JSON_VALUE), produces = Array(APPLICATION_JSON_VALUE))
+  def allocate(@PathVariable smallGroupSet: SmallGroupSet, @RequestBody request: AllocateStudentsToSmallGroupsRequest, @ModelAttribute("allocateCommand") command: SmallGroupAllocationCommand, errors: Errors): Mav = {
+    request.smallGroupSet = smallGroupSet
+    request.validate(errors)
+    request.copyTo(command, errors)
+    globalValidator.validate(command, errors)
+    command.validate(errors)
+
+    if (errors.hasErrors) {
+      Mav(new JSONErrorView(errors))
+    } else {
+      val result = command.apply()
+      Mav(new JSONView(Map(
+        "success" -> true,
+        "status" -> "ok",
+        "allocations" -> result.groups.asScala.map(smallGroup => smallGroup.id -> smallGroup.students.users.map(_.getUserId)).toMap.asJava
+      )))
+    }
+  }
+}
+
+class AllocateStudentsToSmallGroupsRequest extends JsonApiRequest[GroupsObjects[User, SmallGroup]] with AutowiringUserLookupComponent with AutowiringSmallGroupDaoComponent {
+  @BeanProperty var allocations: JMap[String, JList[User]] = _
+
+  var smallGroupSet: SmallGroupSet = _
+
+  def validate(errors: Errors): Unit = {
+    allocations.asScala.keys.foreach { smallGroupId =>
+      if (!smallGroupSet.groups.asScala.exists(_.id == smallGroupId)) {
+        errors.reject("smallGroupSet.allocate.groupNotFound", Array(smallGroupId), "The small group {0} is not found in this small group set")
+      }
+    }
+
+    allocations.asScala.values.flatMap(_.asScala).toSet[User].filterNot(smallGroupSet.isStudentMember).foreach { user =>
+      if (user.isFoundUser) {
+        errors.reject("smallGroupSet.allocate.userNotMember", Array(user.getUserId), "The user {0} is not a member of this small group set")
+      } else {
+        errors.reject("smallGroupSet.allocate.userNotFound", Array(user.getUserId), "The user {0} does not exist")
+      }
+    }
+  }
+
+  override def copyTo(state: GroupsObjects[User, SmallGroup], errors: Errors): Unit = {
+    state.mapping = allocations.asScala.flatMap { case (smallGroupId, users) =>
+      smallGroupSet.groups.asScala.find(_.id == smallGroupId).map(_ -> users.asScala.filter(smallGroupSet.isStudentMember).asJava)
+    }.asJava
   }
 }
