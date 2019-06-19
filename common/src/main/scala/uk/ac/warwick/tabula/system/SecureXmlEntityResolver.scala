95a7c826feaf617fd45e7c6aaf4dd2a97bdadb75
Adam Williams <A.Williams.9@warwick.ac.uk>
2019-06-19 13:57:32 +0100

    TAB-7263 Mat suggestion re. empty string bytes

--- common/src/main/scala/uk/ac/warwick/tabula/system/SecureXmlEntityResolver.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/system/SecureXmlEntityResolver.scala
@@ -8,5 +8,5 @@ import org.xml.sax.{EntityResolver, InputSource, SAXException}
 class SecureXmlEntityResolver extends EntityResolver {
   @throws[SAXException]
   @throws[IOException]
-  def resolveEntity(publicId: String, systemId: String) = new InputSource(new ByteArrayInputStream("".getBytes))
+  def resolveEntity(publicId: String, systemId: String) = new InputSource(new ByteArrayInputStream(Array[Byte]()))
 }
