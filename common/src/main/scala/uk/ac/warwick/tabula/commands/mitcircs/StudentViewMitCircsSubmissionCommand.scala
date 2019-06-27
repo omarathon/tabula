ec1f7dcfc3196a6dd991403f0024532a48a569ae
Mat Mannion <M.Mannion@warwick.ac.uk>
2019-06-27 10:15:04 +0100

    TAB-7293 Bounce users with permissions on the Review screen to that when they try and access the View screen

--- common/src/main/scala/uk/ac/warwick/tabula/commands/mitcircs/StudentViewMitCircsSubmissionCommand.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/commands/mitcircs/StudentViewMitCircsSubmissionCommand.scala
@@ -9,29 +9,32 @@ import uk.ac.warwick.tabula.data.model.mitcircs.MitigatingCircumstancesSubmissio
 import uk.ac.warwick.tabula.permissions.{Permission, Permissions}
 import uk.ac.warwick.tabula.services.mitcircs.{AutowiringMitCircsSubmissionServiceComponent, MitCircsSubmissionServiceComponent}
 import uk.ac.warwick.tabula.system.permissions.{PermissionsChecking, PermissionsCheckingMethods, RequiresPermissionsChecking}
+import uk.ac.warwick.userlookup.User
 
 object StudentViewMitCircsSubmissionCommand {
   type Result = MitigatingCircumstancesSubmission
   type Command = Appliable[Result]
   val RequiredPermission: Permission = Permissions.MitigatingCircumstancesSubmission.Read
 
-  def apply(submission: MitigatingCircumstancesSubmission): Command =
-    new StudentViewMitCircsSubmissionCommandInternal(submission)
+  def apply(submission: MitigatingCircumstancesSubmission, user: User): Command =
+    new StudentViewMitCircsSubmissionCommandInternal(submission, user)
       with ComposableCommand[Result]
       with StudentViewMitCircsSubmissionPermissions
       with StudentViewMitCircsSubmissionDescription
       with AutowiringMitCircsSubmissionServiceComponent
 }
 
-abstract class StudentViewMitCircsSubmissionCommandInternal(val submission: MitigatingCircumstancesSubmission)
+abstract class StudentViewMitCircsSubmissionCommandInternal(val submission: MitigatingCircumstancesSubmission, val user: User)
   extends CommandInternal[Result]
     with StudentViewMitCircsSubmissionState {
   self: MitCircsSubmissionServiceComponent =>
 
   override def applyInternal(): MitigatingCircumstancesSubmission = transactional() {
-    // TODO this needs a check that it is the student currently logged in (could be the person submitting on their behalf)
-    submission.lastViewedByStudent = DateTime.now
-    mitCircsSubmissionService.saveOrUpdate(submission)
+    if (user.getWarwickId == submission.student.universityId) {
+      submission.lastViewedByStudent = DateTime.now
+      mitCircsSubmissionService.saveOrUpdate(submission)
+    }
+
     // TODO find out why this is necessary for related submissions which themselves are linked to a related submission
     Option(submission.relatedSubmission).foreach(HibernateHelpers.initialiseAndUnproxy)
     HibernateHelpers.initialiseAndUnproxy(submission)
@@ -56,4 +59,5 @@ trait StudentViewMitCircsSubmissionDescription extends Describable[Result] {
 
 trait StudentViewMitCircsSubmissionState {
   def submission: MitigatingCircumstancesSubmission
+  def user: User
 }
