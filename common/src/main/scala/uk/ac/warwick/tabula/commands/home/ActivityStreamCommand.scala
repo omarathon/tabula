b5bbb4181730a774b33a6ba5d6a6f66d7ae12a03
Mat Mannion <M.Mannion@warwick.ac.uk>
2019-07-05 13:29:52 +0100

    TAB-7301 Add permissions checking to ActivityStreamCommand

--- common/src/main/scala/uk/ac/warwick/tabula/commands/home/ActivityStreamCommand.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/commands/home/ActivityStreamCommand.scala
@@ -1,24 +1,27 @@
 package uk.ac.warwick.tabula.commands.home
 
-import org.springframework.validation.Errors
+import uk.ac.warwick.tabula.{CurrentUser, PermissionDeniedException}
 import uk.ac.warwick.tabula.commands._
 import uk.ac.warwick.tabula.data.Transactions._
+import uk.ac.warwick.tabula.permissions.{CheckablePermission, Permissions}
 import uk.ac.warwick.tabula.services.ActivityService.PagedActivities
 import uk.ac.warwick.tabula.services._
-import uk.ac.warwick.tabula.system.permissions.PubliclyVisiblePermissions
+import uk.ac.warwick.tabula.system.permissions.{PermissionsChecking, PermissionsCheckingMethods, PubliclyVisiblePermissions, RequiresPermissionsChecking}
 
 object ActivityStreamCommand {
-  def apply(request: ActivityStreamRequest) =
-    new ActivityStreamCommandInternal(request)
-      with ComposableCommand[PagedActivities]
-      with Unaudited
-      with PubliclyVisiblePermissions
-      with ActivityStreamCommandValidation
-      with ReadOnly
+  type Result = PagedActivities
+  type Command = Appliable[Result] with ActivityStreamCommandState
+
+  def apply(request: ActivityStreamRequest, user: CurrentUser): Command =
+    new ActivityStreamCommandInternal(request, user)
+      with ComposableCommand[Result]
+      with ActivityStreamPermissions
+      with Unaudited with ReadOnly
 }
 
 abstract class ActivityStreamCommandInternal(
-  val request: ActivityStreamRequest
+  val request: ActivityStreamRequest,
+  val user: CurrentUser,
 ) extends CommandInternal[PagedActivities]
   with ActivityStreamCommandState
   with NotificationServiceComponent {
@@ -33,13 +36,21 @@ abstract class ActivityStreamCommandInternal(
   }
 }
 
-trait ActivityStreamCommandValidation extends SelfValidating {
+trait ActivityStreamPermissions extends RequiresPermissionsChecking with PermissionsCheckingMethods {
   self: ActivityStreamCommandState =>
-  def validate(errors: Errors) {
 
+  override def permissionsCheck(p: PermissionsChecking): Unit = {
+    // Check that the user is logged in (i.e. that it has dismiss notifications)
+    // This is granted on the global scope
+    p.PermissionCheck(Permissions.Notification.Dismiss, null)
+
+    // Check that the current user matches the user in the request
+    if (user.apparentId != request.user.getUserId)
+      throw new PermissionDeniedException(user, Seq(Permissions.Notification.Dismiss), request.user)
   }
 }
 
 trait ActivityStreamCommandState {
   def request: ActivityStreamRequest
+  def user: CurrentUser
 }
