bfebcb39cddfd1a5d0fa6f59d48656660a85ee6b
Adam Williams <A.Williams.9@warwick.ac.uk>
2019-07-05 15:58:12 +0100

    TAB-7349 Import RequestInfo

--- common/src/main/scala/uk/ac/warwick/tabula/helpers/CsrfTokenTemplateMethod.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/helpers/CsrfTokenTemplateMethod.scala
@@ -1,4 +1,6 @@
 import freemarker.template.TemplateMethodModelEx
+import uk.ac.warwick.tabula.RequestInfo
+
 
 class CsrfTokenTemplateMethod extends TemplateMethodModelEx {
   override def exec(args: java.util.List[_]): Object = {
