391efb87e3fb7f3a7fd68953f9541e3920eaa2eb
Adam Williams <A.Williams.9@warwick.ac.uk>
2019-07-05 12:00:27 +0100

    TAB-7348 WIP CSRF filter

--- common/src/main/scala/uk/ac/warwick/tabula/web/controllers/common/ErrorController.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/web/controllers/common/ErrorController.scala
@@ -1,10 +1,10 @@
 package uk.ac.warwick.tabula.web.controllers.common
 
-import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
-
+import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestHeader
+import uk.ac.warwick.sso.client.CSRFFilter
 import uk.ac.warwick.tabula.system.exceptions.ExceptionResolver
 import uk.ac.warwick.tabula.web.Mav
 import uk.ac.warwick.tabula.web.controllers.BaseController
@@ -34,6 +34,24 @@ class ErrorController extends BaseController {
       )))
     } else {
       Mav("errors/404", "requestedUri" -> requestedUri).noLayoutIf(ajax)
+    }
+  }
+
+  @RequestMapping(Array("/error/400"))
+  def badRequest(implicit request: HttpServletRequest, response: HttpServletResponse): Mav = {
+    val csrfError = request.getAttribute(CSRFFilter.CSRF_ERROR)
+    if (request.isJsonRequest) {
+      Mav(new JSONView(Map(
+        "success" -> false,
+        "status" -> "bad_request",
+        "errors" -> Array(Map("message" -> (if (csrfError != null) "Missing/invalid CSRF token" else "The server cannot or will not process the request due to something that is perceived to be a client error.")))
+      )))
+    } else {
+      if (csrfError != null) {
+        Mav("errors/400_csrf", "csrfError" -> csrfError).noLayoutIf(ajax)
+      } else {
+        Mav("errors/400").noLayoutIf(ajax)
+      }
     }
   }
 
---
+++ common/src/main/webapp/WEB-INF/freemarker/errors/400_csrf.ftl
@@ -0,0 +1,8 @@
+<#escape x as x?html>
+  <h1>This request couldn't be processed, it was detected as potentially dangerous</h1>
+
+  <p>Please return to the previous page and submit the form again.</p>
+
+  <p>If the problem persists, please contact the <a href="mailto:webteam@warwick.ac.uk">ITS Web
+      Team</a> and describe the steps that resulted in reaching this page.</p>
+</#escape>
