29077b3495d2feecb228d9b8418d531b0f1ae8c1
Adam Williams <A.Williams.9@warwick.ac.uk>
2019-07-08 09:14:40 +0100

    TAB-7349 Spacing change

--- common/src/main/scala/uk/ac/warwick/tabula/helpers/CsrfTokenTemplateMethod.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/helpers/CsrfTokenTemplateMethod.scala
@@ -1,7 +1,6 @@
 import freemarker.template.TemplateMethodModelEx
 import uk.ac.warwick.tabula.RequestInfo
 
-
 class CsrfTokenTemplateMethod extends TemplateMethodModelEx {
   override def exec(args: java.util.List[_]): Object = {
     RequestInfo.fromThread.get.csrfToken
