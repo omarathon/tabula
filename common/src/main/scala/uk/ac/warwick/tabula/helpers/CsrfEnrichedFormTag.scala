b3341e958dd3db3d3ba65c1b73187842706624e5
Adam Williams <A.Williams.9@warwick.ac.uk>
2019-07-12 11:47:06 +0100

    TAB-7374 Only inject CSRF input for POST forms

--- common/src/main/scala/uk/ac/warwick/tabula/helpers/CsrfEnrichedFormTag.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/helpers/CsrfEnrichedFormTag.scala
@@ -8,7 +8,7 @@ import uk.ac.warwick.tabula.RequestInfo
 class CsrfEnrichedFormTag extends FormTag {
   override def writeTagContent(tagWriter: TagWriter): Int = {
     val retVal = super.writeTagContent(tagWriter)
-    if (retVal == Tag.EVAL_BODY_INCLUDE) {
+    if (retVal == Tag.EVAL_BODY_INCLUDE && getMethod.equalsIgnoreCase("POST")) {
       tagWriter.startTag("input")
       writeOptionalAttribute(tagWriter, "type", "hidden")
       writeOptionalAttribute(tagWriter, "name", CSRFFilter.CSRF_TOKEN_PROPERTY_NAME)
