e32b2f1fa2fdf2838367608717c4e1d9b4185b6b
Kai Lan <K.Lan@warwick.ac.uk>
2019-07-16 16:49:31 +0100

    Merge pull request #2648 in TAB/tabula from TAB-7401 to develop
    Squashed commit of the following:
    
    commit 613853b9454790716add6b132b7c50cc243487dd
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Tue Jul 16 16:29:49 2019 +0100
    
        TAB-7401 do not treat readyState 0 as an error
    
    commit 9d24517fadb58c76928223f5364c3f1bf2e25326
    Author: Kai Lan <k.lan@warwick.ac.uk>
    Date:   Tue Jul 16 16:10:36 2019 +0100
    
        TAB-7401 null guard

--- web/src/main/assets/static/js/id7/scripts.js
+++ web/src/main/assets/static/js/id7/scripts.js
@@ -1532,11 +1532,16 @@ $(function () {
 // TAB-7304 handle ajax error globally
 $(() => {
   $(document).ajaxError((event, jqXhr, settings) => {
+    // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+    // we do not want to do anything if state is 0. e.g. user quickly navigated away.
+    if (jqXhr.readyState === 0) return;
     const pageErrorToken = $('body').data('error-token');
-    const errorModal = $(`#global-error-modal-${pageErrorToken}`);
-    if (errorModal) errorModal.modal('show');
+    if (pageErrorToken) {
+      const errorModal = $(`#global-error-modal-${pageErrorToken}`);
+      if (errorModal) errorModal.modal('show');
+    }
 
     // throw this so it could be handled by /error/js as we cannot assume failed ajax calls would have reached backend (and logged)
-    throw Error(`Ajax network error on ${window.location.href} when trying to ${settings.type} to ${settings.url}. error token: ${errorToken}`);
+    throw Error(`Ajax network error on ${window.location.href} when trying to ${settings.type} to ${settings.url}. error token: ${pageErrorToken || 'NA'}`);
   });
 });
