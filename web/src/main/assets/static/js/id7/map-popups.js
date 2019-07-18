b5718498e1808a950ea9af26ee21cdeb5ae29103
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2019-07-18 12:43:38 +0100

    TAB-7397 - ESLint for map-popups.js

--- web/src/main/assets/static/js/id7/map-popups.js
+++ web/src/main/assets/static/js/id7/map-popups.js
@@ -1,31 +1,31 @@
 import _ from 'lodash-es';
 
-(function ($) {
-  "use strict";
+import $ from 'jquery';
 
-  var Config = {
+$(() => {
+  const Config = {
     Defaults: {
       container: 'body',
       template: '<div class="popover wide"><div class="arrow"></div><div class="popover-inner"><button type="button" class="close" aria-hidden="true">&#215;</button><h3 class="popover-title"></h3><div class="popover-content"><p></p></div></div></div>',
       html: true,
       content: _.template(['<iframe width="300" height="400" frameborder="0" src="<%- mapUrl %>"></iframe>']),
       placement: 'right',
-      expandClickTarget: false
-    }
+      expandClickTarget: false,
+    },
   };
 
-  $.fn.mapPopups = function (o) {
-    this.each(function () {
-      var $this = $(this), options = $.extend({}, Config.Defaults, o);
-      $this.find('.map-location[data-lid]').add($this.filter('[data-lid]')).each(function () {
-        var $el = $(this);
+  $.fn.mapPopups = function mapPopups(o) {
+    this.each((i, popup) => {
+      const $this = $(popup); const options = $.extend({}, Config.Defaults, o);
+      $this.find('.map-location[data-lid]').add($this.filter('[data-lid]')).each((idx, el) => {
+        const $el = $(el);
         if ($el.data('map-wired')) return;
 
-        var mapUrl = '//campus.warwick.ac.uk/?lite=1&search=' + encodeURIComponent($el.text()) + '&slid=' + encodeURIComponent($el.data('lid'));
+        const mapUrl = `//campus.warwick.ac.uk/?lite=1&search=${encodeURIComponent($el.text())}&slid=${encodeURIComponent($el.data('lid'))}`;
 
-        var $icon = $('<i />').addClass('fa').addClass('fa-map-marker');
-        var $a = $('<a />').addClass('use-popover').attr('href', '#');
-        var $srTitle = $('<span />').addClass('sr-only').text(`View ${$el.text()} on the campus map`);
+        const $icon = $('<i />').addClass('fa').addClass('fa-map-marker');
+        const $a = $('<a />').addClass('use-popover').attr('href', '#');
+        const $srTitle = $('<span />').addClass('sr-only').text(`View ${$el.text()} on the campus map`);
 
         if (options.expandClickTarget) {
           $a.insertAfter($el);
@@ -39,8 +39,8 @@ import _ from 'lodash-es';
           container: options.container,
           template: options.template,
           html: options.html,
-          content: options.content({mapUrl: mapUrl}),
-          placement: options.placement
+          content: options.content({ mapUrl }),
+          placement: options.placement,
         });
 
         $el.data('map-wired', true);
@@ -48,12 +48,12 @@ import _ from 'lodash-es';
     });
   };
 
-  $(function () {
+  $(() => {
     $('.map-location[data-lid]').mapPopups();
 
     // Look for popovers being shown
-    $(document.body).on('shown.bs.popover shown.bs.modal', function () {
-      $(this).mapPopups();
+    $(document.body).on('shown.bs.popover shown.bs.modal', (e) => {
+      $(e.currentTarget).mapPopups();
     });
   });
-})(jQuery);
+});
