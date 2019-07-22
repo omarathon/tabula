5a1d0fe3cd7e61b6a1cffb09ff0b1511e299740c
Adam Williams <A.Williams.9@warwick.ac.uk>
2019-07-22 11:52:50 +0100

    TAB-7391 PR feedback from Alec

--- web/src/main/assets/static/js/id7/csrf-form.js
+++ web/src/main/assets/static/js/id7/csrf-form.js
@@ -8,7 +8,7 @@ export default class CsrfForm {
     if ($meta.length > 0 && $meta.attr('content') !== undefined) {
       csrfTokenValue = $meta.attr('content');
     }
-    const $form = $('<form>');
+    const $form = $('<form>').attr('method', 'POST');
     $form.append($('<input>').attr('type', 'hidden').attr('value', csrfTokenValue).attr('name', 'urn:websignon:csrf'));
     return $form;
   }
