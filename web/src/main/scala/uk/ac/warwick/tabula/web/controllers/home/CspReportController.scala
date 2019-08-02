bd6eecd01b90b718151e724f94c345bc47fd5d8d
Adam Williams <A.Williams.9@warwick.ac.uk>
2019-08-02 10:30:03 +0100

    Suggestion from Mat wrt how to parse the query string

--- web/src/main/scala/uk/ac/warwick/tabula/web/controllers/home/CspReportController.scala
+++ web/src/main/scala/uk/ac/warwick/tabula/web/controllers/home/CspReportController.scala
@@ -7,7 +7,7 @@ import net.logstash.logback.argument.StructuredArguments
 import org.slf4j.{Logger, LoggerFactory}
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.{PostMapping, RequestBody, RequestMapping}
+import org.springframework.web.bind.annotation.{PostMapping, RequestBody, RequestMapping, RequestParam}
 import uk.ac.warwick.tabula.helpers.Logging
 import uk.ac.warwick.tabula.web.Mav
 import uk.ac.warwick.tabula.web.controllers.BaseController
@@ -20,10 +20,10 @@ class CspReportController extends BaseController {
   val SecurityLogger: Logger = LoggerFactory.getLogger("uk.ac.warwick.SECURITY_REPORTS")
 
   @PostMapping(consumes = Array(MediaType.APPLICATION_JSON_VALUE, "application/csp-report"), produces = Array("application/json"))
-  def consumeReport(@RequestBody report: Map[String, Any], request: HttpServletRequest): Mav = {
+  def consumeReport(@RequestBody report: Map[String, Any], @RequestParam(required = false) enforced: Boolean, request: HttpServletRequest): Mav = {
     val payload = report("csp-report").asInstanceOf[Map[String, Object]]
     val data = Map(
-      "csp-report" -> (payload + ("mode" -> getCspMode(request))),
+      "csp-report" -> (payload + ("mode" -> getCspMode(enforced))),
       "request_headers" -> Map(
         "user-agent" -> request.getHeader("User-Agent").maybeText
       )
@@ -37,16 +37,16 @@ class CspReportController extends BaseController {
     )))
   }
 
-  private def getCspMode(request: HttpServletRequest): String = {
-    if (request.getQueryString != null && request.getQueryString.contains("enforced")) "enforced" else "reported"
+  private def getCspMode(enforced: Boolean): String = {
+    if (enforced) "enforced" else "reported"
   }
 
   @PostMapping(consumes = Array("application/reports+json"), produces = Array("application/json"))
-  def consumeReport(@RequestBody reports: Seq[Map[String, Any]], request: HttpServletRequest): Mav = {
+  def consumeReport(@RequestBody reports: Seq[Map[String, Any]], @RequestParam(required = false) enforced: Boolean): Mav = {
     reports.filter { r => r.get("type").contains("csp") && r.contains("body") }.foreach { report => {
       val payload = report("body").asInstanceOf[Map[String, Object]]
       val data = Map(
-        "csp-report" -> (payload + ("mode" -> getCspMode(request))),
+        "csp-report" -> (payload + ("mode" -> getCspMode(enforced))),
         "request_headers" -> Map(
           "user-agent" -> report.get("user_agent")
         )
