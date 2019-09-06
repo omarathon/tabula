e136dcd07a5416caa65afadb5b08245d558b43fc
Kai Lan <k.lan@warwick.ac.uk>
2019-09-06 14:27:06 +0100

    TAB-7365 WIP

--- common/src/main/scala/uk/ac/warwick/tabula/web/views/JSONView.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/web/views/JSONView.scala
@@ -1,20 +1,33 @@
 package uk.ac.warwick.tabula.web.views
 
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
-
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.web.servlet.View
 import uk.ac.warwick.spring.Wire
+import uk.ac.warwick.tabula.Features
 import uk.ac.warwick.tabula.JavaImports._
 
 class JSONView(var json: Any) extends View {
   var objectMapper: ObjectMapper = Wire[ObjectMapper]
+  var features: Features = Wire[Features]
 
   override def getContentType() = "application/json"
 
   override def render(model: JMap[String, _], request: HttpServletRequest, response: HttpServletResponse): Unit = {
     response.setContentType(getContentType)
     val out = response.getWriter
-    objectMapper.writeValue(out, json)
+    if (features.renderStackTracesForAllUsers) {
+      objectMapper.writeValue(out, json)
+    } else {
+      val fullJson = json.asInstanceOf[Map[String, Any]]
+      val result = fullJson.filterNot {
+        case (key, _) => key == "errors"
+      } ++ fullJson.filter {
+        case (key, _) => key == "errors"
+      }.filterNot {
+        case (key, _) => key == "stackTrace"
+      }
+      objectMapper.writeValue(out, result)
+    }
   }
 }
