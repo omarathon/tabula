6c5b168fc630926b4533caf14a3aa42841e10f7a
Kai Lan <k.lan@warwick.ac.uk>
2019-09-09 11:49:02 +0100

    TAB-7365 revert previous changes

--- common/src/main/scala/uk/ac/warwick/tabula/web/views/JSONView.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/web/views/JSONView.scala
@@ -1,38 +1,20 @@
 package uk.ac.warwick.tabula.web.views
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import javax.servlet.http.{HttpServletRequest, HttpServletResponse}
+
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.web.servlet.View
 import uk.ac.warwick.spring.Wire
-import uk.ac.warwick.tabula.Features
 import uk.ac.warwick.tabula.JavaImports._
-
-import scala.util.Try
 
 class JSONView(var json: Any) extends View {
   var objectMapper: ObjectMapper = Wire[ObjectMapper]
-  var features: Features = Wire[Features]
 
   override def getContentType() = "application/json"
 
   override def render(model: JMap[String, _], request: HttpServletRequest, response: HttpServletResponse): Unit = {
     response.setContentType(getContentType)
     val out = response.getWriter
-    if (features.renderStackTracesForAllUsers) {
-      objectMapper.writeValue(out, json)
-    } else {
-      val fullJson = Try(json.asInstanceOf[Map[String, Any]]).getOrElse(Map.empty)
-      val errors = Try(fullJson.get("errors").map(_.asInstanceOf[Array[Map[String, Any]]].flatten.toMap.filterNot {
-        case (key, _) => key.equalsIgnoreCase("stackTrace")
-      })).toOption.flatten
-
-      if (errors.nonEmpty) {
-        objectMapper.writeValue(out, fullJson.filterNot {
-          case (key, _) => key.equalsIgnoreCase("errors")
-        } ++ Map(
-          "errors" -> errors
-        ))
-      } else objectMapper.writeValue(out, json)
-    }
+    objectMapper.writeValue(out, json)
   }
 }
--- common/src/test/scala/uk/ac/warwick/tabula/web/views/JSONViewTest.scala
+++ common/src/test/scala/uk/ac/warwick/tabula/web/views/JSONViewTest.scala
@@ -14,36 +14,10 @@ class JSONViewTest extends TestBase {
 
     val view = new JSONView(Map("yes" -> "no", "bool" -> true, "seq" -> Seq("yes", "no")))
     view.objectMapper = json
-    view.features = emptyFeatures
     view.render(null, req, res)
 
     res.getContentType should be("application/json")
     res.getContentAsString should be("""{"yes":"no","bool":true,"seq":["yes","no"]}""")
-  }
-
-  @Test def notRenderStackTraceIfFeaturedOff() {
-    val req = new MockHttpServletRequest
-    val res = new MockHttpServletResponse
-
-    val view = new JSONView(Map(
-      "yes" -> "no",
-      "bool" -> true,
-      "seq" -> Seq("yes", "no"),
-      "errors" -> Array(
-        Map(
-          "message" -> "wrong",
-          "stacktrace" -> "not this"
-        )
-      ),
-    ))
-    view.objectMapper = json
-    view.features = emptyFeatures
-    view.features.renderStackTracesForAllUsers = false
-
-    view.render(null, req, res)
-
-    res.getContentType should be("application/json")
-    res.getContentAsString should be("""{"yes":"no","bool":true,"seq":["yes","no"],"errors":{"message":"wrong"}}""")
   }
 
   @Test def renderStackTraceIfFeaturedOn() {
@@ -62,8 +36,6 @@ class JSONViewTest extends TestBase {
       ),
     ))
     view.objectMapper = json
-    view.features = emptyFeatures
-    view.features.renderStackTracesForAllUsers = true
 
     view.render(null, req, res)
 
