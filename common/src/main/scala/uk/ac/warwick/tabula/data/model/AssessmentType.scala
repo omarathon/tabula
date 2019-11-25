68071235eb06b10075d41952e2f47e9b3d2c3bdc
Ritchie Allen <Ritchie.Allen@warwick.ac.uk>
2019-11-25 09:30:24 +0000

    TAB-7866 - Mention that the full list of assessment types comes from MAP

--- common/src/main/scala/uk/ac/warwick/tabula/data/model/AssessmentType.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/AssessmentType.scala
@@ -15,6 +15,8 @@ object TabulaAssessmentSubtype extends Enum[TabulaAssessmentSubtype] {
   override val values: immutable.IndexedSeq[TabulaAssessmentSubtype] = findValues
 }
 
+// full list of types taken from here - https://repo.elab.warwick.ac.uk/projects/MAPP/repos/app/browse/app/sits/AssessmentHelper.scala#92-156
+// TODO - fetch the list via an API call to module approval rather than maintaining it twice
 sealed abstract class AssessmentType(val astCode: String, val subtype: TabulaAssessmentSubtype) extends EnumEntry with Convertible[String] {
   val code:String = subtype.code
   override val value: String = astCode
