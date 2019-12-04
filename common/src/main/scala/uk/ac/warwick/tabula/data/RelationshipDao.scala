2d2004ef212b184ba0a110c0bbfeb2051aab69c6
Matt Jones <matthew.jones@warwick.ac.uk>
2019-12-04 10:19:44 +0000

    TAB-7919 - Filter members to active only

--- common/src/main/scala/uk/ac/warwick/tabula/data/RelationshipDao.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/RelationshipDao.scala
@@ -611,17 +611,17 @@ class RelationshipDaoImpl extends RelationshipDao with Daoisms with Logging {
   }
 
   override def getAllCurrentRelationshipsOfType(studentRelationshipType: StudentRelationshipType): Seq[Array[Object]] = {
-    val wasEnabled = isFilterEnabled(Member.ActiveOnlyFilter)
-    if (!wasEnabled) {
-      session.enableFilter(Member.ActiveOnlyFilter)
-    }
-    val results = currentRelationshipBaseCriteria().add(is("relationshipType", studentRelationshipType)).createAlias("_agentMember", "_agentMember").project[Array[Object]](Projections.projectionList()
+    currentRelationshipBaseCriteria()
+      .add(is("relationshipType", studentRelationshipType))
+      .createAlias("_agentMember", "_agentMember")
+      .add(disjunction(
+        is("_agentMember.inUseFlag", "Active"),
+        like("_agentMember.inUseFlag", "Inactive - Starts %")
+      ))
+      .project[Array[Object]](Projections.projectionList()
       .add(groupProperty("_agentMember.universityId"))
       .add(property("_agentMember.firstName"))
-      .add(property("_agentMember.lastName"))).seq
-    if (!wasEnabled) {
-      session.disableFilter(Member.ActiveOnlyFilter)
-    }
-    results
+      .add(property("_agentMember.lastName")))
+      .seq
   }
 }
