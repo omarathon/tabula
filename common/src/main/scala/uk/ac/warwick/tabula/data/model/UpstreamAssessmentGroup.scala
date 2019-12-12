d10280ba66c3bb4d1f7fce212d858e6eeacb4030
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2019-12-12 15:26:12 +0000

    TAB-7962 - SubmissionAndFeedback data should fall back to using the profile service if SSO doesn't return a user

--- common/src/main/scala/uk/ac/warwick/tabula/data/model/UpstreamAssessmentGroup.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/UpstreamAssessmentGroup.scala
@@ -146,5 +146,5 @@ trait UpstreamAssessmentGroupMemberProperties {
 /** currentMembers are all members excluding PWD)  **/
 case class UpstreamAssessmentGroupInfo(upstreamAssessmentGroup: UpstreamAssessmentGroup, currentMembers: Seq[UpstreamAssessmentGroupMember]) {
   lazy val allMembers: Seq[UpstreamAssessmentGroupMember] = upstreamAssessmentGroup.members.asScala.toSeq
-  lazy val resitMembers: Seq[UpstreamAssessmentGroupMember] = allMembers.filter(_.resitExpected.exists(identity))
+  lazy val resitMembers: Seq[UpstreamAssessmentGroupMember] = currentMembers.filter(_.resitExpected.exists(identity))
 }
