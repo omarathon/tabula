e3b4bbffd70cd2108d6f6351715e3cce7c1e5db7
Mat Mannion <M.Mannion@warwick.ac.uk>
2019-12-20 09:48:30 +0000

    TAB-6799 Ain't no party like a route filter party because a route filter party is mandatory

--- common/src/main/scala/uk/ac/warwick/tabula/commands/FiltersStudents.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/commands/FiltersStudents.scala
@@ -68,7 +68,7 @@ trait FiltersStudents extends FilterStudentsOrRelationships {
   def department: Department
 
   def routeRestriction: Option[ScalaRestriction] = inIfNotEmpty(
-    "mostSignificantCourse.currentRoute.code", routes.asScala.map(_.code),
+    "mostSignificantCourse.currentRoute.code", routes.asScala.map(mandatory(_)).map(_.code),
     getAliasPaths("mostSignificantCourse"): _*
   )
 
