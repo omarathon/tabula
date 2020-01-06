a7108fcbb7f41eb74deab9e0cca47d5619eb1f6b
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-01-06 15:58:08 +0000

    Remove test code

--- web/src/main/scala/uk/ac/warwick/tabula/web/controllers/sysadmin/DepartmentMandatoryPermissionsController.scala
+++ web/src/main/scala/uk/ac/warwick/tabula/web/controllers/sysadmin/DepartmentMandatoryPermissionsController.scala
@@ -1,13 +1,9 @@
 package uk.ac.warwick.tabula.web.controllers.sysadmin
 
-import org.quartz.Scheduler
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{ModelAttribute, RequestMapping}
-import uk.ac.warwick.spring.Wire
 import uk.ac.warwick.tabula.commands.sysadmin.DepartmentMandatoryPermissionsCommand
 import uk.ac.warwick.tabula.data.model.CourseType
-import uk.ac.warwick.tabula.helpers.SchedulingHelpers._
-import uk.ac.warwick.tabula.services.scheduling.jobs.DepartmentMandatoryPermissionsWarningJob
 import uk.ac.warwick.tabula.web.Mav
 
 @Controller
@@ -21,11 +17,8 @@ class DepartmentMandatoryPermissionsController extends BaseSysadminController {
   def courseTypes: Seq[CourseType] = CourseType.all
 
   @RequestMapping
-  def summary(@ModelAttribute("command") command: DepartmentMandatoryPermissionsCommand.Command): Mav = {
-    Wire[Scheduler].scheduleNow[DepartmentMandatoryPermissionsWarningJob]()
-
+  def summary(@ModelAttribute("command") command: DepartmentMandatoryPermissionsCommand.Command): Mav =
     Mav("sysadmin/departments/mandatory-permissions", "departmentInfo" -> command.apply())
       .crumbs(SysadminBreadcrumbs.Departments.Home)
-  }
 
 }
