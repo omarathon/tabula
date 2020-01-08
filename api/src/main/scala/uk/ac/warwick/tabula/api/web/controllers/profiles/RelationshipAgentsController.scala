11b2b911ef3774247f42f1af75032d140fac7a60
Adam Williams <A.Williams.9@warwick.ac.uk>
2020-01-08 09:22:51 +0000

    TAB-7997 Open up private webdev relationship agents API to the masses

--- api/src/main/scala/uk/ac/warwick/tabula/api/web/controllers/profiles/RelationshipAgentsController.scala
+++ api/src/main/scala/uk/ac/warwick/tabula/api/web/controllers/profiles/RelationshipAgentsController.scala
@@ -4,22 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{ModelAttribute, PathVariable, RequestMapping}
 import uk.ac.warwick.tabula.api.web.controllers.ApiController
-import uk.ac.warwick.tabula.commands.{Command, ReadOnly, Unaudited, ViewViewableCommand}
-import uk.ac.warwick.tabula.data.model.{Module, StudentRelationshipType}
+import uk.ac.warwick.tabula.commands.{Command, ReadOnly, Unaudited}
+import uk.ac.warwick.tabula.data.model.{Department, StudentRelationshipType}
 import uk.ac.warwick.tabula.permissions.{Permission, Permissions, PermissionsTarget}
 import uk.ac.warwick.tabula.services.RelationshipService
 import uk.ac.warwick.tabula.web.Mav
 import uk.ac.warwick.tabula.web.views.JSONView
 
 @Controller
-@RequestMapping(Array("/v1/relationships/agents/{studentRelationshipType}"))
+@RequestMapping(Array("/v1/relationships/agents/{studentRelationshipType}", "/v1/relationships/agents/{department}/{studentRelationshipType}"))
 class RelationshipAgentsController extends ApiController {
 
   @Autowired var relationshipsService: RelationshipService = _
 
   @ModelAttribute("getCommand")
-  def getCommand(@PathVariable studentRelationshipType: StudentRelationshipType): ViewRelationshipAgentsCommand =
-    new ViewRelationshipAgentsCommand(Permissions.Profiles.StudentRelationship.Read(studentRelationshipType), PermissionsTarget.Global, mandatory(studentRelationshipType))
+  def getCommand(@PathVariable studentRelationshipType: StudentRelationshipType, @PathVariable(required = false) department: Department): ViewRelationshipAgentsCommand =
+    new ViewRelationshipAgentsCommand(Permissions.Profiles.StudentRelationship.Read(studentRelationshipType), Option(department).getOrElse(PermissionsTarget.Global), mandatory(studentRelationshipType))
 
 
   @RequestMapping(method = Array(GET), produces = Array("application/json"))
@@ -40,7 +40,12 @@ class RelationshipAgentsController extends ApiController {
   class ViewRelationshipAgentsCommand(val permission: Permission, val permissionsTarget: PermissionsTarget, val relationshipType: StudentRelationshipType) extends Command[Seq[Array[Object]]] with ReadOnly with Unaudited {
     PermissionCheck(permission, permissionsTarget)
 
-    override def applyInternal(): Seq[Array[Object]] = relationshipsService.listCurrentRelationshipsGlobally(relationshipType)
+    override def applyInternal(): Seq[Array[Object]] = permissionsTarget match {
+      case dept: Department => relationshipsService.listCurrentStudentRelationshipsByDepartment(relationshipType, dept).map(sr =>
+        Seq(sr.agent, sr.agentName, sr.agentLastName).toArray.asInstanceOf[Array[Object]]
+      )
+      case PermissionsTarget.Global => relationshipsService.listCurrentRelationshipsGlobally(relationshipType)
+    }
   }
 
 }
