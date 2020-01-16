633f1577626fabaf2f681ff21a5241f909956fff
Adam Williams <A.Williams.9@warwick.ac.uk>
2020-01-16 16:27:10 +0000

    TAB-8032 Add batching for CM2 plagiarism notifications

--- common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/Cm2MarkedPlagiarisedNotification.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/Cm2MarkedPlagiarisedNotification.scala
@@ -2,10 +2,10 @@ package uk.ac.warwick.tabula.data.model.notifications.cm2
 
 import javax.persistence.{DiscriminatorValue, Entity}
 import org.hibernate.annotations.Proxy
-import uk.ac.warwick.tabula.cm2.web.Routes
 import uk.ac.warwick.tabula.data.model.NotificationPriority.Warning
 import uk.ac.warwick.tabula.data.model._
 import uk.ac.warwick.tabula.services.AutowiringUserLookupComponent
+import uk.ac.warwick.tabula.web.Routes
 import uk.ac.warwick.userlookup.User
 
 @Entity
@@ -13,7 +13,7 @@ import uk.ac.warwick.userlookup.User
 @DiscriminatorValue(value = "Cm2MarkedPlagiarised")
 class Cm2MarkedPlagiarisedNotification extends NotificationWithTarget[Submission, Assignment]
   with SingleItemNotification[Submission] with AutowiringUserLookupComponent
-  with MyWarwickActivity {
+  with MyWarwickActivity with BatchedNotification[NotificationWithTarget[Submission, Assignment] with Cm2MarkedPlagiarisedNotification] {
 
   def submission: Submission = item.entity
 
@@ -34,7 +34,7 @@ class Cm2MarkedPlagiarisedNotification extends NotificationWithTarget[Submission
     moduleManagers.users.toSeq ++ departmentAdmins.users.toSeq
   }
 
-  def url: String = Routes.admin.assignment.submissionsandfeedback(assignment)
+  def url: String = Routes.cm2.admin.assignment.submissionsandfeedback(assignment)
 
   def urlTitle = "view the submissions for this assignment"
 
@@ -48,4 +48,33 @@ class Cm2MarkedPlagiarisedNotification extends NotificationWithTarget[Submission
     "module" -> module,
     "student" -> student
   ))
+
+  override def titleForBatchInternal(notifications: Seq[NotificationWithTarget[Submission, Assignment] with Cm2MarkedPlagiarisedNotification], user: User): String = {
+    if (notifications.length > 1) s"${notifications.length} assignment submissions are suspected of plagiarism" else "1 assignment submission is suspected of plagiarism"
+  }
+
+  override def contentForBatchInternal(notifications: Seq[NotificationWithTarget[Submission, Assignment] with Cm2MarkedPlagiarisedNotification]): FreemarkerModel = {
+    val byAssignment = groupedByAssignment(notifications)
+    FreemarkerModel("/WEB-INF/freemarker/emails/marked_plagiarised_batch.ftl", Map(
+      "batches" -> byAssignment.map { case (assignmentName, batch) =>
+        Map(
+          "assignmentName" -> assignmentName,
+          "submissions" -> batch.map(_.content.model.get("submission"))
+        )
+      }
+    ))
+  }
+
+  override def urlForBatchInternal(notifications: Seq[NotificationWithTarget[Submission, Assignment] with Cm2MarkedPlagiarisedNotification], user: User): String = {
+    Routes.cm2.home
+  }
+
+  override def urlTitleForBatchInternal(notifications: Seq[NotificationWithTarget[Submission, Assignment] with Cm2MarkedPlagiarisedNotification]): String = {
+    "open Coursework Management"
+  }
+
+  private def groupedByAssignment(batch: Seq[NotificationWithTarget[Submission, Assignment]]): Seq[(String, Seq[NotificationWithTarget[Submission, Assignment]])] =
+    batch.groupBy(_.target.entity.name)
+      .view
+      .toSeq
 }
---
+++ common/src/main/webapp/WEB-INF/freemarker/emails/marked_plagiarised_batch.ftl
@@ -0,0 +1,9 @@
+
+The following submissions have been marked as plagiarised.
+
+<#list batches as batch>
+For assignment "${batch.assignmentName}":
+
+<#list batch.submissions as item>
+* Submission by ${item.usercode}
+</#list></#list>
