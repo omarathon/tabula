0943e9b4b1fae055e4af3994d0e1f3730aa4103b
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-01-27 14:17:18 +0000

    TAB-8032 PR feedback: Add a test to make sure the groupings are correct

--- common/src/test/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/Cm2MarkedPlagiarisedNotificationTest.scala
+++ common/src/test/scala/uk/ac/warwick/tabula/data/model/notifications/cm2/Cm2MarkedPlagiarisedNotificationTest.scala
@@ -14,7 +14,7 @@ class Cm2MarkedPlagiarisedNotificationTest extends TestBase with Mockito with Fr
   mockUserLookup.getUserByUserId("1412345") returns new User("1412345")
   mockUserLookup.getUserByUserId("1673477") returns new User("1673477")
 
-  @Test def title() = withUser("cuscav", "cuscav") {
+  @Test def title(): Unit = withUser("cuscav", "cuscav") {
     val assignment = Fixtures.assignment("5,000 word essay")
     assignment.module = Fixtures.module("cs118", "Programming for Computer Scientists")
 
@@ -26,7 +26,7 @@ class Cm2MarkedPlagiarisedNotificationTest extends TestBase with Mockito with Fr
     notification.title should be("CS118: A submission by 1412345 for \"5,000 word essay\" is suspected of plagiarism")
   }
 
-  @Test def batched() = withUser("cuscav", "cuscav") {
+  @Test def batched(): Unit = withUser("cuscav", "cuscav") {
     val assignment = Fixtures.assignment("5,000 word essay")
     assignment.module = Fixtures.module("cs118", "Programming for Computer Scientists")
 
@@ -52,7 +52,35 @@ class Cm2MarkedPlagiarisedNotificationTest extends TestBase with Mockito with Fr
         |- 1673477
         |""".stripMargin
     )
+  }
 
+  @Test def batchedGrouping(): Unit = withUser("cuscav", "cuscav") {
+    val assignment1 = Fixtures.assignment("5,000 word essay")
+    assignment1.module = Fixtures.module("cs118", "Programming for Computer Scientists")
+
+    val assignment2 = Fixtures.assignment("10,000 word essay")
+    assignment2.module = Fixtures.module("cs123", "Long Essays for Computer Scientists")
+
+    val submission1 = Fixtures.submission("1412345", "1412345")
+    submission1.assignment = assignment1
+
+    val submission2 = Fixtures.submission("1673477", "1673477")
+    submission2.assignment = assignment2
+
+    val submission3 = Fixtures.submission("1673477", "1673477")
+    submission3.assignment = assignment1
+
+    val notification1 = Notification.init(new Cm2MarkedPlagiarisedNotification, currentUser.apparentUser, submission1, assignment1)
+    val notification2 = Notification.init(new Cm2MarkedPlagiarisedNotification, currentUser.apparentUser, submission2, assignment2)
+    val notification3 = Notification.init(new Cm2MarkedPlagiarisedNotification, currentUser.apparentUser, submission3, assignment1)
+    notification1.userLookup = mockUserLookup
+    notification2.userLookup = mockUserLookup
+    notification3.userLookup = mockUserLookup
+
+    val batch = Seq(notification1, notification2, notification3)
+    val groups = Cm2MarkedPlagiarisedBatchedNotificationHandler.groupBatch(batch)
+    groups should have length 2
+    groups should (contain (Seq(notification1, notification3)) and contain (Seq(notification2)))
   }
 
 }
