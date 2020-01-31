b27d4cbb3f262a9fe85ef81aa0f3affa9bb14eb9
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-01-31 15:36:08 +0000

    TAB-8090 Drop the existing index first

--- common/src/main/resources/db/migration/V89__2020-01-31_prevent_nonunique_submissionvalue_for_markerfeedback.sql
+++ common/src/main/resources/db/migration/V89__2020-01-31_prevent_nonunique_submissionvalue_for_markerfeedback.sql
@@ -1,2 +1,3 @@
 -- Prevent TAB-8090 breaking things (will probably just push the exception up the stack to where the bad happens)
+drop index if exists idx_submissionvalue_markerfeedback;
 create unique index idx_submissionvalue_markerfeedback on submissionvalue (marker_feedback_id, name);
