cfa13019f063d409feb45776771804bc5acfbccf
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-01-31 09:32:06 +0000

    TAB-8096 Correctly handle null similarity scores coming back from Turnitin (thanks, Scala type coercion of null to non-null https://warwick.slack.com/archives/C0P6GUAJC/p1580462959015100)

--- common/src/test/scala/uk/ac/warwick/tabula/services/turnitinlti/TurnitinLtiResponseTest.scala
+++ common/src/test/scala/uk/ac/warwick/tabula/services/turnitinlti/TurnitinLtiResponseTest.scala
@@ -13,7 +13,7 @@ class TurnitinLtiResponseTest extends TestBase {
     submissionInfo.web_overlap should be(Some(0))
   }
 
-  val SubmittedPaperInfo =
+  val SubmittedPaperInfo: String =
     """
 	{
 		"outcome_originalfile":{
@@ -74,5 +74,96 @@ class TurnitinLtiResponseTest extends TestBase {
 	}
 
 	"""
+
+  @Test def pendingInfo(): Unit = {
+    val response = TurnitinLtiResponse.fromJson(PendingPaperInfo)
+    val submissionInfo = response.submissionInfo()
+    submissionInfo.overlap should be(None)
+    submissionInfo.publication_overlap should be(None)
+    submissionInfo.student_overlap should be(None)
+    submissionInfo.web_overlap should be(None)
+  }
+
+  val PendingPaperInfo: String =
+    """
+      |{
+      |  "outcome_grammar": {
+      |    "launch_url": "https://www.turnitinuk.com/api/lti/1p0/dv/grammar/119388178?lang=en_us",
+      |    "roles": [
+      |      "Learner",
+      |      "Instructor"
+      |    ],
+      |    "label": "Open grammar feedback"
+      |  },
+      |  "outcome_originalityreport": {
+      |    "breakdown": {
+      |      "internet_score": null,
+      |      "publications_score": null,
+      |      "submitted_works_score": null
+      |    },
+      |    "roles": [
+      |      "Instructor"
+      |    ],
+      |    "text": "Pending",
+      |    "launch_url": "https://www.turnitinuk.com/api/lti/1p0/dv/report/119388178?lang=en_us",
+      |    "numeric": {
+      |      "score": null,
+      |      "max": 100
+      |    },
+      |    "label": "Open Originality Report"
+      |  },
+      |  "meta": {
+      |    "date_uploaded": "2020-01-31T09:05:48Z",
+      |    "deleted": false
+      |  },
+      |  "outcome_pdffile": {
+      |    "roles": [
+      |      "Learner",
+      |      "Instructor"
+      |    ],
+      |    "launch_url": "https://www.turnitinuk.com/api/lti/1p0/download/pdf/119388178?lang=en_us",
+      |    "text": null,
+      |    "label": "Download File in PDF Format"
+      |  },
+      |  "outcome_grademark": {
+      |    "numeric": {
+      |      "max": 100,
+      |      "score": null
+      |    },
+      |    "roles": [
+      |      "Instructor"
+      |    ],
+      |    "launch_url": "https://www.turnitinuk.com/api/lti/1p0/dv/grademark/119388178?lang=en_us",
+      |    "text": "--",
+      |    "feedback_exists": false,
+      |    "marks_exist": false,
+      |    "label": "Open GradeMark"
+      |  },
+      |  "outcome_originalfile": {
+      |    "label": "Download File in Original Format",
+      |    "roles": [
+      |      "Learner",
+      |      "Instructor"
+      |    ],
+      |    "launch_url": "https://www.turnitinuk.com/api/lti/1p0/download/orig/119388178?lang=en_us",
+      |    "text": null
+      |  },
+      |  "outcome_resubmit": {
+      |    "label": "Resubmit File",
+      |    "roles": [
+      |      "Learner"
+      |    ],
+      |    "text": null,
+      |    "launch_url": "https://www.turnitinuk.com/api/lti/1p0/upload/resubmit/119388178?lang=en_us"
+      |  },
+      |  "outcome_submission_delete": {
+      |    "roles": [
+      |      "Instructor"
+      |    ],
+      |    "launch_url": "https://www.turnitinuk.com/api/lti/1p0/submission/delete/119388178?lang=en_us",
+      |    "label": "Delete Submission"
+      |  }
+      |}
+      |""".stripMargin
 }
 
