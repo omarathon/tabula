5435a1c0898702828223de307975179b9f9b79ab
Sarah O'Toole <s.o-toole@warwick.ac.uk>
2020-02-06 17:03:37 +0000

    TAB-7775

--- web/src/main/scala/uk/ac/warwick/tabula/web/controllers/attendance/view/RecordMonitoringPointUploadTemplateController.scala
+++ web/src/main/scala/uk/ac/warwick/tabula/web/controllers/attendance/view/RecordMonitoringPointUploadTemplateController.scala
@@ -1,11 +1,10 @@
 package uk.ac.warwick.tabula.web.controllers.attendance.view
 
-import javax.validation.Valid
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{ModelAttribute, PathVariable, RequestMapping}
 import uk.ac.warwick.tabula.AcademicYear
-import uk.ac.warwick.tabula.commands.{Appliable, FiltersStudentsBase, PopulateOnForm}
 import uk.ac.warwick.tabula.commands.attendance.view.{FilterMonitoringPointsCommand, FilterMonitoringPointsCommandResult, RecordMonitoringPointTemplateCommand, SetFilterPointsResultOnRecordMonitoringPointCommand}
+import uk.ac.warwick.tabula.commands.{Appliable, FiltersStudentsBase, PopulateOnForm}
 import uk.ac.warwick.tabula.data.model.Department
 import uk.ac.warwick.tabula.data.model.attendance.AttendanceMonitoringPoint
 import uk.ac.warwick.tabula.web.controllers.BaseController
@@ -28,7 +27,7 @@ class RecordMonitoringPointUploadTemplateController extends BaseController {
   @RequestMapping
   def getTemplate(
     @ModelAttribute("filterCommand") filterCommand: Appliable[FilterMonitoringPointsCommandResult] with FiltersStudentsBase,
-    @Valid @ModelAttribute("templateCommand") templateCommand: RecordMonitoringPointTemplateCommand) : ExcelView = {
+    @ModelAttribute("templateCommand") templateCommand: RecordMonitoringPointTemplateCommand) : ExcelView = {
     val filterResult = filterCommand.apply()
     templateCommand.setFilteredPoints(filterResult)
     templateCommand.populate()
