26b3ac4f40c6d940f837c99fb9cf0b1910070b47
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-02-21 09:50:54 +0000

    Feedback from Helen Greenwood: Add descriptions to 'legacy' ast types

--- common/src/main/scala/uk/ac/warwick/tabula/data/model/ExaminationType.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/ExaminationType.scala
@@ -7,19 +7,19 @@ import scala.collection.immutable
 
 // full list of types taken from here - https://repo.elab.warwick.ac.uk/projects/MAPP/repos/app/browse/app/domain/fields/Assessment.scala#493-520
 // TODO - fetch the list via an API call to module approval rather than maintaining it twice
-sealed abstract class ExaminationType(override val entryName: String, val name: String, val description: Option[String] = None) extends EnumEntry
+sealed abstract class ExaminationType(override val entryName: String, val name: String, val description: String) extends EnumEntry
 object ExaminationType extends Enum[ExaminationType] {
-  case object Standard extends ExaminationType("STAN", "Standard", Some("A standard written exam paper, not seen in advance, with students not permitted to bring any texts."))
-  case object SeenPaper extends ExaminationType("SEEN", "Seen paper", Some("Paper is available to candidates in advance."))
-  case object OpenBook extends ExaminationType("OPEN", "Open book", Some("Students are permitted to bring any materials to the examination."))
-  case object Restricted extends ExaminationType("REST", "Restricted", Some("Students are permitted to bring specific materials to the examination, eg annotated case study, 2 x A4 sides of handwritten notes, 2 x A4 sides of typed notes."))
+  case object Standard extends ExaminationType("STAN", "Standard", "A standard written exam paper, not seen in advance, with students not permitted to bring any texts.")
+  case object SeenPaper extends ExaminationType("SEEN", "Seen paper", "Paper is available to candidates in advance.")
+  case object OpenBook extends ExaminationType("OPEN", "Open book", "Students are permitted to bring any materials to the examination.")
+  case object Restricted extends ExaminationType("REST", "Restricted", "Students are permitted to bring specific materials to the examination, eg annotated case study, 2 x A4 sides of handwritten notes, 2 x A4 sides of typed notes.")
 
   // Legacy?
-  case object Sectioned extends ExaminationType("S", "Sectioned")
-  case object NonSectioned extends ExaminationType("NS", "Not sectioned")
-  case object OpenBookDictionary extends ExaminationType("OD", "Open book - dictionary")
-  case object OpenBookRestricted extends ExaminationType("OR", "Open book restricted")
-  case object OpenBookUnrestricted extends ExaminationType("OU", "Open book unrestricted")
+  case object Sectioned extends ExaminationType("S", "Sectioned", "The paper has more than one section, each of which examines a different module.")
+  case object NonSectioned extends ExaminationType("NS", "Not sectioned", "The paper examines only one module.")
+  case object OpenBookDictionary extends ExaminationType("OD", "Open book - dictionary", "Students are permitted a dictionary in the exam (as specified and approved by their department).")
+  case object OpenBookRestricted extends ExaminationType("OR", "Open book restricted", "Students are permitted specific text(s) in the exam (as specified by their department).")
+  case object OpenBookUnrestricted extends ExaminationType("OU", "Open book unrestricted", "Students are permitted any text(s) in the exam (excluding University of Warwick Library books).")
 
   override def values: immutable.IndexedSeq[ExaminationType] = findValues
 }
