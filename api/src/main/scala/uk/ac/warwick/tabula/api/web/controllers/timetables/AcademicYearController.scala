cb6f4e62ec656656b091c2676d1accccadd91ad8
Michael Gale <m.gale@warwick.ac.uk>
2020-03-25 18:36:39 +0000

    Add API endpoint to retrieve the current academic year

---
+++ api/src/main/scala/uk/ac/warwick/tabula/api/web/controllers/timetables/AcademicYearController.scala
@@ -0,0 +1,20 @@
+package uk.ac.warwick.tabula.api.web.controllers.timetables
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import uk.ac.warwick.tabula.AcademicYear
+import uk.ac.warwick.tabula.api.web.controllers.ApiController
+import uk.ac.warwick.tabula.web.Mav
+import uk.ac.warwick.tabula.web.views.JSONView
+
+@Controller
+@RequestMapping(Array("/v1/academicyear"))
+class AcademicYearController extends ApiController {
+  @RequestMapping(method = Array(GET), produces = Array("application/json"))
+  def list(): Mav =
+    Mav(new JSONView(Map(
+      "success" -> true,
+      "status" -> "ok",
+      "academicYear" -> AcademicYear.now().toString()
+    )))
+}
