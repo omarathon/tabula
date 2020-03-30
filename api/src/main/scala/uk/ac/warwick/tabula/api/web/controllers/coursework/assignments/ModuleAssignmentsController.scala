9f77a6b094dc4ec8de7f1a0b94c3e0b96e4153bc
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2020-03-30 22:21:33 +0100

    TAB-8250 - avoid breaking changes to the assignment creation API when 1000 - 1600 deadlines are supported

--- api/src/main/scala/uk/ac/warwick/tabula/api/web/controllers/coursework/assignments/ModuleAssignmentsController.scala
+++ api/src/main/scala/uk/ac/warwick/tabula/api/web/controllers/coursework/assignments/ModuleAssignmentsController.scala
@@ -1,7 +1,7 @@
 package uk.ac.warwick.tabula.api.web.controllers.coursework.assignments
 
 import javax.servlet.http.HttpServletResponse
-import org.joda.time.{DateTime, LocalDate}
+import org.joda.time.{DateTime, LocalDate, LocalTime}
 import org.springframework.http.{HttpStatus, MediaType}
 import org.springframework.stereotype.Controller
 import org.springframework.validation.Errors
@@ -122,7 +122,8 @@ trait AssignmentPropertiesRequest[A <: ModifyAssignmentMonolithRequest] extends 
 
   @BeanProperty var name: String = null
   @BeanProperty var openDate: LocalDate = null
-  @BeanProperty var closeDate: DateTime = null
+  @BeanProperty var closeDate: LocalDate = null
+  @BeanProperty var closeTime: LocalTime = null
   @BeanProperty var academicYear: AcademicYear = null
   @BeanProperty var feedbackTemplate: FeedbackTemplate = null
   @BeanProperty var markingWorkflow: CM2MarkingWorkflow = null
@@ -139,12 +140,12 @@ trait AssignmentPropertiesRequest[A <: ModifyAssignmentMonolithRequest] extends 
   override def copyTo(state: A, errors: Errors): Unit = {
     if (Option(openDate).isEmpty && Option(closeDate).nonEmpty) {
       if (openEnded) openDate = LocalDate.now
-      else openDate = closeDate.minusWeeks(2).toLocalDate
+      else openDate = closeDate.minusWeeks(2)
     }
 
     Option(name).foreach(state.name = _)
     Option(openDate).foreach(state.openDate = _)
-    Option(closeDate).foreach(state.closeDate = _)
+    Option(closeDate).foreach(d => state.closeDate = d.toDateTime(Option(closeTime).getOrElse(Assignment.defaultCloseTime)))
 
     state match {
       case createState: CreateAssignmentMonolithRequest =>
