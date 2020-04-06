4936c116c8db78939ae39818e0bb11e85130e736
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-04-06 15:34:24 +0100

    Pull tabula-sandbox exams back into April

--- common/src/main/scala/uk/ac/warwick/tabula/services/scheduling/AssignmentImporter.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/services/scheduling/AssignmentImporter.scala
@@ -323,7 +323,7 @@ class SandboxAssignmentImporter extends AssignmentImporter {
       a.locationSequence = "001"
       a.academicYear = year
       a.startTime =
-        new LocalDate(year.endYear, DateTimeConstants.JUNE, 1)
+        new LocalDate(year.endYear, DateTimeConstants.APRIL, 6)
           .plusDays(index / 10)
           .toDateTime(if (index % 10 < 5) new LocalTime(9, 0) else new LocalTime(14, 0))
       a.examPaperCode = s"${moduleCode.toUpperCase}0"
