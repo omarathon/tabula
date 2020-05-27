952a741f04a980fed01fd0c1c9f050d236bc80cb
Ritchie Allen <ritchie.allen@warwick.ac.uk>
2020-05-27 07:35:38 +0100

    TAB-8330 - Recorded module registration

---
+++ common/src/main/resources/db/migration/V106__2020-05-26_recorded_module_marks.sql
@@ -0,0 +1,30 @@
+create table recordedmoduleregistration (
+  id varchar not null,
+  hib_version numeric default 0,
+  module_code varchar not null,
+  cats numeric(5,2),
+  academic_year smallint not null,
+  assessment_group varchar not null,
+  occurrence varchar not null,
+  scjCode varchar not null,
+  needs_writing_to_sits boolean default false,
+  last_written_to_sits timestamp(6),
+  constraint pk_recordedmoduleregistration primary key (id)
+);
+
+create index idx_recordedmoduleregistration_um on recordedmoduleregistration (module_code, assessment_group, occurrence, academic_year);
+create unique index ck_recordedmoduleregistration on recordedmoduleregistration (module_code, assessment_group, occurrence, academic_year, scjCode);
+
+create table recordedmodulemark (
+  id varchar not null,
+  recorded_module_registration_id varchar not null,
+  mark int,
+  grade varchar,
+  comments text,
+  updated_by varchar not null,
+  updated_date timestamp(6) not null,
+  constraint pk_recordedmodulemark primary key (id),
+  constraint fk_recordedmodulemark foreign key (recorded_module_registration_id) references recordedmoduleregistration
+);
+
+create index idx_recordedmodulemark_registration on recordedmodulemark (recorded_module_registration_id);
