4b731c8f668ed788871ae354e5e1fedd63c06c8e
Adam Williams <A.Williams.9@warwick.ac.uk>
2020-05-29 14:00:27 +0100

    TAB-8397 Start writing out marks onto the page for JS

--- web/src/main/scala/uk/ac/warwick/tabula/web/controllers/marks/ComponentScalingController.scala
+++ web/src/main/scala/uk/ac/warwick/tabula/web/controllers/marks/ComponentScalingController.scala
@@ -1,11 +1,13 @@
 package uk.ac.warwick.tabula.web.controllers.marks
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import javax.validation.Valid
 import org.springframework.stereotype.Controller
 import org.springframework.ui.ModelMap
 import org.springframework.validation.Errors
 import org.springframework.web.bind.annotation.{ModelAttribute, PathVariable, PostMapping, RequestMapping}
 import org.springframework.web.servlet.mvc.support.RedirectAttributes
+import uk.ac.warwick.spring.Wire
 import uk.ac.warwick.tabula.commands.SelfValidating
 import uk.ac.warwick.tabula.commands.marks.ComponentScalingCommand
 import uk.ac.warwick.tabula.data.model.{AssessmentComponent, UpstreamAssessmentGroup, UpstreamAssessmentGroupMember}
@@ -17,6 +19,8 @@ import uk.ac.warwick.tabula.web.{BreadCrumb, Routes}
 class ComponentScalingController extends BaseController {
 
   validatesSelf[SelfValidating]
+
+  var json: ObjectMapper = Wire[ObjectMapper]
 
   @ModelAttribute("command")
   def command(@PathVariable assessmentComponent: AssessmentComponent, @PathVariable upstreamAssessmentGroup: UpstreamAssessmentGroup): ComponentScalingCommand.Command =
@@ -38,7 +42,13 @@ class ComponentScalingController extends BaseController {
 
   // We run validation when showing the form so we can avoid people clicking the button
   @RequestMapping
-  def showForm(@Valid @ModelAttribute("command") cmd: ComponentScalingCommand.Command, errors: Errors): String = {
+  def showForm(@Valid @ModelAttribute("command") cmd: ComponentScalingCommand.Command, errors: Errors, model: ModelMap): String = {
+    if (!errors.hasErrors) {
+      val markValues = cmd.studentsToSet.filter(s => s._2.isDefined).map { case (upstreamAssessmentGroupMember, originalMark, grd) =>
+        upstreamAssessmentGroupMember.universityId -> originalMark
+      }.toMap
+      model.addAttribute("marks", json.writeValueAsString(markValues))
+    }
     formView
   }
 
