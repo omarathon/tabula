56cc54f856e7e21558b7a03178c86eb8c0109045
Adam Williams <A.Williams.9@warwick.ac.uk>
2020-06-01 11:10:04 +0100

    TAB-8394 Apply patch to iframeSrc

--- web/src/main/assets/static/js/jquery.form.js
+++ web/src/main/assets/static/js/jquery.form.js
@@ -106,7 +106,7 @@
       url: url,
       success: $.ajaxSettings.success,
       type: method || $.ajaxSettings.type,
-      iframeSrc: /^https/i.test(window.location.href || '') ? 'javascript:false' : 'about:blank'
+      iframeSrc: 'about:blank'
     }, options);
 
     // hook for manipulating the form data before it is extracted;
