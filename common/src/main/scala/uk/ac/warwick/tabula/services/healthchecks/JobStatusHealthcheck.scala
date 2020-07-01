a7a3c30aaea7f07bbd81049d685cd0652c823033
Archana Mathur <archana.mathur@warwick.ac.uk>
2020-07-01 17:55:51 +0100

    TAB-8533 -Healthchek for pending jobs

---
+++ common/src/main/scala/uk/ac/warwick/tabula/services/healthchecks/JobStatusHealthcheck.scala
@@ -0,0 +1,60 @@
+package uk.ac.warwick.tabula.services.healthchecks
+
+import java.time.LocalDateTime
+
+import humanize.Humanize._
+import org.springframework.context.annotation.Profile
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import uk.ac.warwick.spring.Wire
+import uk.ac.warwick.tabula.services.jobs.JobService
+import uk.ac.warwick.util.core.DateTimeUtils
+import uk.ac.warwick.util.service.{ServiceHealthcheck, ServiceHealthcheckProvider}
+
+import scala.concurrent.duration._
+import scala.jdk.CollectionConverters._
+
+object JobStatusHealthcheck {
+  val Name = "pending-jobs"
+  val InitialState = new ServiceHealthcheck(Name, ServiceHealthcheck.Status.Unknown, LocalDateTime.now(DateTimeUtils.CLOCK_IMPLEMENTATION))
+
+  val WarningThreshold: Duration = 30.minutes
+  val ErrorThreshold: Duration = 1.hour
+}
+
+
+@Component
+@Profile(Array("scheduling"))
+class JobStatusHealthcheck
+  extends ServiceHealthcheckProvider(JobStatusHealthcheck.InitialState) {
+
+  @Scheduled(fixedRate = 60 * 1000) // 1 minute
+  override def run(): Unit = {
+
+    val service = Wire[JobService]
+    val pendingJobs = service.unfinishedInstances.filter(_.started == false).sortBy(_.createdDate)
+    val oldestPendingJob = pendingJobs.headOption
+    val status = if (oldestPendingJob.exists(_.createdDate.plusMillis(JobStatusHealthcheck.ErrorThreshold.toMillis.toInt).isBeforeNow))
+      ServiceHealthcheck.Status.Error
+    else if (oldestPendingJob.exists(_.createdDate.plusMillis(JobStatusHealthcheck.WarningThreshold.toMillis.toInt).isBeforeNow))
+      ServiceHealthcheck.Status.Warning
+    else
+      ServiceHealthcheck.Status.Okay
+
+    val message =
+      oldestPendingJob.map { job => s"Oldest job created ${naturalTime(job.createdDate.toDate)}" }.getOrElse("There are no pending jobs that  haven't started yet.")
+
+
+    update(new ServiceHealthcheck(
+      JobStatusHealthcheck.Name,
+      status,
+      LocalDateTime.now(DateTimeUtils.CLOCK_IMPLEMENTATION),
+      message,
+      Seq[ServiceHealthcheck.PerformanceData[_]](
+        new ServiceHealthcheck.PerformanceData("pending_jobs_total", pendingJobs.size, JobStatusHealthcheck.WarningThreshold.toHours, JobStatusHealthcheck.ErrorThreshold.toHours)
+      ).asJava
+    ))
+
+  }
+}
+
