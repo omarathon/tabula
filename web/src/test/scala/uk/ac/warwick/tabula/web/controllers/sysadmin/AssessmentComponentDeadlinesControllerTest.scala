a45072670f16c9293f41f5438e03b394c6b6159a
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-08-03 13:33:07 +0100

    Fix time-sensitive test

--- web/src/test/scala/uk/ac/warwick/tabula/web/controllers/sysadmin/AssessmentComponentDeadlinesControllerTest.scala
+++ web/src/test/scala/uk/ac/warwick/tabula/web/controllers/sysadmin/AssessmentComponentDeadlinesControllerTest.scala
@@ -1,7 +1,7 @@
 package uk.ac.warwick.tabula.web.controllers.sysadmin
 
 import org.joda.time.{DateTimeConstants, LocalDate}
-import uk.ac.warwick.tabula.TestBase
+import uk.ac.warwick.tabula.{AcademicYear, TestBase}
 import uk.ac.warwick.tabula.web.controllers.sysadmin.AssessmentComponentDeadlinesController._
 
 class AssessmentComponentDeadlinesControllerTest extends TestBase {
@@ -14,8 +14,8 @@ class AssessmentComponentDeadlinesControllerTest extends TestBase {
     parseDeadlineDate("12/4/19") should be (new LocalDate(2019, DateTimeConstants.DECEMBER, 4))
     parseDeadlineDate("1/9/20") should be (new LocalDate(2020, DateTimeConstants.JANUARY, 9))
     parseDeadlineDate("Sep-20") should be (new LocalDate(2020, DateTimeConstants.SEPTEMBER, 1))
-    parseDeadlineDate("WEEK 40") should be (new LocalDate(2020, DateTimeConstants.JUNE, 29))
-    parseDeadlineDate("week 40") should be (new LocalDate(2020, DateTimeConstants.JUNE, 29))
+    parseDeadlineDate("WEEK 40", AcademicYear.starting(2019)) should be (new LocalDate(2020, DateTimeConstants.JUNE, 29))
+    parseDeadlineDate("week 40", AcademicYear.starting(2019)) should be (new LocalDate(2020, DateTimeConstants.JUNE, 29))
     parseDeadlineDate("17.1.2020") should be (new LocalDate(2020, DateTimeConstants.JANUARY, 17))
   }
 
