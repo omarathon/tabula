1147d7aad4584333a8485a753f817b6dc4aaed21
Mat Mannion <M.Mannion@warwick.ac.uk>
2020-08-04 12:45:51 +0100

    TAB-8521 TAB-8522 Batch (and test) confirmed module mark changed notification

--- common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/marks/ConfirmedModuleMarkChangedNotification.scala
+++ common/src/main/scala/uk/ac/warwick/tabula/data/model/notifications/marks/ConfirmedModuleMarkChangedNotification.scala
@@ -10,15 +10,15 @@ import uk.ac.warwick.tabula.marks.web.{Routes => MarksRoutes}
 import uk.ac.warwick.tabula.services.ProfileService
 import uk.ac.warwick.userlookup.User
 
-object ConfirmModuleMarkChangedNotification {
+object ConfirmedModuleMarkChangedNotification {
   val templateLocation: String = "/WEB-INF/freemarker/notifications/marks/module_confirmed_actualmark_changed.ftl"
 }
 
 @Entity
 @Proxy
 @DiscriminatorValue("ConfirmModuleMarkChanged")
-class ConfirmModuleMarkChangedNotification
-  extends NotificationWithTarget[RecordedModuleRegistration, Department]
+class ConfirmedModuleMarkChangedNotification
+  extends BatchedNotificationWithTarget[RecordedModuleRegistration, Department, ConfirmedModuleMarkChangedNotification](ConfirmedModuleMarkChangedBatchedNotificationHandler)
     with SingleItemNotification[RecordedModuleRegistration]
     with MyWarwickNotification
     with Logging {
@@ -49,8 +49,33 @@ class ConfirmModuleMarkChangedNotification
 
   override def url: String = MarksRoutes.Admin.home(department)
 
-  override def content: FreemarkerModel = FreemarkerModel(ConfirmModuleMarkChangedNotification.templateLocation, Map("memberLinks" -> studentList.map(scyd => s"${scyd.studentCourseDetails.student.universityId}: $topLevelUrl${Routes.Grids.assessmentdetails(scyd)}")))
+  override def content: FreemarkerModel = FreemarkerModel(ConfirmedModuleMarkChangedNotification.templateLocation, Map("memberLinks" -> studentList.map(scyd => s"${scyd.studentCourseDetails.student.universityId}: $topLevelUrl${Routes.Grids.assessmentdetails(scyd)}")))
 
   override def recipients: Seq[User] = department.owners.users.toSeq.filterNot(u => u == agent)
 
+}
+
+object ConfirmedModuleMarkChangedBatchedNotificationHandler extends BatchedNotificationHandler[ConfirmedModuleMarkChangedNotification] {
+  override def groupBatchInternal(notifications: Seq[ConfirmedModuleMarkChangedNotification]): Seq[Seq[ConfirmedModuleMarkChangedNotification]] =
+    // Batch notifications for modules in the same department
+    notifications.groupBy(_.department).values.toSeq
+
+  override def titleForBatchInternal(notifications: Seq[ConfirmedModuleMarkChangedNotification], user: User): String =
+    "Student confirmed actual marks modified"
+
+  // We can use the same template as the main notification, we just combine the students and add in the module code
+  override def contentForBatchInternal(notifications: Seq[ConfirmedModuleMarkChangedNotification]): FreemarkerModel =
+    FreemarkerModel(ConfirmedModuleMarkChangedNotification.templateLocation, Map(
+      "memberLinks" -> notifications.groupBy(_.module).toSeq.sortBy(_._1).flatMap { case (moduleCode, notifications) =>
+        notifications.flatMap(_.studentList).distinct.sortBy(_.studentCourseDetails.scjCode).map { scyd =>
+          s"$moduleCode - ${scyd.studentCourseDetails.student.universityId}: ${notifications.head.topLevelUrl}${Routes.Grids.assessmentdetails(scyd)}"
+        }
+      }
+    ))
+
+  override def urlForBatchInternal(notifications: Seq[ConfirmedModuleMarkChangedNotification], user: User): String =
+    notifications.head.url
+
+  override def urlTitleForBatchInternal(notifications: Seq[ConfirmedModuleMarkChangedNotification]): String =
+    notifications.head.urlTitle
 }
---
+++ common/src/test/scala/uk/ac/warwick/tabula/data/model/notifications/marks/ConfirmedModuleMarkChangedNotificationTest.scala
@@ -0,0 +1,104 @@
+package uk.ac.warwick.tabula.data.model.notifications.marks
+
+import uk.ac.warwick.tabula.data.model.{ModuleRegistration, ModuleSelectionStatus, Notification, RecordedModuleRegistration}
+import uk.ac.warwick.tabula.services.ProfileService
+import uk.ac.warwick.tabula.{Fixtures, Mockito, TestBase}
+import uk.ac.warwick.tabula.web.views.{FreemarkerRendering, ScalaFreemarkerConfiguration}
+
+class ConfirmedModuleMarkChangedNotificationTest extends TestBase with Mockito with FreemarkerRendering {
+
+  val freeMarkerConfig: ScalaFreemarkerConfiguration = newFreemarkerConfiguration()
+
+  @Test def single(): Unit = withUser("cuscav") {
+    val student = Fixtures.student(universityId = "8347243", courseDepartment = Fixtures.department("cs"))
+    val scd = student.mostSignificantCourse
+    val academicYear = scd.latestStudentCourseYearDetails.academicYear
+
+    val module = Fixtures.module("in101")
+
+    val moduleRegistration = new ModuleRegistration(scd.sprCode, module, BigDecimal(30).underlying, "IN101-30", academicYear, "A", "WMR")
+    moduleRegistration._allStudentCourseDetails.add(scd)
+    moduleRegistration.assessmentGroup = "A1"
+    moduleRegistration.selectionStatus = ModuleSelectionStatus.OptionalCore
+
+    val recordedModuleRegistration = new RecordedModuleRegistration(moduleRegistration)
+
+    val department = Fixtures.department("in")
+
+    val notification = Notification.init(new ConfirmedModuleMarkChangedNotification, currentUser.apparentUser, recordedModuleRegistration, department)
+    notification.profileService = smartMock[ProfileService]
+    notification.profileService.getStudentCourseDetailsBySprCode(scd.sprCode) returns Seq(scd)
+    notification.topLevelUrl = "https://tabula.ac.uk"
+    notification.title should be ("IN101-30: Student confirmed actual marks modified")
+
+    val content = notification.content
+    renderToString(freeMarkerConfig.getTemplate(content.template), content.model) should be(
+      s"""Confirmed module marks have been modified for 1 student. Follow the link below to view the student marks:
+        |
+        |* 8347243: https://tabula.ac.uk/exams/grids/cs/${academicYear.startYear}/8347243_1/assessmentdetails
+        |""".stripMargin)
+  }
+
+  @Test def batched(): Unit = withUser("cuscav") {
+    val student1 = Fixtures.student(universityId = "8347243", courseDepartment = Fixtures.department("cs"))
+    val scd1 = student1.mostSignificantCourse
+
+    val student2 = Fixtures.student(universityId = "8347246", courseDepartment = Fixtures.department("cs"))
+    val scd2 = student2.mostSignificantCourse
+
+    val academicYear = scd1.latestStudentCourseYearDetails.academicYear
+
+    val module1 = Fixtures.module("in101")
+
+    val moduleRegistration1 = new ModuleRegistration(scd1.sprCode, module1, BigDecimal(30).underlying, "IN101-30", academicYear, "A", "WMR")
+    moduleRegistration1._allStudentCourseDetails.add(scd1)
+    moduleRegistration1.assessmentGroup = "A1"
+    moduleRegistration1.selectionStatus = ModuleSelectionStatus.OptionalCore
+
+    val module2 = Fixtures.module("in102")
+
+    val moduleRegistration2 = new ModuleRegistration(scd1.sprCode, module2, BigDecimal(30).underlying, "IN102-30", academicYear, "A", "WMR")
+    moduleRegistration2._allStudentCourseDetails.add(scd1)
+    moduleRegistration2.assessmentGroup = "A1"
+    moduleRegistration2.selectionStatus = ModuleSelectionStatus.OptionalCore
+
+    val moduleRegistration3 = new ModuleRegistration(scd2.sprCode, module2, BigDecimal(30).underlying, "IN102-30", academicYear, "A", "WMR")
+    moduleRegistration3._allStudentCourseDetails.add(scd2)
+    moduleRegistration3.assessmentGroup = "A1"
+    moduleRegistration3.selectionStatus = ModuleSelectionStatus.OptionalCore
+
+    val recordedModuleRegistration1 = new RecordedModuleRegistration(moduleRegistration1)
+    val recordedModuleRegistration2 = new RecordedModuleRegistration(moduleRegistration2)
+    val recordedModuleRegistration3 = new RecordedModuleRegistration(moduleRegistration3)
+
+    val department = Fixtures.department("in")
+
+    val notification1 = Notification.init(new ConfirmedModuleMarkChangedNotification, currentUser.apparentUser, recordedModuleRegistration1, department)
+    notification1.profileService = smartMock[ProfileService]
+    notification1.profileService.getStudentCourseDetailsBySprCode(scd1.sprCode) returns Seq(scd1)
+    notification1.topLevelUrl = "https://tabula.ac.uk"
+
+    val notification2 = Notification.init(new ConfirmedModuleMarkChangedNotification, currentUser.apparentUser, recordedModuleRegistration2, department)
+    notification2.profileService = smartMock[ProfileService]
+    notification2.profileService.getStudentCourseDetailsBySprCode(scd1.sprCode) returns Seq(scd1)
+    notification2.topLevelUrl = "https://tabula.ac.uk"
+
+    val notification3 = Notification.init(new ConfirmedModuleMarkChangedNotification, currentUser.apparentUser, recordedModuleRegistration3, department)
+    notification3.profileService = smartMock[ProfileService]
+    notification3.profileService.getStudentCourseDetailsBySprCode(scd2.sprCode) returns Seq(scd2)
+    notification3.topLevelUrl = "https://tabula.ac.uk"
+
+    val batch = Seq(notification1, notification2, notification3)
+    ConfirmedModuleMarkChangedBatchedNotificationHandler.titleForBatch(batch, currentUser.apparentUser) should be ("Student confirmed actual marks modified")
+
+    val content = ConfirmedModuleMarkChangedBatchedNotificationHandler.contentForBatch(batch)
+    renderToString(freeMarkerConfig.getTemplate(content.template), content.model) should be(
+      s"""Confirmed module marks have been modified for 3 students. Follow the links below to view the student marks:
+        |
+        |* IN101-30 - 8347243: https://tabula.ac.uk/exams/grids/cs/${academicYear.startYear}/8347243_1/assessmentdetails
+        |* IN102-30 - 8347243: https://tabula.ac.uk/exams/grids/cs/${academicYear.startYear}/8347243_1/assessmentdetails
+        |* IN102-30 - 8347246: https://tabula.ac.uk/exams/grids/cs/${academicYear.startYear}/8347246_1/assessmentdetails
+        |""".stripMargin)
+  }
+
+}
